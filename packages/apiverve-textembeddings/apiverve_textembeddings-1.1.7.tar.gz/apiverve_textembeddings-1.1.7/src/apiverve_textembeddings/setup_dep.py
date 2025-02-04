a7a7e101479224a875a2a27e153b16473133e667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:19 +0000

    Add package apiverve-textembeddings/1.1.7/apiverve_textembeddings-1.1.7.tar.gz

---
+++ packages/apiverve-textembeddings/apiverve_textembeddings-1.1.7.tar.gz/apiverve_textembeddings-1.1.7/src/apiverve_textembeddings/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_textembeddings',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Embeddings is a simple tool for generating text embeddings. It returns the text embeddings based on the text provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
