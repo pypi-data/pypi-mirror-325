72efc7dbea1ca7f7c37fae7f63e63a21152145d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:05:00 +0000

    Add package prefect/3.1.16.dev4/prefect-3.1.16.dev4.tar.gz

---
+++ packages/prefect/prefect-3.1.16.dev4.tar.gz/prefect-3.1.16.dev4/src/prefect/server/ui/assets/404-C-9kYEXw.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,l as a,o as t,dC as r}from"./index-qaIncb5u.js";import{u as s}from"./usePageTitle-BSROX4uK.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,p)=>(t(),o(a(r)))}});export{m as default};
+//# sourceMappingURL=404-C-9kYEXw.js.map
---
+++ packages/prefect/prefect-3.1.16.dev4.tar.gz/prefect-3.1.16.dev4/src/prefect/server/ui/assets/useCan-CQhVMsH7.js
@@ -0,0 +1,2 @@
+import{cD as n}from"./index-qaIncb5u.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-CQhVMsH7.js.map
