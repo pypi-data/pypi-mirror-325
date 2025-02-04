3b399cb36dc49f45bc34b6a6487e83ab6fec0e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-countydatalookup/1.1.7/apiverve_countydatalookup-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-countydatalookup/apiverve_countydatalookup-1.1.7-py3-none-any.whl/apiverve_countydatalookup/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_countydatalookup',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='County Data is a simple tool for getting data about US counties. It returns information such as average income, area, and more based on the county name provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
