215afe1efba58ed29664664a89000dec6eb5bef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:15 +0000

    Add package dl8/0.5.0/dl8-0.5.0.tar.gz

---
+++ packages/dl8/dl8-0.5.0.tar.gz/dl8-0.5.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dl8",
+    version="0.5.0",
+    packages=find_packages(),
+    install_requires=["pyperclip>=1.8.2"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
