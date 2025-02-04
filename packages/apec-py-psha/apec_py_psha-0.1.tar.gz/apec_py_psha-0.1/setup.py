92c44510c9177650a08e1999bebbf95ab2adc6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:03:55 +0000

    Add package apec-py-psha/0.1/apec_py_psha-0.1.tar.gz

---
+++ packages/apec-py-psha/apec_py_psha-0.1.tar.gz/apec_py_psha-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apec-py-psha',
+    version='0.1',
+    description='A Python package for probabilistic seismic hazard analysis (PSHA)',
+    author='Albert Pamonag',
+    author_email='albert@apeconsultancy.net',
+    url='https://github.com/albertp16/apec-py-psha',
+    packages=find_packages(),
+    install_requires=[
+        # Add required dependencies here, e.g., 'numpy', 'pandas', etc.
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    company='Albert Pamonag Engineering Consultancy',
+)
