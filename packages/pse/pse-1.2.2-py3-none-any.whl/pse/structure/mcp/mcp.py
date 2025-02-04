a1f238471f07b51703a6399dc1aa623c58eed4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:06 +0000

    Add package pse/1.2.2/pse-1.2.2-py3-none-any.whl

---
+++ packages/pse/pse-1.2.2-py3-none-any.whl/pse/structure/mcp/mcp.py
@@ -0,0 +1,4 @@
+# anthropics model context protocol
+# can be represented as a json schema (mcp_schema.json)
+# we can feed this schema to the pse and allow
+# any model to use the model context protocol.
