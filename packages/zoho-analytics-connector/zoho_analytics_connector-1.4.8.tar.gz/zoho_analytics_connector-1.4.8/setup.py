f63540efb77fd07658fa5522dda474c895c7eedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:03 +0000

    Add package zoho-analytics-connector/1.4.8/zoho_analytics_connector-1.4.8.tar.gz

---
+++ packages/zoho-analytics-connector/zoho_analytics_connector-1.4.8.tar.gz/zoho_analytics_connector-1.4.8/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+from sphinx.setup_command import BuildDoc
+from os import path
+cmdclass = {'build_sphinx': BuildDoc}
+
+# https://pypi.org/classifiers/
+
+name = 'zoho_analytics_connector'
+keywords = 'zoho analytics'
+version = '1.4.8'
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=name,
+    keywords=keywords,
+    version=version,
+    packages=['zoho_analytics_connector'],
+    python_requires='>=3.9',
+    install_requires=['requests','emoji'],
+    setup_requires=['pytest-runner', 'wheel', 'sphinx'],
+    tests_require=["pytest", ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Office/Business',
+    ],
+    url='https://github.com/timrichardson/zoho_analytics_connector',
+    license='MPL-2.0',
+    author='Tim Richardson',
+    author_email='tim@growthpath.com.au',
+    description='Zoho Analytics connector',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    cmdclass=cmdclass,
+    # these are optional and override conf.py settings
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', version),
+            'source_dir': ('setup.py', 'docs')}},
+
+)
