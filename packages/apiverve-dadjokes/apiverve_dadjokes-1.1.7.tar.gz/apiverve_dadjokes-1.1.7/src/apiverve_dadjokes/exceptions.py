b28bdf3b9b3088bd5328d1cefa375bf1e149ef69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-dadjokes/1.1.7/apiverve_dadjokes-1.1.7.tar.gz

---
+++ packages/apiverve-dadjokes/apiverve_dadjokes-1.1.7.tar.gz/apiverve_dadjokes-1.1.7/src/apiverve_dadjokes/exceptions.py
@@ -0,0 +1,3 @@
+class DadjokesDadJokesAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-dadjokes/apiverve_dadjokes-1.1.7.tar.gz/apiverve_dadjokes-1.1.7/src/apiverve_dadjokes/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_dadjokes',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Dad Jokes is a simple tool for getting dad jokes. It returns a random dad joke.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
