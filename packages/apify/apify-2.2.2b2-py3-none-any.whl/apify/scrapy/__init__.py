f668f3afa383edbf8ca2319404fed390c0b2c08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:05:02 +0000

    Add package apify/2.2.2b2/apify-2.2.2b2-py3-none-any.whl

---
+++ packages/apify/apify-2.2.2b2-py3-none-any.whl/apify/scrapy/__init__.py
@@ -0,0 +1,11 @@
+from apify.scrapy.requests import to_apify_request, to_scrapy_request
+from apify.scrapy.scheduler import ApifyScheduler
+from apify.scrapy.utils import get_basic_auth_header, get_running_event_loop_id
+
+__all__ = [
+    'ApifyScheduler',
+    'get_basic_auth_header',
+    'get_running_event_loop_id',
+    'to_apify_request',
+    'to_scrapy_request',
+]
