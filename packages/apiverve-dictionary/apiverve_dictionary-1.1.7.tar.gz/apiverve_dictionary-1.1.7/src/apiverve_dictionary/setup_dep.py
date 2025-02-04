d517b5e267e12c3e4d0cfe82b9b288fba30876a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-dictionary/1.1.7/apiverve_dictionary-1.1.7.tar.gz

---
+++ packages/apiverve-dictionary/apiverve_dictionary-1.1.7.tar.gz/apiverve_dictionary-1.1.7/src/apiverve_dictionary/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_dictionary',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Dictionary is a simple tool for getting word definitions. It returns the definition a word.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
