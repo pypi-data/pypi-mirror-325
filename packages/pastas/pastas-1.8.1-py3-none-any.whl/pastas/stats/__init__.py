88359f9d49a17dc36d4f26ca16df09f0363dcdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:30 +0000

    Add package pastas/1.8.1/pastas-1.8.1-py3-none-any.whl

---
+++ packages/pastas/pastas-1.8.1-py3-none-any.whl/pastas/stats/__init__.py
@@ -0,0 +1,27 @@
+"""The stats sub-package contains statistical methods for Pastas.
+
+.. currentmodule:: pastas.stats
+
+.. autosummary::
+    :toctree: ./generated
+    :nosignatures:
+
+    core
+    metrics
+    dutch
+    sgi
+    signatures
+    tests
+
+"""
+
+# flake8: noqa
+
+import pastas.stats.metrics as metrics
+import pastas.stats.signatures as signatures
+
+from .core import acf, ccf, mean, std, var
+from .dutch import ghg, glg, gvg, q_ghg, q_glg, q_gvg, gg
+from .metrics import aic, bic, evp, kge, kge_2012, mae, nse, pearsonr, rmse, rsq, sse
+from .sgi import sgi
+from .tests import diagnostics, durbin_watson, ljung_box, runs_test, stoffer_toloi
