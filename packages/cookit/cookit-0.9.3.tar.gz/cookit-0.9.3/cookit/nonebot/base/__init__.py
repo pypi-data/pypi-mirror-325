041434edadc1a4c98f893eef5e0a6a5a59c8b529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:39 +0000

    Add package cookit/0.9.3/cookit-0.9.3.tar.gz

---
+++ packages/cookit/cookit-0.9.3.tar.gz/cookit-0.9.3/cookit/nonebot/base/__init__.py
@@ -0,0 +1,7 @@
+from .dependency import (
+    CommandArgPlaintext as CommandArgPlaintext,
+    dep_command_arg_plaintext as dep_command_arg_plaintext,
+)
+from .plugin import (
+    assert_plugin_loaded as assert_plugin_loaded,
+)
