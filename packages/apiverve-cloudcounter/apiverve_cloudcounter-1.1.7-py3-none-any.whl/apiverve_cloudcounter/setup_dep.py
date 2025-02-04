1c07e5daac255169b4eddcf007fda3ed4a678c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:12 +0000

    Add package apiverve-cloudcounter/1.1.7/apiverve_cloudcounter-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-cloudcounter/apiverve_cloudcounter-1.1.7-py3-none-any.whl/apiverve_cloudcounter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_cloudcounter',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Counter is a simple tool for incrementing, decrementing, and resetting a cloud counter. It returns the current value of the counter.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
