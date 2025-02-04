4fc297e6a223f86b60e3df3af69c544ffd04120a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:42 +0000

    Add package matebotpy/1.1.4/matebotpy-1.1.4-py3-none-any.whl

---
+++ packages/matebotpy/matebotpy-1.1.4-py3-none-any.whl/matebot/fortnite/tracks.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class SparkTrack:
+    name: str
+    releaseYear: float
+    duration: float
+    album: str
+    artist: str
+    bpm: float
+    scale: str
+    albumArt: str
+    midiData: str
