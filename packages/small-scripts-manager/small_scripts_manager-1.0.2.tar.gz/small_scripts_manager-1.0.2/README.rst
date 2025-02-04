19ab6787961770134f62a833a04a09baa630802b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:30 +0000

    Add package small-scripts-manager/1.0.2/small_scripts_manager-1.0.2.tar.gz

---
+++ packages/small-scripts-manager/small_scripts_manager-1.0.2.tar.gz/small_scripts_manager-1.0.2/README.rst
@@ -0,0 +1,15 @@
+==========================
+CPUlib
+==========================
+This module provides a simple and responsive GUI interface to navigate your collection of small scripts.
+The name of the imported module has to be the same as of the function inside it that is to be executed.
+Download the full example from: https://github.com/CPUcademy/small_python_projects/tree/main/small_scripts_manager.
+
+Usage
+==========================
+Example of running the project:
+
+   .. code-block:: bash
+
+	import small_scripts_manager as ssm
+	s = ssm.SmallScriptsManager()
---
+++ packages/small-scripts-manager/small_scripts_manager-1.0.2.tar.gz/small_scripts_manager-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Education",
+  "Operating System :: Microsoft :: Windows :: Windows 10",
+  "License :: OSI Approved :: MIT License",
+  "Programming Language :: Python :: 3"
+]
+ 
+setup(
+  name="small_scripts_manager",
+  version="1.0.2",
+  description="This module provides a simple and responsive GUI interface to navigate your collection of small scripts.",
+  long_description=open("README.rst").read(),
+  long_description_content_type="text/x-rst",
+  url="",  
+  author="CPUcademy",
+  author_email="cpucademy@gmail.com",
+  license="MIT", 
+  classifiers=classifiers,
+  keywords="cpucademy small scripts manager application gui",
+  packages=find_packages(),
+  install_requires=["cryptography"] 
+)
