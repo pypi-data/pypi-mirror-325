52028f1c759ad969f2ade1ad9dc1e5587ebb4cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:47 +0000

    Add package opensafely/1.52.0/opensafely-1.52.0.tar.gz

---
+++ packages/opensafely/opensafely-1.52.0.tar.gz/opensafely-1.52.0/opensafely/jupyter.py
@@ -0,0 +1,13 @@
+from opensafely import launch
+
+
+DESCRIPTION = "DEPRECATED. Use: opensafely launch jupyter"
+
+add_arguments = launch.add_base_args
+
+
+def main(directory, name, port, no_browser):
+    print(
+        "opensafely jupyter command is deprecated - instead use: opensafely launch jupyter"
+    )
+    return 1
---
+++ packages/opensafely/opensafely-1.52.0.tar.gz/opensafely-1.52.0/opensafely/rstudio.py
@@ -0,0 +1,13 @@
+from opensafely import launch
+
+
+DESCRIPTION = "DEPRECATED. Use: opensafely launch rstudio"
+
+add_arguments = launch.add_base_args
+
+
+def main(directory, name, port, no_browser):
+    print(
+        "opensafely rstudio command is deprecated - instead use: opensafely launch rstudio"
+    )
+    return 1
