2604c9c7329d0956a7f7f75d1b38ee7f64d254b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:05:10 +0000

    Add package django-tag-me/2025.2.4.1/django_tag_me-2025.2.4.1.tar.gz

---
+++ packages/django-tag-me/django_tag_me-2025.2.4.1.tar.gz/django_tag_me-2025.2.4.1/tag_me/apps.py
@@ -0,0 +1,40 @@
+"""django-tag-me Apps file."""
+
+# from typing import override
+
+from django.apps import AppConfig
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+# from django.utils.translation.trans_real import settings
+
+
+class DjangoTagMeConfig(AppConfig):
+    """tag_me App name config."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tag_me"
+    verbose_name = _("Django Tag Me")
+    verbose_name_plural = _("Django Tag Me")
+
+    # @override
+    def ready(self):
+        super().ready()
+
+        if not hasattr(settings, "DJ_TAG_ME_USE_CUSTOM_MIGRATE"):
+            settings.DJ_TAG_ME_USE_CUSTOM_MIGRATE: bool = False  # type: ignore[attr-defined]
+
+        if not hasattr(settings, "PROJECT_APPS"):
+            settings.PROJECT_APPS: list = settings.INSTALLED_APPS  # type: ignore[attr-defined]
+        if not hasattr(settings, "DJ_TAG_ME_TEMPLATES"):
+            settings.DJ_TAG_ME_TEMPLATES = {
+                "default": "tag_me/tag_me_select.html",
+            }
+        if not hasattr(settings, "DJ_TAG_ME_MAX_NUMBER_DISPLAYED"):
+            settings.DJ_TAG_ME_MAX_NUMBER_DISPLAYED = 2
+
+        if not hasattr(settings, "DJ_TAG_ME_URLS"):
+            settings.DJ_TAG_ME_URLS: dict = {  # type: ignore[attr-defined]
+                "help_url": "",
+                "mgmt_url": "",
+            }
