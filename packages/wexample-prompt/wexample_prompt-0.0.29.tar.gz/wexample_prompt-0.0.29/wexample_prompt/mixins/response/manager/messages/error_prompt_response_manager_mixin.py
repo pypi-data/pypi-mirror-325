0d1d9974832a973e1df503b4606a1b968111a02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:26 +0000

    Add package wexample-prompt/0.0.29/wexample_prompt-0.0.29.tar.gz

---
+++ packages/wexample-prompt/wexample_prompt-0.0.29.tar.gz/wexample_prompt-0.0.29/wexample_prompt/mixins/response/manager/messages/error_prompt_response_manager_mixin.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING, Optional, Any, Dict
+
+from wexample_prompt.common.error_context import ErrorContext
+
+if TYPE_CHECKING:
+    from wexample_prompt.responses.messages import ErrorPromptResponse
+
+
+class ErrorPromptResponseManagerMixin:
+    def error(
+        self,
+        message: str,
+        params: Optional[Dict[str, Any]] = None,
+        exception: Optional[Any] = None,
+        fatal: bool = False,
+        **kwargs
+    ) -> "ErrorPromptResponse":
+        from wexample_prompt.responses.messages.error_prompt_response import ErrorPromptResponse
+
+        context = ErrorContext(
+            fatal=fatal,
+            params=params,
+            indentation=self.log_indent
+        )
+        response = ErrorPromptResponse.create_error(
+            message=message,
+            context=context,
+            exception=exception,
+            **kwargs
+        )
+
+        # Log to file/system if configured
+        if self._logger.handlers:
+            self._logger.error(message, extra={"params": params} if params else None)
+
+        self.print_response(response)
+
+        # Only call _on_fatal() if fatal is True
+        # if fatal:
+        response._on_fatal()
+
+        return response
