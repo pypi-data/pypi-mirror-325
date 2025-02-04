71e439e3da9e31160396c471f55c063d1e652076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:14 +0000

    Add package driftdetector/0.1.0/driftdetector-0.1.0.tar.gz

---
+++ packages/driftdetector/driftdetector-0.1.0.tar.gz/driftdetector-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="driftdetector",  
+    version="0.1.0",  
+    author="Kiplangat Korir",
+    author_email="korirkiplangat22@gmail.com",
+    description="A lightweight Python library for monitoring data and concept drift in machine learning models.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kiplangatkorir/driftmonitor",  
+    packages=find_packages(),  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  
+    install_requires=[  
+        "numpy",
+        "scikit-learn",
+        "pandas",
+        "scipy",
+        "matplotlib",  
+    ],
+    entry_points={  
+        'console_scripts': [
+            'driftmonitor=driftmonitor.cli:main',
+        ],
+    },
+)
