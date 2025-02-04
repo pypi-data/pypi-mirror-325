62d88d07e6449f22cbfcfec78936a4a54de8f705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:00 +0000

    Add package dataclay/4.1.1/dataclay-4.1.1.tar.gz

---
+++ packages/dataclay/dataclay-4.1.1.tar.gz/dataclay-4.1.1/src/dataclay/__init__.py
@@ -0,0 +1,13 @@
+"""
+Public dataclay functions exported to use (friendly) "from dataclay import ..."
+"""
+
+from dataclay.client.api import Client
+from dataclay.dataclay_object import DataClayObject, activemethod
+
+from dataclay.alien import AlienDataClayObject  # isort: skip
+
+StorageObject = DataClayObject
+
+__version__ = "4.1.1"
+__all__ = ["Client", "DataClayObject", "AlienDataClayObject", "activemethod", "StorageObject"]
