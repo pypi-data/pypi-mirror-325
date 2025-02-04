f36068f056ebd971c7adb12e16657e2724a920ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 17:04:23 +0000

    Add package pygifconvt-yyummmm/1.0.2/pygifconvt_yyummmm-1.0.2.tar.gz

---
+++ packages/pygifconvt-yyummmm/pygifconvt_yyummmm-1.0.2.tar.gz/pygifconvt_yyummmm-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'pygifconvt_yyummmm',
+    version          = '1.0.2',
+    description      = 'Test package for distribution',
+    author           = 'yyummmmmmmm',
+    author_email     = 'yyeom1995@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pillow'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['GIFCONVERTER', 'gifconverter'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
