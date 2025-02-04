0d45dfde7be731bcb27c080f535327ed9225b3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:12 +0000

    Add package apiverve-coordinatesaresea/1.1.7/apiverve_coordinatesaresea-1.1.7.tar.gz

---
+++ packages/apiverve-coordinatesaresea/apiverve_coordinatesaresea-1.1.7.tar.gz/apiverve_coordinatesaresea-1.1.7/src/apiverve_coordinatesaresea/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_coordinatesaresea',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Coordinates are Sea is a simple tool for checking if coordinates are in the sea. It returns if the coordinates are in the sea or not.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
