e89f84b5be2ef9699242cf5cc3652be795363e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:03:58 +0000

    Add package torcb/7/torcb-7.tar.gz

---
+++ packages/torcb/torcb-7.tar.gz/torcb-7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='torcb',
+      version='7',
+      description='none',
+      packages=['torcb'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
