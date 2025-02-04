53b4a3372438b848038f1b7d9f424d36c38324c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:16 +0000

    Add package liteauto/0.0.90/liteauto-0.0.90-py3-none-any.whl

---
+++ packages/liteauto/liteauto-0.0.90-py3-none-any.whl/liteauto/arxivlite/main.py
@@ -0,0 +1,18 @@
+from pandas import DataFrame
+
+from .schema import ArxivTags,Paper
+from .utils import (build_urls_from_tag,
+                    download_html,
+                    multiprocessing_parse_arxiv_html,
+                    post_process_deduplication)
+
+
+def get_todays_arxiv_papers() -> DataFrame:
+    tags = ArxivTags()
+
+    urls = build_urls_from_tag(tags)
+    data = download_html(urls)
+    results: list[Paper] = multiprocessing_parse_arxiv_html(data)
+
+    df_filtered: DataFrame = post_process_deduplication(results)
+    return df_filtered
