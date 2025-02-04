d12c94ee1005c8c0a224deb61d13a97c9420313d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:59 +0000

    Add package prefect/3.1.16.dev4/prefect-3.1.16.dev4-py3-none-any.whl

---
+++ packages/prefect/prefect-3.1.16.dev4-py3-none-any.whl/prefect/server/ui/assets/WorkPoolCreate-DGA3cIp0.js
@@ -0,0 +1,2 @@
+import{d as r,c as s,i as e,j as n,o as l,k as o,l as a,d6 as _,d7 as u}from"./index-qaIncb5u.js";import{u as c}from"./usePageTitle-BSROX4uK.js";const k=r({__name:"WorkPoolCreate",setup(p){return c("Create Work Pool"),(d,f)=>{const t=n("p-layout-default");return l(),s(t,null,{header:e(()=>[o(a(_))]),default:e(()=>[o(a(u))]),_:1})}}});export{k as default};
+//# sourceMappingURL=WorkPoolCreate-DGA3cIp0.js.map
---
+++ packages/prefect/prefect-3.1.16.dev4-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-BSROX4uK.js
@@ -0,0 +1,2 @@
+import{f as a,bw as c,l as n}from"./index-qaIncb5u.js";function f(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>n(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{f as u};
+//# sourceMappingURL=usePageTitle-BSROX4uK.js.map
