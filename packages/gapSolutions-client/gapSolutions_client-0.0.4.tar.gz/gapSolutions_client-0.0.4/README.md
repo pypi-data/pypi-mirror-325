e976a8bad7fda898e9528664148d1463b8ba10d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:03:41 +0000

    Add package gapSolutions-client/0.0.4/gapSolutions_client-0.0.4.tar.gz

---
+++ packages/gapSolutions-client/gapSolutions_client-0.0.4.tar.gz/gapSolutions_client-0.0.4/README.md
@@ -0,0 +1,25 @@
+# gapSolutions Client
+
+**gapSolutions Client** is a Python library designed to interact with the gapSolutions API, enabling developers to seamlessly integrate gapSolutions features into their applications.
+
+This client simplifies communication with the API, offering functions to interact with **measurement locations**, **organizations**, and **users**. Whether you are looking to fetch data, modify records, or perform deletion tasks, **gapSolutions Client** has got you covered.
+
+---
+
+## Features
+
+- **Easy Authentication**: Use an API token for quick, secure access.
+- **Measurement Locations**: Fetch, create, update, and delete measurement locations.
+- **Organizations**: Manage organizations easily with simple API requests.
+- **User Management**: Handle users' data with ease, including adding, deleting, and updating.
+- **Automatic Handling of Responses**: Built-in error handling for clean and efficient workflows.
+
+---
+
+## Installation
+
+You can install **gapSolutions Client** from PyPI using pip:
+
+```bash
+pip install gapSolutions_client
+```
---
+++ packages/gapSolutions-client/gapSolutions_client-0.0.4.tar.gz/gapSolutions_client-0.0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from os import path
+
+# Read the content of your README.md file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'gapSolutions client package'
+
+# Setting up
+setup(
+       # the name must match the folder name 'gapSolutions_client'
+        name="gapSolutions_client", 
+        version=VERSION,
+        author="Samer Hisham",
+        author_email="samerrhisham@gmail.com",
+        description=DESCRIPTION,
+        long_description=long_description,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'gapSolutions'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
