edf82d825dc4242578a0a1a3af6805584416f6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:25 +0000

    Add package brainbase-labs/2.2.0/brainbase_labs-2.2.0-py3-none-any.whl

---
+++ packages/brainbase-labs/brainbase_labs-2.2.0-py3-none-any.whl/brainbase/types/workers/deployments/voice_create_params.py
@@ -0,0 +1,23 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, Annotated, TypedDict
+
+from ...._utils import PropertyInfo
+
+__all__ = ["VoiceCreateParams"]
+
+
+class VoiceCreateParams(TypedDict, total=False):
+    name: Required[str]
+    """Name of the voice deployment"""
+
+    phone_number: Annotated[str, PropertyInfo(alias="phoneNumber")]
+    """Phone number for the voice deployment"""
+
+    voice_id: Annotated[str, PropertyInfo(alias="voiceId")]
+    """Voice ID for the deployment"""
+
+    voice_provider: Annotated[str, PropertyInfo(alias="voiceProvider")]
+    """Voice provider service"""
