da4378713757710e7108c9b473e2b77c7170bf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:30 +0000

    Add package TruthTorchLM/0.1.13/TruthTorchLM-0.1.13-py3-none-any.whl

---
+++ packages/TruthTorchLM/TruthTorchLM-0.1.13-py3-none-any.whl/TruthTorchLM/long_form_generation/claim_check_methods/claim_check_method.py
@@ -0,0 +1,46 @@
+import torch
+import random
+from typing import Union
+from abc import ABC, abstractmethod
+from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
+
+
+class ClaimCheckMethod(ABC):
+    def __init__(self):
+        pass
+
+
+    def __call__(self, model:Union[PreTrainedModel, str], input_text:str = '', generated_text:str = '', question_context:str = '', 
+                 claim:str='', text_so_far:str='', all_ids:Union[list, torch.Tensor] = None, 
+                    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = None, generation_seed = None, 
+                    messages:list = [], **kwargs) -> dict:
+        if generation_seed is not None:
+            torch.manual_seed(generation_seed)
+            random.seed(generation_seed)
+        if isinstance(model, str):
+            output_dict = self.check_claim_api(model=model, messages=messages, generated_text=generated_text, question_context=question_context, 
+                                                   claim=claim, text_so_far=text_so_far, generation_seed=generation_seed, **kwargs)
+        else:
+            output_dict = self.check_claim_local(model=model, input_text=input_text, generated_text=generated_text, question_context=question_context, 
+                                                     claim=claim, text_so_far=text_so_far, all_ids=all_ids, tokenizer=tokenizer, generation_seed=generation_seed, 
+                                                     messages=messages, **kwargs)
+
+        return output_dict
+
+
+    @abstractmethod
+    def check_claim_local(self, model:PreTrainedModel, input_text:str, generated_text:str, 
+                              question_context:str, claim:str, text_so_far:str, all_ids:Union[list, torch.Tensor], 
+                              tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = None, generation_seed=None, 
+                              messages:list = [], **kwargs) -> dict:
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+    @abstractmethod
+    def check_claim_api(self, model:str, messages:list, generated_text:str, 
+                            question_context:str, claim:str, text_so_far:str, generation_seed=None, **kwargs) -> dict:
+        raise NotImplementedError("Subclasses must implement this method")
+
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError("Subclasses must implement this method")
