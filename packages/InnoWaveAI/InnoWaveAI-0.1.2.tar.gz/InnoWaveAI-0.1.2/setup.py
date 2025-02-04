56e8c03e8fdd3211972246094f4f970c42978e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:04:06 +0000

    Add package InnoWaveAI/0.1.2/InnoWaveAI-0.1.2.tar.gz

---
+++ packages/InnoWaveAI/InnoWaveAI-0.1.2.tar.gz/InnoWaveAI-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InnoWaveAI",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="InnoWave",
+    author_email="bodnar.2009@inbox.ru",
+    description="Библиотека для генерации кода с использованием Together AI",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
