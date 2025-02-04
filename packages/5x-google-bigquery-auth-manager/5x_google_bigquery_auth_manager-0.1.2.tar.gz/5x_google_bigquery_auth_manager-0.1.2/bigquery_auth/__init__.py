f648cad2ea7aa91b0c7519ddb9e917059e32635c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:56 +0000

    Add package 5x-google-bigquery-auth-manager/0.1.2/5x_google_bigquery_auth_manager-0.1.2.tar.gz

---
+++ packages/5x-google-bigquery-auth-manager/5x_google_bigquery_auth_manager-0.1.2.tar.gz/5x_google_bigquery_auth_manager-0.1.2/bigquery_auth/__init__.py
@@ -0,0 +1,3 @@
+from .bigquery_auth import create_bigquery_client
+
+__all__ = ["create_bigquery_client"]  # Defines what gets imported with `from bigquery_auth import *`
---
+++ packages/5x-google-bigquery-auth-manager/5x_google_bigquery_auth_manager-0.1.2.tar.gz/5x_google_bigquery_auth_manager-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="5x-google-bigquery-auth-manager",
+    version="0.1.2",
+    author="5X",
+    author_email="support@5x.co",
+    description="A simple Python library for creating a Google BigQuery client using either an access token or a service account JSON file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/5x-Platform/5x-nextgen-python-libraries.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "google-api-core",
+        "google-cloud-bigquery",
+        "google-cloud-bigquery-storage",
+        "db-dtypes"
+    ]
+)
