23a8cb23611e7689d5d48496220e7231fcd1be26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:12 +0000

    Add package apiverve-worldwideearthquakes/1.1.7/apiverve_worldwideearthquakes-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-worldwideearthquakes/apiverve_worldwideearthquakes-1.1.7-py3-none-any.whl/apiverve_worldwideearthquakes/exceptions.py
@@ -0,0 +1,3 @@
+class EarthquakeWorldwideEarthquakesAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-worldwideearthquakes/apiverve_worldwideearthquakes-1.1.7-py3-none-any.whl/apiverve_worldwideearthquakes/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_worldwideearthquakes',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Earthquake is a simple tool for getting earthquake data. It returns the earthquake data for the past hour.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
