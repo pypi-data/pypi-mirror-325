4cc1134adb9b1e95caf629150afb5ce6fa996f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:33 +0000

    Add package aui2/0.1.9/aui2-0.1.9.tar.gz

---
+++ packages/aui2/aui2-0.1.9.tar.gz/aui2-0.1.9/aui2/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'aui2'
+
+VER_MAJOR        = 0
+VER_MINOR        = 1
+VER_RELEASE      = 9
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
