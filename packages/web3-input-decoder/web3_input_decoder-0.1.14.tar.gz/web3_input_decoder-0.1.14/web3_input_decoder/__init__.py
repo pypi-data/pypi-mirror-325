71a61b08108f8629b87b21bd245a32b469e9fab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:24 +0000

    Add package web3-input-decoder/0.1.14/web3_input_decoder-0.1.14.tar.gz

---
+++ packages/web3-input-decoder/web3_input_decoder-0.1.14.tar.gz/web3_input_decoder-0.1.14/web3_input_decoder/__init__.py
@@ -0,0 +1,55 @@
+from typing import Any, List, Optional, Tuple, Union
+
+from .decoder import InputDecoder
+
+__all__ = (
+    "decode_constructor",
+    "decode_function",
+    "InputDecoder",
+)
+
+
+def decode_constructor(
+    abi: List[dict],
+    tx_input: Union[str, bytes],
+    bytecode: Optional[Union[str, bytes]] = None,
+) -> List[Tuple[str, str, Any]]:
+    """Decode constructor transaction input
+
+    Parameters
+    ----------
+    abi: List[dict]
+        Contract ABI
+    tx_input: Union[str, bytes]
+        Transaction input to decode, with or without deployed contract bytecode
+    bytecode: Union[str, bytes], optional
+        Optional deployed contract bytecode. If this is set, `tx_input` should include bytecode
+
+    Returns
+    -------
+    List[Tuple[str, str, Any]]
+        Decoded type-name-value tuples
+    """
+    decoder = InputDecoder(abi)  # type:ignore[arg-type]
+    return decoder.decode_constructor(tx_input, bytecode).arguments
+
+
+def decode_function(
+    abi: List[dict], tx_input: Union[str, bytes]
+) -> List[Tuple[str, str, Any]]:
+    """Decode function transaction input
+
+    Parameters
+    ----------
+    abi: List[dict]
+        Contract ABI
+    tx_input: Union[str, bytes]
+        Transaction input to decode
+
+    Returns
+    -------
+    List[Tuple[str, str, Any]]
+        Decoded type-name-value tuples
+    """
+    decoder = InputDecoder(abi)  # type:ignore[arg-type]
+    return decoder.decode_function(tx_input).arguments
