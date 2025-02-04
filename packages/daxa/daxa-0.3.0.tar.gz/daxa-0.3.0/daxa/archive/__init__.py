6023455c2c6dc58f2bb223f2db7144784ed814cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package daxa/0.3.0/daxa-0.3.0.tar.gz

---
+++ packages/daxa/daxa-0.3.0.tar.gz/daxa-0.3.0/daxa/archive/__init__.py
@@ -0,0 +1,4 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 22/03/2023, 11:37. Copyright (c) The Contributors
+
+from .base import Archive
