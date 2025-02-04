cc22cf25df4d5f0d7c7c7087e09641b13128c667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:04:49 +0000

    Add package encryptJS/0.0.1/encryptJS-0.0.1.tar.gz

---
+++ packages/encryptJS/encryptJS-0.0.1.tar.gz/encryptJS-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="encryptJS",
+    version="0.0.1",
+    author="jinmingzhou",
+    author_email="17816765317@163.com",
+    description="一个简易工具提供常见加密解密功能如MD5,AES,SHA1等",
+    long_description=open("README.md",encoding='utf8').read(),
+    long_description_content_type="text/markdown",
+    url="",
+    download_url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "PyExecJS2>=1.6.1",
+        'requests',
+        'pycryptodome',
+        'gmssl',
+        'cryptography'
+    ],
+    python_requires=">=3.10"
+)
