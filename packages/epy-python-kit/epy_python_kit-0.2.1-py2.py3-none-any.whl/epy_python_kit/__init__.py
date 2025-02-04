ab888bec44ca4bf664aea94b300eef9b0f4a113c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:55 +0000

    Add package epy-python-kit/0.2.1/epy_python_kit-0.2.1-py2.py3-none-any.whl

---
+++ packages/epy-python-kit/epy_python_kit-0.2.1-py2.py3-none-any.whl/epy_python_kit/__init__.py
@@ -0,0 +1,5 @@
+"""
+A module designed to perform package installations, and verification of install,
+in preparation for the estructúrate courses and estructuraPy (epy) Python Libraries
+"""
+__version__ = "0.2.1"
