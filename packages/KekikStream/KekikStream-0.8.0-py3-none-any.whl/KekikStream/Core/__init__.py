69cfdd16b896294dfbee326778a7f9395626bd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:04:02 +0000

    Add package KekikStream/0.8.0/KekikStream-0.8.0-py3-none-any.whl

---
+++ packages/KekikStream/KekikStream-0.8.0-py3-none-any.whl/KekikStream/Core/__init__.py
@@ -0,0 +1,9 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from .PluginBase      import PluginBase
+from .PluginLoader    import PluginLoader
+from .PluginModels    import SearchResult, MovieInfo, Episode, SeriesInfo
+from .ExtractorBase   import ExtractorBase
+from .ExtractorLoader import ExtractorLoader
+from .ExtractorModels import ExtractResult, Subtitle
+from .MediaHandler    import MediaHandler
