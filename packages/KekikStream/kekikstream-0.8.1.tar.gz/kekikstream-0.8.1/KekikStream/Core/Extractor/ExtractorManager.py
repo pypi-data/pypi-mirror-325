7eb63f202b88a3aaec3ee09527fa7eb140644bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 17:04:24 +0000

    Add package KekikStream/0.8.1/kekikstream-0.8.1.tar.gz

---
+++ packages/KekikStream/kekikstream-0.8.1.tar.gz/kekikstream-0.8.1/KekikStream/Core/Extractor/ExtractorManager.py
@@ -0,0 +1,28 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from .ExtractorLoader import ExtractorLoader
+from .ExtractorBase   import ExtractorBase
+
+class ExtractorManager:
+    def __init__(self, extractor_dir="Extractors"):
+        self.extractor_loader = ExtractorLoader(extractor_dir)
+        self.extractors       = self.extractor_loader.load_all()
+
+    def find_extractor(self, link):
+        for extractor_cls in self.extractors:
+            extractor:ExtractorBase = extractor_cls()
+            if extractor.can_handle_url(link):
+                return extractor
+
+        return None
+
+    def map_links_to_extractors(self, links):
+        mapping = {}
+        for link in links:
+            for extractor_cls in self.extractors:
+                extractor:ExtractorBase = extractor_cls()
+                if extractor.can_handle_url(link):
+                    mapping[link] = f"{extractor.name:<30} » {link.replace(extractor.main_url, '')}"
+                    break
+
+        return mapping
