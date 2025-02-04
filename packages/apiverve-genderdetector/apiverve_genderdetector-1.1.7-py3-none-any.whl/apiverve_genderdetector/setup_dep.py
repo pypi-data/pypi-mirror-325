ef956f3057b4a41480544d017f2a230208d895f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:26 +0000

    Add package apiverve-genderdetector/1.1.7/apiverve_genderdetector-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-genderdetector/apiverve_genderdetector-1.1.7-py3-none-any.whl/apiverve_genderdetector/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_genderdetector',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Easily detect the gender of an individual based on their name. This API uses AI to predict the likely gender of a person by their name',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
