7de6ff93e7744c6ad150dd61124dcd398276a1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:29 +0000

    Add package rspylog/0.1.3/rspylog-0.1.3.tar.gz

---
+++ packages/rspylog/rspylog-0.1.3.tar.gz/rspylog-0.1.3/rspylog/footprint.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def leave(
+        log_type: str,
+        message: str | None = None,
+        **kwargs,
+):
+    logger = logging.getLogger()
+
+    data = dict(
+        msg=message,
+        extra={
+            'details': kwargs,
+        }
+    )
+
+    error_mapper = {
+        'error': logger.error,
+        'warning': logger.warning,
+        'debug': logger.debug
+    }.get(log_type.lower(), logger.info)
+
+    error_mapper(**data)
