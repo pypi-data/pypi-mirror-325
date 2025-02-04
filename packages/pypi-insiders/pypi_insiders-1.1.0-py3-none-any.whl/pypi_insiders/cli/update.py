d6cada168d79f07ca14b890730516a86e0f63d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:26 +0000

    Add package pypi-insiders/1.1.0/pypi_insiders-1.1.0-py3-none-any.whl

---
+++ packages/pypi-insiders/pypi_insiders-1.1.0-py3-none-any.whl/pypi_insiders/cli/update.py
@@ -0,0 +1,30 @@
+"""CLI `update` command."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pypi_insiders.update import update_packages
+
+if TYPE_CHECKING:
+    import argparse
+
+
+def run_update(opts: argparse.Namespace) -> int:
+    """Command to update packages.
+
+    Parameters:
+        opts: The parsed CLI arguments.
+
+    Returns:
+        A CLI exit code.
+    """
+    update_packages(
+        conf_path=opts.conf_path,
+        repo_dir=opts.repo_dir,
+        index_url=opts.index_url,
+        index_user=opts.index_user,
+        index_password=opts.index_password,
+        repos=opts.repositories,
+    )
+    return 0
