1a6e005511e467adb15b31c7ed319fd1b13b628b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:26 +0000

    Add package tuxlava/0.1.1/tuxlava-0.1.1.tar.gz

---
+++ packages/tuxlava/tuxlava-0.1.1.tar.gz/tuxlava-0.1.1/tuxlava/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# vim: set ts=4
+#
+# Copyright 2024-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Command line tool for generating LAVA jobs
+"""
+__version__ = "0.1.1"
