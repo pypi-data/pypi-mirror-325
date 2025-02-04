74fd516fcdc142271c3f464651fd08598494c530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-countrylookup/1.1.7/apiverve_countrylookup-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-countrylookup/apiverve_countrylookup-1.1.7-py3-none-any.whl/apiverve_countrylookup/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_countrylookup',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Country Lookup is a simple tool for looking up country data. It returns the country name, capital, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
