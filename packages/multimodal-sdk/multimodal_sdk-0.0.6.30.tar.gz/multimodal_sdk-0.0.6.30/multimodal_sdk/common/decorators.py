489b3e868c4da345e95f79c36871e162bd3e3710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:58 +0000

    Add package multimodal-sdk/0.0.6.30/multimodal_sdk-0.0.6.30.tar.gz

---
+++ packages/multimodal-sdk/multimodal_sdk-0.0.6.30.tar.gz/multimodal_sdk-0.0.6.30/multimodal_sdk/common/decorators.py
@@ -0,0 +1,30 @@
+import logging
+from functools import wraps
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+REQUIRED_PARAMS = {
+    "oauth_url": "",
+    "base_url": "",
+    "authz_url": "",
+    "access_token": "",
+    "refresh_token": "",
+}
+
+def log_and_authorize(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        
+        filtered_kwargs = {k: v for k, v in kwargs.items() if k not in REQUIRED_PARAMS}
+        
+        # logger.info(f"{'#' * 10} {func_name} called with arguments: {{'args': {args}, 'kwargs': {filtered_kwargs}}} {'#' * 10}")
+        
+        headers = {"Authorization": f"Bearer {kwargs['access_token']}"}
+        kwargs['headers'] = headers
+        
+        result = await func(*args, **kwargs)
+        
+        return result
+    return wrapper
