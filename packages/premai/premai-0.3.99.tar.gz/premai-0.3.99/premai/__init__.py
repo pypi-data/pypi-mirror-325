9fd17572d9d9d58814c6d0ee4ed3b8ad41ff84f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:03:54 +0000

    Add package premai/0.3.99/premai-0.3.99.tar.gz

---
+++ packages/premai/premai-0.3.99.tar.gz/premai-0.3.99/premai/__init__.py
@@ -0,0 +1,38 @@
+from .client import AuthenticatedClient
+
+from .api import (
+    RepositoriesModule,
+    EmbeddingsModule,
+    V1Module,
+    ModelsModule,
+    FeedbacksModule,
+    TracesModule,
+    RepositoryModuleWrapper,
+    ChatModuleWrapper,
+)
+
+class Prem:
+    repositories: RepositoriesModule
+    embeddings: EmbeddingsModule
+    v1: V1Module
+    models: ModelsModule
+    feedbacks: FeedbacksModule
+    traces: TracesModule
+    repository: RepositoryModuleWrapper
+    chat: ChatModuleWrapper
+
+    def __init__(self, api_key: str, base_url='https://app.premai.io'):
+        client = AuthenticatedClient(token=api_key, base_url=base_url)
+        # Init modules
+        self.repositories = RepositoriesModule(client)
+        self.embeddings = EmbeddingsModule(client)
+        self.v1 = V1Module(client)
+        self.models = ModelsModule(client)
+        self.feedbacks = FeedbacksModule(client)
+        self.traces = TracesModule(client)
+        self.repository = RepositoryModuleWrapper(client)
+        self.chat = ChatModuleWrapper(client)
+
+__all__ = [
+    "Prem"
+]
