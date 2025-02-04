c508c6c2b74642eda460193beb53e7a954c8190b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:28 +0000

    Add package apiverve-goldprice/1.1.7/apiverve_goldprice-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-goldprice/apiverve_goldprice-1.1.7-py3-none-any.whl/apiverve_goldprice/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_goldprice',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Gold Price is a simple tool for getting the current price of gold. It returns the current price of gold in USD and in various units.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
