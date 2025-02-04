aca1271909a06fe0c30615f4def750d4f32d9a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:03:53 +0000

    Add package nexify/0.4.0/nexify-0.4.0-py3-none-any.whl

---
+++ packages/nexify/nexify-0.4.0-py3-none-any.whl/nexify/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.4.0"
+
+from .applications import Nexify as Nexify
+from .param_functions import Body as Body
+from .param_functions import Context as Context
+from .param_functions import Event as Event
+from .param_functions import Path as Path
+from .param_functions import Query as Query
