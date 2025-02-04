ddd48db9e6a0dcd6781c2eac399fcdc5194dad63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:29 +0000

    Add package textualicious/0.0.1/textualicious-0.0.1.tar.gz

---
+++ packages/textualicious/textualicious-0.0.1.tar.gz/textualicious-0.0.1/src/textualicious/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1"
+
+from textualicious.dataclass_table import DataClassTable
+from textualicious.dataclass_viewer import DataClassViewer
+from textualicious.log_widget import LoggingWidget
+
+__all__ = ["DataClassTable", "DataClassViewer", "LoggingWidget"]
