b906e07b311da385f01012cb69feaaf81c0a656e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:04 +0000

    Add package openstructuralpy/0.1/openstructuralpy-0.1.tar.gz

---
+++ packages/openstructuralpy/openstructuralpy-0.1.tar.gz/openstructuralpy-0.1/README.md
@@ -0,0 +1,7 @@
+# OpenStructuralPy
+
+OpenStructuralPy is a Python library for structural engineering calculations.
+
+## Installation
+```sh
+pip install openstructuralpy
---
+++ packages/openstructuralpy/openstructuralpy-0.1.tar.gz/openstructuralpy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openstructuralpy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add required dependencies here, e.g., 'numpy', 'pandas', etc.
+    ],
+)
