52ee39476b345ec8f3d454bcbe30e3125137a5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:03 +0000

    Add package ingestify/0.4.1/ingestify-0.4.1.tar.gz

---
+++ packages/ingestify/ingestify-0.4.1.tar.gz/ingestify-0.4.1/ingestify/infra/store/file/s3_file_repository.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from typing import BinaryIO
+
+import boto3 as boto3
+
+from ingestify.domain import Dataset
+from ingestify.domain.models import FileRepository
+
+
+class S3FileRepository(FileRepository):
+    _s3 = None
+
+    @property
+    def s3(self):
+        if not self._s3:
+            self._s3 = boto3.resource("s3")
+        return self._s3
+
+    def __getstate__(self):
+        return {
+            "base_dir": self.base_dir,
+            "_s3": None,
+            "identifier_transformer": self.identifier_transformer,
+        }
+
+    def save_content(
+        self,
+        bucket: str,
+        dataset: Dataset,
+        revision_id: int,
+        filename: str,
+        stream: BinaryIO,
+    ) -> Path:
+        key = self.get_write_path(bucket, dataset, revision_id, filename)
+        s3_bucket = Path(key.parts[0])
+
+        self.s3.Object(str(s3_bucket), str(key.relative_to(s3_bucket))).put(Body=stream)
+        return key
+
+    def load_content(self, storage_path: str) -> BinaryIO:
+        key = self.get_read_path(storage_path)
+        s3_bucket = Path(key.parts[0])
+        return self.s3.Object(str(s3_bucket), str(key.relative_to(s3_bucket))).get()[
+            "Body"
+        ]
+
+    @classmethod
+    def supports(cls, url: str) -> bool:
+        return url.startswith("s3://")
