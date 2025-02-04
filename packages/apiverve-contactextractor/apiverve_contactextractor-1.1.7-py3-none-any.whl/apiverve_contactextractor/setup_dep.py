c65d4004eab91cd98f145f6b6650baf50aceae71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:12 +0000

    Add package apiverve-contactextractor/1.1.7/apiverve_contactextractor-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-contactextractor/apiverve_contactextractor-1.1.7-py3-none-any.whl/apiverve_contactextractor/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_contactextractor',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Contact Extractor is a simple tool for extracting contact data from a website URL. It returns the contact emails, phone numbers, and places.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
