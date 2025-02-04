d275363c1b82e17704b5f2e4e3d757041647a081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:03:54 +0000

    Add package supersullytools/14.0.0/supersullytools-14.0.0.tar.gz

---
+++ packages/supersullytools/supersullytools-14.0.0.tar.gz/supersullytools-14.0.0/README.md
@@ -0,0 +1,7 @@
+SuperSully Tools
+================
+
+**Latest Version:** 14.0.0
+
+**SupersullyTools** is a Python package that brings together a suite of utilities and helpers across several domains of
+software development; this is my personal prototyping toolkit.
---
+++ packages/supersullytools/supersullytools-14.0.0.tar.gz/supersullytools-14.0.0/src/supersullytools/utils/common_init.py
@@ -0,0 +1,79 @@
+import os
+from typing import Optional
+
+from simplesingletable import DynamoDbMemory
+
+from supersullytools.llm.completions import CompletionHandler
+from supersullytools.llm.trackers import (
+    CompletionTracker,
+    DailyUsageTracking,
+    GlobalUsageTracker,
+    SessionUsageTracking,
+    TopicUsageTracking,
+)
+from supersullytools.utils.media_manager import MediaManager
+
+
+def get_standard_completion_dynamodb_memory(logger=None) -> DynamoDbMemory:
+    if logger is None:
+        from logzero import logger
+    memory = DynamoDbMemory(logger=logger, table_name=os.environ["COMPLETION_TRACKING_DYNAMODB_TABLE"])
+    return memory
+
+
+def get_standard_completion_media_manager(logger=None) -> MediaManager:
+    if logger is None:
+        from logzero import logger
+    return MediaManager(
+        bucket_name=os.environ["COMPLETION_TRACKING_BUCKET_NAME"],
+        logger=logger,
+        dynamodb_memory=get_standard_completion_dynamodb_memory(logger),
+        global_prefix="completion_tracking",
+    )
+
+
+def get_standard_completion_handler(
+    extra_trackers: Optional[list] = None,
+    topics: Optional[list[str]] = None,
+    logger=None,
+    include_session_tracker: bool = False,
+    store_source_tag: Optional[str] = None,
+    **kwargs,
+) -> CompletionHandler:
+    if logger is None:
+        from logzero import logger
+    trackers = []
+    if os.getenv("COMPLETION_TRACKING_DYNAMODB_TABLE"):
+        memory = get_standard_completion_dynamodb_memory(logger=logger)
+        trackers.append(GlobalUsageTracker.ensure_exists(memory))
+        trackers.append(DailyUsageTracking.get_for_today(memory))
+        trackers.extend(TopicUsageTracking.get_for_topic(memory, x) for x in (topics or []))
+        if os.getenv("DISABLE_COMPLETION_TRACKING_RESPONSE_STORAGE"):
+            store_prompt_and_response = False
+        else:
+            store_prompt_and_response = True
+    else:
+        memory = None
+        store_prompt_and_response = False
+
+    if os.getenv("COMPLETION_TRACKING_BUCKET_NAME"):
+        media_manager = get_standard_completion_media_manager(logger)
+    else:
+        media_manager = None
+
+    trackers.extend(extra_trackers or [])
+    if include_session_tracker:
+        trackers.append(SessionUsageTracking())
+
+    return CompletionHandler(
+        logger=logger,
+        completion_tracker=CompletionTracker(
+            memory=memory,
+            trackers=trackers,
+            store_prompt_and_response=store_prompt_and_response,
+            store_prompt_images_media_manager=media_manager,
+            store_source_tag=store_source_tag,
+            logger=logger,
+        ),
+        **kwargs,
+    )
