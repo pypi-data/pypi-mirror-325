e66fef864850ea42ea28d5d0d93b2e1fe50c1b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:20 +0000

    Add package pyany-serde/0.1.10/pyany_serde-0.1.10-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/pyany-serde/pyany_serde-0.1.10-cp311-cp311-musllinux_1_2_aarch64.whl/pyany_serde/python_serde.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class PythonSerde(Generic[T]):
+    @abstractmethod
+    def to_bytes(self, obj: T) -> bytes:
+        """
+        Function to convert obj to bytes, for passing between batched agent and the agent manager.
+        :return: bytes b such that from_bytes(b) == obj.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def from_bytes(self, byts: bytes) -> T:
+        """
+        Function to convert bytes to T, for passing between batched agent and the agent manager.
+        :return: T obj such that from_bytes(to_bytes(obj)) == obj.
+        """
+        raise NotImplementedError
