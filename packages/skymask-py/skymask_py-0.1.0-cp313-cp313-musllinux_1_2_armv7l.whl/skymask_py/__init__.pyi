60a52d142e8bd51d2c635963f26921cca40e1559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:17 +0000

    Add package skymask-py/0.1.0/skymask_py-0.1.0-cp313-cp313-musllinux_1_2_armv7l.whl

---
+++ packages/skymask-py/skymask_py-0.1.0-cp313-cp313-musllinux_1_2_armv7l.whl/skymask_py/__init__.pyi
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import List, Tuple
+
+class World:
+    def __init__(self, path: str, max_dist: float, eps: float = 1e-6):
+        self.lines: NDArray[np.float64]
+        self.max_dist: float
+        self.eps: float
+    @classmethod
+    def from_lines(
+        cls, lines: NDArray[np.float64], max_dist: float, eps: float = 1e-6
+    ) -> "World": ...
+    def skymask(self, pos: Tuple[float, float]) -> SkymaskMap: ...
+    def par_skymask(self, pos: List[Tuple[float, float]]) -> List[SkymaskMap]: ...
+    def par_samples(
+        self, pos: List[Tuple[float, float]], samples: NDArray[np.float64]
+    ) -> NDArray[np.float64]: ...
+
+class SkymaskMap:
+    def at(self, theta: float) -> float: ...
+    def samples(self, samples: NDArray[np.float64]) -> NDArray[np.float64]: ...
+    def segments(self) -> List[Tuple[Tuple[float, float], Tuple[float, float]]]: ...
