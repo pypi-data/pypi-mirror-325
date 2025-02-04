035badc82b3d3fff41df96b8ca471dca4a69294c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:03:51 +0000

    Add package matrixplot/0.4/matrixplot-0.4.tar.gz

---
+++ packages/matrixplot/matrixplot-0.4.tar.gz/matrixplot-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="matrixplot",
+    version="0.4",
+    py_modules=["matrixplot"],
+    install_requires=["matplotlib"],
+    description="📊 Conveniently plot matrices in matplotlib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["License :: OSI Approved :: MIT License"],
+    license_file="MIT"
+)
