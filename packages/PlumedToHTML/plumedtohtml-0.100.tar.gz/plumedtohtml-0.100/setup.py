bc799c9d67f9598554324a85e56e6210b4999413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:37 +0000

    Add package PlumedToHTML/0.100/plumedtohtml-0.100.tar.gz

---
+++ packages/PlumedToHTML/plumedtohtml-0.100.tar.gz/plumedtohtml-0.100/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='PlumedToHTML',  
+     version='0.100',
+     author="Gareth Tribello",
+     author_email="gareth.tribello@gmail.com",
+     description="A package for creating pretified HTML for PLUMED files",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/plumed/PlumedToHTML",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Freely Distributable",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=['lxml','pygments','requests','bs4'],
+    # This adds the assets that PlumedToHTML.get_html_header() asks for ()
+     data_files=[('PlumedToHTML', ['PlumedToHTML/assets/header.html'])],
+     include_package_data=True,
+ )
+# print(setuptools.find_packages())
