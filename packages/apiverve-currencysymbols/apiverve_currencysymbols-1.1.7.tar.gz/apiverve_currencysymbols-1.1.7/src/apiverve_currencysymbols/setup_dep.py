5188642ae0d2f984a9e34c180657a8aed8dd9b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-currencysymbols/1.1.7/apiverve_currencysymbols-1.1.7.tar.gz

---
+++ packages/apiverve-currencysymbols/apiverve_currencysymbols-1.1.7.tar.gz/apiverve_currencysymbols-1.1.7/src/apiverve_currencysymbols/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_currencysymbols',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Currency Symbols is a simple tool for getting currency symbols. It returns the currency symbol, name, and more of a currency.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
