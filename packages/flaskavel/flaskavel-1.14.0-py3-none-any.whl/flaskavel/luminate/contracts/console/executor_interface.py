28a92d7e9538540477232014bf49210bace74d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:39 +0000

    Add package flaskavel/1.14.0/flaskavel-1.14.0-py3-none-any.whl

---
+++ packages/flaskavel/flaskavel-1.14.0-py3-none-any.whl/flaskavel/luminate/contracts/console/executor_interface.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+class IExecutor(ABC):
+    """
+    Interface for execution state logging.
+
+    This interface defines methods for logging different execution states:
+    "RUNNING", "DONE", and "FAIL".
+    """
+
+    @staticmethod
+    @abstractmethod
+    def running(program: str, time: str = ''):
+        """
+        Logs the execution of a program in a "RUNNING" state.
+
+        Parameters
+        ----------
+        program : str
+            The name of the program being executed.
+        time : str, optional
+            The time duration of execution, default is an empty string.
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def done(program: str, time: str = ''):
+        """
+        Logs the execution of a program in a "DONE" state.
+
+        Parameters
+        ----------
+        program : str
+            The name of the program being executed.
+        time : str, optional
+            The time duration of execution, default is an empty string.
+        """
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fail(program: str, time: str = ''):
+        """
+        Logs the execution of a program in a "FAIL" state.
+
+        Parameters
+        ----------
+        program : str
+            The name of the program being executed.
+        time : str, optional
+            The time duration of execution, default is an empty string.
+        """
+        pass
