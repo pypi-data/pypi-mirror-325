6f2520eb28308865f07db7ff8682e2d3e970ca54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:20 +0000

    Add package brainbase-labs/1.4.0/brainbase_labs-1.4.0.tar.gz

---
+++ packages/brainbase-labs/brainbase_labs-1.4.0.tar.gz/brainbase_labs-1.4.0/src/brainbase/types/workers/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .flow_create_params import FlowCreateParams as FlowCreateParams
+from .flow_update_params import FlowUpdateParams as FlowUpdateParams
