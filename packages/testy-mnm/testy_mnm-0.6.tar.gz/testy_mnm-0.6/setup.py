c6cf1a42736b12fb54cfa6e3c4fc8aeb4bb89af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:05 +0000

    Add package testy-mnm/0.6/testy_mnm-0.6.tar.gz

---
+++ packages/testy-mnm/testy_mnm-0.6.tar.gz/testy_mnm-0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages, Extension
+from distutils.command.build import build
+import os
+import shutil
+import sys
+
+testy_c_module = Extension(
+    'testy_c_module',
+    sources=['testy_mnm/core/src/module.c'],
+)
+
+setup(
+    name='testy_mnm',
+    version='0.6',
+    packages=find_packages(),
+    ext_modules=[testy_c_module],
+)
