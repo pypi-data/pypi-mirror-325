174dffb8fa8d0b3715d1e92d0a31e74e50ee747c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:38 +0000

    Add package boostedautocomplete/0.1.7/boostedautocomplete-0.1.7.tar.gz

---
+++ packages/boostedautocomplete/boostedautocomplete-0.1.7.tar.gz/boostedautocomplete-0.1.7/source/update_checker.py
@@ -0,0 +1,48 @@
+import json
+import os
+from datetime import datetime, timedelta
+
+# absolute path to the module directory
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+LAST_UPDATE_FILE = os.path.join(MODULE_DIR, 'last_updated_boostedautocomplete.json')
+
+def load_last_update_time():
+
+    if os.path.exists(LAST_UPDATE_FILE):
+        try:
+            with open(LAST_UPDATE_FILE, 'r') as file:
+                data = json.load(file)
+                last_update_time = datetime.fromisoformat(data['last_updated'])
+                # print("Loaded last update time from file")
+                return last_update_time
+        except (json.JSONDecodeError, KeyError) as e:
+            print(f"Error loading last update time from file: {e}")
+            return None
+    else:
+        # print("No last update file found.")
+        save_last_update_time()
+        return None
+
+def save_last_update_time():
+
+    now = datetime.now().isoformat()
+    try:
+        with open(LAST_UPDATE_FILE, 'w') as file:
+            json.dump({'last_updated': now}, file)
+        print("Last update time saved to file.")
+    except Exception as e:
+        print(f"Error saving last update time: {e}")
+
+def should_check_for_updates(interval_days):
+
+    last_update_time = load_last_update_time()
+    if last_update_time is None:
+        # print("No last update time found.")
+        return True
+    time_delta = datetime.now()- last_update_time
+    if time_delta > timedelta(days=interval_days):
+        # print(f"More than {interval_days} have passed")
+        return True
+    else:
+        # print(f"Last update was {time_delta.days} days ago.")
+        return False
---
+++ packages/boostedautocomplete/boostedautocomplete-0.1.7.tar.gz/boostedautocomplete-0.1.7/source/update_package.py
@@ -0,0 +1,74 @@
+import subprocess
+import sys
+import pkg_resources
+import requests
+import platform
+import json
+from .update_checker import save_last_update_time
+
+def get_python_command():
+    # Determine whether to use 'python' or 'python3'
+    return "python3" if platform.system() != "Windows" else "python"
+
+def get_latest_version(package_name):
+    # Fetch the latest version of a package from PyPI
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        return data['info']['version']
+    except (requests.RequestException, json.JSONDecodeError) as e:
+        print(f"Erro fetching the latest version: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+    return None
+
+def is_package_installed(package_name):
+    # Check if a package is installed
+    try:
+        return pkg_resources.get_distribution(package_name).version
+    except pkg_resources.DistributionNotFound:
+        print(f"{package_name} is not installed.")
+    except Exception as e:
+        print(f"Error checking if {package_name} is installed: {e}")
+    return None
+
+def install_or_update_package(package_name, action="install"):
+    
+    # install or update a package using pip
+    python_cmd = get_python_command()
+    action_str = "Installing" if action == "install" else "Updating"
+
+    try:
+        print(f"{action_str} {package_name}...")
+        subprocess.check_call([python_cmd, "-m", "pip", "install", "--upgrade", package_name])
+        print(f"{package_name} {action.lower()}ed successfully")
+        save_last_update_time()
+
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to {action.lower()} {package_name}: {e}")
+    except Exception as e:
+        print(f"Unexpected error during {action.lower()} of {package_name}: {e}")
+    
+
+
+def main(package_name):
+    # Check installation, and update if necessary
+
+    installed_version = is_package_installed(package_name)
+    latest_version = get_latest_version(package_name)
+
+    if latest_version and installed_version:
+        if latest_version != installed_version:
+            print(f"New version available: {latest_version}. Installed version: {installed_version}")
+            user_input = input(f"{package_name} is out of date. update to {latest_version}? (y/n):").strip().lower()
+            if user_input == 'y':
+                install_or_update_package(package_name, action="update")
+            else:
+                print(f"{package_name} update skipped.")
+        else:
+            print(f"{package_name} is up to date")
+    elif not installed_version:
+        print(f"{package_name} is not installed. installing...")
+        install_or_update_package(package_name, action="install")
