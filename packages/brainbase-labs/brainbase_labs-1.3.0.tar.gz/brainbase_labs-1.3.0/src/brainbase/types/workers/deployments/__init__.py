4c59a72c01e9742787ef99a421a62410fd165bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:18 +0000

    Add package brainbase-labs/1.3.0/brainbase_labs-1.3.0.tar.gz

---
+++ packages/brainbase-labs/brainbase_labs-1.3.0.tar.gz/brainbase_labs-1.3.0/src/brainbase/types/workers/deployments/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .voice_create_params import VoiceCreateParams as VoiceCreateParams
+from .voice_update_params import VoiceUpdateParams as VoiceUpdateParams
