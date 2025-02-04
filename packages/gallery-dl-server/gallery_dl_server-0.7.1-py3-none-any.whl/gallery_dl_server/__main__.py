22db36867130a117c92acf2f7d467472d9810fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:28 +0000

    Add package gallery-dl-server/0.7.1/gallery_dl_server-0.7.1-py3-none-any.whl

---
+++ packages/gallery-dl-server/gallery_dl_server-0.7.1-py3-none-any.whl/gallery_dl_server/__main__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+
+import uvicorn
+
+from gallery_dl_server import output, options, app
+
+
+log = output.initialise_logging(__name__)
+
+
+def main():
+    multiprocessing.freeze_support()
+
+    args = options.parse_args(__name__)
+
+    opts = {
+        "app": app,
+        "host": args.host,
+        "port": args.port,
+        "log_level": args.log_level,
+        "access_log": args.access_log,
+    }
+
+    config = uvicorn.Config(**opts)
+    server = uvicorn.Server(config)
+
+    try:
+        server.run()
+    except KeyboardInterrupt as e:
+        log.debug(f"Exception: {type(e).__name__}")
+
+
+if __name__ == "__main__":
+    main()
