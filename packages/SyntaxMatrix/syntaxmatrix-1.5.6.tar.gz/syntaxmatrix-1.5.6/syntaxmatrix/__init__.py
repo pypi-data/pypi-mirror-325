82cd238f791f391c2eebc20fa5e9438cea8cc109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:26 +0000

    Add package SyntaxMatrix/1.5.6/syntaxmatrix-1.5.6.tar.gz

---
+++ packages/SyntaxMatrix/syntaxmatrix-1.5.6.tar.gz/syntaxmatrix-1.5.6/syntaxmatrix/__init__.py
@@ -0,0 +1,34 @@
+
+# # syntaxmatrix/__init__.py
+# from .synta_mui import SyntaxMUI
+
+# _app_instance = SyntaxMUI()
+
+# # Standard exports so devs can do: import syntaxmatrix as st
+# run = _app_instance.run
+# text_input = _app_instance.text_input
+# button = _app_instance.button
+# set_widget_position = _app_instance.set_widget_position
+# clear_chat_history = _app_instance.clear_chat_history
+
+# # If you want text input helpers
+# get_text_input_value = _app_instance.get_text_input_value
+# clear_text_input_value = _app_instance.clear_text_input_value
+
+# # Chat history helpers
+# get_chat_history = _app_instance.get_chat_history
+# set_chat_history = _app_instance.set_chat_history
+
+# # Final streaming function
+# stream_chat = _app_instance.stream_chat
+
+from .synta_mui import SyntaxMUI
+
+# Create a single global instance
+_app_instance = SyntaxMUI()
+
+# Expose public functions
+run = _app_instance.run
+text_input = _app_instance.text_input
+button = _app_instance.button
+get_text_input_value = _app_instance.get_text_input_value
