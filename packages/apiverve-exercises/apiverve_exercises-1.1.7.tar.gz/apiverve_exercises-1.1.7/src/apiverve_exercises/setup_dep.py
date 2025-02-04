90d206a9de550c7b0e7dbc5b6ced4af07d2f000c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:23 +0000

    Add package apiverve-exercises/1.1.7/apiverve_exercises-1.1.7.tar.gz

---
+++ packages/apiverve-exercises/apiverve_exercises-1.1.7.tar.gz/apiverve_exercises-1.1.7/src/apiverve_exercises/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_exercises',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Exercises is a simple tool for getting exercise information. It returns information on various exercises.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
