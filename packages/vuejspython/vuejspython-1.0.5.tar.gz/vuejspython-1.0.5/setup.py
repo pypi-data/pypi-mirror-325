e7f7d47d7502f13f5389e577450c0e9883991d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:20 +0000

    Add package vuejspython/1.0.5/vuejspython-1.0.5.tar.gz

---
+++ packages/vuejspython/vuejspython-1.0.5.tar.gz/vuejspython-1.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read().split('----')[0]
+
+setuptools.setup(
+    name="vuejspython",
+    version="1.0.5",
+    author="Rémi Emonet",
+    author_email="remi+242-e2f8@heeere.com",
+    description="Vuejs runner, with filesystem, and allowing python extension (e.g., to leverage numpy)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/twitwi/vuejs-python/",
+    install_requires=['fastapi', 'uvicorn'],
+    packages=setuptools.find_packages(),
+    package_data={
+        'vuejspython': ['index.html', '*.js', 'builtin/*'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'vjspy = vuejspython.run:cli',                  
+        ],              
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
