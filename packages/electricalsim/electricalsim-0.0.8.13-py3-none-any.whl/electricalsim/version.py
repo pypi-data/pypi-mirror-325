79f68c9202344a769d6a4ee3a1aa7dced963567e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:03:57 +0000

    Add package electricalsim/0.0.8.13/electricalsim-0.0.8.13-py3-none-any.whl

---
+++ packages/electricalsim/electricalsim-0.0.8.13-py3-none-any.whl/electricalsim/version.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    VERSION = version('electricalsim')
+except PackageNotFoundError:
+    VERSION = 'development version'
+
+DATE = '2024-02-03'
+AUTHOR = 'Dr. Ing. Ariel S. Loyarte'
+CONTACT = 'aloyarte@frsf.utn.edu.ar'
