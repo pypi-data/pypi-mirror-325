e0cc1c52bece936d261483d320db61d75348eff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:27 +0000

    Add package brainbase-labs/3.0.0/brainbase_labs-3.0.0.tar.gz

---
+++ packages/brainbase-labs/brainbase_labs-3.0.0.tar.gz/brainbase_labs-3.0.0/CHANGELOG.md
@@ -0,0 +1,62 @@
+# Changelog
+
+## 3.0.0 (2025-02-04)
+
+Full Changelog: [v2.2.0...v3.0.0](https://github.com/BrainbaseHQ/brainbase-python-sdk/compare/v2.2.0...v3.0.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#55](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/55)) ([21dbb8d](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/21dbb8db01ce953e1154f77ac1cc795ba727cf4c))
+
+## 2.2.0 (2025-02-04)
+
+Full Changelog: [v2.1.0...v2.2.0](https://github.com/BrainbaseHQ/brainbase-python-sdk/compare/v2.1.0...v2.2.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#50](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/50)) ([a665c00](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/a665c00b172f0c41789098e6e50b959c5bb38c15))
+* **api:** update via SDK Studio ([#52](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/52)) ([95bff55](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/95bff55484b4d252fdd34cc1bd14f096059eaf45))
+* **api:** update via SDK Studio ([#53](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/53)) ([f84535e](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/f84535eec8e1162afe8f0046d32955cd904d7d78))
+
+## 2.1.0 (2025-02-04)
+
+Full Changelog: [v2.0.0...v2.1.0](https://github.com/BrainbaseHQ/brainbase-python-sdk/compare/v2.0.0...v2.1.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#47](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/47)) ([3ae3d6f](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/3ae3d6f3c42b706ce9e640f399723fe0165cfffd))
+
+## 2.0.0 (2025-02-04)
+
+Full Changelog: [v0.1.0-alpha.1...v2.0.0](https://github.com/BrainbaseHQ/brainbase-python-sdk/compare/v0.1.0-alpha.1...v2.0.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#43](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/43)) ([6a4d364](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/6a4d364cc3740780ade0a8bb2168fe5060125848))
+* **api:** update via SDK Studio ([#45](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/45)) ([1011b13](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/1011b13fd27121a9e8ef357811ddac287a28de0b))
+
+## 0.1.0-alpha.1 (2025-02-04)
+
+Full Changelog: [v0.0.1-alpha.0...v0.1.0-alpha.1](https://github.com/BrainbaseHQ/brainbase-python-sdk/compare/v0.0.1-alpha.0...v0.1.0-alpha.1)
+
+### Features
+
+* **api:** update via SDK Studio ([#10](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/10)) ([4fb75c8](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/4fb75c8c78c172cb0a80be304334803956968247))
+* **api:** update via SDK Studio ([#12](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/12)) ([a363549](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/a3635498129383f20cae75014ad9720e18cc7ec6))
+* **api:** update via SDK Studio ([#18](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/18)) ([5f5451c](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/5f5451cebcda05a40deef4104207be19e5f8e8ac))
+* **api:** update via SDK Studio ([#23](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/23)) ([b2bbdf6](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/b2bbdf640fa7b0b9163f0a7a6b8ab964cb2c1fa5))
+* **api:** update via SDK Studio ([#33](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/33)) ([9254242](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/9254242f08a8f3f0da2a3b101fd07776022487d4))
+* **api:** update via SDK Studio ([#35](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/35)) ([91c7c39](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/91c7c396731fc46c41a031b2235e0e9562acea8f))
+* **api:** update via SDK Studio ([#38](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/38)) ([bc32c04](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/bc32c04421def2b0a84a5def7f5a4bb08546d704))
+* **api:** update via SDK Studio ([#40](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/40)) ([801bbaa](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/801bbaaddaddba829cf554d8b0ee0bc4c229ee40))
+* **api:** update via SDK Studio ([#41](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/41)) ([fb611f9](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/fb611f97127b4b491884acf8ccd636ebea0e858c))
+* **api:** update via SDK Studio ([#8](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/8)) ([312b657](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/312b657cf73569a314b3c64ed83521ca2bb576a5))
+
+
+### Chores
+
+* go live ([#1](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/1)) ([db8a29c](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/db8a29c23e871872bcfc26b15bc7e4b27b0b08b3))
+* remove custom code ([e685c86](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/e685c866f3f2b482e1d899a475a5d140ca134938))
+* sync repo ([cce7924](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/cce79248b7058f58c4fe7d98ea36ed534403c7a7))
+* update SDK settings ([#3](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/3)) ([c5e3c22](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/c5e3c223ba6a7df56e290877e55c31e81c1d4b52))
+* update SDK settings ([#5](https://github.com/BrainbaseHQ/brainbase-python-sdk/issues/5)) ([1e74403](https://github.com/BrainbaseHQ/brainbase-python-sdk/commit/1e744032bc5800bb1b4f97a703cfdd1a7a7dd922))
---
+++ packages/brainbase-labs/brainbase_labs-3.0.0.tar.gz/brainbase_labs-3.0.0/tests/api_resources/test_workers.py
@@ -0,0 +1,441 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import os
+from typing import Any, cast
+
+import pytest
+
+from brainbase import Brainbase, AsyncBrainbase
+from tests.utils import assert_matches_type
+from brainbase.types import (
+    WorkerListResponse,
+    WorkerCreateResponse,
+    WorkerUpdateResponse,
+    WorkerRetrieveResponse,
+)
+
+base_url = os.environ.get("TEST_API_BASE_URL", "http://127.0.0.1:4010")
+
+
+class TestWorkers:
+    parametrize = pytest.mark.parametrize("client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_create(self, client: Brainbase) -> None:
+        worker = client.workers.create(
+            name="name",
+        )
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_create_with_all_params(self, client: Brainbase) -> None:
+        worker = client.workers.create(
+            name="name",
+            description="description",
+        )
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_raw_response_create(self, client: Brainbase) -> None:
+        response = client.workers.with_raw_response.create(
+            name="name",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = response.parse()
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_streaming_response_create(self, client: Brainbase) -> None:
+        with client.workers.with_streaming_response.create(
+            name="name",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = response.parse()
+            assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_retrieve(self, client: Brainbase) -> None:
+        worker = client.workers.retrieve(
+            "id",
+        )
+        assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_raw_response_retrieve(self, client: Brainbase) -> None:
+        response = client.workers.with_raw_response.retrieve(
+            "id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = response.parse()
+        assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_streaming_response_retrieve(self, client: Brainbase) -> None:
+        with client.workers.with_streaming_response.retrieve(
+            "id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = response.parse()
+            assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_path_params_retrieve(self, client: Brainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            client.workers.with_raw_response.retrieve(
+                "",
+            )
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_update(self, client: Brainbase) -> None:
+        worker = client.workers.update(
+            id="id",
+        )
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_update_with_all_params(self, client: Brainbase) -> None:
+        worker = client.workers.update(
+            id="id",
+            description="description",
+            name="name",
+        )
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_raw_response_update(self, client: Brainbase) -> None:
+        response = client.workers.with_raw_response.update(
+            id="id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = response.parse()
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_streaming_response_update(self, client: Brainbase) -> None:
+        with client.workers.with_streaming_response.update(
+            id="id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = response.parse()
+            assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_path_params_update(self, client: Brainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            client.workers.with_raw_response.update(
+                id="",
+            )
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_list(self, client: Brainbase) -> None:
+        worker = client.workers.list()
+        assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_raw_response_list(self, client: Brainbase) -> None:
+        response = client.workers.with_raw_response.list()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = response.parse()
+        assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_streaming_response_list(self, client: Brainbase) -> None:
+        with client.workers.with_streaming_response.list() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = response.parse()
+            assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_method_delete(self, client: Brainbase) -> None:
+        worker = client.workers.delete(
+            "id",
+        )
+        assert worker is None
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_raw_response_delete(self, client: Brainbase) -> None:
+        response = client.workers.with_raw_response.delete(
+            "id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = response.parse()
+        assert worker is None
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_streaming_response_delete(self, client: Brainbase) -> None:
+        with client.workers.with_streaming_response.delete(
+            "id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = response.parse()
+            assert worker is None
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    def test_path_params_delete(self, client: Brainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            client.workers.with_raw_response.delete(
+                "",
+            )
+
+
+class TestAsyncWorkers:
+    parametrize = pytest.mark.parametrize("async_client", [False, True], indirect=True, ids=["loose", "strict"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_create(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.create(
+            name="name",
+        )
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_create_with_all_params(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.create(
+            name="name",
+            description="description",
+        )
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_raw_response_create(self, async_client: AsyncBrainbase) -> None:
+        response = await async_client.workers.with_raw_response.create(
+            name="name",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = await response.parse()
+        assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_streaming_response_create(self, async_client: AsyncBrainbase) -> None:
+        async with async_client.workers.with_streaming_response.create(
+            name="name",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = await response.parse()
+            assert_matches_type(WorkerCreateResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_retrieve(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.retrieve(
+            "id",
+        )
+        assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_raw_response_retrieve(self, async_client: AsyncBrainbase) -> None:
+        response = await async_client.workers.with_raw_response.retrieve(
+            "id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = await response.parse()
+        assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_streaming_response_retrieve(self, async_client: AsyncBrainbase) -> None:
+        async with async_client.workers.with_streaming_response.retrieve(
+            "id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = await response.parse()
+            assert_matches_type(WorkerRetrieveResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_path_params_retrieve(self, async_client: AsyncBrainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            await async_client.workers.with_raw_response.retrieve(
+                "",
+            )
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_update(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.update(
+            id="id",
+        )
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_update_with_all_params(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.update(
+            id="id",
+            description="description",
+            name="name",
+        )
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_raw_response_update(self, async_client: AsyncBrainbase) -> None:
+        response = await async_client.workers.with_raw_response.update(
+            id="id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = await response.parse()
+        assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_streaming_response_update(self, async_client: AsyncBrainbase) -> None:
+        async with async_client.workers.with_streaming_response.update(
+            id="id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = await response.parse()
+            assert_matches_type(WorkerUpdateResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_path_params_update(self, async_client: AsyncBrainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            await async_client.workers.with_raw_response.update(
+                id="",
+            )
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_list(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.list()
+        assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_raw_response_list(self, async_client: AsyncBrainbase) -> None:
+        response = await async_client.workers.with_raw_response.list()
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = await response.parse()
+        assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_streaming_response_list(self, async_client: AsyncBrainbase) -> None:
+        async with async_client.workers.with_streaming_response.list() as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = await response.parse()
+            assert_matches_type(WorkerListResponse, worker, path=["response"])
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_method_delete(self, async_client: AsyncBrainbase) -> None:
+        worker = await async_client.workers.delete(
+            "id",
+        )
+        assert worker is None
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_raw_response_delete(self, async_client: AsyncBrainbase) -> None:
+        response = await async_client.workers.with_raw_response.delete(
+            "id",
+        )
+
+        assert response.is_closed is True
+        assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+        worker = await response.parse()
+        assert worker is None
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_streaming_response_delete(self, async_client: AsyncBrainbase) -> None:
+        async with async_client.workers.with_streaming_response.delete(
+            "id",
+        ) as response:
+            assert not response.is_closed
+            assert response.http_request.headers.get("X-Stainless-Lang") == "python"
+
+            worker = await response.parse()
+            assert worker is None
+
+        assert cast(Any, response.is_closed) is True
+
+    @pytest.mark.skip()
+    @parametrize
+    async def test_path_params_delete(self, async_client: AsyncBrainbase) -> None:
+        with pytest.raises(ValueError, match=r"Expected a non-empty value for `id` but received ''"):
+            await async_client.workers.with_raw_response.delete(
+                "",
+            )
