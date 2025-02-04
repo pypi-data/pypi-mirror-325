c8282c6998819f66662519bcffdfd83ecb465a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:04:58 +0000

    Add package langroid/0.39.2/langroid-0.39.2-py3-none-any.whl

---
+++ packages/langroid/langroid-0.39.2-py3-none-any.whl/langroid/vector_store/__init__.py
@@ -0,0 +1,61 @@
+from . import base
+
+from . import qdrantdb
+
+from .base import VectorStoreConfig, VectorStore
+from .qdrantdb import QdrantDBConfig, QdrantDB
+
+__all__ = [
+    "base",
+    "VectorStore",
+    "VectorStoreConfig",
+    "qdrantdb",
+    "QdrantDBConfig",
+    "QdrantDB",
+]
+
+
+try:
+    from . import meilisearch
+    from .meilisearch import MeiliSearch, MeiliSearchConfig
+
+    meilisearch
+    MeiliSearch
+    MeiliSearchConfig
+    __all__.extend(["meilisearch", "MeiliSearch", "MeiliSearchConfig"])
+except ImportError:
+    pass
+
+
+try:
+    from . import lancedb
+    from .lancedb import LanceDB, LanceDBConfig
+
+    lancedb
+    LanceDB
+    LanceDBConfig
+    __all__.extend(["lancedb", "LanceDB", "LanceDBConfig"])
+except ImportError:
+    pass
+
+try:
+    from . import chromadb
+    from .chromadb import ChromaDBConfig, ChromaDB
+
+    chromadb  # silence linters
+    ChromaDB
+    ChromaDBConfig
+    __all__.extend(["chromadb", "ChromaDBConfig", "ChromaDB"])
+except ImportError:
+    pass
+
+try:
+    from . import weaviatedb
+    from .weaviatedb import WeaviateDBConfig, WeaviateDB
+
+    weaviatedb
+    WeaviateDB
+    WeaviateDBConfig
+    __all__.extend(["weaviatedb", "WeaviateDB", "WeaviateDBConfig"])
+except ImportError:
+    pass
