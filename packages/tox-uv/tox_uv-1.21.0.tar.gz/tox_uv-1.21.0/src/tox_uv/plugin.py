7fb95831f6aac61fb921c3bc929c0d6a5598e150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 15:05:37 +0000

    Add package tox-uv/1.21.0/tox_uv-1.21.0.tar.gz

---
+++ packages/tox-uv/tox_uv-1.21.0.tar.gz/tox_uv-1.21.0/src/tox_uv/plugin.py
@@ -0,0 +1,33 @@
+"""GitHub Actions integration."""
+
+from __future__ import annotations
+
+from importlib.metadata import version
+from typing import TYPE_CHECKING
+
+from tox.plugin import impl
+
+from ._package import UvVenvCmdBuilder, UvVenvPep517Packager
+from ._run import UvVenvRunner
+from ._run_lock import UvVenvLockRunner
+
+if TYPE_CHECKING:
+    from tox.tox_env.register import ToxEnvRegister
+
+
+@impl
+def tox_register_tox_env(register: ToxEnvRegister) -> None:
+    register.add_run_env(UvVenvRunner)
+    register.add_run_env(UvVenvLockRunner)
+    register.add_package_env(UvVenvPep517Packager)
+    register.add_package_env(UvVenvCmdBuilder)
+    register._default_run_env = UvVenvRunner.id()  # noqa: SLF001
+
+
+def tox_append_version_info() -> str:
+    return f"with uv=={version('uv')}"
+
+
+__all__ = [
+    "tox_register_tox_env",
+]
