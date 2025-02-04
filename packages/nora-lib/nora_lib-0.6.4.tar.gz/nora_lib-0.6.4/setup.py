2d81be35b3107b2f0b8c5d7e419c4d592c94a640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:26 +0000

    Add package nora-lib/0.6.4/nora_lib-0.6.4.tar.gz

---
+++ packages/nora-lib/nora_lib-0.6.4.tar.gz/nora_lib-0.6.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+runtime_requirements = ["pydantic>=2,<3", "requests", "boto3", "aws_requests_auth"]
+
+# For running tests, linting, etc
+dev_requirements = ["mypy", "pytest", "black", "types-requests"]
+
+setuptools.setup(
+    name="nora_lib",
+    version="0.6.4",
+    description="For making and coordinating agents and tools",
+    url="https://github.com/allenai/nora_lib",
+    packages=setuptools.find_packages(exclude=(["tests"])),
+    install_requires=runtime_requirements,
+    package_data={
+        "nora_lib": ["py.typed"],
+    },
+    extras_require={
+        "dev": dev_requirements,
+    },
+    python_requires=">=3.9",
+)
