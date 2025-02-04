4a8fd3bad73968709cbbd18c6ec4cbbde634abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 05:04:11 +0000

    Add package apiverve-currencyconverter/1.1.7/apiverve_currencyconverter-1.1.7-py3-none-any.whl

---
+++ packages/apiverve-currencyconverter/apiverve_currencyconverter-1.1.7-py3-none-any.whl/apiverve_currencyconverter/exceptions.py
@@ -0,0 +1,3 @@
+class CurrencyconverterCurrencyConverterAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-currencyconverter/apiverve_currencyconverter-1.1.7-py3-none-any.whl/apiverve_currencyconverter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_currencyconverter',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Currency Converter is a simple tool for converting currency rates. It returns the converted currency rate based on the amount and currency provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
