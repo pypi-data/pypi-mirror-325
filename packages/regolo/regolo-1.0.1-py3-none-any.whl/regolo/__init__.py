4074e0c4547be4416e269fca6f5c5d1d223a08b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:00 +0000

    Add package regolo/1.0.1/regolo-1.0.1-py3-none-any.whl

---
+++ packages/regolo/regolo-1.0.1-py3-none-any.whl/regolo/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.0.1"
+
+default_key = None
+default_model = None
+
+from regolo.client.regolo_client import RegoloClient
+
+static_completions = RegoloClient.static_completions
+static_chat_completions = RegoloClient.static_chat_completions
