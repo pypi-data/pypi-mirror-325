cade0d023017dd66b98637c19f061a1e06ae3f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 04:04:10 +0000

    Add package KekikStream/0.7.13/kekikstream-0.7.13.tar.gz

---
+++ packages/KekikStream/kekikstream-0.7.13.tar.gz/kekikstream-0.7.13/KekikStream/Managers/__init__.py
@@ -0,0 +1,6 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from .PluginManager    import PluginManager
+from .ExtractorManager import ExtractorManager
+from .UIManager        import UIManager
+from .MediaManager     import MediaManager
