8e0ef20dfb7a1b0325a342eb359a76111c393fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 16:04:19 +0000

    Add package crxsnake/1.4.4/crxsnake-1.4.4-py3-none-any.whl

---
+++ packages/crxsnake/crxsnake-1.4.4-py3-none-any.whl/crxsnake/tortoise.py
@@ -0,0 +1,21 @@
+from .logger import log
+from tortoise import Tortoise
+
+
+class Database:
+    DB_URL = "sqlite://settings/database/database.db"
+    DB_MODEL = {"models": ["src.utils"]}
+
+    @staticmethod
+    async def db_connect() -> None:
+        try:
+            await Tortoise.init(db_url=Database.DB_URL, modules=Database.DB_MODEL)
+            await Tortoise.generate_schemas()
+            log.info("[Database] SqLite connected")
+
+        except Exception as e:
+            log.error(f"[Database] Failed to connect SqLite: {e}")
+
+    @staticmethod
+    async def disconnect_db() -> None:
+        await Tortoise.close_connections()
