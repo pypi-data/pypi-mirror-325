b0b90b96726c227e5a5f7e0d7074a89ce4071470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:28 +0000

    Add package epmwebapi/6.0.39/epmwebapi-6.0.39.tar.gz

---
+++ packages/epmwebapi/epmwebapi-6.0.39.tar.gz/epmwebapi-6.0.39/epmwebapi/epmnodeids.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class EpmNodeIds(Enum):
+    """
+    Enumeration with all EPM Node IDs.
+    """
+
+    HasTags = 'ns=4;i=96'
+
+    Organizes = 'ns=0;i=35'
+
+    HasComponent = 'ns=0;i=47'
+
+    HasProperty = 'ns=0;i=46'
+
+    BasicVariableType = 'ns=4;i=94'
+
+    ExpressionVariableType = 'ns=4;i=289'
+
+    AnnotationType = 'ns=0;i=891'
+
+    HasTypeDefinition = 'ns=0;i=40'
+
+
