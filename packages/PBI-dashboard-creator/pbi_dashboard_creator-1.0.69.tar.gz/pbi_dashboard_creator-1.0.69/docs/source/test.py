1ecc05a954f60d80b883a53bfc5f0c9a69316ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:05 +0000

    Add package PBI-dashboard-creator/1.0.69/pbi_dashboard_creator-1.0.69.tar.gz

---
+++ packages/PBI-dashboard-creator/pbi_dashboard_creator-1.0.69.tar.gz/pbi_dashboard_creator-1.0.69/docs/source/test.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+test_path = sys.path.insert(0, os.path.abspath("../PBI_dashboard_creator/src/PBI_dashboard_creator/"))
+
+print(os.path.abspath("../PBI_dashboard_creator/src/PBI_dashboard_creator/"))
+
+print(sys.path)
