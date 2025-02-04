6d5236597a69ae1e38e11192c856901f5fa2265a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 14:04:04 +0000

    Add package valyu/1.0.2a1/valyu-1.0.2a1.tar.gz

---
+++ packages/valyu/valyu-1.0.2a1.tar.gz/valyu-1.0.2a1/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='valyu',
+    version='1.0.2a1',
+    author='Valyu',
+    author_email='contact@valyu.network',
+    maintainer='Harvey Yorke',
+    maintainer_email='harvey@valyu.network',
+    description='Content Monetisation Rails for AI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://valyu.network",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0',
+        'tqdm>=4.66.4',
+        'boto3==1.35.47',
+        'pydantic>=2.5.2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
