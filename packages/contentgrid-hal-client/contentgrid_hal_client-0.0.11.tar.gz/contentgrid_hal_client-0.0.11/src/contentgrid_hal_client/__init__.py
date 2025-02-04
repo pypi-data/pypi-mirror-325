4949501a3f4d2eab400180fa1de08c0139ab05dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:08 +0000

    Add package contentgrid-hal-client/0.0.11/contentgrid_hal_client-0.0.11.tar.gz

---
+++ packages/contentgrid-hal-client/contentgrid_hal_client-0.0.11.tar.gz/contentgrid_hal_client-0.0.11/src/contentgrid_hal_client/__init__.py
@@ -0,0 +1,2 @@
+from .hal import HALLink, CurieRegistry, Curie, HALResponse, InteractiveHALResponse, HALFormsClient
+from .exceptions import NotFound, Unauthorized, BadRequest, IncorrectAttributeType, NonExistantAttribute, MissingRequiredAttribute, MissingHALTemplate
