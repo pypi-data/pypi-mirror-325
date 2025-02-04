be2eec77f6c6454fd5a78af645584a9bc79b60c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-04 03:05:29 +0000

    Add package repo-reasoner/0.0.33/repo_reasoner-0.0.33.tar.gz

---
+++ packages/repo-reasoner/repo_reasoner-0.0.33.tar.gz/repo_reasoner-0.0.33/setup.py
@@ -0,0 +1,78 @@
+# MIT License
+#
+# Copyright (c) 2024 Vakhidov Dzhovidon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+setup.py
+"""
+import os
+from setuptools import setup
+
+def version():
+    """
+    Fetch version from version.txt.
+    :return: version
+    """
+    version_file = 'version.txt'
+    if os.path.exists(version_file):
+        with open(version_file, 'r', encoding='utf-8') as file:
+            return file.read().strip()
+    else:
+        return '0.0.0'
+
+actual = version()
+
+# Read long description from README.md
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read dependencies from requirements.txt
+def parse_requirements(filename):
+    """Parse the requirements.txt file and return a list of dependencies"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return f.read().splitlines()
+
+install_requires = parse_requirements('requirements.txt')
+
+setup(
+    name='repo-reasoner',
+    version=actual,
+    packages=['source'],
+    entry_points={
+        'console_scripts': [
+            'repo-reasoner=source.__main__:main',
+        ],
+    },
+    author='Vakhidov Dzhovidon',
+    author_email='asunabestwaifu1@gmail.com',
+    description='Repository reasoner that uses LLM, instead of a real project or framework or library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dzhovi/repositories-reasoner',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=install_requires,  # This is where we add dependencies
+)
