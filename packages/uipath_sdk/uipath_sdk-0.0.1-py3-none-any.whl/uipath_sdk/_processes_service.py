62d7062551406e85b03f103f1a806c325f12ea47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:15 +0000

    Add package uipath_sdk/0.0.1/uipath_sdk-0.0.1-py3-none-any.whl

---
+++ packages/uipath_sdk/uipath_sdk-0.0.1-py3-none-any.whl/uipath_sdk/_processes_service.py
@@ -0,0 +1,7 @@
+from ._base_service import BaseService
+
+
+class ProcessesService(BaseService):
+    # FIXME: endpoint?
+    def invoke_process(self) -> None:
+        pass
---
+++ packages/uipath_sdk/uipath_sdk-0.0.1-py3-none-any.whl/uipath_sdk/_uipath_client.py
@@ -0,0 +1,39 @@
+from httpx import Client, Headers
+
+from ._assets_service import RobotAssetsService
+from ._uipath_client_config import UiPathClientConfig
+
+
+class UiPathClient:
+    def __init__(self, secret: str | None) -> None:
+        self.config = UiPathClientConfig()
+
+        if secret is not None:
+            self.config.secret = secret
+
+        if self.config.secret is None:
+            raise ValueError("Secret is required")
+
+        self._init_http_client()
+        self._init_services()
+
+    def change_folder(self, folder_id: str) -> None:
+        self.config.folder_id = folder_id
+        self._init_http_client()
+        self._init_services()
+
+    def _init_http_client(self) -> None:
+        headers = Headers(
+            {
+                k: v
+                for k, v in {
+                    "Authorization": f"Bearer {self.config.secret}",
+                    "x-uipath-organizationunitid": self.config.folder_id,
+                }.items()
+                if v is not None
+            }
+        )
+        self._http_client = Client(base_url=self.config.base_url, headers=headers)
+
+    def _init_services(self) -> None:
+        self.robot_assets = RobotAssetsService(self._http_client)
