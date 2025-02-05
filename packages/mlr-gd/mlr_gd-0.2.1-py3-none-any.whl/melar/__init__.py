5c12c9b9b4c8af11a3375f336c741766b4b63049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:26 +0000

    Add package mlr-gd/0.2.1/mlr_gd-0.2.1-py3-none-any.whl

---
+++ packages/mlr-gd/mlr_gd-0.2.1-py3-none-any.whl/melar/__init__.py
@@ -0,0 +1,13 @@
+"""Melar is the module for mlr-gd.
+"""
+from melar.main import LinearRegression
+from melar import cfuncs
+
+__author__ = ["DrSolidDevil"]
+__copyright__ = "Copyright (c) 2025, DrSolidDevil"
+__credits__ = ["DrSolidDevil", "RedDingleBerry"]
+__license__ = "BSD-3-Clause"
+__maintainer__ = ["DrSoliDevil"]
+__status__ = "Development"
+__version__ = '0.2.1'
+__version_info__ = (0, 2, 1)
