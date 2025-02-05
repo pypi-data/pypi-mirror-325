207f138c290f2aba8dafa34b4547d4727e0f8170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:04 +0000

    Add package vectice/25.1.5.0/vectice-25.1.5.0-py2.py3-none-any.whl

---
+++ packages/vectice/vectice-25.1.5.0-py2.py3-none-any.whl/vectice/api/json/attachment.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Any
+
+from vectice.api.json.json_type import TJSON
+
+
+class AttachmentTypeEnum(Enum):
+    """Enumeration of the attachment type."""
+
+    ENTITY_FILE = "ENTITY_FILE"
+    ENTITY_METADATA = "ENTITY_METADATA"
+
+
+class AttachmentOutput(TJSON):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def id(self) -> int:
+        return self["id"]
+
+    @property
+    def name(self) -> str:
+        return self["name"]
+
+    @property
+    def type(self) -> AttachmentTypeEnum:
+        return AttachmentTypeEnum(self["attachmentType"])
