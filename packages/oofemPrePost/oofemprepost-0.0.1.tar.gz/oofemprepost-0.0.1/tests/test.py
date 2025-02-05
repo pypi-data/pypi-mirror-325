d10b6be8b281fc8a80ef87fa10d566570222a77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:08 +0000

    Add package oofemPrePost/0.0.1/oofemprepost-0.0.1.tar.gz

---
+++ packages/oofemPrePost/oofemprepost-0.0.1.tar.gz/oofemprepost-0.0.1/tests/test.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append('../oofemPrePost')
+import timeCount as timeCount
+import hm2of2d as hm2of2d
+import hm2of3d as hm2of3d
+
+## count time and iterations required for each increment
+timeCount.log2csv('./extractorTest.log', 'extractorTest.csv')
+
+### two materials: one is concrete, another is steel
+# 2d
+hm2of2d.hm2of2d('circleTPB_cps4.inp','circleTPB_cps4.in')
+# 3d
+hm2of3d.hm2of3d('circleTPB_c3d8r.inp','circleTPB_c3d8r.in')
+
+hm2of2d.hm2of2d('threeMaterials.inp','threeMaterials.in')
+
+# os.system('python ../oofemPrePost/extract.py -f extractorTest.in > extractorTest.csv')
