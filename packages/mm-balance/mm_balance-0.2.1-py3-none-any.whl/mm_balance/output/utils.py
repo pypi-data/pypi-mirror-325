7ba8a35ebf5215981a3e21af0b829a65e5f64b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:00 +0000

    Add package mm-balance/0.2.1/mm_balance-0.2.1-py3-none-any.whl

---
+++ packages/mm-balance/mm_balance-0.2.1-py3-none-any.whl/mm_balance/output/utils.py
@@ -0,0 +1,20 @@
+from decimal import Decimal
+
+from rich.progress import BarColumn, MofNCompleteColumn, Progress, TaskID, TextColumn
+
+
+def format_number(value: Decimal, separator: str, extra: str | None = None) -> str:
+    str_value = f"{value:,}".replace(",", separator)
+    if extra == "$":
+        return "$" + str_value
+    if extra == "%":
+        return str_value + "%"
+    return str_value
+
+
+def create_progress_bar(disable: bool) -> Progress:
+    return Progress(TextColumn("[progress.description]{task.description}"), BarColumn(), MofNCompleteColumn(), disable=disable)
+
+
+def create_progress_task(progress: Progress, description: str, total: int) -> TaskID:
+    return progress.add_task("[green]" + description, total=total)
