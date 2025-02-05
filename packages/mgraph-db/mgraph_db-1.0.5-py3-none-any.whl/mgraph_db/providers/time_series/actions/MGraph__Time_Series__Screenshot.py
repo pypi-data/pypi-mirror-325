87af97c730f89f33350d1adc9dad1470d8baf77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:40 +0000

    Add package mgraph-db/1.0.5/mgraph_db-1.0.5-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.0.5-py3-none-any.whl/mgraph_db/providers/time_series/actions/MGraph__Time_Series__Screenshot.py
@@ -0,0 +1,13 @@
+from typing import Type
+
+from mgraph_db.mgraph.actions.MGraph__Export import MGraph__Export
+from mgraph_db.mgraph.actions.MGraph__Screenshot import MGraph__Screenshot
+
+
+class MGraph__Time_Series__Screenshot(MGraph__Screenshot):
+    pass
+    # def dot(self):
+    #     dot_code  = self.export().to__dot()
+    #     png_bytes = self.dot_to_png(dot_code)
+    #     return png_bytes
+
