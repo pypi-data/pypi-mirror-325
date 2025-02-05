1091b0ab037be624bdf6b773c0483a572703fefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:08 +0000

    Add package filterutil/1.0.4/filterutil-1.0.4.tar.gz

---
+++ packages/filterutil/filterutil-1.0.4.tar.gz/filterutil-1.0.4/tests/test_or_filters.py
@@ -0,0 +1,84 @@
+import pytest
+from filterutil import Filter, Filters, OrFilters, FilterCouplingPolicy
+
+
+def test_or_filters_init():
+    filters = OrFilters()
+    assert isinstance(filters, Filters)
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, int))}, True),
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, str))}, False),
+    ],
+)
+def test_or_filters_filtering(value, filters_dict, expected_result):
+    filters = OrFilters(**filters_dict)
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, int))}, True),
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, str))}, False),
+    ],
+)
+def test_or_filters_setitem(value, filters_dict, expected_result):
+    filters = OrFilters()
+    for k, v in filters_dict.items():
+        filters[k] = v
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, int))}, True),
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, str))}, False),
+    ],
+)
+def test_or_filters_update(value, filters_dict, expected_result):
+    filters = OrFilters()
+    filters.update(filters_dict)
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, int))}, True),
+        (1, {'a': Filter(lambda x: x == 2), 'b': Filter(lambda x: isinstance(x, str))}, False),
+    ],
+)
+def test_filters_as_or(value, filters_dict, expected_result):
+    filters = Filters(
+        FilterCouplingPolicy.OR,
+        **filters_dict,
+    )
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 2), 
+                'b': Filter(lambda x: isinstance(x, int)),
+                'c': Filter(lambda x: isinstance(x, str)),
+            },
+            {
+                ('a', 'b'): True,
+                ('a', 'c'): False,
+            },
+        ),
+    ],
+)
+def test_or_filters_select(value, filters_dict, expected_result):
+    filters = OrFilters(**filters_dict)
+    for filter_names, filters_expected_result in expected_result.items():
+        assert filters.apply(value, filter_names=filter_names) == filters_expected_result
---
+++ packages/filterutil/filterutil-1.0.4.tar.gz/filterutil-1.0.4/tests/test_xor_filters.py
@@ -0,0 +1,164 @@
+import pytest
+from filterutil import Filter, Filters, XorFilters, FilterCouplingPolicy
+
+
+def test_xor_filters_init():
+    filters = XorFilters()
+    assert isinstance(filters, Filters)
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+            },
+            True,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 2),
+            },
+            False,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, str)),
+            },
+            True,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, int)),
+            },
+            False,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, str)),
+                'c': Filter(lambda x: isinstance(x, int)),
+            },
+            False,
+        ),
+    ],
+)
+def test_xor_filters_filtering(value, filters_dict, expected_result):
+    filters = XorFilters(**filters_dict)
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, str)),
+            },
+            True,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, int)),
+            },
+            False,
+        ),
+    ],
+)
+def test_xor_filters_setitem(value, filters_dict, expected_result):
+    filters = XorFilters()
+    for k, v in filters_dict.items():
+        filters[k] = v
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, str)),
+            },
+            True,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, int)),
+            },
+            False,
+        ),
+    ],
+)
+def test_xor_filters_update(value, filters_dict, expected_result):
+    filters = XorFilters()
+    filters.update(filters_dict)
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, str)),
+            },
+            True,
+        ),
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, int)),
+            },
+            False,
+        ),
+    ],
+)
+def test_filters_as_xor(value, filters_dict, expected_result):
+    filters = Filters(
+        FilterCouplingPolicy.XOR,
+        **filters_dict,
+    )
+    assert filters.apply(value) == expected_result
+
+
+@pytest.mark.parametrize(
+    'value, filters_dict, expected_result',
+    [
+        (
+            1,
+            {
+                'a': Filter(lambda x: x == 1),
+                'b': Filter(lambda x: isinstance(x, int)),
+                'c': Filter(lambda x: isinstance(x, str)),
+            },
+            {
+                ('a', 'c'): True,
+                ('a', 'b'): False,
+                ('b', 'c'): True,
+            },
+        ),
+    ],
+)
+def test_xor_filters_select(value, filters_dict, expected_result):
+    filters = XorFilters(**filters_dict)
+    for filter_names, filters_expected_result in expected_result.items():
+        assert filters.apply(value, filter_names=filter_names) == filters_expected_result
