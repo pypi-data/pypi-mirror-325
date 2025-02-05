8b961618420b96846eef9c646bb9b497df972b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:26:54 +0000

    Add package gdal-installer/1.0.2/gdal_installer-1.0.2.tar.gz

---
+++ packages/gdal-installer/gdal_installer-1.0.2.tar.gz/gdal_installer-1.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README content
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="gdal-installer",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.0',
+        'tqdm>=4.65.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'install-gdal=gdal_installer.cli:main',
+        ],
+    },
+    author="Celray James CHAWANDA",
+    author_email="celray.chawanda@outlook.com",
+    description="A tool to install GDAL wheels on Windows and wrapper for pip on Unix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="gdal, gis, installer",
+    url="https://github.com/celray/python-gdal-installer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    package_data={
+        'gdal_installer': ['*'],
+    },
+    include_package_data=True,
+    license="MIT",
+)
