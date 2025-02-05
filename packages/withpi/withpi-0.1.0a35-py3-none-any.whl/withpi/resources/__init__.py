052be359fbee0bb1e5939b972bfb93b2192ac5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:13 +0000

    Add package withpi/0.1.0a35/withpi-0.1.0a35-py3-none-any.whl

---
+++ packages/withpi/withpi-0.1.0a35-py3-none-any.whl/withpi/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .data import (
+    DataResource,
+    AsyncDataResource,
+    DataResourceWithRawResponse,
+    AsyncDataResourceWithRawResponse,
+    DataResourceWithStreamingResponse,
+    AsyncDataResourceWithStreamingResponse,
+)
+from .model import (
+    ModelResource,
+    AsyncModelResource,
+    ModelResourceWithRawResponse,
+    AsyncModelResourceWithRawResponse,
+    ModelResourceWithStreamingResponse,
+    AsyncModelResourceWithStreamingResponse,
+)
+from .prompt import (
+    PromptResource,
+    AsyncPromptResource,
+    PromptResourceWithRawResponse,
+    AsyncPromptResourceWithRawResponse,
+    PromptResourceWithStreamingResponse,
+    AsyncPromptResourceWithStreamingResponse,
+)
+from .feedback import (
+    FeedbackResource,
+    AsyncFeedbackResource,
+    FeedbackResourceWithRawResponse,
+    AsyncFeedbackResourceWithRawResponse,
+    FeedbackResourceWithStreamingResponse,
+    AsyncFeedbackResourceWithStreamingResponse,
+)
+from .contracts import (
+    ContractsResource,
+    AsyncContractsResource,
+    ContractsResourceWithRawResponse,
+    AsyncContractsResourceWithRawResponse,
+    ContractsResourceWithStreamingResponse,
+    AsyncContractsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "DataResource",
+    "AsyncDataResource",
+    "DataResourceWithRawResponse",
+    "AsyncDataResourceWithRawResponse",
+    "DataResourceWithStreamingResponse",
+    "AsyncDataResourceWithStreamingResponse",
+    "PromptResource",
+    "AsyncPromptResource",
+    "PromptResourceWithRawResponse",
+    "AsyncPromptResourceWithRawResponse",
+    "PromptResourceWithStreamingResponse",
+    "AsyncPromptResourceWithStreamingResponse",
+    "ModelResource",
+    "AsyncModelResource",
+    "ModelResourceWithRawResponse",
+    "AsyncModelResourceWithRawResponse",
+    "ModelResourceWithStreamingResponse",
+    "AsyncModelResourceWithStreamingResponse",
+    "ContractsResource",
+    "AsyncContractsResource",
+    "ContractsResourceWithRawResponse",
+    "AsyncContractsResourceWithRawResponse",
+    "ContractsResourceWithStreamingResponse",
+    "AsyncContractsResourceWithStreamingResponse",
+    "FeedbackResource",
+    "AsyncFeedbackResource",
+    "FeedbackResourceWithRawResponse",
+    "AsyncFeedbackResourceWithRawResponse",
+    "FeedbackResourceWithStreamingResponse",
+    "AsyncFeedbackResourceWithStreamingResponse",
+]
