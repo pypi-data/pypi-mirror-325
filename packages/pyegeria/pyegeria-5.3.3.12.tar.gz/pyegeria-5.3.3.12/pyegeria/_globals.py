8ead35c335d3e865baca9311870c83e7076bc4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:06:00 +0000

    Add package pyegeria/5.3.3.12/pyegeria-5.3.3.12.tar.gz

---
+++ packages/pyegeria/pyegeria-5.3.3.12.tar.gz/pyegeria-5.3.3.12/pyegeria/_globals.py
@@ -0,0 +1,37 @@
+"""
+SPDX-License-Identifier: Apache-2.0
+Copyright Contributors to the ODPi Egeria project.
+"""
+
+"""
+This common file is used to set some global values and enumerations used by the overall package. 
+
+"""
+
+
+is_debug = False
+disable_ssl_warnings = True
+enable_ssl_check = False
+max_paging_size = 500
+default_time_out = 30
+
+comment_types = (
+    "ANSWER",
+    "OTHER",
+    "QUESTION",
+    "STANDARD_COMMENT",
+    "SUGGESTION",
+    "USAGE_EXPERIENCE",
+)
+star_ratings = (
+    "FIVE_STARS",
+    "FOUR_STARS",
+    "NO_RECOMMENDATION",
+    "ONE_STAR",
+    "THREE_STARS",
+    "TWO_STARS",
+)
+
+TEMPLATE_GUIDS: dict = {}
+INTEGRATION_GUIDS: dict = {}
+
