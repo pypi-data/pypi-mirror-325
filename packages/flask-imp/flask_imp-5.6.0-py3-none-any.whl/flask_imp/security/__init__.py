8a5375d0b53cf07aa88f39c65f22234fbd0271a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:05:54 +0000

    Add package flask-imp/5.6.0/flask_imp-5.6.0-py3-none-any.whl

---
+++ packages/flask-imp/flask_imp-5.6.0-py3-none-any.whl/flask_imp/security/__init__.py
@@ -0,0 +1,31 @@
+"""
+This module contains the security utilities for a Flask application.
+
+Functions:
+
+    - api_login_check: Checks if the user is logged in for an API request using sessions.
+    - include_csrf: Includes a CSRF token in a GET response, and checks it during a POST request.
+    - login_check: Checks if the user is logged in using sessions.
+    - permission_check: Checks if the user has the required permissions using sessions.
+    - pass_function_check: Checks is a function you give passes before hitting a route.
+
+"""
+
+from ._api_login_check import api_login_check
+from ._include_csrf import include_csrf
+from ._login_check import login_check
+from ._pass_function_check import pass_function_check
+from ._permission_check import permission_check
+
+from ._api_checkpoint import api_checkpoint
+from ._checkpoint import checkpoint
+
+__all__ = [
+    "api_login_check",
+    "include_csrf",
+    "login_check",
+    "pass_function_check",
+    "permission_check",
+    "api_checkpoint",
+    "checkpoint",
+]
