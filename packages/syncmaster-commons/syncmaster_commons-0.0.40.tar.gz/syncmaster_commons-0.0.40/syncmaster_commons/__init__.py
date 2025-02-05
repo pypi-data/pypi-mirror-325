2aa1e9865b509a9f33ee8767aad5c0a7b58bf4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:17 +0000

    Add package syncmaster-commons/0.0.40/syncmaster_commons-0.0.40.tar.gz

---
+++ packages/syncmaster-commons/syncmaster_commons-0.0.40.tar.gz/syncmaster_commons-0.0.40/syncmaster_commons/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "0.0.40"
+
+from .abstract import SMBaseClass
+from .agents import AgentRequestPayload, AgentResponsePayload
+from .gupshup import (AgentResponsePayloadGupshup, AudioPayload,
+                      GupshupIncomingPayLoad, GupshupOutgoingPayload,
+                      ImagePayload, LocationPayload, TextPayload, VideoPayload)
+from .keys import KEYS
+from .task_names import TaskNames
+
+__all__ = [
+    "AgentRequestPayload",
+    "AgentResponsePayloadGupshup",
+    "GupshupIncomingPayLoad",
+    "KEYS",
+    "SMBaseClass",
+    "TaskNames",
+    "AgentResponsePayload",
+    "GupshupOutgoingPayload",
+    "ImagePayload",
+    "TextPayload",
+    "VideoPayload",
+    "AudioPayload",
+    "LocationPayload",
+
+]
