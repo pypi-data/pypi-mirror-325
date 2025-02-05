3f9e73eadf75eae21d7230d4f8b24d45cee22c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:10 +0000

    Add package tensorlake/0.1.18/tensorlake-0.1.18.tar.gz

---
+++ packages/tensorlake/tensorlake-0.1.18.tar.gz/tensorlake-0.1.18/src/tensorlake/documentai/__init__.py
@@ -0,0 +1,10 @@
+from tensorlake.documentai.common import Document, JobResult
+from tensorlake.documentai.client import DocumentAI, ParsingOptions, ExtractionOptions
+
+__all__ = [
+    "DocumentAI",
+    "ParsingOptions",
+    "ExtractionOptions",
+    "Document",
+    "JobResult",
+]
