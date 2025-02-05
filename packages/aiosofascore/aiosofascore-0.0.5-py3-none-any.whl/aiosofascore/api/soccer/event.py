7bacda600230b36fdf50107d6d59baee14edea5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:12:20 +0000

    Add package aiosofascore/0.0.5/aiosofascore-0.0.5-py3-none-any.whl

---
+++ packages/aiosofascore/aiosofascore-0.0.5-py3-none-any.whl/aiosofascore/api/soccer/event.py
@@ -0,0 +1,19 @@
+from aiosofascore.api.soccer.schemas.base import PregameTeamForm, H2H, EventManagers
+
+
+class SoccerEventApi:
+
+    async def get_pregame_form(self, event_id: str) -> PregameTeamForm:
+        async with self:
+            result = self._get(f"api/v1/event/{event_id}/pregame-form")
+            return PregameTeamForm(**result)
+
+    async def get_h2h(self, event_id: str) -> H2H:
+        async with self:
+            result = self._get(f"api/v1/event/{event_id}/h2h")
+            return H2H(**result)
+
+    async def get_managers(self, event_id: str) -> EventManagers:
+        async with self:
+            result = self._get(f"api/v1/event/{event_id}/managers")
+            return EventManagers(**result)
---
+++ packages/aiosofascore/aiosofascore-0.0.5-py3-none-any.whl/aiosofascore/exeptions.py
@@ -0,0 +1,18 @@
+from aiohttp import ClientResponse
+
+
+class ResponseParseContentError(Exception):
+    def __init__(self, response: ClientResponse, path: str):
+        self._response = response
+        self._path = path
+
+    @property
+    def response(self):
+        return self._response
+
+    def __str__(self):
+        return (
+            "Response processing error:\n"
+            f"Api call: {self._path}\n"
+            f"Response status: {self._response.status}\n"
+        )
