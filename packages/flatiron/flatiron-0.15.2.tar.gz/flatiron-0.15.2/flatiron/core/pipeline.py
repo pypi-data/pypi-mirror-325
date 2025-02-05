9664bd79eac41c6709efe88b192b14d76dc6128a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:19:23 +0000

    Add package flatiron/0.15.2/flatiron-0.15.2.tar.gz

---
+++ packages/flatiron/flatiron-0.15.2.tar.gz/flatiron-0.15.2/flatiron/core/pipeline.py
@@ -0,0 +1,329 @@
+from typing import Optional, Union  # noqa F401
+import numpy as np  # noqa F401
+import schematics.models as scm  # noqa F401
+from tensorflow import keras  # noqa F401
+from keras import models as tfm  # noqa F401
+
+from abc import ABC, abstractmethod
+from copy import deepcopy
+from pathlib import Path
+import math
+
+from keras import losses as tfl
+from keras import metrics as tfmet
+from keras import optimizers as tfo
+import yaml
+
+from flatiron.core.dataset import Dataset
+import flatiron.core.config as cfg
+import flatiron.core.logging as filog
+import flatiron.core.loss as ficl
+import flatiron.core.metric as ficm
+import flatiron.core.tools as fict
+
+Filepath = Union[str, Path]
+# ------------------------------------------------------------------------------
+
+
+class PipelineBase(ABC):
+    @classmethod
+    def read_yaml(cls, filepath):
+        # type: (Filepath) -> PipelineBase
+        '''
+        Construct PipelineBase instance from given yaml file.
+
+        Args:
+            filepath (str or Path): YAML file.
+
+        Returns:
+            PipelineBase: PipelineBase instance.
+        '''
+        with open(filepath) as f:
+            config = yaml.safe_load(f)
+        return cls(config)
+
+    @classmethod
+    def from_string(cls, text):
+        # type: (str) -> PipelineBase
+        '''
+        Construct PipelineBase instance from given YAML text.
+
+        Args:
+            text (str): YAML text.
+
+        Returns:
+            PipelineBase: PipelineBase instance.
+        '''
+        config = yaml.safe_load(text)
+        return cls(config)
+
+    def __init__(self, config):
+        # type: (dict) -> None
+        '''
+        PipelineBase is a base class for machine learning pipelines.
+
+        Args:
+            config (dict): PipelineBase config.
+        '''
+        config = deepcopy(config)
+
+        # model
+        model = config.pop('model', {})
+        model = self.model_config()(model)
+        model.validate()
+        model = model.to_native()
+
+        # pipeline
+        config = cfg.PipelineConfig(config)
+        config.validate()
+        config = config.to_native()
+        config['model'] = model
+        self.config = config
+
+        # create Dataset instance
+        src = config['dataset']['source']
+        if Path(src).is_file():
+            self.dataset = Dataset.read_csv(src)
+        else:
+            self.dataset = Dataset.read_directory(src)
+
+        self.x_train = None  # type: Optional[np.ndarray]
+        self.x_test = None  # type: Optional[np.ndarray]
+        self.y_train = None  # type: Optional[np.ndarray]
+        self.y_test = None  # type: Optional[np.ndarray]
+
+    def _logger(self, method, message, config):
+        # type: (str, str, dict) -> filog.SlackLogger
+        '''
+        Retreives a logger given a message, config and slack flag.
+
+        Args:
+            method (str): Name of method calling logger.
+            message (str): Log message or Slack title.
+            config (dict): Config dict.
+
+        Returns:
+            ficl.SlackLogger: Configured logger instance.
+        '''
+        kwargs = deepcopy(self.config['logger'])
+        methods = kwargs['slack_methods']
+        del kwargs['slack_methods']
+        logger = filog.SlackLogger(message, config, **kwargs)
+        if method not in methods:
+            logger._message_func = None
+            logger._callback = None
+        return logger
+
+    def load(self):
+        # type: () -> PipelineBase
+        '''
+        Load dataset into memory.
+        Calls `self.dataset.load` with dataset params.
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        config = self.config['dataset']
+        with self._logger('load', 'LOAD DATASET', dict(dataset=config)):
+            self.dataset.load(
+                limit=config['load_limit'],
+                shuffle=config['load_shuffle'],
+            )
+        return self
+
+    def train_test_split(self):
+        # type: () -> PipelineBase
+        '''
+        Split dataset into train and test sets.
+
+        Assigns the following instance members:
+
+            * x_train
+            * x_test
+            * y_train
+            * y_test
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        config = self.config['dataset']
+
+        with self._logger(
+            'train_test_split', 'TRAIN TEST SPLIT', dict(dataset=config)
+        ):
+            x_train, x_test, y_train, y_test = self.dataset.train_test_split(
+                index=config['split_index'],
+                axis=config['split_axis'],
+                test_size=config['split_test_size'],
+                train_size=config['split_train_size'],
+                random_state=config['split_random_state'],
+                shuffle=config['split_shuffle'],
+            )
+            self.x_train = x_train
+            self.x_test = x_test
+            self.y_train = y_train
+            self.y_test = y_test
+        return self
+
+    def unload(self):
+        # type: () -> PipelineBase
+        '''
+        Unload dataset into memory. Train and test sets will be kept.
+        Calls `self.dataset.unload`.
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        config = self.config['dataset']
+        with self._logger('unload', 'UNLOAD DATASET', dict(dataset=config)):
+            self.dataset.unload()
+        return self
+
+    def build(self):
+        # type: () -> PipelineBase
+        '''
+        Build machine learning model and assign it to self.model.
+        Calls `self.model_func` with model params.
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        config = self.config['model']
+        with self._logger('build', 'BUILD MODEL', dict(model=config)):
+            self.model = self.model_func()(**config)
+        return self
+
+    def compile(self):
+        # type: () -> PipelineBase
+        '''
+        Call `self.model.compile` with compile params.
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        compile_ = self.config['compile']
+
+        cfg = dict(
+            model=self.config['model'],
+            optimizer=self.config['optimizer'],
+            compile=compile_,
+        )
+        with self._logger('compile', 'COMPILE MODEL', cfg):
+            # loss
+            loss = compile_['loss']
+            try:
+                loss = ficl.get(loss)
+            except NotImplementedError:
+                loss = tfl.get(loss)
+
+            # metrics
+            metrics = []
+            for m in compile_['metrics']:
+                try:
+                    metric = ficm.get(m)
+                except NotImplementedError:
+                    metric = tfmet.get(m)
+                metrics.append(metric)
+
+            # create optimizer
+            opt = tfo.get(self.config['optimizer'])
+
+            # compile
+            self.model.compile(
+                optimizer=opt,
+                loss=loss,
+                metrics=metrics,
+                loss_weights=compile_['loss_weights'],
+                weighted_metrics=compile_['weighted_metrics'],
+                run_eagerly=compile_['run_eagerly'],
+                steps_per_execution=compile_['steps_per_execution'],
+                jit_compile=compile_['jit_compile'],
+            )
+        return self
+
+    def fit(self):
+        # type: () -> PipelineBase
+        '''
+        Call `self.model.fit` with fit params.
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        cb = self.config['callbacks']
+        fit = self.config['fit']
+        log = self.config['logger']
+
+        with self._logger('fit', 'FIT MODEL', self.config):
+            # create tensorboard
+            tb = fict.get_tensorboard_project(
+                cb['project'],
+                cb['root'],
+                log['timezone'],
+            )
+
+            # create checkpoint params and callbacks
+            cp = deepcopy(cb)
+            del cp['project']
+            del cp['root']
+            callbacks = fict.get_callbacks(
+                tb['log_dir'], tb['checkpoint_pattern'], cp,
+            )
+
+            # train model
+            n = self.x_train.shape[0]  # type: ignore
+            self.model.fit(
+                x=self.x_train,
+                y=self.y_train,
+                callbacks=callbacks,
+                validation_data=(self.x_test, self.y_test),
+                steps_per_epoch=math.ceil(n / fit['batch_size']),
+                **fit,
+            )
+        return self
+
+    def run(self):
+        # type: () -> PipelineBase
+        '''
+        Run the following pipeline operations:
+
+        * load
+        * train_test_split
+        * unload
+        * build
+        * compile
+        * fit
+
+        Returns:
+            PipelineBase: Self.
+        '''
+        self.load() \
+            .train_test_split() \
+            .unload() \
+            .build() \
+            .compile() \
+            .fit()
+        return self
+
+    @abstractmethod
+    def model_config(self):
+        # type: () -> scm.Model
+        '''
+        Subclasses of PipelineBase wiil need to define a config class for models
+        created in the build method.
+
+        Returns:
+            scm.Model: Model config class.
+        '''
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def model_func(self):
+        # type: () -> tfm.Model
+        '''
+        Subclasses of PipelineBase need to define a function that builds and
+        returns a machine learning model.
+
+        Returns:
+            tfm.Model: Machine learning model.
+        '''
+        pass  # pragma: no cover
---
+++ packages/flatiron/flatiron-0.15.2.tar.gz/flatiron-0.15.2/flatiron/core/tools.py
@@ -0,0 +1,211 @@
+from typing import Any, Callable, Optional, Union  # noqa F401
+from http.client import HTTPResponse  # noqa F401
+from lunchbox.stopwatch import StopWatch  # noqa F401
+
+from datetime import datetime
+from pathlib import Path
+import inspect
+import os
+import re
+import sys
+
+from lunchbox.enforce import Enforce
+import lunchbox.tools as lbt
+import pytz
+import yaml
+
+from tensorflow import keras  # noqa: F401
+from keras import callbacks as tfc
+
+Filepath = Union[str, Path]
+# ------------------------------------------------------------------------------
+
+
+def get_tensorboard_project(project, root='/mnt/storage', timezone='UTC'):
+    # type: (Filepath, Filepath, str) -> dict[str, str]
+    '''
+    Creates directory structure for Tensorboard project.
+
+    Args:
+        project (str): Name of project.
+        root (str or Path): Tensorboard parent directory. Default: /mnt/storage
+        timezone (str, optional): Timezone. Default: UTC.
+
+    Returns:
+        dict: Project details.
+    '''
+    # create timestamp
+    timestamp = datetime \
+        .now(tz=pytz.timezone(timezone)) \
+        .strftime('d-%Y-%m-%d_t-%H-%M-%S')
+
+    # create directories
+    root_dir = Path(root, project, 'tensorboard').as_posix()
+    log_dir = Path(root_dir, timestamp).as_posix()
+    model_dir = Path(log_dir, 'models').as_posix()
+    os.makedirs(root_dir, exist_ok=True)
+    os.makedirs(model_dir, exist_ok=True)
+
+    # checkpoint pattern
+    target = f'p-{project}_{timestamp}' + '_e-{epoch:03d}.keras'
+    target = Path(model_dir, target).as_posix()
+
+    output = dict(
+        root_dir=root_dir,
+        log_dir=log_dir,
+        model_dir=model_dir,
+        checkpoint_pattern=target,
+    )
+    return output
+
+
+def get_callbacks(log_directory, checkpoint_pattern, checkpoint_params={}):
+    # type: (Filepath, str, dict) -> list
+    '''
+    Create a list of callbacks for Tensoflow model.
+
+    Args:
+        log_directory (str or Path): Tensorboard project log directory.
+        checkpoint_pattern (str): Filepath pattern for checkpoint callback.
+        checkpoint_params (dict, optional): Params to be passed to checkpoint
+            callback. Default: {}.
+
+    Raises:
+        EnforceError: If log directory does not exist.
+        EnforeError: If checkpoint pattern does not contain '{epoch}'.
+
+    Returns:
+        list: Tensorboard and ModelCheckpoint callbacks.
+    '''
+    log_dir = Path(log_directory)
+    msg = f'Log directory: {log_dir} does not exist.'
+    Enforce(log_dir.is_dir(), '==', True, message=msg)
+
+    match = re.search(r'\{epoch.*?\}', checkpoint_pattern)
+    msg = "Checkpoint pattern must contain '{epoch}'. "
+    msg += f'Given value: {checkpoint_pattern}'
+    msg = msg.replace('{', '{{').replace('}', '}}')
+    Enforce(match, '!=', None, message=msg)
+    # --------------------------------------------------------------------------
+
+    callbacks = [
+        tfc.TensorBoard(log_dir=log_dir.as_posix(), histogram_freq=1),
+        tfc.ModelCheckpoint(checkpoint_pattern, **checkpoint_params),
+    ]
+    return callbacks
+
+
+# MISC--------------------------------------------------------------------------
+def pad_layer_name(name, length=18):
+    # type: (str, int) -> str
+    '''
+    Pads underscores in a given layer name to make the string achieve a given
+    length.
+
+    Args:
+        name (str): Layer name to be padded.
+        length (int): Length of output string. Default: 18.
+
+    Returns:
+        str: Padded layer name.
+    '''
+    if length == 0:
+        return name
+
+    if '_' not in name:
+        name += '_'
+    delta = length - len(re.sub('_', '', name))
+    return re.sub('_+', '_' * delta, name)
+
+
+def unindent(text, spaces=4):
+    # type: (str, int) -> str
+    '''
+    Unindents given block of text according to given number of spaces.
+
+    Args:
+        text (str): Text block to unindent.
+        spaces (int, optional): Number of spaces to remove. Default: 4.
+
+    Returns:
+        str: Unindented text.
+    '''
+    output = text.split('\n')  # type: Any
+    regex = re.compile('^ {' + str(spaces) + '}')
+    output = [regex.sub('', x) for x in output]
+    output = '\n'.join(output)
+    return output
+
+
+def slack_it(
+    title,  # type: str
+    channel,  # type: str
+    url,  # type: str
+    config=None,  # type: Optional[dict]
+    stopwatch=None,  # type: Optional[StopWatch]
+    timezone='UTC',  # type: str
+    suppress=False,  # type: bool
+):
+    # type: (...) -> Union[str, HTTPResponse]
+    '''
+    Compose a message from given arguments and post it to slack.
+
+    Args:
+        title (str): Post title.
+        channel (str): Slack channel.
+        url (str): Slack URL.
+        config (dict, optional): Parameter dict. Default: None.
+        stopwatch (StopWatch, optional): StopWatch instance. Default: None.
+        timezone (str, optional): Timezone. Default: UTC.
+        suppress (bool, optional): Return message, rather than post it to Slack.
+            Default: False.
+
+    Returns:
+        HTTPResponse: Slack response.
+    '''
+    now = datetime.now(tz=pytz.timezone(timezone)).isoformat()
+    cfg = config or {}
+    delta = 'none'
+    hdelta = 'none'
+    if stopwatch is not None:
+        hdelta = stopwatch.human_readable_delta
+        delta = str(stopwatch.delta)
+
+    config_ = yaml.safe_dump(cfg, indent=4)
+    message = f'''
+        {title.upper()}
+
+        RUN TIME:
+        ```{hdelta} ({delta})```
+        POST TIME:
+        ```{now}```
+        CONFIG:
+        ```{config_}```
+    '''[1:-1]
+    message = unindent(message, spaces=8)
+
+    if suppress:
+        return message
+    return lbt.post_to_slack(url, channel, message)  # pragma: no cover
+
+
+def get_module_function(name, module):
+    # type: (str, str) -> Callable[[Any], Any]
+    '''
+    Get a function from a given module.
+
+    Args:
+        name (str): Function name.
+        module (str): Module name.
+
+    Raises:
+        NotImplementedError: If function is not found in module.
+
+    Returns:
+        function: Module function.
+    '''
+    members = inspect.getmembers(sys.modules[module])
+    funcs = dict(filter(lambda x: inspect.isfunction(x[1]), members))
+    if name in funcs:
+        return funcs[name]
+    raise NotImplementedError(f'Function not found: {name}')
