1b6c58c63d77bf72169cf9a75c66c2f92b0db1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:19:23 +0000

    Add package supadef/0.0.62/supadef-0.0.62.tar.gz

---
+++ packages/supadef/supadef-0.0.62.tar.gz/supadef-0.0.62/supadef/__init__.py
@@ -0,0 +1,23 @@
+from . import platform
+
+
+def server_task():
+    """
+    Deploy this function to your servers as a queue-worker
+    :return:
+    """
+    def g(f):
+        # intentionally do nothing :)
+        return f
+    return g
+
+
+def server_endpoint(method='GET'):
+    """
+    Deploy this function to your servers as an API endpoint
+    :return:
+    """
+    def g(f):
+        # intentionally do nothing :)
+        return f
+    return g
