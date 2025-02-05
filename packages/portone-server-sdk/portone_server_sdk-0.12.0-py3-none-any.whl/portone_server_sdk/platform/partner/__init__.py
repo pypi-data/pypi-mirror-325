8588599c3a0ff42726052b1adad2b447d6858a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:12:23 +0000

    Add package portone-server-sdk/0.12.0/portone_server_sdk-0.12.0-py3-none-any.whl

---
+++ packages/portone-server-sdk/portone_server_sdk-0.12.0-py3-none-any.whl/portone_server_sdk/platform/partner/__init__.py
@@ -0,0 +1,93 @@
+from ..._generated.platform.partner.errors.archive_platform_partner_error import (
+    ArchivePlatformPartnerError,
+)
+from ..._generated.platform.partner.errors.create_platform_partner_error import (
+    CreatePlatformPartnerError,
+)
+from ..._generated.platform.partner.errors.create_platform_partners_error import (
+    CreatePlatformPartnersError,
+)
+from ..._generated.platform.partner.errors.get_platform_partner_error import (
+    GetPlatformPartnerError,
+)
+from ..._generated.platform.partner.errors.get_platform_partners_error import (
+    GetPlatformPartnersError,
+)
+from ..._generated.platform.partner.errors.recover_platform_partner_error import (
+    RecoverPlatformPartnerError,
+)
+from ..._generated.platform.partner.errors.update_platform_partner_error import (
+    UpdatePlatformPartnerError,
+)
+from ..._generated.platform.partner.archive_platform_partner_response import (
+    ArchivePlatformPartnerResponse,
+)
+from ..._generated.platform.partner.create_platform_partner_body import (
+    CreatePlatformPartnerBody,
+)
+from ..._generated.platform.partner.create_platform_partner_body_account import (
+    CreatePlatformPartnerBodyAccount,
+)
+from ..._generated.platform.partner.create_platform_partner_body_contact import (
+    CreatePlatformPartnerBodyContact,
+)
+from ..._generated.platform.partner.create_platform_partner_body_type import (
+    CreatePlatformPartnerBodyType,
+)
+from ..._generated.platform.partner.create_platform_partner_body_type_business import (
+    CreatePlatformPartnerBodyTypeBusiness,
+)
+from ..._generated.platform.partner.create_platform_partner_body_type_non_wht_payer import (
+    CreatePlatformPartnerBodyTypeNonWhtPayer,
+)
+from ..._generated.platform.partner.create_platform_partner_body_type_wht_payer import (
+    CreatePlatformPartnerBodyTypeWhtPayer,
+)
+from ..._generated.platform.partner.create_platform_partner_response import (
+    CreatePlatformPartnerResponse,
+)
+from ..._generated.platform.partner.create_platform_partners_body import (
+    CreatePlatformPartnersBody,
+)
+from ..._generated.platform.partner.create_platform_partners_response import (
+    CreatePlatformPartnersResponse,
+)
+from ..._generated.platform.partner.get_platform_partners_body import (
+    GetPlatformPartnersBody,
+)
+from ..._generated.platform.partner.get_platform_partners_response import (
+    GetPlatformPartnersResponse,
+)
+from ..._generated.platform.partner.recover_platform_partner_response import (
+    RecoverPlatformPartnerResponse,
+)
+from ..._generated.platform.partner.update_platform_partner_response import (
+    UpdatePlatformPartnerResponse,
+)
+from ..._generated.platform.partner.client import PartnerClient
+
+__all__ = [
+    "ArchivePlatformPartnerError",
+    "CreatePlatformPartnerError",
+    "CreatePlatformPartnersError",
+    "GetPlatformPartnerError",
+    "GetPlatformPartnersError",
+    "RecoverPlatformPartnerError",
+    "UpdatePlatformPartnerError",
+    "ArchivePlatformPartnerResponse",
+    "CreatePlatformPartnerBody",
+    "CreatePlatformPartnerBodyAccount",
+    "CreatePlatformPartnerBodyContact",
+    "CreatePlatformPartnerBodyType",
+    "CreatePlatformPartnerBodyTypeBusiness",
+    "CreatePlatformPartnerBodyTypeNonWhtPayer",
+    "CreatePlatformPartnerBodyTypeWhtPayer",
+    "CreatePlatformPartnerResponse",
+    "CreatePlatformPartnersBody",
+    "CreatePlatformPartnersResponse",
+    "GetPlatformPartnersBody",
+    "GetPlatformPartnersResponse",
+    "RecoverPlatformPartnerResponse",
+    "UpdatePlatformPartnerResponse",
+    "PartnerClient",
+]
