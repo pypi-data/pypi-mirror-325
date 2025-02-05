0ccfc32350df5597ee28a73af91cb63c3dbfeaa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:51 +0000

    Add package mteb/1.34.1/mteb-1.34.1-py3-none-any.whl

---
+++ packages/mteb/mteb-1.34.1-py3-none-any.whl/mteb/models/mxbai_models.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from functools import partial
+
+from mteb.model_meta import ModelMeta, sentence_transformers_loader
+
+mxbai_embed_large_v1 = ModelMeta(
+    loader=partial(  # type: ignore
+        sentence_transformers_loader,
+        model_name="mixedbread-ai/mxbai-embed-large-v1",
+        revision="990580e27d329c7408b3741ecff85876e128e203",
+        model_prompts={
+            "query": "Represent this sentence for searching relevant passages: "
+        },
+    ),
+    name="mixedbread-ai/mxbai-embed-large-v1",
+    languages=["eng_Latn"],
+    open_weights=True,
+    revision="990580e27d329c7408b3741ecff85876e128e203",
+    release_date="2024-03-07",  # initial commit of hf model.
+    n_parameters=335_000_000,
+    max_tokens=512,
+    embed_dim=1024,
+    license="apache-2.0",
+    reference="https://huggingface.co/mixedbread-ai/mxbai-embed-large-v1",
+    similarity_fn_name="cosine",
+    framework=["Sentence Transformers", "PyTorch"],
+    use_instructions=True,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets=None,
+)
---
+++ packages/mteb/mteb-1.34.1-py3-none-any.whl/mteb/tasks/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from .aggregated_tasks import *
+from .BitextMining import *
+from .Classification import *
+from .Clustering import *
+from .InstructionRetrieval import *
+from .MultiLabelClassification import *
+from .PairClassification import *
+from .Reranking import *
+from .Retrieval import *
+from .SpeedTask import *
+from .STS import *
+from .Summarization import *
+from .SummaryRetrieval import *
