c02e4243e5a211413f246a3828aeae6c029416f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:24 +0000

    Add package synth-sdk/0.3.1.dev1/synth_sdk-0.3.1.dev1.tar.gz

---
+++ packages/synth-sdk/synth_sdk-0.3.1.dev1.tar.gz/synth_sdk-0.3.1.dev1/synth_sdk/__init__.py
@@ -0,0 +1,11 @@
+"""
+Synth SDK - A Python SDK for building and managing AI agents
+"""
+
+from importlib.metadata import version
+
+__version__ = version("synth-sdk")
+
+from .tracing import *  # noqa
+
+# You can also add other package-level imports and initialization here
