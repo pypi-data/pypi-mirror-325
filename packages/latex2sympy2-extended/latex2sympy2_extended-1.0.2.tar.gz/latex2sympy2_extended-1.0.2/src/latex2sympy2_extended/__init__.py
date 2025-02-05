a461f9d7dec80efd42f2fbde19e46d01594bd9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:11:52 +0000

    Add package latex2sympy2-extended/1.0.2/latex2sympy2_extended-1.0.2.tar.gz

---
+++ packages/latex2sympy2-extended/latex2sympy2_extended-1.0.2.tar.gz/latex2sympy2_extended-1.0.2/src/latex2sympy2_extended/__init__.py
@@ -0,0 +1,5 @@
+from .latex2sympy2 import latex2sympy
+from .math_normalization import normalize_latex, NormalizationConfig
+from .latex2sympy2 import is_assignment_symbol
+
+__all__ = ['latex2sympy', 'normalize_latex', 'NormalizationConfig', 'is_assignment_symbol']
