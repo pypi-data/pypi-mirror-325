82e539ee818c999754cd6aa07e428956778d2bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:42 +0000

    Add package eckitlib/1.28.5/eckitlib-1.28.5-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/eckitlib/eckitlib-1.28.5-cp311-cp311-macosx_13_0_arm64.whl/eckitlib/include/eckit/utils/AECCompressor.h
@@ -0,0 +1,37 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef eckit_utils_AECCompressor_H
+#define eckit_utils_AECCompressor_H
+
+#include "eckit/utils/Compressor.h"
+
+namespace eckit {
+
+class Buffer;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class AECCompressor : public eckit::Compressor {
+
+public:  // methods
+    AECCompressor();
+
+    ~AECCompressor() override;
+
+    size_t compress(const void* in, size_t len, eckit::Buffer& out) const override;
+    void uncompress(const void* in, size_t len, eckit::Buffer& out, size_t outlen) const override;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // end namespace eckit
+
+#endif
