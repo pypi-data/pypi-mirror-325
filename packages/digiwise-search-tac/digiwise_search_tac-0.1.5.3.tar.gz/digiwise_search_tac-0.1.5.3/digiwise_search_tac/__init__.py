f35d95bc04ccb653ece37c0634081cda3794ef3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:11:48 +0000

    Add package digiwise-search-tac/0.1.5.3/digiwise_search_tac-0.1.5.3.tar.gz

---
+++ packages/digiwise-search-tac/digiwise_search_tac-0.1.5.3.tar.gz/digiwise_search_tac-0.1.5.3/digiwise_search_tac/__init__.py
@@ -0,0 +1,8 @@
+
+from .digiwise_search_tac import init
+from .batch_search import batch_search, search_list, save_csv
+
+
+__all__ = ["init", "batch_search", "search_list", "save_csv"]
+__version__ = "0.1.5.3"
+__author__ = "DIGIWISE"
