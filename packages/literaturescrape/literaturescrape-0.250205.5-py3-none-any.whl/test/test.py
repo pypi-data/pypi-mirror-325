400daf3183b8484451d4541c3da603b276ee426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:46 +0000

    Add package literaturescrape/0.250205.5/literaturescrape-0.250205.5-py3-none-any.whl

---
+++ packages/literaturescrape/literaturescrape-0.250205.5-py3-none-any.whl/test/test.py
@@ -0,0 +1,9 @@
+from literaturescrape import sync_summary
+
+# from sys import stdout
+# from literaturescrape.core.settings import logger
+# logger.remove()
+# logger.add(stdout, level="DEBUG")
+
+print(sync_summary("10.1186/s12967-024-05786-4"))
+
