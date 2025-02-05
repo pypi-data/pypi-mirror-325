fe99ce6fd9bdc661e249048a4e70b276cb3c27a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:10 +0000

    Add package python-desfire/0.0.5/python_desfire-0.0.5-py3-none-any.whl

---
+++ packages/python-desfire/python_desfire-0.0.5-py3-none-any.whl/desfire/__init__.py
@@ -0,0 +1,7 @@
+from .DESFire import DESFire
+from .diversify import diversify_key
+from .key import DESFireKey
+from .pcsc import PCSCDevice
+from .util import get_list
+
+__all__ = ["DESFire", "PCSCDevice", "diversify_key", "DESFireKey", "get_list"]
