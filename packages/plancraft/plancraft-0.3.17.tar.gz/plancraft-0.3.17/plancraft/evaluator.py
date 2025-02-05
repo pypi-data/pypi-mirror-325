849aa9b140fe3a09b9ac658cae7b725361591f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:36 +0000

    Add package plancraft/0.3.17/plancraft-0.3.17.tar.gz

---
+++ packages/plancraft/plancraft-0.3.17.tar.gz/plancraft-0.3.17/plancraft/evaluator.py
@@ -0,0 +1,291 @@
+import json
+import os
+from typing import Optional
+from copy import deepcopy
+
+import imageio
+from loguru import logger
+from tqdm import tqdm
+
+import wandb
+from plancraft.config import PlancraftExample
+from plancraft.environment.actions import (
+    ActionHandlerBase,
+    MoveActionHandler,
+    SmeltActionHandler,
+    StopAction,
+)
+from plancraft.environment.env import (
+    PlancraftEnvironment,
+    get_objective_str,
+    target_and_inventory_to_text_obs,
+)
+from plancraft.models.base import PlancraftBaseModel
+from plancraft.utils import History
+
+
+class Evaluator:
+    """
+    The evaluator class handles the environment loop and model interaction
+
+    The environment is created based on the configuration and the examples are loaded from the dataset.
+
+    The Evaluator uses the dataset examples and initializes the environment with the example's inventory.
+
+    It is also responsible for early stopping and verifying the target object has been craft.
+    Finally, it also saves the results of the evaluation and the images generated during the evaluation.
+    """
+
+    def __init__(
+        self,
+        run_name: str,
+        actions: list[ActionHandlerBase] = [MoveActionHandler(), SmeltActionHandler()],
+        output_dir: str = "output",
+        split: str = "val.small",
+        resolution: str = "high",
+        max_steps: int = 30,
+        resume: bool = False,
+        use_multimodal_content_format: bool = False,
+        use_images: bool = False,
+        use_text_inventory: bool = False,
+        use_fasterrcnn: bool = False,
+        system_prompt: Optional[dict] = None,
+        prompt_examples: list[dict] = [],
+        prompt_images: list[str] = [],
+        few_shot: bool = True,
+    ):
+        self.run_name = run_name
+        self.use_multimodal_content_format = use_multimodal_content_format
+        self.use_images = use_images
+        self.use_text_inventory = use_text_inventory
+        self.use_fasterrcnn = use_fasterrcnn
+        self.max_steps = max_steps
+        self.resume = resume
+        self.resolution = resolution
+
+        # history args
+        self.system_prompt = system_prompt
+        self.prompt_examples = prompt_examples
+        self.prompt_images = prompt_images
+        self.few_shot = few_shot
+
+        self.output_dir = f"{output_dir}/{run_name}/{split}"
+        self.generation_number = 0
+        self.actions = actions
+
+        # load all examples
+        self.examples: list[PlancraftExample] = self.load_dataset(split)
+
+    def save_results_dict(self, example: PlancraftExample, results_dict: dict):
+        output_dir = f"{self.output_dir}/{self.generation_number}"
+        os.makedirs(output_dir, exist_ok=True)
+        json_path = f"{output_dir}/{example.id}.json"
+        with open(json_path, "w") as f:
+            json.dump(results_dict, f, indent=4)
+
+        if wandb.run is not None:
+            wandb.save(json_path, policy="now")
+
+    def save_images(self, example: PlancraftExample, frames: list):
+        if len(frames) == 0:
+            return
+        output_dir = f"{self.output_dir}/{self.generation_number}"
+        os.makedirs(output_dir, exist_ok=True)
+        imageio.mimsave(f"{output_dir}/{example.id}.gif", frames)
+        # upload to wandb
+        if wandb.run is not None:
+            wandb.save(f"{output_dir}/{example.id}.gif", policy="now")
+
+    def load_results_dict(self, example: PlancraftExample) -> dict:
+        path = f"{self.output_dir}/{self.generation_number}/{example.id}.json"
+        if not os.path.exists(path) or not self.resume:
+            return None
+        with open(path, "r") as f:
+            return json.load(f)
+
+    def load_dataset(self, dataset_split: str) -> list[PlancraftExample]:
+        folder = os.path.dirname(os.path.abspath(__file__))
+        with open(f"{folder}/data/{dataset_split}.json", "r") as f:
+            dataset = json.load(f)
+            return [PlancraftExample(**example) for example in dataset]
+
+    def check_done(self, inventory: dict, target: str):
+        """
+        Check that target object is obtained
+        """
+        for slot, item in inventory.items():
+            # ensure the target is in the inventory (not in slot 0)
+            if target == item["type"] and slot != 0:
+                return True
+        return False
+
+    def parse_raw_model_response(
+        self, generated_text: str, observation=None, history=None
+    ) -> str:
+        """
+        Given a message and set of action handlers, parse the content to return the action
+        or a message if the action is not valid/requires message response
+        """
+        for handler in self.actions:
+            match_output = handler.match(
+                generated_text, observation=observation, history=history
+            )
+            if match_output:
+                return match_output
+        action_names = [handler.action_name for handler in self.actions]
+        return f"Only select actions from the following: {', '.join(action_names)}"
+
+    def convert_observation_to_message(
+        self,
+        observation: dict,
+        model: PlancraftBaseModel = None,
+    ) -> str | dict:
+        """
+        Convert an environment observation to the message format used by an LLM chat model
+
+        Parameters:
+        - observation: dict - The observation to convert.
+        - use_text_inventory: bool - Whether to use text inventory.
+        - use_multimodal_content_format: bool - Whether to use multimodal content format.
+        - use_images: bool - Whether to append an image to the message content - must be used with use_multimodal_content_format.
+        """
+        if self.use_fasterrcnn:
+            assert model is not None, "Model must be provided to convert image to text"
+            # convert image to inventory using fasterrcnn
+            inventory = model.bbox_model.get_inventory(observation["image"].copy())
+            text_content = target_and_inventory_to_text_obs(
+                observation["target"], inventory
+            )
+        elif not self.use_text_inventory:
+            text_content = get_objective_str(observation["target"])
+        else:
+            # if not multimodal, we only have text - we format the inventory as text
+            text_content = target_and_inventory_to_text_obs(
+                observation["target"], observation["inventory"]
+            )
+        if not self.use_multimodal_content_format:
+            return text_content
+
+        content_list = [{"type": "text", "text": text_content}]
+        if self.use_images:
+            content_list.append({"type": "image"})
+        return {"content": content_list}
+
+    def eval_example(
+        self,
+        example: PlancraftExample,
+        model: PlancraftBaseModel,
+    ) -> dict:
+        """Given the loaded model and an example from Plancraft
+        run the episode until success or termination."""
+
+        # start environment
+        environment = PlancraftEnvironment(
+            inventory=example.slotted_inventory,
+            resolution=self.resolution,
+        )
+
+        # initialise history/dialogue tracking
+        history = History(
+            actions=self.actions,
+            use_multimodal_content_format=self.use_multimodal_content_format,
+            use_images=self.use_images,
+            use_text_inventory=self.use_text_inventory,
+            resolution=self.resolution,
+            few_shot=self.few_shot,
+            system_prompt=deepcopy(self.system_prompt),
+            prompt_examples=deepcopy(self.prompt_examples),
+            prompt_images=deepcopy(self.prompt_images),
+        )
+
+        success = False
+        action = None
+
+        # run episode until stuck or until max steps is reached
+        while history.num_steps < self.max_steps:
+            # if the action is stop then we end the episode
+            if isinstance(action, StopAction):
+                # if the action is stop and task is impossible then success
+                # otherwise we should not have stopped
+                success = example.impossible
+                break
+            # action is external tool then it is str
+            if isinstance(action, str):
+                observation = environment.step()
+                observation["target"] = example.target
+                observation["message"] = action
+            # action is environment action
+            else:
+                observation = environment.step(action)
+                # convert inventory observation to text message
+                observation["target"] = example.target
+                observation["message"] = self.convert_observation_to_message(
+                    observation, model=model
+                )
+                # check if the episode is done
+                success = self.check_done(observation["inventory"], example.target)
+            # exit if success
+            if success:
+                break
+
+            # add observation to history
+            history.add_observation_to_history(observation)
+            # add observation message to history
+            history.add_message_to_history(content=observation["message"], role="user")
+            # predict next action
+            raw_action = model.step(observation, dialogue_history=history)
+            # add message to history
+            history.add_message_to_history(content=raw_action, role="assistant")
+            # parse the raw action
+            action = self.parse_raw_model_response(
+                raw_action, observation=observation, history=history
+            )
+
+        # save results and reset
+        return {
+            "success": success,
+            "recipe_type": example.recipe_type,
+            "complexity": example.complexity_split,
+            "number_of_steps": history.num_steps,
+            "model_trace": history.trace(),
+            "example_id": example.id,
+            "images": history.images,
+        }
+
+    def eval_all_examples(self, model, progress_bar=False) -> list:
+        results = []
+        pbar = tqdm(
+            total=len(self.examples),
+            disable=not progress_bar,
+        )
+        correct = 0
+        count = 0
+        for example in self.examples:
+            if resume_result := self.load_results_dict(example):
+                pbar.update(self.max_steps)
+                results.append(resume_result)
+                continue
+
+            # skip impossible examples if impossible is not in valid actions
+            if example.impossible and "impossible" not in [
+                action.action_name for action in self.actions
+            ]:
+                continue
+
+            result = self.eval_example(example, model=model)
+            model.reset()
+
+            # save images and results
+            self.save_images(example, result["images"])
+            del result["images"]
+            results.append(result)
+            self.save_results_dict(example, result)
+
+            correct += int(result["success"])
+            count += 1
+
+            acc = correct / count
+            pbar.set_postfix(correct=correct, count=count, acc=acc)
+            pbar.update(1)
+
+        return results
---
+++ packages/plancraft/plancraft-0.3.17.tar.gz/plancraft-0.3.17/tests/test_evaluator.py
@@ -0,0 +1,284 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from plancraft.config import EvalConfig, PlancraftExample
+from plancraft.environment import (
+    GoldSearchActionHandler,
+    ImpossibleActionHandler,
+    MoveActionHandler,
+    SmeltActionHandler,
+    ThinkActionHandler,
+)
+from plancraft.environment.actions import MoveAction, SmeltAction, StopAction
+from plancraft.evaluator import Evaluator
+from plancraft.models import get_model
+
+
+@pytest.fixture
+def mock_cfg():
+    cfg = MagicMock(spec=EvalConfig)
+    cfg.plancraft = MagicMock()
+    cfg.plancraft.output_dir = "test_output"
+    cfg.plancraft.split = "test_split"
+    cfg.plancraft.environment = MagicMock()
+    cfg.plancraft.environment.resolution = "high"
+    cfg.plancraft.model = "test_model"
+    cfg.plancraft.adapter = ""
+    cfg.plancraft.mode = "test_mode"
+    cfg.plancraft.valid_actions = ["move", "smelt"]
+    cfg.plancraft.max_steps = 10
+    cfg.plancraft.num_generations = 1
+    cfg.plancraft.resume = False
+    cfg.wandb = MagicMock()
+    cfg.wandb.project = "test_project"
+    cfg.wandb.entity = "test_entity"
+    cfg.wandb.mode = "disabled"
+    return cfg
+
+
+@pytest.fixture
+def mock_example_json():
+    return {
+        "inventory": {
+            "cyan_stained_glass": 6,
+            "purpur_stairs": 26,
+            "birch_trapdoor": 28,
+            "zombie_head": 30,
+            "jungle_fence": 8,
+            "acacia_slab": 60,
+            "netherite_helmet": 1,
+            "cooked_salmon": 61,
+            "purple_terracotta": 48,
+            "cod_bucket": 1,
+            "ancient_debris": 42,
+            "cobblestone_wall": 60,
+            "magenta_bed": 1,
+            "cat_spawn_egg": 32,
+            "yellow_dye": 6,
+            "chain": 42,
+            "purple_concrete": 22,
+        },
+        "slotted_inventory": {
+            45: {"type": "cyan_stained_glass", "quantity": 6},
+            20: {"type": "purpur_stairs", "quantity": 26},
+            12: {"type": "birch_trapdoor", "quantity": 28},
+            38: {"type": "zombie_head", "quantity": 30},
+            25: {"type": "jungle_fence", "quantity": 8},
+            39: {"type": "acacia_slab", "quantity": 60},
+            22: {"type": "netherite_helmet", "quantity": 1},
+            42: {"type": "cooked_salmon", "quantity": 61},
+            10: {"type": "purple_terracotta", "quantity": 48},
+            23: {"type": "cod_bucket", "quantity": 1},
+            19: {"type": "ancient_debris", "quantity": 42},
+            13: {"type": "cobblestone_wall", "quantity": 60},
+            16: {"type": "magenta_bed", "quantity": 1},
+            44: {"type": "cat_spawn_egg", "quantity": 32},
+            37: {"type": "yellow_dye", "quantity": 6},
+            24: {"type": "chain", "quantity": 42},
+            28: {"type": "purple_concrete", "quantity": 22},
+        },
+        "target": "cyan_stained_glass_pane",
+        "num_distractors": 16,
+        "impossible": False,
+        "optimal_path_length": 1.0,
+        "optimal_path": ["cyan_stained_glass_pane"],
+        "inventory_trace": [
+            {
+                "purpur_stairs": 26,
+                "birch_trapdoor": 28,
+                "zombie_head": 30,
+                "jungle_fence": 8,
+                "acacia_slab": 60,
+                "netherite_helmet": 1,
+                "cooked_salmon": 61,
+                "purple_terracotta": 48,
+                "cod_bucket": 1,
+                "ancient_debris": 42,
+                "cobblestone_wall": 60,
+                "magenta_bed": 1,
+                "cat_spawn_egg": 32,
+                "yellow_dye": 6,
+                "chain": 42,
+                "purple_concrete": 22,
+                "cyan_stained_glass_pane": 16,
+            }
+        ],
+        "items_used": 6.0,
+        "unique_items_used": 1.0,
+        "complexity": 6.0,
+        "complexity_bin": 1.0,
+        "complexity_split": "easy",
+        "unseen_in_train": False,
+        "unseen_in_val": False,
+        "split": "train",
+        "id": "TRAIN0000",
+    }
+
+
+@pytest.fixture
+def mock_example(mock_example_json):
+    return PlancraftExample(**mock_example_json)
+
+
+@pytest.fixture
+def evaluator(mock_cfg, mock_example):
+    mock_model = MagicMock()
+    mock_model.history.trace.return_value = {"tokens_used": 10}
+    mock_model.history.images = [MagicMock()]
+
+    with patch("plancraft.evaluator.Evaluator.load_dataset") as mock_load_dataset:
+        mock_load_dataset.return_value = [mock_example]
+        return Evaluator(run_name="test_run")
+
+
+def test_load_dataset(evaluator):
+    with pytest.raises(FileNotFoundError):
+        evaluator.load_dataset("fake_split")
+
+
+def test_check_done(evaluator):
+    inventory = {1: {"type": "iron_ingot", "quantity": 1}}
+    assert evaluator.check_done(inventory, "iron_ingot")
+    assert not evaluator.check_done(inventory, "diamond")
+
+
+# Sample test function for parse_raw_model_response
+def test_parse_raw_model_response(evaluator):
+    evaluator.actions = [
+        MoveActionHandler(),
+        SmeltActionHandler(),
+        ThinkActionHandler(),
+        ImpossibleActionHandler(),
+        GoldSearchActionHandler(),
+    ]
+
+    # Define example inputs and expected outputs
+    content_move = "move: from [A1] to [B2] with quantity 10"
+    content_move_format_err = "move: from AAA to [B2] with quantity 10"
+    content_smelt = "smelt: from [A1] to [I1] with quantity 1"
+    content_think = "think: some thought"
+    content_impossible = "impossible: Cannot reach the target"
+    content_search = "search: bucket"
+    invalid_content = "dance: some random action"
+
+    # Test case: move action
+    result = evaluator.parse_raw_model_response(content_move)
+    assert isinstance(result, MoveAction)
+    assert result.slot_from == 1
+    assert result.slot_to == 5
+    assert result.quantity == 10
+
+    # Test case: invalid format
+    result = evaluator.parse_raw_model_response(content_move_format_err)
+    assert "Format Error" in result
+
+    # Test case: smelt action
+    result = evaluator.parse_raw_model_response(content_smelt)
+    assert isinstance(result, SmeltAction)
+    assert result.slot_from == 1
+    assert result.slot_to == 10
+    assert result.quantity == 1
+
+    # Test case: think action
+    result = evaluator.parse_raw_model_response(content_think)
+    assert result == "Ok"
+
+    # Test case: impossible action
+    result = evaluator.parse_raw_model_response(content_impossible)
+    assert isinstance(result, StopAction)
+    assert result.reason == "Cannot reach the target"
+
+    # Test case: search action
+    result = evaluator.parse_raw_model_response(content_search)
+    assert "Recipes to craft bucket:\nrecipe 1:\niron_ingot at" in result
+
+    # Test case: invalid action
+    result = evaluator.parse_raw_model_response(invalid_content)
+    assert "Only select actions from the following:" in result
+
+
+def test_convert_observation_to_message(evaluator):
+    target = "iron_ingot"
+    inventory = {1: {"type": "iron_ingot", "quantity": 1}}
+    observation = {
+        "target": target,
+        "inventory": inventory,
+    }
+
+    evaluator.use_fasterrcnn = False
+    evaluator.use_text_inventory = False
+    evaluator.use_multimodal_content_format = False
+
+    message = evaluator.convert_observation_to_message(observation)
+    assert message == "Craft an item of type: iron_ingot"
+
+    evaluator.use_text_inventory = True
+    message = evaluator.convert_observation_to_message(observation)
+    assert (
+        message
+        == "Craft an item of type: iron_ingot\ninventory:\n - iron_ingot [A1] quantity 1"
+    )
+
+    evaluator.use_multimodal_content_format = True
+    evaluator.use_images = False
+    message = evaluator.convert_observation_to_message(observation)
+    message = {
+        "content": [
+            {
+                "type": "text",
+                "text": "Craft an item of type: iron_ingot\ninventory:\n - iron_ingot [A1] quantity 1",
+            }
+        ],
+    }
+
+    evaluator.use_multimodal_content_format = True
+    evaluator.use_images = True
+    message = evaluator.convert_observation_to_message(observation)
+    message = {
+        "content": [
+            {
+                "type": "text",
+                "text": "Craft an item of type: iron_ingot\ninventory:\n - iron_ingot [A1] quantity 1",
+            },
+            {"type": "image"},
+        ],
+    }
+
+
+def test_dummy_model(mock_cfg, mock_example_json):
+    mock_cfg.plancraft.mode = "dummy"
+    mock_cfg.plancraft.use_fasterrcnn = False
+    example = PlancraftExample(**mock_example_json)
+    model = get_model(mock_cfg)
+    with patch("plancraft.evaluator.Evaluator.load_dataset") as mock_load_dataset:
+        mock_load_dataset.return_value = [example]
+        evaluator = Evaluator(run_name="test_run")
+        result = evaluator.eval_example(example, model=model)
+        assert result["example_id"] == "TRAIN0000"
+        assert result["model_trace"]["tokens_used"] == 0
+        assert not result["success"]
+
+
+def test_oracle_model(mock_cfg, mock_example_json):
+    mock_cfg.plancraft.mode = "oracle"
+    mock_cfg.plancraft.use_fasterrcnn = False
+    example = PlancraftExample(**mock_example_json)
+    model = get_model(mock_cfg)
+    with patch("plancraft.evaluator.Evaluator.load_dataset") as mock_load_dataset:
+        mock_load_dataset.return_value = [example]
+        evaluator = Evaluator(run_name="test_run")
+        result = evaluator.eval_example(example, model=model)
+        assert result["example_id"] == "TRAIN0000"
+        assert result["model_trace"]["tokens_used"] == 0
+        assert result["success"]
+
+
+# def test_eval_all_examples(evaluator):
+#     evaluator.save_images = MagicMock()
+#     evaluator.save_results_dict = MagicMock()
+#     results = evaluator.eval_all_examples()
+#     assert len(results) == 1
+#     assert results[0]["example_id"] == "TRAIN0000"
+#     assert evaluator.save_images.call_count == 1
+#     assert evaluator.save_results_dict.call_count == 1
