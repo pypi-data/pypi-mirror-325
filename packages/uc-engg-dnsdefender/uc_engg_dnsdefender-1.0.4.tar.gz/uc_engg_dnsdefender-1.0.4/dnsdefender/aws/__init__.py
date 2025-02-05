450a710e0f85a7db00f0bf136abaf2ee3f4b93e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:34 +0000

    Add package uc-engg-dnsdefender/1.0.4/uc_engg_dnsdefender-1.0.4.tar.gz

---
+++ packages/uc-engg-dnsdefender/uc_engg_dnsdefender-1.0.4.tar.gz/uc_engg_dnsdefender-1.0.4/dnsdefender/aws/__init__.py
@@ -0,0 +1,10 @@
+"""
+AWS Module for dnsdefender
+"""
+
+from .ec2 import get_ec2_instances
+from .elb import get_load_balancers
+from .eips import get_eips
+from .route53 import get_route53_records
+
+__all__ = ["get_ec2_instances", "get_load_balancers", "get_eips", "get_route53_records"]
---
+++ packages/uc-engg-dnsdefender/uc_engg_dnsdefender-1.0.4.tar.gz/uc_engg_dnsdefender-1.0.4/dnsdefender/utils/constants.py
@@ -0,0 +1,10 @@
+# constant[key] = KEY
+
+Result_keys = {
+  "takeovers": "TAKEOVERS",
+  "unused_cnames": "UNUSED CNAME",
+  "stopped_ec2": "STOPPED EC2",
+  "updated_entries": "UPDATED ENTRIES",
+  "new_entries": "NEW ENTRIES",
+  "deleted_entries": "DELETED ENTRIES"
+}
