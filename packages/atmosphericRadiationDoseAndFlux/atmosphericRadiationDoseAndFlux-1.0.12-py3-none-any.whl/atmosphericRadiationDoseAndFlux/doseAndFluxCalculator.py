7a597a72c2a08f95fb7a8a65d3012632811570f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:02 +0000

    Add package atmosphericRadiationDoseAndFlux/1.0.12/atmosphericRadiationDoseAndFlux-1.0.12-py3-none-any.whl

---
+++ packages/atmosphericRadiationDoseAndFlux/atmosphericRadiationDoseAndFlux-1.0.12-py3-none-any.whl/atmosphericRadiationDoseAndFlux/doseAndFluxCalculator.py
@@ -0,0 +1,164 @@
+import numpy as np
+import pandas as pd
+
+from . import particle
+from . import particleResponse
+from . import settings
+from . import units
+
+import ParticleRigidityCalculationTools as PRCT
+
+import inspect
+
+from typing import Callable
+
+print("Warning from atmosphericRadiationDoseAndFlux module: currently using an alpha particle as input actually calculates the contribution from alpha + all simulated heavier ions, rather than just alpha particles!")
+
+@settings.allowCalculationForTotalOfParticles
+def calculate_from_energy_spec(
+                            inputEnergyDistributionFunctionMeV:Callable, 
+                            altitudesInkm:list, 
+                            particleName="proton", 
+                            inputEnergyBins = 10**(0.1*(np.array(range(1,52))-1)+1),
+                            verticalCutOffRigidity = 0.0):
+
+    # flux function units: particles/cm2/sr/MeV/s 
+    # altitudesInkm: altitude units: km
+    # verticalCutOffRigidity units: GV
+
+    energyBinMidPoints = (inputEnergyBins[1:] + inputEnergyBins[:-1])/2
+    inputFluxesMeV = list(map(inputEnergyDistributionFunctionMeV,energyBinMidPoints))
+
+    outputDF = calculate_from_energy_spec_array(
+                            inputEnergyBins,
+                            inputFluxesMeV, 
+                            altitudesInkm, 
+                            particleName = particleName,
+                            verticalCutOffRigidity = verticalCutOffRigidity)
+
+    return outputDF
+
+@settings.allowCalculationForTotalOfParticles
+def calculate_from_rigidity_spec(
+                            inputRigidityDistributionFunctionGV:Callable, 
+                            altitudesInkm:list, 
+                            particleName="proton", 
+                            inputRigidityBins = None,
+                            verticalCutOffRigidity = 0.0):
+
+    # flux function units: particles/cm2/sr/GV/s 
+    # altitudesInkm: altitude units: km
+    # verticalCutOffRigidity units: GV
+
+    if inputRigidityBins == None:
+        particleForCalculations = particle.Particle(particleName)
+
+        inputEnergyBins = 10**(0.1*(np.array(range(1,52))-1)+1)
+        inputRigidityBins = np.array(PRCT.convertParticleEnergyToRigidity(inputEnergyBins,
+                                    particleMassAU = particleForCalculations.atomicMass,
+                                    particleChargeAU = particleForCalculations.atomicCharge))
+
+    rigidityBinMidPoints = (inputRigidityBins[1:] + inputRigidityBins[:-1])/2
+    inputFluxesGV = list(map(inputRigidityDistributionFunctionGV,
+                              rigidityBinMidPoints))
+
+    outputDF = calculate_from_rigidity_spec_array(
+                            inputRigidityBins,
+                            inputFluxesGV, 
+                            altitudesInkm, 
+                            particleName = particleName,
+                            verticalCutOffRigidity = verticalCutOffRigidity)
+
+    return outputDF
+
+@settings.allowCalculationForTotalOfParticles
+def calculate_from_energy_spec_array(
+                            inputEnergyBins:list,
+                            inputFluxesMeV:list, 
+                            altitudesInkm:list, 
+                            particleName="proton", 
+                            #inputEnergyBins = 10**(0.1*(np.array(range(1,52))-1)+1),
+                            verticalCutOffRigidity = 0.0):
+    
+    # flux units: particles/cm2/sr/MeV/s 
+    # altitudesInkm: altitude:km
+    # energy units: MeV
+    # verticalCutOffRigidity units: GV
+
+    particleForCalculations, inputEnergyBinsArray, altitudesInkmArray, inputFluxesArray = settings.formatInputVariables(inputEnergyBins, 
+                                                                                                               inputFluxesMeV, 
+                                                                                                               altitudesInkm, 
+                                                                                                               particleName, 
+                                                                                                               verticalCutOffRigidity)
+
+    ############################################################
+
+    inputEnergyDifferences = inputEnergyBinsArray[1:] - inputEnergyBinsArray[:-1]
+    inputFluxesIntegrated = inputFluxesArray * inputEnergyDifferences * np.pi # units of particles / cm2 / s
+
+    ############################################################
+
+    outputDF = pd.DataFrame({
+        "altitude (km)":altitudesInkmArray,
+    })
+
+    for doseType in particleResponse.fullListOfDoseResponseTypes:
+        doseResponseForParticle = particleResponse.fullDoseResponseDict[doseType](particleForCalculations,doseType)
+    
+        coordinateDosesList = []
+        for altitudeInkm in altitudesInkmArray:
+            altitude = units.Distance(altitudeInkm * 1000.0)
+            totalDose = doseResponseForParticle.calculateDose(altitude, inputEnergyBinsArray, inputFluxesIntegrated)
+            coordinateDosesList.append(totalDose)
+
+        outputDF[doseType] = coordinateDosesList
+
+    outputDF["SEU"] = outputDF["tn2"] * 1e-13
+    outputDF["SEL"] = outputDF["tn2"] * 1e-8
+
+    return outputDF
+
+@settings.allowCalculationForTotalOfParticles
+def calculate_from_rigidity_spec_array(
+                            inputRigidityBins:list,
+                            inputFluxesGV:list, 
+                            altitudesInkm:list, 
+                            particleName="proton",
+                            verticalCutOffRigidity = 0.0):
+
+    # flux units: particles/cm2/sr/GV/s 
+    # altitudesInkm: altitude:km
+    # rigidity units: GV
+    # verticalCutOffRigidity units: GV
+
+    particleForCalculations = particle.Particle(particleName)
+
+    inputEnergyBins = np.array(PRCT.convertParticleRigidityToEnergy(inputRigidityBins,
+                                particleMassAU = particleForCalculations.atomicMass,
+                                particleChargeAU = particleForCalculations.atomicCharge))
+
+    rigidityMidPoints = (inputRigidityBins[1:] + inputRigidityBins[:-1])/2
+
+    if inspect.isfunction(inputFluxesGV):
+        inputFluxesGVarray = np.array(list(map(inputFluxesGV,rigidityMidPoints)))
+
+    elif type(inputFluxesGV) in [int, float, list, np.ndarray]:
+        inputFluxesGVarray = settings.convertListOrFloatToArray(inputFluxesGV)
+    
+    else:
+        raise Exception("inputFluxesGV not specified as a valid type!")
+
+    inputFluxesMeV = PRCT.convertParticleRigiditySpecToEnergySpec(rigidityMidPoints,
+                                                                             inputFluxesGVarray, 
+                                                                             particleMassAU=particleForCalculations.atomicMass,
+                                                                             particleChargeAU=particleForCalculations.atomicCharge)["Energy distribution values"]
+
+    outputDoseFluxRates = calculate_from_energy_spec_array(
+                            inputEnergyBins,
+                            inputFluxesMeV, 
+                            altitudesInkm, 
+                            particleName = particleName,
+                            verticalCutOffRigidity = verticalCutOffRigidity)
+
+    return outputDoseFluxRates
+
---
+++ packages/atmosphericRadiationDoseAndFlux/atmosphericRadiationDoseAndFlux-1.0.12-py3-none-any.whl/tests/test_general.py
@@ -0,0 +1,124 @@
+from atmosphericRadiationDoseAndFlux.doseAndFluxCalculator import calculate_from_energy_spec_array
+import numpy as np
+import atmosphericRadiationDoseAndFlux.doseAndFluxCalculator as DAFcalc
+
+def getDefaultInputParameters():
+
+    inputEnergyBins = 10**(0.1*(np.array(range(1,52))-1)+1)
+
+    inputLatitudes = np.full(60,0.0)
+    inputLongitudes = np.full(60,0.0)
+    inputAltitudes = np.linspace(0,60,60)
+
+    return inputEnergyBins,inputLatitudes,inputLongitudes,inputAltitudes
+
+def runOverSingleStepSpectrumOnly(particleSpecies:str):
+
+    inputEnergyBins, inputLatitudes, inputLongitudes, inputAltitudes = getDefaultInputParameters()
+
+    inputFluxes = np.append(1,np.full(49,0))
+
+    outputDF = calculate_from_energy_spec_array(inputEnergyBins,inputFluxes,inputAltitudes,particleName = particleSpecies)
+
+    return outputDF
+
+def runOverFlatSpectrum(particleSpecies:str):
+
+    inputEnergyBins, inputLatitudes, inputLongitudes, inputAltitudes = getDefaultInputParameters()
+
+    inputFluxes = np.full(50,1)
+
+    outputDF = calculate_from_energy_spec_array(inputEnergyBins,inputFluxes,inputAltitudes,particleName = particleSpecies)
+
+    return outputDF
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+def test_function_input_rigidity():
+
+    outputDFproton = DAFcalc.calculate_from_rigidity_spec(lambda x:x**-7, [60.0], particleName="proton")
+    outputDFalpha = DAFcalc.calculate_from_rigidity_spec(lambda x:x**-7, [60.0], particleName="alpha")
+    outputDFboth = DAFcalc.calculate_from_rigidity_spec(lambda x:x**-7, [60.0], particleName="both")
+
+    for doseType in ["adose","edose","dosee","tn1","tn2","tn3","SEU","SEL"]:
+        assert outputDFproton[doseType].iloc[0] + outputDFalpha[doseType].iloc[0] == outputDFboth[doseType].iloc[0]
+
+    print(outputDFboth)
+
+def test_function_input_rigidity2():
+
+    inputRigidityFunction = lambda x:27593.36 * (x**-2.82844)
+
+    outputDFproton = DAFcalc.calculate_from_rigidity_spec(inputRigidityFunction, [60.0], particleName="proton")
+    outputDFalpha = DAFcalc.calculate_from_rigidity_spec(inputRigidityFunction, [60.0], particleName="alpha")
+    outputDFboth = DAFcalc.calculate_from_rigidity_spec(inputRigidityFunction, [60.0], particleName="both")
+
+    for doseType in ["adose","edose","dosee","tn1","tn2","tn3","SEU","SEL"]:
+        assert outputDFproton[doseType].iloc[0] + outputDFalpha[doseType].iloc[0] == outputDFboth[doseType].iloc[0]
+
+    print(outputDFboth)
+
+def test_function_input_energy():
+
+    outputDFproton = DAFcalc.calculate_from_energy_spec(lambda x:x**-7, [60.0], particleName="proton")
+    outputDFalpha = DAFcalc.calculate_from_energy_spec(lambda x:x**-7, [60.0], particleName="alpha")
+    outputDFboth = DAFcalc.calculate_from_energy_spec(lambda x:x**-7, [60.0], particleName="both")
+
+    for doseType in ["adose","edose","dosee","tn1","tn2","tn3","SEU","SEL"]:
+        assert outputDFproton[doseType].iloc[0] + outputDFalpha[doseType].iloc[0] == outputDFboth[doseType].iloc[0]
+
+    print(outputDFboth)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+def test_comparison_to_original_DAF_proton_SingleStep():
+
+    runOverSingleStepSpectrumOnly("proton")
+
+
+def test_comparison_to_original_DAF_alpha_SingleStep():
+
+    runOverSingleStepSpectrumOnly("alpha")
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+def test_comparison_to_original_DAF_proton_Flat():
+
+    protonFlatSpec = runOverFlatSpectrum("proton")
+
+    protonSlice = protonFlatSpec[protonFlatSpec["altitude (km)"] == 60.0]
+
+    assert int(protonSlice["tn3"].round(-4)) == round(59828888,-4) #178580000
+    assert int(protonSlice["adose"].round(-6).iloc[0]) == round(681326272,-6) #1.974e9
+
+    print(protonFlatSpec)
+
+
+def test_comparison_to_original_DAF_alpha_Flat():
+
+    alphaFlatSpec = runOverFlatSpectrum("alpha")
+
+    alphaSlice = alphaFlatSpec[alphaFlatSpec["altitude (km)"] == 60.0]
+
+    assert int(alphaSlice["tn3"].round(-4)) == round(292044128,-4) #int(1.84192e9)
+    assert int(alphaSlice["adose"].round(-6).iloc[0]) == round(3320603140,-6) #int(9.871e9)
+
+    print(alphaFlatSpec)
+
+def test_comparison_to_original_DAF_both_Flat():
+
+    protonFlatSpec = runOverFlatSpectrum("proton")
+    alphaFlatSpec = runOverFlatSpectrum("alpha")
+    bothFlatSpec = runOverFlatSpectrum("both")
+
+    protonSlice = protonFlatSpec[protonFlatSpec["altitude (km)"] == 60.0]
+    alphaSlice = alphaFlatSpec[alphaFlatSpec["altitude (km)"] == 60.0]
+    bothSlice = bothFlatSpec[bothFlatSpec["altitude (km)"] == 60.0]
+
+    for doseType in ["adose","edose","dosee","tn1","tn2","tn3","SEU","SEL"]:
+        assert protonFlatSpec[doseType].iloc[0] + alphaFlatSpec[doseType].iloc[0] == bothFlatSpec[doseType].iloc[0]
+
+    print(protonFlatSpec)
+    print(alphaFlatSpec)
+    print(bothFlatSpec)
+
