339ccee9134bacfbf1b3712f8a4b50c2fec73065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:14 +0000

    Add package sink-pypi/1.21.0a4/sink_pypi-1.21.0a4-py3-none-any.whl

---
+++ packages/sink-pypi/sink_pypi-1.21.0a4-py3-none-any.whl/sink/api/sdk/resources/resource_refs/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .parent import (
+    ParentResource,
+    AsyncParentResource,
+    ParentResourceWithRawResponse,
+    AsyncParentResourceWithRawResponse,
+    ParentResourceWithStreamingResponse,
+    AsyncParentResourceWithStreamingResponse,
+)
+from .resource_refs import (
+    ResourceRefsResource,
+    AsyncResourceRefsResource,
+    ResourceRefsResourceWithRawResponse,
+    AsyncResourceRefsResourceWithRawResponse,
+    ResourceRefsResourceWithStreamingResponse,
+    AsyncResourceRefsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ParentResource",
+    "AsyncParentResource",
+    "ParentResourceWithRawResponse",
+    "AsyncParentResourceWithRawResponse",
+    "ParentResourceWithStreamingResponse",
+    "AsyncParentResourceWithStreamingResponse",
+    "ResourceRefsResource",
+    "AsyncResourceRefsResource",
+    "ResourceRefsResourceWithRawResponse",
+    "AsyncResourceRefsResourceWithRawResponse",
+    "ResourceRefsResourceWithStreamingResponse",
+    "AsyncResourceRefsResourceWithStreamingResponse",
+]
---
+++ packages/sink-pypi/sink_pypi-1.21.0a4-py3-none-any.whl/sink/api/sdk/types/resource_refs/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .parent_model_with_child_ref import ParentModelWithChildRef as ParentModelWithChildRef
