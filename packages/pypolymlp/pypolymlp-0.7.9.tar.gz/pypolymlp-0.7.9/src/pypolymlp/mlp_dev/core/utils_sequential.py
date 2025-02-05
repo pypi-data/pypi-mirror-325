4bf7f43e0ab8627518362f55fc5b838fff3ef52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:05:23 +0000

    Add package pypolymlp/0.7.9/pypolymlp-0.7.9.tar.gz

---
+++ packages/pypolymlp/pypolymlp-0.7.9.tar.gz/pypolymlp-0.7.9/src/pypolymlp/mlp_dev/core/utils_sequential.py
@@ -0,0 +1,24 @@
+"""Utility functions for sequential regression."""
+
+import os
+
+
+def get_batch_slice(n_data, batch_size):
+    """Calculate slice indices for a given batch size."""
+    begin_batch = list(range(0, n_data, batch_size))
+    if len(begin_batch) > 1:
+        end_batch = list(begin_batch[1:]) + [n_data]
+    else:
+        end_batch = [n_data]
+    return begin_batch, end_batch
+
+
+def get_auto_batch_size(n_features: int, verbose: bool = False):
+    """Return optimal batch size determined automatically."""
+    mem_bytes = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
+    # Allocate of X.T @ X
+    mem_bytes = mem_bytes * 0.9 - (n_features**2 * 8)
+    # Allocate X of size (n_str * N3, n_features)
+    N3, max_mem = 300, 2e11
+    batch_size = round(min(mem_bytes, max_mem) / (N3 * n_features * 8))
+    return batch_size
