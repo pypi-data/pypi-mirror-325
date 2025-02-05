5e96563b42fbfc4aabf753a678d74c2de234495b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:01 +0000

    Add package mantik/0.5.8/mantik-0.5.8.tar.gz

---
+++ packages/mantik/mantik-0.5.8.tar.gz/mantik-0.5.8/mantik/__init__.py
@@ -0,0 +1,9 @@
+import mantik.cli
+import mantik.data_repository
+import mantik.remote_file_service
+import mantik.utils
+from mantik.data_repository.data_repository import data_download
+from mantik.init import init
+from mantik.runs.local import start_local_run
+from mantik.runs.remote import submit_run
+from mantik.tracking.track import init_tracking
