f23c3dc4d1082c9bc6e18fad69e025c1633c829b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:06:09 +0000

    Add package NChess/1.0.24/NChess-1.0.24.tar.gz

---
+++ packages/NChess/NChess-1.0.24.tar.gz/NChess-1.0.24/NChess/core/__init__.py
@@ -0,0 +1,4 @@
+from .nchess import (
+    Board,
+    Move,
+)
---
+++ packages/NChess/NChess-1.0.24.tar.gz/NChess-1.0.24/NChess/sq/__init__.py
@@ -0,0 +1,7 @@
+from ..core.nchess import (
+    uci_as_square,
+    square_file,
+    square_rank,
+    square_distance,
+    square_mirror,
+)
