d39eaf5912b6ae0ed22c2a4d14a6a36934c4fbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:05:19 +0000

    Add package bitmovin-api-sdk/1.218.0/bitmovin-api-sdk-1.218.0.tar.gz

---
+++ packages/bitmovin-api-sdk/bitmovin-api-sdk-1.218.0.tar.gz/bitmovin-api-sdk-1.218.0/bitmovin_api_sdk/models/av1_per_title_configuration.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+
+from six import string_types, iteritems
+from bitmovin_api_sdk.common.poscheck import poscheck_model
+from bitmovin_api_sdk.models.auto_representation import AutoRepresentation
+from bitmovin_api_sdk.models.per_title_configuration import PerTitleConfiguration
+from bitmovin_api_sdk.models.per_title_fixed_resolution_and_bitrate_configuration import PerTitleFixedResolutionAndBitrateConfiguration
+import pprint
+
+
+class Av1PerTitleConfiguration(PerTitleConfiguration):
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        if hasattr(super(Av1PerTitleConfiguration, self), "to_dict"):
+            result = super(Av1PerTitleConfiguration, self).to_dict()
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self):
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, Av1PerTitleConfiguration):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
