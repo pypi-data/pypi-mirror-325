1afbd97e3c641ecf013b02dc3167f0d78f65d521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:10 +0000

    Add package multimodal-sdk/0.0.6.35/multimodal_sdk-0.0.6.35.tar.gz

---
+++ packages/multimodal-sdk/multimodal_sdk-0.0.6.35.tar.gz/multimodal_sdk-0.0.6.35/multimodal_sdk/common/retry_decorator.py
@@ -0,0 +1,20 @@
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception, before_sleep_log
+import aiohttp
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def custom_retry_condition(exception):
+    # Retry for all ClientError exceptions except 401
+    return isinstance(exception, aiohttp.ClientResponseError) and exception.status != 401
+
+def retry_on_client_error():
+    # 2, 4, 8, 16, 20
+    return retry(
+        retry=retry_if_exception(custom_retry_condition),
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(multiplier=2, min=1, max=20),
+        reraise=True,
+        before_sleep=before_sleep_log(logger, logging.INFO),
+    )
