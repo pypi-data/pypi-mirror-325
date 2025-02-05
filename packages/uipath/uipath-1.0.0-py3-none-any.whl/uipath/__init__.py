4ae0fc7f637b629611f071c8fa3d70f906ecc8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:26:59 +0000

    Add package uipath/1.0.0/uipath-1.0.0-py3-none-any.whl

---
+++ packages/uipath/uipath-1.0.0-py3-none-any.whl/uipath/__init__.py
@@ -0,0 +1,9 @@
+from uipath.__version__ import __version__
+from uipath.auth.authentication import UiPathAuth
+from uipath.client.api_client import UiPathClient
+
+__all__ = [
+    '__version__',
+    'UiPathAuth',
+    'UiPathClient',
+] 
