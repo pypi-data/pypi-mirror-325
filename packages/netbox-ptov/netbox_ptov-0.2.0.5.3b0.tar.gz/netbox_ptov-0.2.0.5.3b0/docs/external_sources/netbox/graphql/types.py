a06a700c9ad5f8c2f43982e4d4664c1d4743f523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:18:40 +0000

    Add package netbox-ptov/0.2.0.5.3b0/netbox_ptov-0.2.0.5.3b0.tar.gz

---
+++ packages/netbox-ptov/netbox_ptov-0.2.0.5.3b0.tar.gz/netbox_ptov-0.2.0.5.3b0/docs/external_sources/netbox/graphql/types.py
@@ -0,0 +1,96 @@
+import strawberry
+import strawberry_django
+from django.contrib.contenttypes.models import ContentType
+
+from core.graphql.mixins import ChangelogMixin
+from core.models import ObjectType as ObjectType_
+from extras.graphql.mixins import CustomFieldsMixin, JournalEntriesMixin, TagsMixin
+
+__all__ = (
+    'BaseObjectType',
+    'ObjectType',
+    'OrganizationalObjectType',
+    'NetBoxObjectType',
+)
+
+
+#
+# Base types
+#
+
+@strawberry.type
+class BaseObjectType:
+    """
+    Base GraphQL object type for all NetBox objects. Restricts the model queryset to enforce object permissions.
+    """
+
+    @classmethod
+    def get_queryset(cls, queryset, info, **kwargs):
+        # Enforce object permissions on the queryset
+        if hasattr(queryset, 'restrict'):
+            return queryset.restrict(info.context.request.user, 'view')
+        else:
+            return queryset
+
+    @strawberry_django.field
+    def display(self) -> str:
+        return str(self)
+
+    @strawberry_django.field
+    def class_type(self) -> str:
+        return self.__class__.__name__
+
+
+class ObjectType(
+    ChangelogMixin,
+    BaseObjectType
+):
+    """
+    Base GraphQL object type for unclassified models which support change logging
+    """
+    pass
+
+
+class OrganizationalObjectType(
+    ChangelogMixin,
+    CustomFieldsMixin,
+    TagsMixin,
+    BaseObjectType
+):
+    """
+    Base type for organizational models
+    """
+    pass
+
+
+class NetBoxObjectType(
+    ChangelogMixin,
+    CustomFieldsMixin,
+    JournalEntriesMixin,
+    TagsMixin,
+    BaseObjectType
+):
+    """
+    GraphQL type for most NetBox models. Includes support for custom fields, change logging, journaling, and tags.
+    """
+    pass
+
+
+#
+# Miscellaneous types
+#
+
+@strawberry_django.type(
+    ContentType,
+    fields=['id', 'app_label', 'model'],
+)
+class ContentTypeType:
+    pass
+
+
+@strawberry_django.type(
+    ObjectType_,
+    fields=['id', 'app_label', 'model'],
+)
+class ObjectTypeType:
+    pass
---
+++ packages/netbox-ptov/netbox_ptov-0.2.0.5.3b0.tar.gz/netbox_ptov-0.2.0.5.3b0/docs/external_sources/netbox/views/generic/__init__.py
@@ -0,0 +1,3 @@
+from netbox.views.generic.bulk_views import *
+from netbox.views.generic.feature_views import *
+from netbox.views.generic.object_views import *
