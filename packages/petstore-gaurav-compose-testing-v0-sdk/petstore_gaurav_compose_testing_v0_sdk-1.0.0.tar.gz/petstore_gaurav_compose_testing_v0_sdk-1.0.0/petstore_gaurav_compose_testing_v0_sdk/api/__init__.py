40394e5eb3b45e0b5d4e7cc2d6ebc2e7e25f943c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:05:28 +0000

    Add package petstore-gaurav-compose-testing-v0-sdk/1.0.0/petstore_gaurav_compose_testing_v0_sdk-1.0.0.tar.gz

---
+++ packages/petstore-gaurav-compose-testing-v0-sdk/petstore_gaurav_compose_testing_v0_sdk-1.0.0.tar.gz/petstore_gaurav_compose_testing_v0_sdk-1.0.0/petstore_gaurav_compose_testing_v0_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from petstore_gaurav_compose_testing_v0_sdk.api.pet_api import PetApi
+from petstore_gaurav_compose_testing_v0_sdk.api.store_api import StoreApi
+from petstore_gaurav_compose_testing_v0_sdk.api.user_api import UserApi
+
