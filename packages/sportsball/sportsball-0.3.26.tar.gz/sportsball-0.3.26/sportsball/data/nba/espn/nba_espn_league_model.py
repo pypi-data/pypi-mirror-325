e0614845bb08a1f1f8f5c28a400b460a4b11f963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:05 +0000

    Add package sportsball/0.3.26/sportsball-0.3.26.tar.gz

---
+++ packages/sportsball/sportsball-0.3.26.tar.gz/sportsball-0.3.26/sportsball/data/nba/espn/nba_espn_league_model.py
@@ -0,0 +1,17 @@
+"""NBA ESPN league model."""
+
+import requests_cache
+
+from ...espn.espn_league_model import ESPNLeagueModel
+from ...league import League
+
+_SEASON_URL = (
+    "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons?limit=100"
+)
+
+
+class NBAESPNLeagueModel(ESPNLeagueModel):
+    """NBA ESPN implementation of the league model."""
+
+    def __init__(self, session: requests_cache.CachedSession) -> None:
+        super().__init__(_SEASON_URL, League.NBA, session)
---
+++ packages/sportsball/sportsball-0.3.26.tar.gz/sportsball-0.3.26/sportsball/data/ncaab/sportsreference/ncaab_sportsreference_league_model.py
@@ -0,0 +1,18 @@
+"""NCAAB sports reference league model."""
+
+# pylint: disable=line-too-long
+
+import requests_cache
+
+from ...league import League
+from ...sportsreference.sportsreference_league_model import \
+    SportsReferenceLeagueModel
+
+
+class NCAABSportsReferenceLeagueModel(SportsReferenceLeagueModel):
+    """NCAAB Sports Reference implementation of the league model."""
+
+    def __init__(self, session: requests_cache.CachedSession) -> None:
+        super().__init__(
+            session, League.NCAAB, "https://www.sports-reference.com/cbb/boxscores/"
+        )
