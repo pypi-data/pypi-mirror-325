898ee4bb499d2cdc633c31130674cb3e2a8dff2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:12:30 +0000

    Add package nonebot-plugin-ba-tools/0.5.9.post1/nonebot_plugin_ba_tools-0.5.9.post1.tar.gz

---
+++ packages/nonebot-plugin-ba-tools/nonebot_plugin_ba_tools-0.5.9.post1.tar.gz/nonebot_plugin_ba_tools-0.5.9.post1/nonebot_plugin_ba_tools/webui/driver.py
@@ -0,0 +1,20 @@
+from fastapi.middleware.cors import CORSMiddleware
+from nonebot import get_driver
+from nonebot.drivers.fastapi import Driver as FastAPIDriver
+
+
+driver = get_driver()
+
+if not isinstance(driver, FastAPIDriver):
+    raise NotImplementedError("This feature only support FastAPI driver.")
+
+app = driver.server_app
+
+app.separate_input_output_schemas = False
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
