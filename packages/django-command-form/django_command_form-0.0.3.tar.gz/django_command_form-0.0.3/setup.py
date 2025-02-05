a7fd8222336d49b2caba6bc2c11af5b3b7122451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:21 +0000

    Add package django-command-form/0.0.3/django_command_form-0.0.3.tar.gz

---
+++ packages/django-command-form/django_command_form-0.0.3.tar.gz/django_command_form-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="django-command-form",
+    description=("A package that allows you to execute commands from DjangoAdmin."),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/naohide/django-command-form/",
+    license="MIT",
+    author="naohide",
+    author_email="n.anahara@fragment.co.jp",
+    packages=["django_command_form"],
+    version="0.0.3",
+    install_requires=[
+        "Django>=4.0",
+    ],
+)
