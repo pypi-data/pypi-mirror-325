9d4a82108c85e7235135787874cce8ddcbf468e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:18:34 +0000

    Add package tik-afriton/1.1/tik_afriton-1.1.tar.gz

---
+++ packages/tik-afriton/tik_afriton-1.1.tar.gz/tik_afriton-1.1/TIK_afrit/__init__.py
@@ -0,0 +1,12 @@
+
+from .AFRI import AFRITON
+from .AFRI import create_device
+from .AFRI import Xgorgon
+from .AFRI import TTEncrypt
+from .AFRI import List_PROM
+from .AFRI import tim
+from .Arg import Ladon
+from .Arg import SM3
+from .Arg import Argus
+from .Arg import Gorgon
+from .Arg import UserAgent
