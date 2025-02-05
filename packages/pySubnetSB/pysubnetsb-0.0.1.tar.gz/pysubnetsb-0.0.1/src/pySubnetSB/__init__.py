80ee17eca4b14a45e089b1b60380a8d10351020a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:12:49 +0000

    Add package pySubnetSB/0.0.1/pysubnetsb-0.0.1.tar.gz

---
+++ packages/pySubnetSB/pysubnetsb-0.0.1.tar.gz/pysubnetsb-0.0.1/src/pySubnetSB/__init__.py
@@ -0,0 +1,7 @@
+"""
+Key structures in pySubnetSB
+
+A NamedMatrix is a two dimensional numpy array with row and column names.
+A Network is a representation of a chemical reaction network that includes a NamedMatrix for reactants and products.
+A Constraint describes numerical or categorical charateristics of a network that are organized as one or more NamedMatrix.
+"""
