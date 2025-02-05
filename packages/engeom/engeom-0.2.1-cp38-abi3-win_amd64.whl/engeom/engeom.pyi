7cc7c83004c60b7ff482090794cade0255946344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:50 +0000

    Add package engeom/0.2.1/engeom-0.2.1-cp38-abi3-win_amd64.whl

---
+++ packages/engeom/engeom-0.2.1-cp38-abi3-win_amd64.whl/engeom/engeom.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from enum import Enum
+
+type Resample = Resample_Count | Resample_Spacing | Resample_MaxSpacing
+
+class DeviationMode(Enum):
+    Point = 0
+    Plane = 1
