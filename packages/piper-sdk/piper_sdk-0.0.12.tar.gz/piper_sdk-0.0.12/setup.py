8c3b2e2da0cc989ca9edfa9d9d50c08f3d8ce117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:44 +0000

    Add package piper-sdk/0.0.12/piper_sdk-0.0.12.tar.gz

---
+++ packages/piper-sdk/piper_sdk-0.0.12.tar.gz/piper_sdk-0.0.12/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages # 导入setuptools打包工具
+
+setup(
+    name='piper_sdk',
+    version='0.0.12',    # 包版本号，便于维护版本
+    packages=find_packages(include=['piper_sdk', 'piper_sdk.*']),
+    long_description=open('piper_sdk/DESCRIPTION.MD').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    package_data={
+        '': ['LICENSE','*.sh','*.MD'],  # 指定包含的文件
+        'piper_sdk/asserts': ['*'],  # 包括 asserts 文件夹下的所有文件
+    },
+    install_requires=[
+        'python-can>=3.3.4',
+    ],
+    entry_points={
+    },
+    author='RosenYin',  # 作者
+    author_email='yinruocheng321@gmail.com',
+    description='A sdk to control piper',   #包的简述
+    # url="https://github.com/agilexrobotics/piper_sdk",
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求,18.04及以上
+)
+
