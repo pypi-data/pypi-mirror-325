89d4a3d6d2dec23973e232f7315fa0de160b141b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:19:56 +0000

    Add package eqc-models/0.10.0/eqc_models-0.10.0.tar.gz

---
+++ packages/eqc-models/eqc_models-0.10.0.tar.gz/eqc_models-0.10.0/eqc_models/__init__.py
@@ -0,0 +1,15 @@
+# (C) Quantum Computing Inc., 2024.
+"""
+eqc-models package for high-level optimization modeling for EQC and other devices
+
+"""
+
+from .base import QuadraticModel, PolynomialModel
+from .solvers import (Dirac1CloudSolver, Dirac3CloudSolver, Dirac3DirectSolver)
+from .allocation import AllocationModel, AllocationModelX, ResourceRuleEnum
+from .assignment import QAPModel
+
+__all__ = ["QuadraticModel", "PolynomialModel", "Dirac1CloudSolver", 
+           "Dirac3CloudSolver", "AllocationModel", "AllocationModelX",
+           "Dirac3DirectSolver", "ResourceRuleEnum",
+           "QAPModel"]
