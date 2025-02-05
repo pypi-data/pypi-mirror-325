32fdf755a3b3568a09741a1f1c57e964b3627f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:04 +0000

    Add package mylocale/2025.2.4/mylocale-2025.2.4-py3-none-any.whl

---
+++ packages/mylocale/mylocale-2025.2.4-py3-none-any.whl/mylocale/TR.py
@@ -0,0 +1,19 @@
+import csv
+import locale
+
+
+def tr(csv_file, target_key, langcode):
+    f = open(csv_file, newline="")
+    locale_csv = csv.DictReader(f=f, delimiter=",")
+    for item in locale_csv:
+        if item["stringname"] == target_key:
+            try:
+                if item[langcode] == "":
+                    f.close()
+                    return item["en"]
+                else:
+                    f.close()
+                    return item[langcode]
+            except:
+                f.close()
+                return item["en"]
