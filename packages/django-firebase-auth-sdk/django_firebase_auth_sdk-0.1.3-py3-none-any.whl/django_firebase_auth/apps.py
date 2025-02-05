9866a709668de2a857c2ad1a2f9cbc6be6d5c802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:33 +0000

    Add package django-firebase-auth-sdk/0.1.3/django_firebase_auth_sdk-0.1.3-py3-none-any.whl

---
+++ packages/django-firebase-auth-sdk/django_firebase_auth_sdk-0.1.3-py3-none-any.whl/django_firebase_auth/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoFirebaseAuthConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_firebase_auth"
+    label = "django_firebase_auth"
