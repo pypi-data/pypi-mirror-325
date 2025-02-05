bef17cf1fd1e14d2dc68c81a8b8dd8bb430694e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:19:09 +0000

    Add package dstack/0.18.40rc1/dstack-0.18.40rc1-py3-none-any.whl

---
+++ packages/dstack/dstack-0.18.40rc1-py3-none-any.whl/dstack/_internal/core/models/pools.py
@@ -0,0 +1,40 @@
+import datetime
+from typing import List, Optional
+from uuid import UUID
+
+from dstack._internal.core.models.backends.base import BackendType
+from dstack._internal.core.models.common import CoreModel
+from dstack._internal.core.models.instances import InstanceStatus, InstanceType
+
+
+class Pool(CoreModel):
+    name: str
+    default: bool
+    created_at: datetime.datetime
+    total_instances: int
+    available_instances: int
+
+
+class Instance(CoreModel):
+    id: UUID
+    project_name: str
+    backend: Optional[BackendType] = None
+    instance_type: Optional[InstanceType] = None
+    name: str
+    fleet_id: Optional[UUID] = None
+    fleet_name: Optional[str] = None
+    instance_num: int
+    pool_name: Optional[str] = None
+    job_name: Optional[str] = None
+    hostname: Optional[str] = None
+    status: InstanceStatus
+    unreachable: bool = False
+    termination_reason: Optional[str] = None
+    created: datetime.datetime
+    region: Optional[str] = None
+    price: Optional[float] = None
+
+
+class PoolInstances(CoreModel):
+    name: str
+    instances: List[Instance]
