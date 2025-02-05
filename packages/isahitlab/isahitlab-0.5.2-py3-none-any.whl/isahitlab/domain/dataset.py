a5efdadd8f5d14bd410f8e3076451641d9a40484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:29 +0000

    Add package isahitlab/0.5.2/isahitlab-0.5.2-py3-none-any.whl

---
+++ packages/isahitlab/isahitlab-0.5.2-py3-none-any.whl/isahitlab/domain/dataset.py
@@ -0,0 +1,24 @@
+from typing import NewType, Union
+from dataclasses import dataclass
+
+DatasetId = NewType("DatasetId", str)
+
+@dataclass
+class FilePayload(dict):
+    """Task payload for task create
+
+    Inherit from dict to make it JSON serializable
+    
+    Args:
+        file: Name of the task (appended to the path, it must be unique in a dataset)
+        path: Path of a resource (ex.: "./images/image1.jpg")
+    """
+
+    file: str
+    path:  Union[str, None]
+    
+
+    def __init__(self, file, path):
+        self.file = file
+        self.path = path
+        dict.__init__(self, file=file, path=path)
