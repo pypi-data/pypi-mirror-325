f9160500425aa593e083fb172dd7005649745337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:37 +0000

    Add package nixt/172/nixt-172.tar.gz

---
+++ packages/nixt/nixt-172.tar.gz/nixt-172/nixt/lookups.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "ask": "nixt.modules.llm",
+    "cfg": "nixt.modules.irc",
+    "dpl": "nixt.modules.rss",
+    "err": "nixt.modules.err",
+    "exp": "nixt.modules.rss",
+    "flt": "nixt.modules.flt",
+    "fnd": "nixt.modules.fnd",
+    "imp": "nixt.modules.rss",
+    "log": "nixt.modules.log",
+    "mod": "nixt.modules.mod",
+    "mre": "nixt.modules.irc",
+    "nme": "nixt.modules.rss",
+    "pwd": "nixt.modules.irc",
+    "rem": "nixt.modules.rss",
+    "res": "nixt.modules.rss",
+    "rss": "nixt.modules.rss",
+    "syn": "nixt.modules.rss",
+    "thr": "nixt.modules.thr",
+    "upt": "nixt.modules.upt",
+    "ver": "nixt.modules.upt"
+}
