d37d0fed8665f65f0e4001a63ab2d1e9399cd663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:11 +0000

    Add package nuse/0.1.4/nuse-0.1.4.tar.gz

---
+++ packages/nuse/nuse-0.1.4.tar.gz/nuse-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="nuse",
+    version="0.1.4",
+    author='Jacob Molin Nielsen',
+    author_email='jacob.molin@me.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "nuse = nuse.nuse:main",
+        ],
+    },
+)
