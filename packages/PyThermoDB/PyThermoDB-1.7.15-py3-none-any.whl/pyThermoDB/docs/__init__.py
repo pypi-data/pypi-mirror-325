e94d8683f94182cfc1de7246433d9a29a8855470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:24 +0000

    Add package PyThermoDB/1.7.15/PyThermoDB-1.7.15-py3-none-any.whl

---
+++ packages/PyThermoDB/PyThermoDB-1.7.15-py3-none-any.whl/pyThermoDB/docs/__init__.py
@@ -0,0 +1,8 @@
+from .thermo import SettingDatabook
+from .tableref import TableReference
+from .managedata import ManageData
+from .customref import CustomRef
+from .compbuilder import CompBuilder
+
+__all__ = ['SettingDatabook', 'TableReference',
+           'ManageData', 'CompBuilder', 'CustomRef']
