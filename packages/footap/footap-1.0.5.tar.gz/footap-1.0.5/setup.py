730feeef1bf3a91a7d34c32899bd2f07911d3821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:05:33 +0000

    Add package footap/1.0.5/footap-1.0.5.tar.gz

---
+++ packages/footap/footap-1.0.5.tar.gz/footap-1.0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="footap",
+    version="1.0.5",
+    author="Dims",
+    description="Package d'analyse des touches de balle au football",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-contrib-python>=4.5.0",
+        "mediapipe>=0.8.0",
+        "numpy>=1.19.0",
+        "ultralytics>=8.0.0",
+        "Pillow>=8.0.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'footap=footap.main:main',
+        ],
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Video :: Capture",
+    ],
+)
