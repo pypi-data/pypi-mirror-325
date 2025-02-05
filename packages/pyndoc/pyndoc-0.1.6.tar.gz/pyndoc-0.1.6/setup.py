580def633902af1f5e219acfd47e7079c0e3706a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:43 +0000

    Add package pyndoc/0.1.6/pyndoc-0.1.6.tar.gz

---
+++ packages/pyndoc/pyndoc-0.1.6.tar.gz/pyndoc-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="pyndoc",
+    version="0.1.6",
+    packages=["pyndoc"],
+    entry_points={"console_scripts": ["pyndoc = pyndoc.__main__:main"]},
+    install_requires=[
+        "panflute",
+        "pyyaml",
+        "pint",
+    ],
+    package_data = {
+        "pyndoc": ["filter.py"]
+    },
+    include_package_data=True,
+)
