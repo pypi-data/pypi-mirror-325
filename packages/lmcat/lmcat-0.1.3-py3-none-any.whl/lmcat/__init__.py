590eed90119c08393404c988f9288c18233f1e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:44 +0000

    Add package lmcat/0.1.3/lmcat-0.1.3-py3-none-any.whl

---
+++ packages/lmcat/lmcat-0.1.3-py3-none-any.whl/lmcat/__init__.py
@@ -0,0 +1,15 @@
+"""
+.. include:: ../README.md
+"""
+
+from lmcat.lmcat import main
+
+__all__ = [
+	# funcs
+	"main",
+	# submodules
+	"lmcat",
+	"file_stats",
+	"processing_pipeline",
+	"processors",
+]
