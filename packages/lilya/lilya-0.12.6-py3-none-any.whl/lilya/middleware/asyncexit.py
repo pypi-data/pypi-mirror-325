87944851d847f30610237f83144ed26836939ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:34 +0000

    Add package lilya/0.12.6/lilya-0.12.6-py3-none-any.whl

---
+++ packages/lilya/lilya-0.12.6-py3-none-any.whl/lilya/middleware/asyncexit.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from contextlib import AsyncExitStack
+
+from lilya.protocols.middleware import MiddlewareProtocol
+from lilya.types import ASGIApp, Receive, Scope, Send
+
+
+class AsyncExitStackMiddleware(MiddlewareProtocol):
+    def __init__(self, app: ASGIApp):
+        """AsyncExitStack Middleware class.
+
+        Args:
+            app: The 'next' ASGI app to call.
+        """
+        super().__init__(app)
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if not AsyncExitStack:
+            await self.app(scope, receive, send)  # pragma: no cover
+
+        exception: Exception | None = None
+        async with AsyncExitStack() as stack:
+            scope["lilya_astack"] = stack
+            try:
+                await self.app(scope, receive, send)
+            except Exception as e:
+                exception = e
+        if exception:
+            raise exception
