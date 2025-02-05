8f51da02629de7ebc52c16ebe5962847c0bf0cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:24 +0000

    Add package firebolt-sdk/1.9.1/firebolt_sdk-1.9.1-py3-none-any.whl

---
+++ packages/firebolt-sdk/firebolt_sdk-1.9.1-py3-none-any.whl/firebolt/common/base_connection.py
@@ -0,0 +1,32 @@
+from typing import Any, List, Type
+
+from firebolt.utils.exception import ConnectionClosedError
+
+ASYNC_QUERY_STATUS_RUNNING = "RUNNING"
+ASYNC_QUERY_STATUS_SUCCESSFUL = "ENDED_SUCCESSFULLY"
+ASYNC_QUERY_STATUS_REQUEST = "CALL fb_GetAsyncStatus('{token}')"
+
+
+class BaseConnection:
+    def __init__(self, cursor_type: Type) -> None:
+        self.cursor_type = cursor_type
+        self._cursors: List[Any] = []
+        self._is_closed = False
+
+    def _remove_cursor(self, cursor: Any) -> None:
+        # This way it's atomic
+        try:
+            self._cursors.remove(cursor)
+        except ValueError:
+            pass
+
+    @property
+    def closed(self) -> bool:
+        """`True` if connection is closed; `False` otherwise."""
+        return self._is_closed
+
+    def commit(self) -> None:
+        """Does nothing since Firebolt doesn't have transactions."""
+
+        if self.closed:
+            raise ConnectionClosedError("Unable to commit: Connection closed.")
