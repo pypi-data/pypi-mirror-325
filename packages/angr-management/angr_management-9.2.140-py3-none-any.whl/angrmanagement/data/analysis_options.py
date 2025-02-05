f564f9a40f140e398ca09ee0fbdc9a8332dbbb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:19:33 +0000

    Add package angr-management/9.2.140/angr_management-9.2.140-py3-none-any.whl

---
+++ packages/angr-management/angr_management-9.2.140-py3-none-any.whl/angrmanagement/data/analysis_options.py
@@ -0,0 +1,359 @@
+from __future__ import annotations
+
+import multiprocessing
+import platform
+from enum import Enum
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping, Sequence
+
+    import angr
+
+    from angrmanagement.data.instance import Instance
+
+
+def extract_first_paragraph_from_docstring(desc: str) -> str:
+    desc = desc.splitlines()
+    last_line, first_line = -1, -1
+    for idx, line in enumerate(desc):
+        if first_line < 0:
+            if len(line.strip()) > 0:
+                first_line = idx
+        else:
+            if len(line.strip()) == 0:
+                last_line = idx
+                break
+
+    if first_line >= 0:
+        if last_line < 0:
+            last_line = len(desc)
+        desc = desc[first_line:last_line]
+        num_whitespace_chars = len(desc[0]) - len(desc[0].lstrip())
+        desc = " ".join(line[num_whitespace_chars:] for line in desc)
+    else:
+        desc = ""
+
+    return desc
+
+
+class AnalysesConfiguration:
+    """
+    Configuration for a sequence of analyses.
+    """
+
+    def __init__(self, analyses: Sequence[AnalysisConfiguration], instance: Instance) -> None:
+        self.instance = instance
+        self.analyses: Sequence[AnalysisConfiguration] = analyses
+
+    def __len__(self) -> int:
+        return len(self.analyses)
+
+    def __iter__(self):
+        return iter(self.analyses)
+
+    def __getitem__(self, key: int | str):
+        if isinstance(key, int):
+            return self.analyses[key]
+        return self.by_name(key)
+
+    def by_name(self, name: str) -> AnalysisConfiguration:
+        for a in self.analyses:
+            if a.name == name:
+                return a
+        raise KeyError(name)
+
+
+class AnalysisConfiguration:
+    """
+    Configuration for an analysis.
+    """
+
+    def __init__(self, instance: Instance) -> None:
+        self.instance = instance
+        self.project: angr.Project = self.instance.project.am_obj
+        self.enabled: bool = False
+        self.name: str = ""
+        self.display_name: str = ""
+        self.description: str = "Description not available"
+        self.options: Mapping[str, AnalysisOption] = {}
+
+    def __getitem__(self, key: str):
+        return self.options[key]
+
+    def to_dict(self):
+        """
+        Return dictionary with configuration for this option.
+        """
+        o = {}
+        self.update_dict(o)
+        return o
+
+    def update_dict(self, out: Mapping[str, Any]) -> None:
+        """
+        Update dictionary `out` with configuration for this option.
+        """
+        for o in self.options.values():
+            if getattr(o, "enabled", None) is False:
+                continue
+            o.update_dict(out)
+
+
+class AnalysisOption:
+    """
+    Configurable option for an analysis.
+    """
+
+    def __init__(self, name: str, display_name: str, tooltip: str) -> None:
+        self.name: str = name
+        self.display_name: str = display_name
+        self.tooltip: str = tooltip
+
+    def update_dict(self, out: Mapping[str, Any]) -> None:
+        """
+        Update dictionary `out` with configuration for this option.
+        """
+
+
+class PrimitiveAnalysisOption(AnalysisOption):
+    """
+    Configurable option for an analysis, with a fundamental type (e.g. bool)
+    """
+
+    def __init__(self, name: str, description: str, default: Any, tooltip: str) -> None:
+        super().__init__(name, description, tooltip)
+        self.default: Any = default
+        self.value: Any = default
+
+    def update_dict(self, out: Mapping[str, Any]) -> None:
+        """
+        Update `out` dictionary with configuration for this option.
+        """
+        out[self.name] = self.value
+
+
+class BoolAnalysisOption(PrimitiveAnalysisOption):
+    """
+    Boolean option for an analysis.
+    """
+
+    def __init__(self, name: str, description: str, default: bool = False, tooltip: str = "") -> None:
+        super().__init__(name, description, default, tooltip)
+
+
+class StringAnalysisOption(PrimitiveAnalysisOption):
+    """
+    String option for an analysis.
+
+    :ivar optional: If this option is optional or mandatory.
+    :ivar enabled:  Is this option enabled by the user or not.
+    """
+
+    def __init__(
+        self, name: str, description: str, default: str = "", tooltip: str = "", optional: bool = False
+    ) -> None:
+        self.optional = optional
+        self.enabled = not self.optional
+        super().__init__(name, description, default, tooltip)
+
+
+class IntAnalysisOption(PrimitiveAnalysisOption):
+    """
+    Integer option for an analysis.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        default: int = 0,
+        tooltip: str = "",
+        minimum: int | None = None,
+        maximum: int | None = None,
+    ) -> None:
+        super().__init__(name, description, default, tooltip)
+        self.minimum_value = minimum
+        self.maximum_value = maximum
+
+
+class ChoiceAnalysisOption(PrimitiveAnalysisOption):
+    """
+    A multi-value choice.
+    """
+
+    def __init__(
+        self, name: str, description: str, choices: Mapping[Any, str], default: Any, tooltip: str = ""
+    ) -> None:
+        super().__init__(name, description, default, tooltip)
+        self.choices = choices
+
+
+class CFGForceScanMode(Enum):
+    """
+    CFG scanning mode options.
+    """
+
+    Disabled = 0
+    SmartScan = 1
+    CompleteScan = 2
+
+
+class CFGAnalysisConfiguration(AnalysisConfiguration):
+    """
+    Configuration for CFGFast analysis.
+    """
+
+    def __init__(self, instance: Instance) -> None:
+        super().__init__(instance)
+        self.name = "cfg"
+        self.display_name = "Control-Flow Graph Recovery"
+        self.description = extract_first_paragraph_from_docstring(self.project.analyses.CFGFast.__doc__)
+        self.enabled = True
+        self.options = {
+            o.name: o
+            for o in [
+                BoolAnalysisOption("resolve_indirect_jumps", "Resolve indirect jumps", True),
+                BoolAnalysisOption("data_references", "Collect cross-references and guess data types", True),
+                BoolAnalysisOption("cross_references", "Perform deep analysis on cross-references (slow)"),
+                BoolAnalysisOption("skip_unmapped_addrs", "Skip unmapped addresses", True),
+                BoolAnalysisOption("exclude_sparse_regions", "Exclude Sparse Regions", True),
+                BoolAnalysisOption(
+                    "explicit_analysis_starts", "Exclude non-explicit functions for analysis (incomplete)", False
+                ),
+                ChoiceAnalysisOption(
+                    "scanning_mode",
+                    "Scan to maximize identified code blocks",
+                    {
+                        CFGForceScanMode.Disabled: "Disabled",
+                        CFGForceScanMode.SmartScan: "Smart Scan",
+                        CFGForceScanMode.CompleteScan: "Complete Scan",
+                    },
+                    CFGForceScanMode.SmartScan,
+                ),
+                StringAnalysisOption(
+                    "regions",
+                    "Regions for analysis",
+                    tooltip="Specify ranges of regions for which to recover CFG. Example: 0x400000-0x401000. You may "
+                    "specify multiple address ranges for analysis.",
+                    optional=True,
+                ),
+                StringAnalysisOption(
+                    "function_starts",
+                    "Start at function addresses",
+                    tooltip="Specify function addresses to start recursive descent of CFG generation to speed up "
+                    "analysis. Example: 0x400000,0x401000",
+                    optional=True,
+                ),
+            ]
+        }
+
+
+class FlirtAnalysisConfiguration(AnalysisConfiguration):
+    """
+    Configuration for Flirt analysis.
+    """
+
+    def __init__(self, instance: Instance) -> None:
+        super().__init__(instance)
+        self.name = "flirt"
+        self.display_name = "Function Signature Matching"
+        self.description = self.project.analyses.Flirt.__doc__.strip()
+        self.enabled = True
+
+
+class CodeTaggingConfiguration(AnalysisConfiguration):
+    """
+    Configuration for Code Tagging.
+    """
+
+    def __init__(self, instance: Instance) -> None:
+        super().__init__(instance)
+        self.name = "code_tagging"
+        self.display_name = "Tag Functions Based on Syntactic Features"
+        self.description = "Add tags to functions based on syntactic features in assembly code and referenced strings."
+        self.enabled = False
+
+
+class VariableRecoveryConfiguration(AnalysisConfiguration):
+    """
+    Configuration for VariableRecovery analysis.
+    """
+
+    SMALL_BINARY_SIZE = 65536
+    MEDIUM_BINARY_SIZE = 400000
+
+    def __init__(self, instance: Instance) -> None:
+        super().__init__(instance)
+        self.name = "varec"
+        self.display_name = "Recover Variables on All Functions"
+        self.description = (
+            "Perform a full-project variable recovery and calling-convention recovery analysis. "
+            "Recommended for small- to medium-sized binaries. This analysis takes a long time to "
+            "finish on large binaries. You can manually perform a variable recovery and "
+            "calling-convention recovery analysis on an individual basis after loading the project."
+        )
+        self.enabled = self.get_main_obj_size() <= self.MEDIUM_BINARY_SIZE
+        self.options = {
+            o.name: o
+            for o in [
+                IntAnalysisOption(
+                    "workers",
+                    "Number of parallel workers",
+                    tooltip="0 to disable parallel analysis. Default to the number of available cores "
+                    "minus one in the local system. Automatically default to 0 for small binaries "
+                    "on all platforms, and small- to medium-sized binaries on Windows and MacOS "
+                    "(to avoid the cost of spawning new angr-management processes).",
+                    default=self.get_default_workers(),
+                    minimum=0,
+                ),
+                BoolAnalysisOption(
+                    "skip_signature_matched_functions",
+                    "Skip variable recovery for signature-matched functions",
+                    True,
+                ),
+            ]
+        }
+
+    def get_main_obj_size(self) -> int:
+        main_obj_size = 0
+        if self.project.loader.main_object is not None:
+            main_obj = self.project.loader.main_object
+            if main_obj.segments:
+                for seg in main_obj.segments:
+                    if seg.is_executable:
+                        main_obj_size += seg.memsize
+            if main_obj_size == 0 and main_obj.sections:
+                # fall back to sections
+                for sec in main_obj.sections:
+                    if sec.is_executable:
+                        main_obj_size += sec.memsize
+        return main_obj_size
+
+    def get_default_workers(self) -> int:
+        main_obj_size = self.get_main_obj_size()
+
+        default_workers = max(multiprocessing.cpu_count() - 1, 1)
+        if default_workers == 1:
+            return 0
+
+        if platform.system() in {"Windows", "Darwin"}:
+            if main_obj_size <= self.MEDIUM_BINARY_SIZE:
+                return 0
+            return default_workers
+
+        if main_obj_size <= self.SMALL_BINARY_SIZE:
+            return 0
+        return default_workers
+
+
+class APIDeobfuscationConfiguration(AnalysisConfiguration):
+    """
+    Configuration for API deobfuscation.
+    """
+
+    def __init__(self, instance: Instance) -> None:
+        super().__init__(instance)
+        self.name = "api_deobfuscation"
+        self.display_name = "Deobfuscate API usage"
+        self.description = "Search for 'obfuscated' API use and attempt to deobfuscate it."
+        self.enabled = False
---
+++ packages/angr-management/angr_management-9.2.140-py3-none-any.whl/angrmanagement/ui/widgets/qdecomp_options.py
@@ -0,0 +1,303 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from angr.analyses.decompiler import DECOMPILATION_PRESETS
+from angr.analyses.decompiler.decompilation_options import options as dec_options
+from angr.analyses.decompiler.optimization_passes import get_optimization_passes
+from angr.analyses.decompiler.peephole_optimizations import EXPR_OPTS, MULTI_STMT_OPTS, STMT_OPTS
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QComboBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
+
+from angrmanagement.config import Conf
+from angrmanagement.ui.icons import icon
+
+if TYPE_CHECKING:
+    from angrmanagement.data.instance import Instance
+    from angrmanagement.ui.views.code_view import CodeView
+
+
+class OptionType:
+    """
+    An enum to determine what the .option field of a QDecompilationOption contains
+    """
+
+    OPTION = 1
+    OPTIMIZATION_PASS = 2
+    PEEPHOLE_OPTIMIZATION = 3
+
+
+class QDecompilationOption(QTreeWidgetItem):
+    """
+    The UI entry for a single decompliation option. Get status with item.state.
+    """
+
+    def __init__(self, parent, option, type_: int, enabled: bool = True) -> None:
+        super().__init__(parent)
+        self.option = option
+        self.type = type_
+
+        # optional and may not exist
+        self._combo_box = None
+
+        if self.type in (OptionType.OPTIMIZATION_PASS, OptionType.OPTION, OptionType.PEEPHOLE_OPTIMIZATION):
+            self.setText(0, option.NAME)
+            self.setToolTip(0, option.DESCRIPTION)
+        else:
+            raise NotImplementedError(f"Unsupported option type {self.type_}.")
+
+        # should make a dropdown option
+        if (
+            hasattr(self.option, "value_type")
+            and not isinstance(self.option.value_type, bool)
+            and self.option.candidate_values
+        ):
+            self._combo_box = QComboBox()
+            self._combo_box.addItems(
+                [self.option.default_value]
+                + [c for c in self.option.candidate_values if c != self.option.default_value]
+            )
+            self._combo_box.setToolTip(f"{option.NAME}: {option.DESCRIPTION}")
+            # XXX: causes an itemChanged event for the tree
+            self._combo_box.currentTextChanged.connect(lambda x: self.setText(0, self._combo_box.currentText()))
+            self.treeWidget().setItemWidget(self, 0, self._combo_box)
+
+        # should make a boolean click option
+        else:
+            self.setFlags(self.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+            if enabled:
+                self.setCheckState(0, Qt.CheckState.Checked)
+            else:
+                self.setCheckState(0, Qt.CheckState.Unchecked)
+
+    @property
+    def state(self):
+        if self._combo_box:
+            return self._combo_box.currentText()
+        else:
+            return bool(self.checkState(0) == Qt.CheckState.Checked)
+
+
+class QDecompilationOptions(QWidget):
+    """
+    The widget for selecting values for decompilation options. Will synchronize its status back to its parent (passed
+    in parameter) code view with the Apply button is pressed.
+
+    Since some options have default values depending on the current arch and os, it is important to call
+    reload(force=True) to reset values to their defaults whenever the current project changes.
+    """
+
+    def __init__(self, code_view, instance: Instance) -> None:
+        super().__init__()
+
+        self.dirty = True
+
+        self._code_view: CodeView = code_view
+        self._instance = instance
+        self._options = None
+        self._opti_passes = None
+        self._peephole_opts = None
+
+        # widgets
+        self._preset_cmb: QComboBox
+        self._search_box: QLineEdit
+        self._treewidget: QTreeWidget
+        self._apply_btn: QPushButton
+
+        self._qoptions = []
+        self._qoptipasses = []
+        self._qpeephole_opts = []
+
+        self._init_widgets()
+
+        self.reload(True)
+
+    def reload(self, force: bool = False) -> None:
+        if force or self._options is None:
+            self._options = self.get_default_options()
+
+        if force or self._opti_passes is None:
+            if not self._instance.project.am_none:
+                self._opti_passes = self.get_all_passes()
+            else:
+                self._opti_passes = []
+
+        if force or self._peephole_opts is None:
+            self._peephole_opts = self.get_all_peephole_opts()
+
+        self._reload_options(force)
+        self._set_visibility(self._search_box.text())
+
+    def _on_item_changed(self, item, _column) -> None:
+        if getattr(item.option, "clears_cache", True):
+            self.dirty = True
+
+    def _on_apply_pressed(self) -> None:
+        if self.dirty:
+            self.dirty = False
+            # clear the cached version
+            self._code_view.decompile(reset_cache=True)
+        else:
+            if not self._code_view.codegen.am_none:
+                self._code_view.codegen.reapply_options(self.option_and_values)
+                self._code_view.codegen.am_event()
+
+    @property
+    def selected_passes(self):
+        selected = []
+        for item in self._qoptipasses:
+            if item.state:
+                selected.append(item.option)
+        return selected
+
+    @property
+    def selected_peephole_opts(self):
+        selected = []
+        for item in self._qpeephole_opts:
+            if item.state:
+                selected.append(item.option)
+        return selected
+
+    @property
+    def option_and_values(self):
+        return [(item.option, item.state) for item in self._qoptions]
+
+    def get_default_options(self):  # pylint: disable=no-self-use
+        return dec_options
+
+    def get_default_passes(self):
+        if self._instance is None or self._instance.project.am_none:
+            return []
+        default_preset = DECOMPILATION_PRESETS[self._preset_cmb.currentText()]
+        return default_preset.get_optimization_passes(
+            self._instance.project.arch, self._instance.project.simos.name
+        ) + [x for x, de in self._code_view.workspace.plugins.optimization_passes() if de]
+
+    def get_all_passes(self):
+        if self._instance is None or self._instance.project.am_none:
+            return []
+        return get_optimization_passes(self._instance.project.arch, self._instance.project.simos.name) + [
+            x for x, _ in self._code_view.workspace.plugins.optimization_passes()
+        ]
+
+    def get_default_peephole_opts(self):  # pylint: disable=no-self-use
+        return MULTI_STMT_OPTS + STMT_OPTS + EXPR_OPTS
+
+    def get_all_peephole_opts(self):  # pylint: disable=no-self-use
+        return MULTI_STMT_OPTS + STMT_OPTS + EXPR_OPTS
+
+    def _init_widgets(self) -> None:
+        preset_lyt = QHBoxLayout()
+        preset_lyt.setContentsMargins(0, 0, 0, 0)
+        preset_lyt.setSpacing(3)
+
+        preset_lyt.addWidget(QLabel("Preset:"))
+        self._preset_cmb = QComboBox()
+        presets = sorted([n for n in DECOMPILATION_PRESETS if n != "default"])
+        self._preset_cmb.addItems(presets)
+        self._preset_cmb.setCurrentIndex(
+            next(i for i, n in enumerate(presets) if DECOMPILATION_PRESETS[n] is DECOMPILATION_PRESETS["default"])
+        )
+        self._preset_cmb.activated.connect(lambda: self.reload(force=True))
+        preset_lyt.addWidget(self._preset_cmb, 1)
+
+        # search box
+        self._search_box = QLineEdit()
+        self._search_box.setClearButtonEnabled(True)
+        self._search_box.addAction(
+            icon("search", color=Conf.palette_placeholdertext), QLineEdit.ActionPosition.LeadingPosition
+        )
+        self._search_box.setPlaceholderText("Filter by name...")
+        self._search_box.textChanged.connect(self._on_search_box_text_changed)
+
+        # tree view
+        self._treewidget = QTreeWidget()
+        self._treewidget.setHeaderHidden(True)
+        self._treewidget.itemChanged.connect(self._on_item_changed)
+
+        # refresh button
+        self._apply_btn = QPushButton("Apply")
+        self._apply_btn.clicked.connect(self._on_apply_pressed)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(3, 3, 3, 3)
+        layout.setSpacing(3)
+        layout.addLayout(preset_lyt)
+        layout.addWidget(self._search_box)
+        layout.addWidget(self._treewidget)
+        layout.addWidget(self._apply_btn)
+
+        self.setLayout(layout)
+
+    def _reload_options(self, reset_values: bool = False) -> None:
+        vals_options = dict(self.option_and_values)
+        vals_peephole = self.selected_peephole_opts
+        vals_passes = self.selected_passes
+
+        self._treewidget.clear()
+        self._qoptions.clear()
+        self._qoptipasses.clear()
+        self._qpeephole_opts.clear()
+
+        categories = {}
+
+        # populate the tree widget with new options
+        for option in sorted(self._options, key=lambda x: x.NAME):
+            if option.category in categories:
+                category = categories[option.category]
+            else:
+                category = QTreeWidgetItem(self._treewidget, [option.category])
+                categories[option.category] = category
+
+            enabled = option.default_value if reset_values else vals_options.get(option, option.default_value)
+            w = QDecompilationOption(category, option, OptionType.OPTION, enabled=enabled)
+            self._qoptions.append(w)
+
+        passes_category = QTreeWidgetItem(self._treewidget, ["Optimization Passes"])
+        categories["passes"] = passes_category
+
+        default_passes = set(self.get_default_passes())
+        for pass_ in self._opti_passes:
+            enabled = pass_ in default_passes if reset_values else pass_ in vals_passes
+            w = QDecompilationOption(passes_category, pass_, OptionType.OPTIMIZATION_PASS, enabled=enabled)
+            self._qoptipasses.append(w)
+
+        po_category = QTreeWidgetItem(self._treewidget, ["Peephole Optimizations"])
+        categories["peephole_opts"] = po_category
+
+        default_peephole_opts = self.get_default_peephole_opts()
+        for opt_ in self._peephole_opts:
+            enabled = opt_ in default_peephole_opts if reset_values else opt_ in vals_peephole
+            w = QDecompilationOption(po_category, opt_, OptionType.PEEPHOLE_OPTIMIZATION, enabled=enabled)
+            self._qpeephole_opts.append(w)
+
+        # expand all
+        self._treewidget.expandAll()
+
+    def _set_visibility(self, filter_by: str | None = None) -> None:
+        for w in self._qoptions:
+            w.setHidden(
+                bool(filter_by) and not (filter_by in w.option.NAME.lower() or filter_by in w.option.category.lower())
+            )
+        for w in self._qoptipasses:
+            w.setHidden(
+                bool(filter_by) and not (filter_by in w.option.__name__.lower() or filter_by in w.option.NAME.lower())
+            )
+        for w in self._qpeephole_opts:
+            w.setHidden(
+                bool(filter_by)
+                and not (filter_by in w.option.NAME.lower() or filter_by in w.option.DESCRIPTION.lower())
+            )
+
+    def _on_search_box_text_changed(self, text: str) -> None:
+        self._set_visibility(filter_by=text)
