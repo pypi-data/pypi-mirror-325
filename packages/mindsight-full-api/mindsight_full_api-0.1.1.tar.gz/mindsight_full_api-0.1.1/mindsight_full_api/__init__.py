3a4fec595a8811534aa57f15f6d256063d97027f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:11:39 +0000

    Add package mindsight-full-api/0.1.1/mindsight_full_api-0.1.1.tar.gz

---
+++ packages/mindsight-full-api/mindsight_full_api-0.1.1.tar.gz/mindsight_full_api-0.1.1/mindsight_full_api/__init__.py
@@ -0,0 +1,19 @@
+"""This module take all methods of scripts.__init__.py"""
+
+from mindsight_full_api.scripts import (
+    Absence,
+    Bonus,
+    CostCenters,
+    ExtraHours,
+    People,
+    RaiseType,
+    Salary,
+    Trainings,
+    VaraibleSalary,
+    User,
+)
+
+__doc__ = """
+**mindsight_full_api** is a python package to providing
+methods to use Full Analytics rest api.
+"""
---
+++ packages/mindsight-full-api/mindsight_full_api-0.1.1.tar.gz/mindsight_full_api-0.1.1/mindsight_full_api/scripts/__init__.py
@@ -0,0 +1,12 @@
+"""This module import all methods scripts"""
+from mindsight_full_api.scripts.absences import Absence
+from mindsight_full_api.scripts.assessments import Assessments
+from mindsight_full_api.scripts.bonus import Bonus
+from mindsight_full_api.scripts.cost_center import CostCenters
+from mindsight_full_api.scripts.extra_hours import ExtraHours
+from mindsight_full_api.scripts.raise_type import RaiseType
+from mindsight_full_api.scripts.salaries import Salary
+from mindsight_full_api.scripts.trainings import Trainings
+from mindsight_full_api.scripts.users import User
+from mindsight_full_api.scripts.variable_salaries import VaraibleSalary
+from mindsight_full_api.scripts.people import People
