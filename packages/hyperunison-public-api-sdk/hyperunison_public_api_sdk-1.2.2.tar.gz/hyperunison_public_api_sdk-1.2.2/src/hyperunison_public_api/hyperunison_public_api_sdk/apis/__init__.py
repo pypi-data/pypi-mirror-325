6f401c55f8bd7919aee484b07737e16e20ec6780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:05:55 +0000

    Add package hyperunison-public-api-sdk/1.2.2/hyperunison_public_api_sdk-1.2.2.tar.gz

---
+++ packages/hyperunison-public-api-sdk/hyperunison_public_api_sdk-1.2.2.tar.gz/hyperunison_public_api_sdk-1.2.2/src/hyperunison_public_api/hyperunison_public_api_sdk/apis/__init__.py
@@ -0,0 +1,18 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from hyperunison_public_api_sdk.api.cohort_api import CohortApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from hyperunison_public_api_sdk.api.cohort_api import CohortApi
+from hyperunison_public_api_sdk.api.pipeline_api import PipelineApi
