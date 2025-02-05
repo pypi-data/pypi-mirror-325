414de45197b8f32e85e6a6517155a7a106ef085d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:18:58 +0000

    Add package maptasker/7.0.0/maptasker-7.0.0-py3-none-any.whl

---
+++ packages/maptasker/maptasker-7.0.0-py3-none-any.whl/maptasker/src/condition.py
@@ -0,0 +1,272 @@
+"""Process profile condition: time, date, state, event, location, app"""
+
+#! /usr/bin/env python3
+#                                                                                      #
+# condition: Process profile condition: time, date, state, event, location, app        #
+#                                                                                      #
+import defusedxml.ElementTree
+
+import maptasker.src.actiond as process_action_codes
+import maptasker.src.actione as action_evaluate
+
+# action_codes: Master dictionary of Task action and Profile condition codes
+from maptasker.src.actionc import action_codes
+from maptasker.src.debug import not_in_dictionary
+from maptasker.src.primitem import PrimeItems
+from maptasker.src.sysconst import logger
+from maptasker.src.taskflag import get_priority
+
+
+# Profile condition: Time
+def condition_time(the_item: defusedxml.ElementTree, the_output_condition: str) -> str:
+    """
+    Handle the "Time" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formated
+        :return: the formatted condition's output string
+    """
+
+    (
+        to_hour,
+        to_minute,
+        from_hour,
+        from_minute,
+        rep,
+        rep_type,
+        from_variable,
+        to_variable,
+    ) = (
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    )
+    child = None
+    for child in the_item:
+        match child.tag:
+            case "fh":
+                from_hour = child.text
+            case "fm":
+                from_minute = child.text
+            case "th":
+                to_hour = child.text
+            case "tm":
+                to_minute = child.text
+            case "rep":
+                rep_type = " minutes " if child.text == "2" else " hours "
+            case "repval":
+                rep = f" repeat every {child.text}{rep_type}"
+            case "fromvar":
+                from_variable = child.text
+            case "tovar":
+                to_variable = child.text
+            case _:
+                child.text = "Rutroh"
+
+    if from_hour or from_minute:
+        the_output_condition = f"{the_output_condition}Time: from {from_hour}:{from_minute.zfill(2)}{rep}"
+
+    if to_hour or to_minute:
+        the_output_condition = f"{the_output_condition} to {to_hour}:{to_minute.zfill(2)}"
+    elif from_variable or to_variable:
+        the_output_condition = f"{the_output_condition}Time: from {from_variable} to {to_variable} {rep}"
+    else:
+        the_output_condition = f"{the_output_condition}{child.text} not yet mapped!"
+        not_in_dictionary("Condition Time", child.text)
+    return the_output_condition
+
+
+# Profile condition: Day
+def condition_day(the_item: defusedxml.ElementTree.XML, the_output_condition: str) -> str:
+    """
+    Handle the "Day" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formated
+        :return: the formatted condition's output string
+    """
+    weekdays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
+    the_days_of_week = ""
+    days_of_month = ""
+    the_months = ""
+    for child in the_item:
+        if "wday" in child.tag:
+            the_days_of_week = f"{the_days_of_week}{weekdays[int(child.text) - 1]} "
+        elif "mday" in child.tag:
+            days_of_month = f"{days_of_month}{child.text} "
+        elif "mnth" in child.tag:
+            the_months = f"{the_months}{months[int(child.text)]} "
+        else:
+            break
+    if the_days_of_week:
+        the_output_condition = f"{the_output_condition}Days of Week: {the_days_of_week}"
+    if days_of_month:
+        the_output_condition = f"{the_output_condition}Days of Month: {days_of_month} "
+    if the_months:
+        the_output_condition = f"{the_output_condition}Months: {the_months} "
+    return the_output_condition
+
+
+# Profile condition: State
+def condition_state(the_item: defusedxml.ElementTree.XMLParse, the_output_condition: str) -> str:
+    """
+    Handle the "State" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formated
+        :return: the formatted condition's output string
+    """
+    for child in the_item:
+        if child.tag == "code":
+            logger.debug(f"condition_state:{child.text}")
+            state_code = f"{child.text}s" if "s" not in child.text else child.text
+            if state_code not in action_codes:
+                process_action_codes.build_action_codes(
+                    child,
+                    the_item,
+                )  # Add it to our action dictionary
+            # child.text = state_code
+            state = action_evaluate.get_action_code(
+                child,
+                the_item,
+                False,
+                "s",
+            )
+
+            # Add this State to any preceding State
+            the_output_condition = f"{the_output_condition}State: {state}"
+            invert = the_item.find("pin")
+            if invert is not None and invert.text == "true":
+                the_output_condition = f"{the_output_condition} <em>[inverted]</em>"
+            if PrimeItems.program_arguments["debug"]:
+                the_output_condition = f"{the_output_condition} (code:{child.text})"
+        return the_output_condition
+    return ""
+
+
+# Profile condition: Event
+def condition_event(the_item: defusedxml.ElementTree.XMLParse, the_output_condition: str) -> str:
+    """
+    Handle the "Event" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formatted
+        :return: the formatted condition's output string
+    """
+    the_event_code = the_item.find("code")
+
+    # Determine what the Event code is and return the actual Event text
+    logger.debug(f"code:{the_event_code.text}")
+    event_code = f"{the_event_code.text}e" if "e" not in the_event_code.text else the_event_code.text
+    if event_code not in action_codes:
+        # Build new (template_ action code if not in our dictionary of codes yet
+        process_action_codes.build_action_codes(the_event_code, the_item)  # Add it to our action dictionary
+    # the_event_code.text = event_code
+    event = action_evaluate.get_action_code(
+        the_event_code,
+        the_item,
+        False,
+        "e",
+    )
+    # Get the event priority
+    event = f"{event}{get_priority(the_item, True)}"
+
+    # Format the Event text
+    the_output_condition = f"{the_output_condition}Event: {event}"
+    if PrimeItems.program_arguments["debug"]:  # if program_args['debug'] then add the code
+        the_output_condition = f"{the_output_condition} (code:{the_event_code.text})"
+    return the_output_condition
+
+
+# Profile condition: App (application)
+def condition_app(item: defusedxml.ElementTree.XMLParse, condition: str) -> str:
+    """
+    Handle the "App" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formatted
+        :return: the formatted condition's output string
+    """
+    the_apps = ""
+    for apps in item:
+        if "label" in apps.tag:
+            the_apps = f"{the_apps} {apps.text}"
+    return f"{condition}Application:{the_apps}"
+
+
+# Profile condition: Loc (location)
+def condition_loc(item: defusedxml.ElementTree.XMLParse, condition: str) -> str:
+    """
+    Handle the "Location" condition
+        :param the_item: the xml element with the Condition
+        :param the_output_condition: text into which the condition output is to
+            be formatted
+        :return: the formatted condition's output string
+    """
+    lat = item.find("lat").text
+    lon = item.find("long").text
+    rad = item.find("rad").text
+    if lat:
+        return f"{condition}Location with latitude {lat} longitude {lon} radius {rad}"
+    return ""
+
+
+# Given a Profile, return its list of conditions
+def parse_profile_condition(the_profile: defusedxml.ElementTree) -> str:
+    """
+    Given a Profile, return its list of conditions
+        :param the_profile: the xml element pointing to <Profile object
+        :return: the formatted condition's output string
+    """
+    # Map of our functions to call based on the tag on the Profile item
+    function_map = {
+        "Time": condition_time,
+        "Day": condition_day,
+        "State": condition_state,
+        "Event": condition_event,
+        "App": condition_app,
+        "Loc": condition_loc,
+    }
+    ignore_items = ["cdate", "edate", "flags", "id", "ProfileVariable"]
+    condition = ""  # Assume no condition
+
+    # Go through Profile'x sub-XML looking for conditions
+    for item in the_profile:
+        if item.tag in ignore_items or "mid" in item.tag:  # Bypass junk we don't care about
+            continue
+        if condition:  # If we already have a condition, add 'and' (italicized)
+            condition = f"{condition} <em>AND</em> "
+
+        # Find out what the condition is and handle it.
+        if item.tag in function_map:
+            condition = function_map[item.tag](item, condition)
+
+    return "" if condition == "" else f"{condition}"
---
+++ packages/maptasker/maptasker-7.0.0-py3-none-any.whl/maptasker/src/xmldata.py
@@ -0,0 +1,215 @@
+"""Module containing action runner logic."""
+
+#! /usr/bin/env python3
+
+#                                                                                      #
+# xmldata: deal with the xml data                                                      #
+#                                                                                      #
+import os
+import shutil
+
+import defusedxml.ElementTree
+
+from maptasker.src.sysconst import clean
+
+
+# See if the xml tag is one of the predefined types and return result
+def tag_in_type(tag: str, flag: bool) -> bool:
+    """
+    Evaluate the xml tag to see if it is one of our predefined types
+
+    Parameters: the tag to evaluate, and whether this is a Scene or not (which
+            determines which list of types to look for)
+
+    Returns: True if tag found, False otherwise
+    """
+    scene_task_element_types = [
+        "ListElementItem",
+        "ListElement",
+        "TextElement",
+        "ImageElement",
+        "ButtonElement",
+        "OvalElement",
+        "EditTextElement",
+        "RectElement",
+        "WebElement",
+        "CheckBoxElement",
+        "DoodleElement",
+        "PickerElement",
+        "SceneElement",
+        "SliderElement",
+        "SpinnerElement",
+        "SwitchElement",
+        "ToggleElement",
+        "VideoElement",
+        "PropertiesElement",  # this element doesn't contain anything of value/import
+    ]
+    scene_task_click_types = [
+        "checkchangeTask",
+        "clickTask",
+        "focuschangeTask",
+        "itemselectedTask",
+        "keyTask",
+        "linkclickTask",
+        "longclickTask",
+        "mapclickTask",
+        "maplongclickTask",
+        "pageloadedTask",
+        "strokeTask",
+        "valueselectedTask",
+        "videoTask",
+        "itemclickTask",
+        "itemlongclickTask",
+    ]
+    # Return a boolean: True if tag found in the appropriate list, False otherwise
+    return (flag and tag in scene_task_element_types) or (not flag and tag in scene_task_click_types)  # Boolean
+
+
+# We have an integer.  Evaluaate it's value based oon the code's evaluation parameters.
+def extract_integer(action: defusedxml.ElementTree, arg: str, argeval: str) -> str:
+    # Don't move import to avoid cirtcular import
+    """
+    Extract an integer value from an XML action element
+    Args:
+        action: {XML element}: The XML action element to search
+        arg: {str}: The name of the argument to search for
+        argeval: {str}: The evaluation to perform on the integer
+    Returns:
+        {str}: The result of the integer evaluation
+    {Processes the XML action element to find the integer value associated with the given argument name.
+    If found, performs the specified evaluation on the integer and returns the result. Returns an empty string if no integer is found.}
+    - Searches child elements of the action for an 'Int' element matching the given argument name
+    - Extracts the integer value or variable name if found
+    - Performs the specified evaluation on the integer/variable, joining results into a string if a list
+    - Returns the result of the evaluation or an empty string if no integer was found
+    """
+    from maptasker.src.action import drop_trailing_comma, process_xml_list
+
+    the_int_value = ""
+    result = []
+    # Find the arg we are looking for.
+    for child in action:
+        if child.tag == "Int":
+            the_arg = child.attrib.get("sr")
+            # Is this our arg?
+            if arg == the_arg:
+                # We have the arg we are looking for.
+                if child.attrib.get("val") is not None:
+                    the_int_value = child.attrib.get("val")  # There a numeric value as a string?
+                elif child.find("var") is not None:  # There is a variable name?
+                    the_int_value = child.find("var").text
+                if the_int_value:  # If we have an integer or variable name
+                    # List of options for this Int?
+                    if isinstance(argeval, list):
+                        process_xml_list(
+                            [argeval],
+                            0,
+                            the_int_value,
+                            result,
+                            [arg],
+                        )
+                        result = " ".join(result)
+                    else:  # Not a list
+                        result = argeval + the_int_value  # Just grab the integer value
+                        break
+
+                # No integer value or variable name found
+                else:
+                    result = ""
+
+    # If we have a result, get rid of the trailing comma if there is one.
+    if result:
+        return drop_trailing_comma([result])[0]
+
+    return ""  # No Integer value or variable found...return empty
+
+
+# Extracts and returns the text from the given argument as a string.
+def extract_string(action: defusedxml.ElementTree, arg: str, argeval: str) -> str:
+    """
+    Extracts a string from an XML action element.
+    Args:
+        action: XML element to search in one line
+        arg: Name of string argument to search for in one line
+        argeval: Prefix to add to matched string in one line
+    Returns:
+        str: Extracted string with prefix or empty string in one line
+    Processes the XML action element:
+    - Loops through child elements looking for matching "Str" tag
+    - Checks if tag attribute matches arg
+    - Appends child text to list with argeval prefix
+    - Returns first item after processing or empty string
+    """
+    from maptasker.src.action import drop_trailing_comma
+
+    match_results = []
+    all_strings = action.findall("Str")
+    for child in all_strings:
+        the_arg = child.attrib.get("sr")
+        if arg == the_arg:
+            if child.text is not None:
+                # Catch the situation in which a newline has been entered for the value (carriage return)
+                if child.text == "\n":
+                    match_results.append(f"{argeval}(carriage return)")
+                else:
+                    match_results.append(f"{argeval}{child.text}")
+            else:
+                match_results.append("")
+            break  # We have what we want.  Go to next child
+    if match_results:
+        return drop_trailing_comma(match_results)[0]
+    return ""
+
+
+# Given a string, remove all HTML (anything between < >) tags from it
+def remove_html_tags(text: str, replacement: str) -> str:
+    """
+    Remove html tags from a string
+    :param text: text from which HTML is to be removed
+    :param replacement: text to replace HTML with, if any
+    :return: the text with HTML removed
+    """
+    import re
+
+    return re.sub(clean, replacement, text)
+
+
+# Append file1 to file2
+def append_files(file1_path: str, file2_path: str) -> None:
+    """Appends the contents of file1 to file2.
+    Parameters:
+        - file1_path (str): Path to file1.
+        - file2_path (str): Path to file2.
+    Returns:
+        - None: No return value.
+    Processing Logic:
+        - Open file1 in read mode.
+        - Open file2 in append mode.
+        - Copy contents of file1 to file2."""
+    with open(file1_path) as file1, open(file2_path, "a") as file2:
+        shutil.copyfileobj(file1, file2)
+
+
+# The XML file hs incorrect encoding.  Let's read it in and rewrite it correctly.
+def rewrite_xml(file_to_parse: str) -> None:
+    """Rewrite XML file with UTF-8 encoding.
+    Parameters:
+        - file_to_parse (str): Name of the file to be parsed.
+    Returns:
+        - None: No return value.
+    Processing Logic:
+        - Create new file with UTF-8 encoding.
+        - Append, rename, and remove files.
+        - Remove temporary file."""
+    utf_xml = '<?xml version = "1.0" encoding = "UTF-8" standalone = "no" ?>\n'
+
+    # Create the XML file with the encoding we want
+    with open(".maptasker_tmp.xml", "w") as new_file:
+        new_file.write(utf_xml)
+        new_file.close()
+
+    # Append, rename and remove.
+    append_files(file_to_parse, ".maptasker_tmp.xml")
+    os.remove(file_to_parse)
+    os.rename(".maptasker_tmp.xml", file_to_parse)
+    os.remove(".maptasker_tmp.xml")
