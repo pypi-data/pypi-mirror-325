201104de84b130997b0b317c0786600c670f119a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:15 +0000

    Add package lastmile/0.7.0/lastmile-0.7.0-py3-none-any.whl

---
+++ packages/lastmile/lastmile-0.7.0-py3-none-any.whl/lastmile/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .datasets import (
+    DatasetsResource,
+    AsyncDatasetsResource,
+    DatasetsResourceWithRawResponse,
+    AsyncDatasetsResourceWithRawResponse,
+    DatasetsResourceWithStreamingResponse,
+    AsyncDatasetsResourceWithStreamingResponse,
+)
+from .projects import (
+    ProjectsResource,
+    AsyncProjectsResource,
+    ProjectsResourceWithRawResponse,
+    AsyncProjectsResourceWithRawResponse,
+    ProjectsResourceWithStreamingResponse,
+    AsyncProjectsResourceWithStreamingResponse,
+)
+from .evaluation import (
+    EvaluationResource,
+    AsyncEvaluationResource,
+    EvaluationResourceWithRawResponse,
+    AsyncEvaluationResourceWithRawResponse,
+    EvaluationResourceWithStreamingResponse,
+    AsyncEvaluationResourceWithStreamingResponse,
+)
+from .fine_tune_jobs import (
+    FineTuneJobsResource,
+    AsyncFineTuneJobsResource,
+    FineTuneJobsResourceWithRawResponse,
+    AsyncFineTuneJobsResourceWithRawResponse,
+    FineTuneJobsResourceWithStreamingResponse,
+    AsyncFineTuneJobsResourceWithStreamingResponse,
+)
+from .label_dataset_jobs import (
+    LabelDatasetJobsResource,
+    AsyncLabelDatasetJobsResource,
+    LabelDatasetJobsResourceWithRawResponse,
+    AsyncLabelDatasetJobsResourceWithRawResponse,
+    LabelDatasetJobsResourceWithStreamingResponse,
+    AsyncLabelDatasetJobsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ProjectsResource",
+    "AsyncProjectsResource",
+    "ProjectsResourceWithRawResponse",
+    "AsyncProjectsResourceWithRawResponse",
+    "ProjectsResourceWithStreamingResponse",
+    "AsyncProjectsResourceWithStreamingResponse",
+    "DatasetsResource",
+    "AsyncDatasetsResource",
+    "DatasetsResourceWithRawResponse",
+    "AsyncDatasetsResourceWithRawResponse",
+    "DatasetsResourceWithStreamingResponse",
+    "AsyncDatasetsResourceWithStreamingResponse",
+    "EvaluationResource",
+    "AsyncEvaluationResource",
+    "EvaluationResourceWithRawResponse",
+    "AsyncEvaluationResourceWithRawResponse",
+    "EvaluationResourceWithStreamingResponse",
+    "AsyncEvaluationResourceWithStreamingResponse",
+    "FineTuneJobsResource",
+    "AsyncFineTuneJobsResource",
+    "FineTuneJobsResourceWithRawResponse",
+    "AsyncFineTuneJobsResourceWithRawResponse",
+    "FineTuneJobsResourceWithStreamingResponse",
+    "AsyncFineTuneJobsResourceWithStreamingResponse",
+    "LabelDatasetJobsResource",
+    "AsyncLabelDatasetJobsResource",
+    "LabelDatasetJobsResourceWithRawResponse",
+    "AsyncLabelDatasetJobsResourceWithRawResponse",
+    "LabelDatasetJobsResourceWithStreamingResponse",
+    "AsyncLabelDatasetJobsResourceWithStreamingResponse",
+]
