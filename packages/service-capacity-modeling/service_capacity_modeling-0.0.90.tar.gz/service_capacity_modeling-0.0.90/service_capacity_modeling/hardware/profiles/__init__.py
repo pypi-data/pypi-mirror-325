a49b8110dcc706a1eb4598feb015e009ad3572df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:14 +0000

    Add package service-capacity-modeling/0.0.90/service_capacity_modeling-0.0.90.tar.gz

---
+++ packages/service-capacity-modeling/service_capacity_modeling-0.0.90.tar.gz/service_capacity_modeling-0.0.90/service_capacity_modeling/hardware/profiles/__init__.py
@@ -0,0 +1,26 @@
+try:
+    from importlib import resources
+    from importlib import import_module
+except ImportError:
+    import importlib_resources as resources  # type: ignore[no-redef]
+
+    import_module = __import__  # type: ignore[assignment]
+
+from service_capacity_modeling.hardware import load_hardware_from_disk
+from pathlib import Path
+
+current_module = import_module(__name__)
+common_profiles = {}
+
+with resources.path(  # pylint: disable=deprecated-method
+    current_module, "profiles.txt"
+) as shape_file:
+    shapes = Path(shape_file.parent, "shapes")
+    for fd in shapes.glob("**/*.json"):
+        shape = fd.stem
+
+        print(f"Loading shape={shape} from {Path(shapes, shape)}")
+        for pricing in Path(shapes.parent, "pricing", shape).glob("**/*.json"):
+            print(f"Loading {pricing}")
+            ghw = load_hardware_from_disk(price_path=pricing, shape_path=fd)
+            common_profiles[f"{shape}-{pricing.stem}"] = ghw
---
+++ packages/service-capacity-modeling/service_capacity_modeling-0.0.90.tar.gz/service_capacity_modeling-0.0.90/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+setuptools.setup(
+    name="service-capacity-modeling",
+    author="Joseph Lynch",
+    author_email="josephl@netflix.com",
+    versioning="distance",
+    version="0.0.90",
+    description="Contains utilities for modeling database capacity on a cloud",
+    packages=setuptools.find_packages(exclude=("tests*", "notebooks*")),
+    install_requires=[
+        "pydantic<2.0",
+        "scipy",
+        "numpy",
+        'importlib_resources; python_version < "3.7"',
+        "isodate",
+    ],
+    extras_require={
+        "aws": ["boto3"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": []},
+    include_package_data=True,
+    package_data={
+        "": [
+            "hardware/profiles/profiles.txt",
+            "hardware/profiles/shapes/*.json",
+            "hardware/profiles/pricing/**/*.json",
+        ]
+    },
+)
