52918786af4b6862bd8c4056fccf2dc3fbe5a8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:46 +0000

    Add package feegaffe/1.1.4/feegaffe-1.1.4.tar.gz

---
+++ packages/feegaffe/feegaffe-1.1.4.tar.gz/feegaffe-1.1.4/feegaffe/__init__.py
@@ -0,0 +1,3 @@
+from feegaffe import hello, get_country_from_ip  # Importe les fonctions hello et get_country_from_ip
+
+__all__ = ["hello", "get_country"]  # Exporte uniquement ces deux fonctions lors de l'import
