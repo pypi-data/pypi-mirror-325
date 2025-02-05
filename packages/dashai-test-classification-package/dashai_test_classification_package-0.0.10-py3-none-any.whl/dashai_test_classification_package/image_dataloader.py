5f73eeb297a5e7f77c2810595baee54b2646f219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:12:19 +0000

    Add package dashai-test-classification-package/0.0.10/dashai_test_classification_package-0.0.10-py3-none-any.whl

---
+++ packages/dashai-test-classification-package/dashai_test_classification_package-0.0.10-py3-none-any.whl/dashai_test_classification_package/image_dataloader.py
@@ -0,0 +1,84 @@
+"""DashAI Image Dataloader."""
+
+from typing import Any, Dict, Union
+
+from beartype import beartype
+from datasets import DatasetDict, load_dataset
+from starlette.datastructures import Headers, UploadFile
+
+from DashAI.back.core.schema_fields import none_type, schema_field, string_field
+from DashAI.back.core.schema_fields.base_schema import BaseSchema
+from DashAI.back.dataloaders.classes.dataloader import (
+    BaseDataLoader,
+    DataloaderMoreOptionsSchema,
+    DatasetSplitsSchema,
+)
+
+
+class ImageDataloaderSchema(BaseSchema):
+    name: schema_field(
+        none_type(string_field()),
+        "",
+        (
+            "Custom name to register your dataset. If no name is specified, "
+            "the name of the uploaded file will be used."
+        ),
+    )  # type: ignore
+    splits: DatasetSplitsSchema
+    more_options: DataloaderMoreOptionsSchema
+
+
+class ImageDataLoader(BaseDataLoader):
+    """Data loader for data from image files."""
+
+    COMPATIBLE_COMPONENTS = ["ImageClassificationTask"]
+    SCHEMA = ImageDataloaderSchema
+
+    @beartype
+    def load_data(
+        self,
+        filepath_or_buffer: Union[UploadFile, str],
+        temp_path: str,
+        params: Dict[str, Any],
+    ) -> DatasetDict:
+        """Load an image dataset.
+
+        Parameters
+        ----------
+        filepath_or_buffer : Union[UploadFile, str], optional
+            An URL where the dataset is located or a FastAPI/Uvicorn uploaded file
+            object.
+        temp_path : str
+            The temporary path where the files will be extracted and then uploaded.
+        params : Dict[str, Any]
+            Dict with the dataloader parameters. The options are:
+            - `separator` (str): The character that delimits the CSV data.
+
+        Returns
+        -------
+        DatasetDict
+            A HuggingFace's Dataset with the loaded data.
+        """
+        if isinstance(filepath_or_buffer, str):
+            dataset = load_dataset("imagefolder", data_files=filepath_or_buffer)
+        elif isinstance(filepath_or_buffer, UploadFile):
+            if filepath_or_buffer.content_type == "application/x-zip-compressed":
+                filepath_or_buffer = UploadFile(
+                    filename=filepath_or_buffer.filename,
+                    file=filepath_or_buffer.file,
+                    headers=Headers({"Content-Type": "application/zip"}),
+                )
+            if filepath_or_buffer.content_type == "application/zip":
+                extracted_files_path = self.extract_files(temp_path, filepath_or_buffer)
+                dataset = load_dataset(
+                    "imagefolder",
+                    data_dir=extracted_files_path,
+                )
+            else:
+                raise Exception(
+                    "The image dataloader requires the input file to be a zip file. "
+                    f"The following content type was delivered: "
+                    f"{filepath_or_buffer.content_type}"
+                )
+
+        return dataset
