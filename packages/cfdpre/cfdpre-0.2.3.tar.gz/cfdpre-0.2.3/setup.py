47819b4a67c9cf1d5ba83785f1a146673a8c03ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:44 +0000

    Add package cfdpre/0.2.3/cfdpre-0.2.3.tar.gz

---
+++ packages/cfdpre/cfdpre-0.2.3.tar.gz/cfdpre-0.2.3/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+
+setup(
+    name='cfdpre',
+    version='0.2.3',
+    description='CFD PreProcessing Library',
+    author='Pushkar Sheth',
+    author_email='siglyserdev@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'CoolProp'
+
+        
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
