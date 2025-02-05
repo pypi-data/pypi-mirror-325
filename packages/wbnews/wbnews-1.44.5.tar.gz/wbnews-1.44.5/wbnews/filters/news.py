a1e8aa4d7d2937bc90bae95d4e1d7bcb3719e270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:40 +0000

    Add package wbnews/1.44.5/wbnews-1.44.5.tar.gz

---
+++ packages/wbnews/wbnews-1.44.5.tar.gz/wbnews-1.44.5/wbnews/filters/news.py
@@ -0,0 +1,8 @@
+from wbcore import filters as wb_filters
+from wbnews.models import News
+
+
+class NewsFilterSet(wb_filters.FilterSet):
+    class Meta:
+        model = News
+        fields = {"title": ["icontains"], "datetime": ["lte", "gte"], "source": ["exact"], "language": ["exact"]}
