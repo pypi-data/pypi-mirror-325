da99a6ff46e01fdc417483613ffafa66bf4722a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:18:40 +0000

    Add package a9x-webstatistics/1.4.49/a9x_webstatistics-1.4.49.tar.gz

---
+++ packages/a9x-webstatistics/a9x_webstatistics-1.4.49.tar.gz/a9x_webstatistics-1.4.49/src/a9x_webstatistics/gencockpitsubV0001/__init__.py
@@ -0,0 +1,6 @@
+from .cockpitlanguages import cockpitLanguages
+from .externalfriends import externalFriends
+from .genheader import genHeader
+from .performance import performance
+from .navchart import navchart
+from .navchartsankey import navchartsankey
