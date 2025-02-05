7258ad3cb38f9ebc1c801bb6ed8d729ccbf0d2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:19:02 +0000

    Add package langchain-dartmouth/0.2.13/langchain_dartmouth-0.2.13-py3-none-any.whl

---
+++ packages/langchain-dartmouth/langchain_dartmouth-0.2.13-py3-none-any.whl/langchain_dartmouth/definitions.py
@@ -0,0 +1,5 @@
+EMBEDDINGS_BASE_URL = "https://ai-api.dartmouth.edu/tei/"
+RERANK_BASE_URL = "https://ai-api.dartmouth.edu/tei/"
+LLM_BASE_URL = "https://ai-api.dartmouth.edu/tgi/"
+CLOUD_BASE_URL = "https://chat.dartmouth.edu/api/"
+MODEL_LISTING_BASE_URL = "https://api.dartmouth.edu/api/ai/models/"
