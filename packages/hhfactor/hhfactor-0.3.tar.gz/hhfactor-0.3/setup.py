0ead2579cf821c132b610ac61922ed440f4dca3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:19 +0000

    Add package hhfactor/0.3/hhfactor-0.3.tar.gz

---
+++ packages/hhfactor/hhfactor-0.3.tar.gz/hhfactor-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name="hhfactor",
+	  version="0.3",
+	  packages=find_packages(),
+	  install_requires=[],
+	  author="hh",
+	  author_email="hehuang0717@outlook.com",
+	  description="factor",
+	  long_description=open('README.md').read(),
+	  long_description_content_type="text/markdown",
+	  url="https://your.project.url",
+	  classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
+				   "Operating System :: OS Independent", ],
+	  python_requires='>=3.9', )
