97f9753ddccf672b9e2ba8e6de54725db1de8bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:08 +0000

    Add package ser-mail-api/2.0.0/ser_mail_api-2.0.0-py3-none-any.whl

---
+++ packages/ser-mail-api/ser_mail_api-2.0.0-py3-none-any.whl/ser_mail_api/v1/data/__init__.py
@@ -0,0 +1,7 @@
+from .attachment import Attachment, Disposition
+from .content import Content, ContentType
+from .mailuser import MailUser
+from .message import Message
+from .result import Result
+
+__all__ = ['Attachment', 'Disposition', 'Content', 'ContentType', 'MailUser', 'Message', 'Result']
