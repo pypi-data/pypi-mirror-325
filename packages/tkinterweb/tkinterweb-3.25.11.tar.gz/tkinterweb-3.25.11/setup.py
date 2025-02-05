f64851475737427c9a41e87c5e1d07cddba24051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:26 +0000

    Add package tkinterweb/3.25.11/tkinterweb-3.25.11.tar.gz

---
+++ packages/tkinterweb/tkinterweb-3.25.11.tar.gz/tkinterweb-3.25.11/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="tkinterweb",
+    version="3.25.11",
+    description="HTML/CSS viewer for Tkinter",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andereoo/TkinterWeb",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+      ],
+    keywords="tkinter, Tkinter, tkhtml, Tkhtml, Tk, HTML, CSS, webbrowser",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pillow"],
+)
