344abc073ec5c05c23f4e7510daf87984c117a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:39 +0000

    Add package wbcommission/1.44.5/wbcommission-1.44.5-py2.py3-none-any.whl

---
+++ packages/wbcommission/wbcommission-1.44.5-py2.py3-none-any.whl/wbcommission/viewsets/commissions.py
@@ -0,0 +1,21 @@
+from wbcommission.models import CommissionType
+from wbcore import viewsets
+
+from ..serializers import (
+    CommissionTypeModelSerializer,
+    CommissionTypeRepresentationSerializer,
+)
+from .display import CommissionTypeDisplayConfigClass
+
+
+class CommissionTypeRepresentationModelViewSet(viewsets.RepresentationViewSet):
+    serializer_class = CommissionTypeRepresentationSerializer
+    queryset = CommissionType.objects.all()
+    ordering_fields = search_fields = ordering = ["name"]
+
+
+class CommissionTypeModelViewSet(viewsets.ModelViewSet):
+    serializer_class = CommissionTypeModelSerializer
+    queryset = CommissionType.objects.all()
+    ordering_fields = search_fields = ordering = ["name"]
+    display_config_class = CommissionTypeDisplayConfigClass
