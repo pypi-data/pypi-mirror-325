67f739cf73a2c4769d0593b4d6bd3df128e141f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:26 +0000

    Add package ckanext-saml/0.3.7/ckanext_saml-0.3.7-py3-none-any.whl

---
+++ packages/ckanext-saml/ckanext_saml-0.3.7-py3-none-any.whl/ckanext/saml/interfaces.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+from ckan.plugins import Interface
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+
+
+class ICKANSAML(Interface):
+    """Implement custom SAML response modification."""
+
+    def after_mapping(self, mapped_data, auth):
+        """Return dictonary mapped fields.
+
+        :returns: dictonary
+        :rtype: dict
+
+        """
+
+        return mapped_data
+
+    def roles_and_organizations(self, mapped_data, auth, user):
+        """Map Roles and assign User to Organizations"""
+        pass
+
+    def saml_auth_class(self) -> Optional[OneLogin_Saml2_Auth]:
+        """Custom SamlAuthenticator(subclass of OneLogin_Saml2_Auth)"""
+        pass
+
+    def saml_before_user_create(self, mapped_data, user_dict):
+        """Update User data before creating the User"""
+        return user_dict
