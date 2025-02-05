95f1cd78a32b165b56e86902f3c41cb278e6a236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 06:18:56 +0000

    Add package rse-api-client/0.1.2/rse_api_client-0.1.2.tar.gz

---
+++ packages/rse-api-client/rse_api_client-0.1.2.tar.gz/rse_api_client-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rse_api_client",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "setuptools",
+        "requests",
+        "python-dotenv",
+    ],
+    entry_points={
+        'console_scripts': [
+            'rse-api=rse_api_client.main:main',
+        ],
+    },
+    include_package_data=True,
+    description='A client for RSE API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Daniel Anzawa',
+    author_email='danielanzawa@gmail.com',
+    url='https://github.com/danicode/rse-api-client',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
