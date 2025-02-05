da3b0e493c9af927017a02178a772794713d732a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:18 +0000

    Add package feffery-maplibre/0.0.1rc2/feffery_maplibre-0.0.1rc2.tar.gz

---
+++ packages/feffery-maplibre/feffery_maplibre-0.0.1rc2.tar.gz/feffery_maplibre-0.0.1rc2/feffery_maplibre/HandleRawMap.py
@@ -0,0 +1,36 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class HandleRawMap(Component):
+    """A HandleRawMap component.
+
+
+Keyword arguments:
+
+- id (string; optional):
+    必填，用于唯一标识当前组件.
+
+- key (string; optional):
+    强制重绘当前组件时使用.
+
+- jsString (string; optional):
+    设置要针对当前地图实例中的map对象执行的javascript代码字符串  每次新的执行完成后会自动重置为None
+    默认：None."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'feffery_maplibre'
+    _type = 'HandleRawMap'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, key=Component.UNDEFINED, jsString=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'key', 'jsString']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'key', 'jsString']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(HandleRawMap, self).__init__(**args)
