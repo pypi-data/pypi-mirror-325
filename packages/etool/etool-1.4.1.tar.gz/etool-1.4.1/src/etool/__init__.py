e34e85f5a9b4aac14b16bc0ca20e229ceb803702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:27:03 +0000

    Add package etool/1.4.1/etool-1.4.1.tar.gz

---
+++ packages/etool/etool-1.4.1.tar.gz/etool-1.4.1/src/etool/__init__.py
@@ -0,0 +1,29 @@
+from ._network._speed import ManagerSpeed
+from ._network._share import ManagerShare
+
+from ._other._password import ManagerPassword
+from ._other._scheduler import ManagerScheduler
+from ._other._install import ManagerInstall
+
+from ._office._image import ManagerImage
+from ._office._email import ManagerEmail
+from ._office._docx import ManagerDocx
+from ._office._excel import ManagerExcel
+from ._office._ipynb import ManagerIpynb
+from ._office._qrcode import ManagerQrcode
+from ._office._pdf import ManagerPdf
+
+__all__ = [
+    "ManagerSpeed",
+    "ManagerShare",
+    "ManagerPassword",
+    "ManagerScheduler",
+    "ManagerInstall",
+    "ManagerImage",
+    "ManagerEmail",
+    "ManagerDocx",
+    "ManagerExcel",
+    "ManagerIpynb",
+    "ManagerQrcode",
+    "ManagerPdf",
+]
