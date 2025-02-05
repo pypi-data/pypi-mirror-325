6b9cee7c022c574422356d23223342998a4ff581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:49 +0000

    Add package open-webui/0.5.8/open_webui-0.5.8-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.8-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.DeEAMcHZ.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Markdown.CUW2x-6N.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.DeEAMcHZ.js.map
---
+++ packages/open-webui/open_webui-0.5.8-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.xkYl_yAM.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.Dl-NaASG.js";export{t as start};
+//# sourceMappingURL=start.xkYl_yAM.js.map
