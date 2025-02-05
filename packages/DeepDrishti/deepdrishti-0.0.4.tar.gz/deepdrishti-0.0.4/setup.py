ec08fe12dce0c2e69e1a190c83604c8f8aa97087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:39 +0000

    Add package DeepDrishti/0.0.4/deepdrishti-0.0.4.tar.gz

---
+++ packages/DeepDrishti/deepdrishti-0.0.4.tar.gz/deepdrishti-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeepDrishti',
+    version='0.0.4',
+    author="Atanu Debnath",
+    author_email="playatanu@gmail.com",
+    description="A set of python modules for Computer Vision",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/playatanu/DeepDrishti",
+    project_urls={
+        "Documentation": "https://playatanu.github.io/DeepDrishti",
+        "Source": "https://github.com/playatanu/DeepDrishti",
+        "Tracker": "https://github.com/playatanu/DeepDrishti/issues",
+    },
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "pillow",
+        "numpy",
+        "torch",
+        "torchvision",
+        "tqdm", 
+    ],
+    python_requires='>=3.10',
+    
+)
