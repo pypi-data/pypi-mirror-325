4a2e54cc21cc9eaf31d74bf1fe333564ed6708ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:35 +0000

    Add package rpmautospec/0.4.1/rpmautospec-0.4.1.tar.gz

---
+++ packages/rpmautospec/rpmautospec-0.4.1.tar.gz/rpmautospec-0.4.1/rpmautospec/subcommands/changelog.py
@@ -0,0 +1,57 @@
+from typing import Any, Optional, Union
+
+from .. import pager
+from ..pkg_history import PkgHistoryProcessor
+
+
+def register_subcommand(subparsers):
+    subcmd_name = "generate-changelog"
+
+    gen_changelog_parser = subparsers.add_parser(
+        subcmd_name,
+        help="Generate changelog entries from git commit logs",
+    )
+
+    gen_changelog_parser.add_argument(
+        "spec_or_path",
+        default=".",
+        nargs="?",
+        help="Path to package worktree or the spec file within",
+    )
+
+    return subcmd_name
+
+
+def _coerce_to_str(str_or_bytes: Union[str, bytes]) -> str:
+    if isinstance(str_or_bytes, bytes):
+        str_or_bytes = str_or_bytes.decode("utf-8", errors="replace")
+    return str_or_bytes
+
+
+def _coerce_to_bytes(str_or_bytes: Union[str, bytes]) -> str:
+    if isinstance(str_or_bytes, str):
+        str_or_bytes = str_or_bytes.encode("utf-8")
+    return str_or_bytes
+
+
+def collate_changelog(
+    processor_results: dict[str, Any], result_type: Optional[type] = str
+) -> Union[str, bytes]:
+    changelog = processor_results["changelog"]
+    if result_type == str:
+        entry_strings = (_coerce_to_str(entry.format()) for entry in changelog)
+    else:
+        entry_strings = (_coerce_to_bytes(entry.format()) for entry in changelog)
+    return "\n\n".join(entry_strings)
+
+
+def produce_changelog(spec_or_repo):
+    processor = PkgHistoryProcessor(spec_or_repo)
+    result = processor.run(visitors=(processor.release_number_visitor, processor.changelog_visitor))
+    return collate_changelog(result)
+
+
+def main(args):
+    """Main method."""
+    changelog = produce_changelog(args.spec_or_path)
+    pager.page(changelog, enabled=args.pager)
