3336dd71b654da76a04aaa9f19bf57f018bff9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:34 +0000

    Add package cb-streamlit-connector/0.0.0/cb_streamlit_connector-0.0.0-py3-none-any.whl

---
+++ packages/cb-streamlit-connector/cb_streamlit_connector-0.0.0-py3-none-any.whl/cb_streamlit_connector/tests/test_integration.py
@@ -0,0 +1,139 @@
+from cb_streamlit_connector.connector import CouchbaseConnector
+import streamlit as st
+
+import os
+
+from couchbase.exceptions import DocumentNotFoundException
+from couchbase.logic.n1ql import QueryStatus
+
+import pytest
+
+# TODO: Add the test_query_consistency
+
+@pytest.fixture
+def connection():
+    connection = st.connection(
+        "couchbase", 
+        type=CouchbaseConnector, 
+        CONNSTR= os.environ["CONNSTR"],
+        USERNAME= os.environ["USERNAME"],
+        PASSWORD= os.environ["PASSWORD"],
+        BUCKET_NAME= os.environ["BUCKET_NAME"],
+        SCOPE_NAME= os.environ["SCOPE_NAME"],
+        COLLECTION_NAME= os.environ["COLLECTION_NAME"]
+    )
+    return connection
+    
+def test_create(connection):
+    """Test the successful creation of an airline"""
+    airline_data = {
+        "name": "Sample Airline",
+        "iata": "SAL",
+        "icao": "SALL",
+        "callsign": "SAM",
+        "country": "Sample Country",
+    }
+    document_id = "airline_test_insert"
+    try:
+        connection.collection.remove(document_id)
+    except DocumentNotFoundException:
+        pass
+    response = connection.insert_document(document_id, airline_data)
+    assert response.key == document_id
+
+    # Check document stored in DB is same as sent & clean up
+    doc_in_db = connection.get_document(document_id)
+    assert doc_in_db == airline_data
+    connection.remove_document(document_id)
+            
+def test_read(connection):
+    """Test the reading of an airline"""
+    airline_data = {
+        "name": "Sample Airline",
+        "iata": "SAL",
+        "icao": "SALL",
+        "callsign": "SAM",
+        "country": "Sample Country",
+    }
+    document_id = "airline_test_read"
+    try:
+        connection.collection.remove(document_id)
+    except DocumentNotFoundException:
+        pass
+    response = connection.insert_document(document_id, airline_data)
+
+    response = connection.get_document(document_id)
+    assert response == airline_data
+
+    connection.remove_document(document_id)
+    
+def test_update(connection):
+    """Test updating an existing airline"""
+    airline_data = {
+        "name": "Sample Airline",
+        "iata": "SAL",
+        "icao": "SALL",
+        "callsign": "SAM",
+        "country": "Sample Country",
+    }
+    document_id = "airline_test_update"
+    try:
+        connection.collection.remove(document_id)
+    except DocumentNotFoundException:
+        pass
+    response = connection.insert_document(document_id, airline_data)
+
+    updated_airline_data = {
+        "name": "Updated Airline",
+        "iata": "SAL",
+        "icao": "SALL",
+        "callsign": "SAM",
+        "country": "Updated Country",
+    }
+
+    response = connection.replace_document(document_id, updated_airline_data)
+    assert response.key == document_id
+    response = connection.get_document(document_id)
+    assert response == updated_airline_data
+
+    connection.remove_document(document_id)
+    
+def test_delete(connection):
+    """Test deleting an existing airline"""
+    airline_data = {
+        "name": "Sample Airline",
+        "iata": "SAL",
+        "icao": "SALL",
+        "callsign": "SAM",
+        "country": "Sample Country",
+    }
+    document_id = "airline_test_delete"
+    try:
+        connection.collection.remove(document_id)
+    except DocumentNotFoundException:
+        pass
+    response = connection.insert_document(document_id, airline_data)
+
+    response = connection.remove_document(document_id)
+    assert response.key == document_id
+    
+def test_query(connection):
+    """Test the destination airports from an airline"""
+    query = """
+        SELECT * FROM `travel-sample`.`inventory`.`airline`
+        WHERE type = "airline"
+        AND country = "United States"
+        LIMIT 5;
+    """
+    result = connection.query(query)
+    data = []
+    for row in result.rows():
+        data.append(row)
+    assert result.metadata().status() == QueryStatus.SUCCESS
+
+# def test_query_consistency(connection):
+    # Insert 100 documents asynchronously one after the another thena also start the query on those 100 docs and see if the results match
+    # for i in range(100):
+    #     connection.insert_document(f"{i}",{f"key_{i}": f"value_{i}"} )
+        
+    # QueryOptions(scan_consistency= QueryScanConsistency.AT_PLUS)
