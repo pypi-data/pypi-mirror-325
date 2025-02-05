3a923c6e32ac7ab93be8f51ed1977450e8d9a7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:35 +0000

    Add package koning/100/koning-100.tar.gz

---
+++ packages/koning/koning-100.tar.gz/koning-100/koning/lookups.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "koning.modules.irc",
+    "dis": "koning.modules.mdl",
+    "dpl": "koning.modules.rss",
+    "err": "koning.modules.err",
+    "exp": "koning.modules.rss",
+    "flt": "koning.modules.flt",
+    "imp": "koning.modules.rss",
+    "mod": "koning.modules.mod",
+    "mre": "koning.modules.irc",
+    "nme": "koning.modules.rss",
+    "now": "koning.modules.mdl",
+    "pwd": "koning.modules.irc",
+    "rem": "koning.modules.rss",
+    "req": "koning.modules.req",
+    "res": "koning.modules.rss",
+    "rss": "koning.modules.rss",
+    "syn": "koning.modules.rss",
+    "thr": "koning.modules.thr",
+    "upt": "koning.modules.upt",
+    "ver": "koning.modules.upt"
+}
