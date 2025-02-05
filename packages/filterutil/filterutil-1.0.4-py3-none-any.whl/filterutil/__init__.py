009c07d2a42d69ffa04ba3e0e944c138d3766c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:12:08 +0000

    Add package filterutil/1.0.4/filterutil-1.0.4-py3-none-any.whl

---
+++ packages/filterutil/filterutil-1.0.4-py3-none-any.whl/filterutil/__init__.py
@@ -0,0 +1,12 @@
+from .filter_coupling_policy import FilterCouplingPolicy
+from .applying import (
+    apply_filters,
+    apply_filters_with_and_policy,
+    apply_filters_with_or_policy,
+    apply_filters_with_xor_policy,
+)
+from .compound_filter import CompoundFilter
+from .simple_filter import Filter
+from .filters import Filters
+from .or_filters import OrFilters
+from .xor_filters import XorFilters
