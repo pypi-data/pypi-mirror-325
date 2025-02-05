724e448b4d7ebe2b2ba2a188535170328b1826a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:41 +0000

    Add package feegaffe/1.1.1/feegaffe-1.1.1.tar.gz

---
+++ packages/feegaffe/feegaffe-1.1.1.tar.gz/feegaffe-1.1.1/feegaffe/hello.py
@@ -0,0 +1,10 @@
+def hello(name="World"):
+    """
+    Retourne un message de salutation personnalisé.
+    
+    :param name: Nom de la personne à saluer.
+    """
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(hello())
