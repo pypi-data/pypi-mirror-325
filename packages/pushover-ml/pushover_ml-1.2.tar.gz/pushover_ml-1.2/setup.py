e23545b9490c1a09df92acda320a5858873dd6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:20:23 +0000

    Add package pushover-ml/1.2/pushover_ml-1.2.tar.gz

---
+++ packages/pushover-ml/pushover_ml-1.2.tar.gz/pushover_ml-1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 11:40:26 2024
+
+@author: HOME
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pushover_ml',
+    version='1.2',
+    author='Carlos Angarita',
+    author_email='carlosantr@unisabana.edu.co',
+    packages=find_packages(),
+    description='User-friendly Graphical User Interface (GUI) to efficiently predict a trilinear approximation of pushover curves for low-rise reinforced concrete (RC) frame buildings, using a Machine Learning (ML) based approach.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'matplotlib>=3.9.1',  # Any version of matplotlib
+        'numpy>=1.26.4',  # Any version of numpy
+        'joblib>=1.4.2',
+        'scikit-learn==1.2.1',
+        'tensorflow==2.17.0',
+        'keras>=3.4.1',
+        'openpyxl>=3.1.5',
+        'Xlsxwriter>=3.2.0',
+        'pandas>=2.2.2'
+    ],
+    python_requires='>=3.11', # Adjust based on your compatibility
+    url='https://github.com/carlosantr/Pushover-ML_repository',
+    license='Apache-2.0', # If you have a license file
+)
