1e24c9839ef01393c88d8edd1a5a02db86ec2830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 03:07:02 +0000

    Add package foxes-opt/0.4/foxes_opt-0.4.tar.gz

---
+++ packages/foxes-opt/foxes_opt-0.4.tar.gz/foxes_opt-0.4/foxes_opt/__init__.py
@@ -0,0 +1,15 @@
+"""
+Wind farm optimization.
+"""
+
+from .core import FarmOptProblem, FarmObjective, FarmConstraint  # noqa: F401
+
+from . import input  # noqa: F401
+from . import problems  # noqa: F401
+from . import constraints  # noqa: F401
+from . import objectives  # noqa: F401
+from . import output  # noqa: F401
+
+from importlib.metadata import version
+
+__version__ = version(__package__ or __name__)
