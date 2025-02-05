3f024fbd79885c2e15b3a8db2a341d50ef2b7ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 17:06:43 +0000

    Add package 6th-Form-Functions/0.0.1/6th_Form_Functions-0.0.1-py3-none-any.whl

---
+++ packages/6th-Form-Functions/6th_Form_Functions-0.0.1-py3-none-any.whl/conversions.py
@@ -0,0 +1,14 @@
+def to_binary(num):
+    binary = bin(num)[2:]
+    return binary
+
+
+def to_decimal(num, base):
+    decimal = int(num, base)
+    return decimal
+
+
+def to_hexadecimal(num):
+    hexadecimal = hex(num)[2:]
+    return hexadecimal
+
