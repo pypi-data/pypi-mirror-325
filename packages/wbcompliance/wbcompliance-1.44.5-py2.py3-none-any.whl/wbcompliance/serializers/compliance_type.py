5468e3b195a6fd5e4ba68c61dc7534def1861ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:39 +0000

    Add package wbcompliance/1.44.5/wbcompliance-1.44.5-py2.py3-none-any.whl

---
+++ packages/wbcompliance/wbcompliance-1.44.5-py2.py3-none-any.whl/wbcompliance/serializers/compliance_type.py
@@ -0,0 +1,26 @@
+from django.utils.translation import gettext_lazy
+from wbcompliance.models import ComplianceType
+from wbcore import serializers as wb_serializers
+from wbcore.contrib.authentication.serializers import GroupRepresentationSerializer
+from wbcore.contrib.directory.serializers import PersonRepresentationSerializer
+
+
+class ComplianceTypeRepresentationSerializer(wb_serializers.RepresentationSerializer):
+    class Meta:
+        model = ComplianceType
+        fields = (
+            "id",
+            "name",
+        )
+
+
+class ComplianceTypeModelSerializer(wb_serializers.ModelSerializer):
+    _in_charge = GroupRepresentationSerializer(source="in_charge", many=True)
+    administrators = wb_serializers.PrimaryKeyRelatedField(
+        label=gettext_lazy("Administrator"), many=True, read_only=True
+    )
+    _administrators = PersonRepresentationSerializer(many=True, source="administrators")
+
+    class Meta:
+        model = ComplianceType
+        fields = ("id", "name", "description", "in_charge", "_in_charge", "administrators", "_administrators")
