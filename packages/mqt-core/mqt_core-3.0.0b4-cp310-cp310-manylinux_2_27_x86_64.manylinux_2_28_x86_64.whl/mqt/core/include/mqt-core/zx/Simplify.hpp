f1ca1fd8c013227a631bbe466fe773ab08124837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:29 +0000

    Add package mqt-core/3.0.0b4/mqt_core-3.0.0b4-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mqt-core/mqt_core-3.0.0b4-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/mqt/core/include/mqt-core/zx/Simplify.hpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2025 Chair for Design Automation, TUM
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#pragma once
+
+#include "Rules.hpp"
+#include "ZXDefinitions.hpp"
+#include "ZXDiagram.hpp"
+
+#include <cstddef>
+
+namespace zx {
+
+template <class VertexCheckFun, class VertexRuleFun>
+std::size_t simplifyVertices(ZXDiagram& diag, VertexCheckFun check,
+                             VertexRuleFun rule) {
+  std::size_t nSimplifications = 0;
+  bool newMatches = true;
+
+  while (newMatches) {
+    newMatches = false;
+    for (const auto& [v, _] : diag.getVertices()) {
+      if (check(diag, v)) {
+        rule(diag, v);
+        newMatches = true;
+        nSimplifications++;
+      }
+    }
+  }
+
+  return nSimplifications;
+}
+
+template <class EdgeCheckFun, class EdgeRuleFun>
+std::size_t simplifyEdges(ZXDiagram& diag, EdgeCheckFun check,
+                          EdgeRuleFun rule) {
+  std::size_t nSimplifications = 0;
+  bool newMatches = true;
+
+  while (newMatches) {
+    newMatches = false;
+    for (const auto& [v0, v1] : diag.getEdges()) {
+      if (diag.isDeleted(v0) || diag.isDeleted(v1) || !check(diag, v0, v1)) {
+        continue;
+      }
+      rule(diag, v0, v1);
+      newMatches = true;
+      nSimplifications++;
+    }
+  }
+
+  return nSimplifications;
+}
+
+std::size_t gadgetSimp(ZXDiagram& diag);
+
+std::size_t idSimp(ZXDiagram& diag);
+
+std::size_t spiderSimp(ZXDiagram& diag);
+
+std::size_t localCompSimp(ZXDiagram& diag);
+
+std::size_t pivotPauliSimp(ZXDiagram& diag);
+
+std::size_t pivotSimp(ZXDiagram& diag);
+
+std::size_t interiorCliffordSimp(ZXDiagram& diag);
+
+std::size_t cliffordSimp(ZXDiagram& diag);
+
+std::size_t pivotgadgetSimp(ZXDiagram& diag);
+
+std::size_t fullReduce(ZXDiagram& diag);
+std::size_t fullReduceApproximate(ZXDiagram& diag, fp tolerance);
+
+} // namespace zx
