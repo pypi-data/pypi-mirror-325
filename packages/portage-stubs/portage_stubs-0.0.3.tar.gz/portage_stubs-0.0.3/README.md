2044fed7f09041a72a26f38472cfd9cf4622f615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:06:16 +0000

    Add package portage-stubs/0.0.3/portage_stubs-0.0.3.tar.gz

---
+++ packages/portage-stubs/portage_stubs-0.0.3.tar.gz/portage_stubs-0.0.3/README.md
@@ -0,0 +1,15 @@
+# Portage types
+
+[![QA](https://github.com/Tatsh/portage-types/actions/workflows/qa.yml/badge.svg)](https://github.com/Tatsh/portage-types/actions/workflows/qa.yml)
+
+Types for portage.
+
+## Installation
+
+```shell
+pip install portage-types
+```
+
+```shell
+poetry add portage-types
+```
---
+++ packages/portage-stubs/portage_stubs-0.0.3.tar.gz/portage_stubs-0.0.3/portage-stubs/versions.pyi
@@ -0,0 +1,13 @@
+from typing import Literal  # noqa: I001
+
+
+def vercmp(a: str, b: str, silent: Literal[0, 1] = ...) -> int | None:
+    ...
+
+
+def catpkgsplit(
+        s: str,
+        silent: Literal[0, 1] = ...,
+        eapi: str | None = ...
+) -> tuple[str, str] | tuple[str, str, str] | tuple[str, str, str, str]:
+    ...
