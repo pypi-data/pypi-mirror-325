97099c0bde6e7e27827abaa0dc4d85e018d21f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:05:50 +0000

    Add package vellum-ai/0.13.23/vellum_ai-0.13.23-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-0.13.23-py3-none-any.whl/vellum/workflows/nodes/core/templating_node/tests/test_templating_node.py
@@ -0,0 +1,157 @@
+import json
+from typing import List
+
+from vellum.client.types.chat_message import ChatMessage
+from vellum.client.types.function_call import FunctionCall
+from vellum.workflows.nodes.bases.base import BaseNode
+from vellum.workflows.nodes.core.templating_node.node import TemplatingNode
+from vellum.workflows.state import BaseState
+from vellum.workflows.types.core import Json
+
+
+def test_templating_node__dict_output():
+    # GIVEN a templating node with a dict input that just returns it
+    class TemplateNode(TemplatingNode):
+        template = "{{ data }}"
+        inputs = {
+            "data": {
+                "key": "value",
+            }
+        }
+
+    # WHEN the node is run
+    node = TemplateNode()
+    outputs = node.run()
+
+    # THEN the output is json serializable
+    # https://app.shortcut.com/vellum/story/6132
+    dump: str = outputs.result  # type: ignore[assignment]
+    assert json.loads(dump) == {"key": "value"}
+
+
+def test_templating_node__int_output():
+    # GIVEN a templating node that outputs an integer
+    class IntTemplateNode(TemplatingNode[BaseState, int]):
+        template = "{{ data }}"
+        inputs = {
+            "data": 42,
+        }
+
+    # WHEN the node is run
+    node = IntTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected integer
+    assert outputs.result == 42
+
+
+def test_templating_node__float_output():
+    # GIVEN a templating node that outputs a float
+    class FloatTemplateNode(TemplatingNode[BaseState, float]):
+        template = "{{ data }}"
+        inputs = {
+            "data": 42.5,
+        }
+
+    # WHEN the node is run
+    node = FloatTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected float
+    assert outputs.result == 42.5
+
+
+def test_templating_node__bool_output():
+    # GIVEN a templating node that outputs a bool
+    class BoolTemplateNode(TemplatingNode[BaseState, bool]):
+        template = "{{ data }}"
+        inputs = {
+            "data": True,
+        }
+
+    # WHEN the node is run
+    node = BoolTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected bool
+    assert outputs.result is True
+
+
+def test_templating_node__json_output():
+    # GIVEN a templating node that outputs JSON
+    class JSONTemplateNode(TemplatingNode[BaseState, Json]):
+        template = "{{ data }}"
+        inputs = {
+            "data": {"key": "value"},
+        }
+
+    # WHEN the node is run
+    node = JSONTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected JSON
+    assert outputs.result == {"key": "value"}
+
+
+def test_templating_node__execution_count_reference():
+    # GIVEN a random node
+    class OtherNode(BaseNode):
+        pass
+
+    # AND a templating node that references the execution count of the random node
+    class TemplateNode(TemplatingNode):
+        template = "{{ total }}"
+        inputs = {
+            "total": OtherNode.Execution.count,
+        }
+
+    # WHEN the node is run
+    node = TemplateNode()
+    outputs = node.run()
+
+    # THEN the output is just the total
+    assert outputs.result == "0"
+
+
+def test_templating_node__pydantic_to_json():
+    # GIVEN a templating node that uses tojson on a pydantic model
+    class JSONTemplateNode(TemplatingNode[BaseState, Json]):
+        template = "{{ function_call | tojson }}"
+        inputs = {
+            "function_call": FunctionCall(name="test", arguments={"key": "value"}),
+        }
+
+    # WHEN the node is run
+    node = JSONTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected JSON
+    assert outputs.result == {"name": "test", "arguments": {"key": "value"}, "id": None}
+
+
+def test_templating_node__chat_history_output():
+    # GIVEN a templating node that outputs a chat history
+    class ChatHistoryTemplateNode(TemplatingNode[BaseState, List[ChatMessage]]):
+        template = '[{"role": "USER", "text": "Hello"}]'
+        inputs = {}
+
+    # WHEN the node is run
+    node = ChatHistoryTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected chat history
+    assert outputs.result == [ChatMessage(role="USER", text="Hello")]
+
+
+def test_templating_node__function_call_output():
+    # GIVEN a templating node that outputs a function call
+    class FunctionCallTemplateNode(TemplatingNode[BaseState, FunctionCall]):
+        template = '{"name": "test", "arguments": {"key": "value"}}'
+        inputs = {}
+
+    # WHEN the node is run
+    node = FunctionCallTemplateNode()
+    outputs = node.run()
+
+    # THEN the output is the expected function call
+    assert outputs.result == FunctionCall(name="test", arguments={"key": "value"})
