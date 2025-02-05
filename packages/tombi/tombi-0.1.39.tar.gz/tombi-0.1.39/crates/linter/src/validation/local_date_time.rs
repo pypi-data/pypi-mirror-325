e926295f57613b8911d8404125bb68a94f896c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 14:12:29 +0000

    Add package tombi/0.1.39/tombi-0.1.39.tar.gz

---
+++ packages/tombi/tombi-0.1.39.tar.gz/tombi-0.1.39/crates/linter/src/validation/local_date_time.rs
@@ -0,0 +1,67 @@
+use document_tree::{LocalDateTime, ValueImpl};
+use schema_store::ValueType;
+
+use super::{validate_all_of, validate_any_of, validate_one_of, Validate};
+
+impl Validate for LocalDateTime {
+    fn validate(
+        &self,
+        toml_version: config::TomlVersion,
+        value_schema: &schema_store::ValueSchema,
+        definitions: &schema_store::SchemaDefinitions,
+    ) -> Result<(), Vec<crate::Error>> {
+        let mut errors = vec![];
+
+        match value_schema.value_type() {
+            ValueType::LocalDateTime
+            | ValueType::OneOf(_)
+            | ValueType::AnyOf(_)
+            | ValueType::AllOf(_) => {}
+            ValueType::Null => return Ok(()),
+            value_schema => {
+                return Err(vec![crate::Error {
+                    kind: crate::ErrorKind::TypeMismatch {
+                        expected: value_schema,
+                        actual: self.value_type(),
+                    },
+                    range: self.range(),
+                }]);
+            }
+        }
+
+        let local_date_time_schema = match value_schema {
+            schema_store::ValueSchema::LocalDateTime(local_date_time_schema) => {
+                local_date_time_schema
+            }
+            schema_store::ValueSchema::OneOf(one_of_schema) => {
+                return validate_one_of(self, toml_version, one_of_schema, definitions)
+            }
+            schema_store::ValueSchema::AnyOf(any_of_schema) => {
+                return validate_any_of(self, toml_version, any_of_schema, definitions)
+            }
+            schema_store::ValueSchema::AllOf(all_of_schema) => {
+                return validate_all_of(self, toml_version, all_of_schema, definitions)
+            }
+            _ => unreachable!("Expected a Local Date-Time schema"),
+        };
+
+        let value_string = self.node().to_string();
+        if let Some(enumerate) = &local_date_time_schema.enumerate {
+            if !enumerate.contains(&value_string) {
+                errors.push(crate::Error {
+                    kind: crate::ErrorKind::Eunmerate {
+                        expected: enumerate.iter().map(ToString::to_string).collect(),
+                        actual: value_string,
+                    },
+                    range: self.range(),
+                });
+            }
+        }
+
+        if errors.is_empty() {
+            Ok(())
+        } else {
+            Err(errors)
+        }
+    }
+}
---
+++ packages/tombi/tombi-0.1.39.tar.gz/tombi-0.1.39/crates/server/src/hover/local_date_time.rs
@@ -0,0 +1,104 @@
+use schema_store::{LocalDateTimeSchema, ValueSchema};
+use tower_lsp::lsp_types::Url;
+
+use super::{
+    all_of::get_all_of_hover_content, any_of::get_any_of_hover_content,
+    one_of::get_one_of_hover_content, GetHoverContent,
+};
+
+impl GetHoverContent for document_tree::LocalDateTime {
+    fn get_hover_content(
+        &self,
+        accessors: &Vec<schema_store::Accessor>,
+        value_schema: Option<&ValueSchema>,
+        toml_version: config::TomlVersion,
+        position: text::Position,
+        keys: &[document_tree::Key],
+        schema_url: Option<&Url>,
+        definitions: &schema_store::SchemaDefinitions,
+    ) -> Option<super::HoverContent> {
+        match value_schema {
+            Some(ValueSchema::LocalDateTime(local_date_schema)) => local_date_schema
+                .get_hover_content(
+                    accessors,
+                    value_schema,
+                    toml_version,
+                    position,
+                    keys,
+                    schema_url,
+                    definitions,
+                )
+                .map(|mut hover_content| {
+                    hover_content.range = Some(self.range());
+                    hover_content
+                }),
+            Some(ValueSchema::OneOf(one_of_schema)) => get_one_of_hover_content(
+                self,
+                accessors,
+                one_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(ValueSchema::AnyOf(any_of_schema)) => get_any_of_hover_content(
+                self,
+                accessors,
+                any_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(ValueSchema::AllOf(all_of_schema)) => get_all_of_hover_content(
+                self,
+                accessors,
+                all_of_schema,
+                toml_version,
+                position,
+                keys,
+                schema_url,
+                definitions,
+            ),
+            Some(_) => None,
+            None => Some(super::HoverContent {
+                title: None,
+                description: None,
+                accessors: schema_store::Accessors::new(accessors.clone()),
+                value_type: schema_store::ValueType::LocalDateTime,
+                enumerated_values: vec![],
+                schema_url: None,
+                range: Some(self.range()),
+            }),
+        }
+    }
+}
+
+impl GetHoverContent for LocalDateTimeSchema {
+    fn get_hover_content(
+        &self,
+        accessors: &Vec<schema_store::Accessor>,
+        _value_schema: Option<&ValueSchema>,
+        _toml_version: config::TomlVersion,
+        _position: text::Position,
+        _keys: &[document_tree::Key],
+        schema_url: Option<&Url>,
+        _definitions: &schema_store::SchemaDefinitions,
+    ) -> Option<super::HoverContent> {
+        Some(super::HoverContent {
+            title: self.title.clone(),
+            description: self.description.clone(),
+            accessors: schema_store::Accessors::new(accessors.clone()),
+            value_type: schema_store::ValueType::LocalDateTime,
+            enumerated_values: self
+                .enumerate
+                .as_ref()
+                .map(|v| v.iter().map(ToString::to_string).collect())
+                .unwrap_or_default(),
+            schema_url: schema_url.cloned(),
+            range: None,
+        })
+    }
+}
