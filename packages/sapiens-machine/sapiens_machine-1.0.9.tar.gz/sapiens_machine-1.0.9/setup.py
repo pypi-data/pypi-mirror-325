7573ff09622e807a6b05023a2706527ea2990816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:05:40 +0000

    Add package sapiens-machine/1.0.9/sapiens_machine-1.0.9.tar.gz

---
+++ packages/sapiens-machine/sapiens_machine-1.0.9.tar.gz/sapiens_machine-1.0.9/setup.py
@@ -0,0 +1,24 @@
+"""
+    ########################################################################################################################################################
+    # This algorithm is part of a code library for optimizing the Artificial Intelligence models of Sapiens Technology®, and its disclosure, distribution, #
+    # or reverse engineering without the company's prior consent will be subject to legal proceedings and actions pursued by our legal department.         #
+    ########################################################################################################################################################
+"""
+from setuptools import setup, find_packages
+package_name = 'sapiens_machine'
+version = '1.0.9'
+setup(
+    name=package_name,
+    version=version,
+    author='OPENSAPI',
+    packages=find_packages(),
+    install_requires=['scipy==1.15.1', 'requests==2.31.0'],
+    url='https://github.com/',
+    license='Proprietary Software'
+)
+"""
+    ########################################################################################################################################################
+    # This algorithm is part of a code library for optimizing the Artificial Intelligence models of Sapiens Technology®, and its disclosure, distribution, #
+    # or reverse engineering without the company's prior consent will be subject to legal proceedings and actions pursued by our legal department.         #
+    ########################################################################################################################################################
+"""
