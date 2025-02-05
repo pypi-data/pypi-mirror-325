a02d9239946a2d063057157525a6c62453f2f464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:05:31 +0000

    Add package literaturescrape/0.250205.2/literatureScrape-0.250205.2-py3-none-any.whl

---
+++ packages/literaturescrape/literatureScrape-0.250205.2-py3-none-any.whl/literatureScrape/core/models.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+@dataclass
+class Literature:
+    title: str
+    doi: Optional[str] = None
+    IF: Optional[str] = None
+    pmid: Optional[str] = None
+    jour_name: Optional[str] = None
+    jour_issn: Optional[str] = None
+    
+    openAccess: bool = False
+    
+    publish_date: Optional[str] = None
+    year: Optional[str] = None
+    volume: Optional[str] = None
+    issue: Optional[str] = None
+    page: Optional[str] = None
+    
+    author: Optional[str] = None
+    abstract: Optional[str] = None
+    
+    def to_dict(self):
+        return asdict(self)
+    
+    
+@dataclass
+class Response:
+    source: str
+    priority: int = 0
+    
+    fetch_flag: bool = False
+    extract_flag: bool = False
+    fetch_err_reason: Optional[str] = None
+    extract_err_reason: Optional[str] = None
+    
+    finished: bool = False
+    literature: Optional[Literature] = None
+
