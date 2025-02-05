758911bc2bfe74e2b177e14b38f3d4329df46ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 07:05:15 +0000

    Add package ansible-ctargetnode-setup/0.1/ansible_ctargetnode_setup-0.1.tar.gz

---
+++ packages/ansible-ctargetnode-setup/ansible_ctargetnode_setup-0.1.tar.gz/ansible_ctargetnode_setup-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ansible_ctargetnode_setup",
+    version="0.1",
+    author="Mohammad Noorain",
+    author_email="your-email@example.com",
+    description="A Python package to automate Ansible TargetNode installation and configuration.",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "ansible_targetnode_setup = ansible_argetnode_setup.controller:setup_ansible",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    install_requires=[],  # Add dependencies if needed
+)
