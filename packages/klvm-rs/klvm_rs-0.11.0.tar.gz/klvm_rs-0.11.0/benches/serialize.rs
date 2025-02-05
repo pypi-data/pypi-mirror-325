905f3ac516e1366bf8527888aaa2e5eeedcc129a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 16:11:35 +0000

    Add package klvm-rs/0.11.0/klvm_rs-0.11.0.tar.gz

---
+++ packages/klvm-rs/klvm_rs-0.11.0.tar.gz/klvm_rs-0.11.0/benches/serialize.rs
@@ -0,0 +1,50 @@
+use criterion::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+use klvmr::allocator::Allocator;
+use klvmr::serde::{
+    node_from_bytes, node_from_bytes_backrefs, node_to_bytes, node_to_bytes_backrefs, Serializer,
+};
+use std::include_bytes;
+use std::time::Instant;
+
+fn serialize_benchmark(c: &mut Criterion) {
+    // the blocks are serialized with back-refs. In order to accurately measure
+    // the cost of the compression itself, we first need to inflate them and
+    // then serialize again.
+    let block0: &[u8] = include_bytes!("0.generator");
+    let block1: &[u8] = include_bytes!("1.generator");
+    let block2: &[u8] = include_bytes!("2.generator");
+
+    let mut group = c.benchmark_group("serialize");
+
+    for (block, name) in [(&block0, "0"), (&block1, "1"), (&block2, "2")] {
+        let mut a = Allocator::new();
+        let node = node_from_bytes_backrefs(&mut a, block).expect("node_from_bytes_backrefs");
+        let inflated = node_to_bytes(&a, node).expect("node_to_bytes");
+        let mut a = Allocator::new();
+        let node = node_from_bytes(&mut a, inflated.as_slice()).expect("node_from_bytes");
+
+        group.bench_function(format!("node_to_bytes_backrefs {name}"), |b| {
+            b.iter(|| {
+                let start = Instant::now();
+                black_box(node_to_bytes_backrefs(&a, node).expect("node_to_bytes_backrefs"));
+                start.elapsed()
+            })
+        });
+
+        group.bench_function(format!("Serializer {name}"), |b| {
+            b.iter(|| {
+                let start = Instant::now();
+                let mut ser = Serializer::default();
+                let _ = ser.add(&a, node, None);
+                black_box(ser.into_inner());
+                start.elapsed()
+            })
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(serialize, serialize_benchmark);
+criterion_main!(serialize);
---
+++ packages/klvm-rs/klvm_rs-0.11.0.tar.gz/klvm_rs-0.11.0/wheel/README.md
@@ -0,0 +1,71 @@
+Rust implementation of klvm.
+
+The cargo workspace includes an rlib crate, for use with rust or other applications, and a python wheel.
+
+The python wheel is in `wheel`. The npm package is in `wasm`.
+
+## Tests
+
+In order to run the unit tests, run:
+
+```
+cargo test
+```
+
+## Fuzzing
+
+The fuzzing infrastructure for `klvm_rs` uses [cargo-fuzz](https://github.com/rust-fuzz/cargo-fuzz).
+
+Documentation for setting up fuzzing in rust can be found [here](https://rust-fuzz.github.io/book/cargo-fuzz.html).
+
+To generate an initial corpus (for the `run_program` fuzzer), run:
+
+```
+cd tools
+cargo run generate-fuzz-corpus
+```
+
+To get started, run:
+
+```
+cargo fuzz run fuzz_run_program --jobs=32 -- -rss_limit_mb=4096
+```
+
+But with whatever number of jobs works best for you.
+
+If you find issues in `klvm_rs` please use our [bug bounty program](https://hackerone.com/chik_network).
+
+## Build Wheel
+
+The `klvm_rs` wheel has python bindings for the rust implementation of klvm.
+
+Use `maturin` to build the python interface. First, install into current virtualenv with
+
+```
+$ pip install maturin
+```
+
+While in the `wheel` directory, build `klvm_rs` into the current virtualenv with
+
+```
+$ maturin develop --release
+```
+
+On UNIX-based platforms, you may get a speed boost on `sha256` operations by building
+with OpenSSL.
+
+```
+$ maturin develop --release --features=openssl
+```
+
+To build the wheel, do
+
+```
+$ maturin build --release
+```
+
+or
+
+```
+$ maturin build --release --features=openssl
+```
