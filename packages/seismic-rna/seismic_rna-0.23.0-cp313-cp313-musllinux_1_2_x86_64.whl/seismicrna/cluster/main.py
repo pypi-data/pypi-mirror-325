a107406e454856562c809dde5e048042f3c7d609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 05:05:20 +0000

    Add package seismic-rna/0.23.0/seismic_rna-0.23.0-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/seismic-rna/seismic_rna-0.23.0-cp313-cp313-musllinux_1_2_x86_64.whl/seismicrna/cluster/main.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+from typing import Iterable
+
+from click import command
+
+from .write import cluster
+from ..core import path
+from ..core.arg import (CMD_CLUSTER,
+                        arg_input_path,
+                        opt_tmp_pfx,
+                        opt_keep_tmp,
+                        opt_min_clusters,
+                        opt_max_clusters,
+                        opt_em_runs,
+                        opt_em_thresh,
+                        opt_min_em_iter,
+                        opt_max_em_iter,
+                        opt_jackpot,
+                        opt_jackpot_conf_level,
+                        opt_max_jackpot_quotient,
+                        opt_max_pearson_run,
+                        opt_min_marcd_run,
+                        opt_max_loglike_vs_best,
+                        opt_min_pearson_vs_best,
+                        opt_max_marcd_vs_best,
+                        opt_try_all_ks,
+                        opt_write_all_ks,
+                        opt_cluster_pos_table,
+                        opt_cluster_abundance_table,
+                        opt_verify_times,
+                        opt_brotli_level,
+                        opt_max_procs,
+                        opt_force)
+from ..core.run import run_func
+from ..core.task import as_list_of_tuples, dispatch
+from ..mask.dataset import load_mask_dataset
+
+
+@run_func(CMD_CLUSTER)
+def run(input_path: Iterable[str | Path], *,
+        tmp_pfx: str | Path,
+        keep_tmp: bool,
+        min_clusters: int,
+        max_clusters: int,
+        em_runs: int,
+        min_em_iter: int,
+        max_em_iter: int,
+        em_thresh: float,
+        min_marcd_run: float,
+        max_pearson_run: float,
+        jackpot: bool,
+        jackpot_conf_level: float,
+        max_jackpot_quotient: float,
+        max_loglike_vs_best: float,
+        min_pearson_vs_best: float,
+        max_marcd_vs_best: float,
+        try_all_ks: bool,
+        write_all_ks: bool,
+        cluster_pos_table: bool,
+        cluster_abundance_table: bool,
+        verify_times: bool,
+        brotli_level: int,
+        max_procs: int,
+        force: bool) -> list[Path]:
+    """ Infer alternative structures by clustering reads' mutations. """
+    report_files = path.find_files_chain(
+        input_path, load_mask_dataset.report_path_seg_types
+    )
+    return dispatch(cluster,
+                    max_procs,
+                    pass_n_procs=True,
+                    args=as_list_of_tuples(report_files),
+                    kwargs=dict(tmp_pfx=tmp_pfx,
+                                keep_tmp=keep_tmp,
+                                min_clusters=min_clusters,
+                                max_clusters=max_clusters,
+                                em_runs=em_runs,
+                                min_iter=min_em_iter,
+                                max_iter=max_em_iter,
+                                em_thresh=em_thresh,
+                                min_marcd_run=min_marcd_run,
+                                max_pearson_run=max_pearson_run,
+                                jackpot=jackpot,
+                                jackpot_conf_level=jackpot_conf_level,
+                                max_jackpot_quotient=max_jackpot_quotient,
+                                max_loglike_vs_best=max_loglike_vs_best,
+                                min_pearson_vs_best=min_pearson_vs_best,
+                                max_marcd_vs_best=max_marcd_vs_best,
+                                try_all_ks=try_all_ks,
+                                write_all_ks=write_all_ks,
+                                cluster_pos_table=cluster_pos_table,
+                                cluster_abundance_table=cluster_abundance_table,
+                                verify_times=verify_times,
+                                brotli_level=brotli_level,
+                                force=force))
+
+
+params = [
+    # Input files
+    arg_input_path,
+    # Clustering options
+    opt_min_clusters,
+    opt_max_clusters,
+    opt_em_runs,
+    opt_em_thresh,
+    opt_min_em_iter,
+    opt_max_em_iter,
+    opt_max_pearson_run,
+    opt_min_marcd_run,
+    opt_jackpot,
+    opt_jackpot_conf_level,
+    opt_max_jackpot_quotient,
+    opt_max_loglike_vs_best,
+    opt_min_pearson_vs_best,
+    opt_max_marcd_vs_best,
+    opt_try_all_ks,
+    opt_write_all_ks,
+    # Table options
+    opt_cluster_pos_table,
+    opt_cluster_abundance_table,
+    # Validation
+    opt_verify_times,
+    # Compression
+    opt_brotli_level,
+    # Parallelization
+    opt_max_procs,
+    # Effort
+    opt_force,
+    opt_tmp_pfx,
+    opt_keep_tmp,
+]
+
+
+@command(CMD_CLUSTER, params=params)
+def cli(*args, **kwargs):
+    """ Infer structure ensembles by clustering reads' mutations. """
+    return run(*args, **kwargs)
---
+++ packages/seismic-rna/seismic_rna-0.23.0-cp313-cp313-musllinux_1_2_x86_64.whl/seismicrna/relate/io.py
@@ -0,0 +1,98 @@
+from abc import ABC
+from collections import defaultdict
+from typing import Any, Iterable
+
+import numpy as np
+
+from .batch import ReadNamesBatch, RelateBatch
+from ..core import path
+from ..core.io import MutsBatchIO, ReadBatchIO, RefIO
+from ..core.logs import logger
+from ..core.seq import DNA, Region
+from ..core.types import fit_uint_type
+
+
+class RelateIO(RefIO, ABC):
+
+    @classmethod
+    def auto_fields(cls):
+        return super().auto_fields() | {path.CMD: path.RELATE_STEP}
+
+
+class ReadNamesBatchIO(ReadBatchIO, RelateIO, ReadNamesBatch):
+
+    @classmethod
+    def file_seg_type(cls):
+        return path.ReadNamesBatSeg
+
+    def __getstate__(self):
+        state = super().__getstate__()
+        # Compress the names from unicode to bytes before pickling.
+        state["names"] = np.char.encode(self.names)
+        return state
+
+    def __setstate__(self, state: dict[str, Any]):
+        super().__setstate__(state)
+        # Decompress the names from bytes to unicode when unpickling.
+        self.names = np.char.decode(state["names"])
+
+
+class RelateBatchIO(MutsBatchIO, RelateIO, RelateBatch):
+
+    @classmethod
+    def file_seg_type(cls):
+        return path.RelateBatSeg
+
+
+def from_reads(reads: Iterable[tuple[str,
+                                     tuple[tuple[list[int], list[int]],
+                                           dict[int, int]]]],
+               sample: str,
+               ref: str,
+               refseq: DNA,
+               batch: int,
+               write_read_names: bool):
+    """ Accumulate reads into relation vectors. """
+    # Initialize empty data.
+    names = list()
+    seg_end5s = list()
+    seg_end3s = list()
+    muts = {pos: defaultdict(list) for pos in range(1, len(refseq) + 1)}
+    # Collect the mutation data from the reads.
+    for (name, ((end5s, end3s), poss)) in reads:
+        if all(end5 > end3 for end5, end3 in zip(end5s, end3s, strict=True)):
+            # Skip a read if no segment has any coverage.
+            logger.warning(f"Skipped read {repr(name)} with 5' end(s) {end5s} "
+                           f"> 3' end(s) {end3s}")
+        else:
+            # Otherwise, add the data for the read to the batch.
+            read = len(names)
+            names.append(name)
+            seg_end5s.append(end5s)
+            seg_end3s.append(end3s)
+            for pos, rel in poss.items():
+                muts[pos][rel].append(read)
+    # Make sure seg_end5s and seg_end3s have two dimensions and at least
+    # one column each.
+    pos_dtype = fit_uint_type(max(muts))
+    seg_end5s = np.array(seg_end5s, dtype=pos_dtype)
+    if seg_end5s.ndim < 2:
+        seg_end5s = seg_end5s[:, np.newaxis]
+    seg_end3s = np.array(seg_end3s, dtype=pos_dtype)
+    if seg_end3s.ndim < 2:
+        seg_end3s = seg_end3s[:, np.newaxis]
+    # Assemble and return the batches.
+    relate_batch = RelateBatchIO(sample=sample,
+                                 batch=batch,
+                                 region=Region(ref, refseq),
+                                 seg_end5s=seg_end5s,
+                                 seg_end3s=seg_end3s,
+                                 muts=muts)
+    if write_read_names:
+        name_batch = ReadNamesBatchIO(sample=sample,
+                                      ref=ref,
+                                      batch=batch,
+                                      names=names)
+    else:
+        name_batch = None
+    return relate_batch, name_batch
