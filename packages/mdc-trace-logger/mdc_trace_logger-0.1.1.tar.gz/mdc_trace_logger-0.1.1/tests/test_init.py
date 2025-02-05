31d45ca7d56fdd21c4b576ae5ad5ef0ca2c167c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 04:05:31 +0000

    Add package mdc-trace-logger/0.1.1/mdc_trace_logger-0.1.1.tar.gz

---
+++ packages/mdc-trace-logger/mdc_trace_logger-0.1.1.tar.gz/mdc_trace_logger-0.1.1/tests/test_init.py
@@ -0,0 +1,6 @@
+from src.mdc_trace_logger import get_logger, MDC
+
+def test_init_imports():
+    """Ensure `get_logger` and `MDC` are correctly imported."""
+    assert get_logger is not None
+    assert MDC is not None
