74773cf5a903d67e15c2e36a5b7d75541e2481f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 15:27:04 +0000

    Add package sinking/0.3.3/sinking-0.3.3.tar.gz

---
+++ packages/sinking/sinking-0.3.3.tar.gz/sinking-0.3.3/sinking/source.py
@@ -0,0 +1,41 @@
+import dataclasses
+import pathlib
+import re
+
+from .options import Options
+from .utils import create_pattern
+
+
+@dataclasses.dataclass
+class SourceFile:
+    path: pathlib.Path
+    destination: pathlib.Path = dataclasses.field(
+        repr=False, default=pathlib.Path("")
+    )
+
+    @property
+    def name(self) -> str:
+        o = Options()
+        name = self.path.name
+        if o.rename and self.needs_rename:
+            name = f"{self.path.parts[-2]}{self.path.suffix}"
+        return name.replace(" ", ".")
+
+    @property
+    def pattern(self) -> str:
+        return create_pattern(self.name)
+
+    @property
+    def needs_rename(self) -> bool:
+        return self.path.parts[-2][:6] != self.path.parts[-1][:6]
+
+    @property
+    def excluded(self) -> bool:
+        o = Options()
+        if re.search(r"sample", self.name, flags=re.IGNORECASE) is not None:
+            return True
+        if o.excludes:
+            return any([True for e in o.excludes if e in self.name])
+        if o.includes:
+            return not any([True for i in o.includes if i in self.name])
+        return False
