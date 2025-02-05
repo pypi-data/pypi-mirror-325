d9f97aacc427aa0a719c38a8146474f1c5a33c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-05 08:06:02 +0000

    Add package xcofdk/2.1/xcofdk-2.1-py3-none-any.whl

---
+++ packages/xcofdk/xcofdk-2.1-py3-none-any.whl/xcofdk/fwapi/xmsg/__init__.py
@@ -0,0 +1,21 @@
+# #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# File   : __init__.py
+#
+# Copyright(c) 2024 Farzad Safa (farzad.safa@xcofdk.de)
+# This software is distributed under the MIT License (http://opensource.org/licenses/MIT).
+# ------------------------------------------------------------------------------
+
+
+"""
+Package 'xcofdk.fwapi.xmsg' provides framework's public API for the subsystem
+of messaging.
+"""
+
+# ------------------------------------------------------------------------------
+# Interface
+# ------------------------------------------------------------------------------
+from .xmsg     import XMessage
+from .xpayload import XPayload
+
