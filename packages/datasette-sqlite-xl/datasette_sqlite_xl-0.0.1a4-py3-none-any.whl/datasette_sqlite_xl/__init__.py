2a92fef767d42718abb20ba1bd176bf0de224a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:26 +0000

    Add package datasette-sqlite-xl/0.0.1a4/datasette_sqlite_xl-0.0.1a4-py3-none-any.whl

---
+++ packages/datasette-sqlite-xl/datasette_sqlite_xl-0.0.1a4-py3-none-any.whl/datasette_sqlite_xl/__init__.py
@@ -0,0 +1,12 @@
+
+from datasette import hookimpl
+import sqlite_xl
+
+__version__ = "0.0.1a4"
+__version_info__ = tuple(__version__.split("."))
+
+@hookimpl
+def prepare_connection(conn):
+  conn.enable_load_extension(True)
+  sqlite_xl.load(conn)
+  conn.enable_load_extension(False)
