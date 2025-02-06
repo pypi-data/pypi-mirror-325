23b34293a881b9be23045baa3d56be30da32db03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:48 +0000

    Add package mat3ra-utils/2025.2.6.post0/mat3ra_utils-2025.2.6.post0.tar.gz

---
+++ packages/mat3ra-utils/mat3ra_utils-2025.2.6.post0.tar.gz/mat3ra_utils-2025.2.6.post0/src/js/index.ts
@@ -0,0 +1,34 @@
+import * as array from "./shared/array";
+import * as assertion from "./shared/assertion";
+import * as cls from "./shared/class";
+import * as clone from "./shared/clone";
+import * as constants from "./shared/constants";
+import * as hash from "./shared/hash";
+import * as math from "./shared/math";
+import * as object from "./shared/object";
+import * as selector from "./shared/selector";
+import * as specific from "./shared/specific";
+import * as str from "./shared/str";
+import * as tree from "./shared/tree";
+import * as url from "./shared/url";
+import * as uuid from "./shared/uuid";
+
+export const sharedUtils = {
+    array,
+    cls,
+    clone,
+    constants,
+    hash,
+    math,
+    object,
+    selector,
+    specific,
+    str,
+    tree,
+    url,
+    uuid,
+    assertion,
+};
+
+export const Utils = sharedUtils;
+export default { ...Utils };
