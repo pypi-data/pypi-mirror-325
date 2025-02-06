e3a1c2e341c8ea4b971edd846ee4a3a6a8ed0490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:06:29 +0000

    Add package vellum-workflow-server/0.13.24.post3/vellum_workflow_server-0.13.24.post3.tar.gz

---
+++ packages/vellum-workflow-server/vellum_workflow_server-0.13.24.post3.tar.gz/vellum_workflow_server-0.13.24.post3/src/workflow_server/utils/sentry.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from typing import Optional
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+
+def before_send(event: dict, hint: dict) -> Optional[dict]:
+    if "exc_info" in hint:
+        _, _, _ = hint["exc_info"]
+
+    return event
+
+
+def init_sentry(*, sentry_log_level: Optional[int], dsn: str) -> None:
+    SENTRY_DSN = os.getenv("SENTRY_DSN")
+    if SENTRY_DSN:
+        init_sentry(sentry_log_level=logging.INFO, dsn=SENTRY_DSN)
+
+    sentry_logging = LoggingIntegration(
+        level=sentry_log_level,  # Capture info and above as breadcrumbs
+        event_level=logging.ERROR,  # Send errors as events
+    )
+    integrations = [sentry_logging]
+
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=integrations,
+        environment="production",
+        traces_sample_rate=0.0,
+        send_default_pii=True,
+        before_send=before_send,  # type: ignore
+        max_request_body_size="always",
+        max_value_length=10_000,
+    )
