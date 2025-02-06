3b44bd126130d1735efa5d58d33a5011eb0576cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:16:45 +0000

    Add package ImageUI/0.6/imageui-0.6.tar.gz

---
+++ packages/ImageUI/imageui-0.6.tar.gz/imageui-0.6/ImageUI/__init__.py
@@ -0,0 +1,16 @@
+from .settings import SourceLanguage
+from .settings import DestinationLanguage
+from .settings import DevelopmentMode
+from .settings import CachePath
+from .settings import FontSize
+from .settings import FontType
+from .settings import CornerRoundness
+
+from .translations import SetTranslator
+from .translations import ManualTranslation
+from .translations import GetAvailableLanguages
+from .translations import SaveCache
+
+from .ImageUI import Button
+from .ImageUI import Update
+from .ImageUI import Exit
---
+++ packages/ImageUI/imageui-0.6.tar.gz/imageui-0.6/ImageUI/states.py
@@ -0,0 +1,16 @@
+Frame = None
+FrameWidth = 0
+FrameHeight = 0
+
+MouseX = 0
+MouseY = 0
+
+TopMostLayer = 0
+
+LeftClicked = False
+RightClicked = False
+LastLeftClicked = False
+LastRightClicked = False
+
+ForegroundWindow = False
+LastForegroundWindow = False
