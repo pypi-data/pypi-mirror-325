430f8fed7ee30c1661856baee052803c857991a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:32:01 +0000

    Add package slangpy/0.19.2/slangpy-0.19.2.tar.gz

---
+++ packages/slangpy/slangpy-0.19.2.tar.gz/slangpy-0.19.2/examples/first_function/main_numpy.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import slangpy as spy
+import pathlib
+import numpy as np
+
+# Create an SGL device with the local folder for slangpy includes
+device = spy.create_device(include_paths=[
+    pathlib.Path(__file__).parent.absolute(),
+])
+
+# Load module
+module = spy.Module.load_from_file(device, "example.slang")
+
+# Create a couple of buffers with 1,000,000 random floats in
+a = np.random.rand(1000000).astype(np.float32)
+b = np.random.rand(1000000).astype(np.float32)
+
+# Call our function and ask for a numpy array back (the default would be a buffer)
+result = module.add(a, b, _result='numpy')
+
+# Print the first 10
+print(result[:10])
---
+++ packages/slangpy/slangpy-0.19.2.tar.gz/slangpy-0.19.2/examples/first_function/main_scalar.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import slangpy as spy
+import pathlib
+
+# Create an SGL device with the local folder for slangpy includes
+device = spy.create_device(include_paths=[
+    pathlib.Path(__file__).parent.absolute(),
+])
+
+# Load module
+module = spy.Module.load_from_file(device, "example.slang")
+
+# Call the function and print the result
+result = module.add(1.0, 2.0)
+print(result)
+
+# SlangPy also supports named parameters
+result = module.add(a=1.0, b=2.0)
+print(result)
