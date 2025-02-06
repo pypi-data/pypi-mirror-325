045020c13b0ba5ab2a60fd642cf9075bc1e5c6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:16:48 +0000

    Add package opensafely/1.53.0/opensafely-1.53.0.tar.gz

---
+++ packages/opensafely/opensafely-1.53.0.tar.gz/opensafely-1.53.0/opensafely/info.py
@@ -0,0 +1,52 @@
+import json
+import subprocess
+
+import opensafely
+from opensafely import pull
+
+
+DESCRIPTION = "Print information about the opensafely tool and available resources"
+
+
+def add_arguments(parser):
+    pass
+
+
+INFO = """
+opensafely version: {version}
+docker version: {docker_version}
+docker memory: {memory}
+docker cpu: {cpu}
+"""
+
+
+def main():
+    print("System information:")
+    try:
+        ps = subprocess.run(
+            ["docker", "info", "-f", "{{json .}}"], capture_output=True, text=True
+        )
+        info = json.loads(ps.stdout)
+
+        memory = float(info["MemTotal"]) / (1024**3)
+        print(
+            INFO.format(
+                version=opensafely.__version__,
+                docker_version=info["ServerVersion"],
+                memory=f"{memory:.1f}G",
+                cpu=info["NCPU"],
+            )
+        )
+    except Exception as exc:
+        raise RuntimeError(f"Error retreiving docker information: {exc}")
+
+    print("OpenSAFELY Docker image versions:")
+    try:
+        local_images = pull.get_local_images()
+        updates = pull.check_version(local_images)
+    except Exception as exc:
+        raise RuntimeError(f"Error retreiving image information: {exc}")
+    else:
+        for image, sha in sorted(local_images.items()):
+            update = "(needs update)" if image in updates else "(latest version)"
+            print(f" - {image:24}: {sha[7:15]} {update}")
