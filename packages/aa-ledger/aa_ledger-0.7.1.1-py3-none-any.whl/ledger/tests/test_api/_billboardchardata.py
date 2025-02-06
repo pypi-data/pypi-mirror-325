c737cab8d173632c44a5ee2c23b6489af64fe92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:39 +0000

    Add package aa-ledger/0.7.1.1/aa_ledger-0.7.1.1-py3-none-any.whl

---
+++ packages/aa-ledger/aa_ledger-0.7.1.1-py3-none-any.whl/ledger/tests/test_api/_billboardchardata.py
@@ -0,0 +1,271 @@
+noData = [
+    {
+        "billboard": {
+            "standard": {
+                "charts": None,
+                "rattingbar": None,
+                "workflowgauge": None,
+            },
+        },
+    }
+]
+
+CharmonthlyMarch = [
+    {
+        "billboard": {
+            "standard": {
+                "charts": {
+                    "title": "Donut Chart",
+                    "date": "2025-01-31",
+                    "categories": [
+                        "bounty",
+                        "contract",
+                        "donation",
+                        "ess",
+                        "insurance",
+                        "market",
+                        "mining",
+                        "production",
+                    ],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "CONTRACT": {"value": 100000.00, "mode": "income"},
+                            "DONATION": {"value": 100000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "INSURANCE": {"value": 100000.00, "mode": "cost"},
+                            "MARKET": {"value": 200000.00, "mode": "cost"},
+                            "MINING": {"value": 200000.00, "mode": "income"},
+                            "PRODUCTION": {"value": 100000.00, "mode": "cost"},
+                        }
+                    ],
+                },
+                "rattingbar": {
+                    "title": "Ratting Chart",
+                    "date": "2025-01-31",
+                    "categories": ["Bounty", "ESS", "Miscellaneous", "Mining"],
+                    "series": [
+                        {"date": "2024-03-16", "mining": 200000},
+                        {
+                            "date": "2024-03-19",
+                            "bounty": 200000,
+                            "ess": 1133333,
+                            "miscellaneous": 200000,
+                        },
+                    ],
+                },
+                "workflowgauge": {
+                    "title": "Workflow Chart",
+                    "date": "2025-01-31",
+                    "categories": ["bounty", "ess", "mining", "miscellaneous"],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "MINING": {"value": 200000.00, "mode": "income"},
+                            "MISCELLANEOUS": {"value": 200000.00, "mode": "income"},
+                        }
+                    ],
+                },
+            }
+        }
+    }
+]
+
+
+CharmonthlyMarchMulti = [
+    {
+        "billboard": {
+            "standard": {
+                "charts": {
+                    "title": "Donut Chart",
+                    "date": "2025-01-31",
+                    "categories": [
+                        "bounty",
+                        "contract",
+                        "donation",
+                        "ess",
+                        "insurance",
+                        "market",
+                        "mining",
+                        "production",
+                    ],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "CONTRACT": {"value": 100000.00, "mode": "income"},
+                            "DONATION": {"value": 100000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "INSURANCE": {"value": 100000.00, "mode": "cost"},
+                            "MARKET": {"value": 200000.00, "mode": "cost"},
+                            "MINING": {"value": 300000.00, "mode": "income"},
+                            "PRODUCTION": {"value": 100000.00, "mode": "cost"},
+                        }
+                    ],
+                },
+                "rattingbar": {
+                    "title": "Ratting Chart",
+                    "date": "2025-01-31",
+                    "categories": ["Bounty", "ESS", "Miscellaneous", "Mining"],
+                    "series": [
+                        {"date": "2024-03-16", "mining": 300000},
+                        {
+                            "date": "2024-03-19",
+                            "bounty": 200000,
+                            "ess": 1133333,
+                            "miscellaneous": 200000,
+                        },
+                    ],
+                },
+                "workflowgauge": {
+                    "title": "Workflow Chart",
+                    "date": "2025-01-31",
+                    "categories": ["bounty", "ess", "mining", "miscellaneous"],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "MINING": {"value": 300000.00, "mode": "income"},
+                            "MISCELLANEOUS": {"value": 200000.00, "mode": "income"},
+                        }
+                    ],
+                },
+            }
+        }
+    }
+]
+
+Charyearly = [
+    {
+        "billboard": {
+            "standard": {
+                "charts": {
+                    "title": "Donut Chart",
+                    "date": "2025-01-31",
+                    "categories": [
+                        "bounty",
+                        "contract",
+                        "donation",
+                        "ess",
+                        "insurance",
+                        "market",
+                        "mining",
+                        "production",
+                    ],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 300000.00, "mode": "income"},
+                            "CONTRACT": {"value": 100000.00, "mode": "income"},
+                            "DONATION": {"value": 100000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "INSURANCE": {"value": 100000.00, "mode": "cost"},
+                            "MARKET": {"value": 200000.00, "mode": "cost"},
+                            "MINING": {"value": 200000.00, "mode": "income"},
+                            "PRODUCTION": {"value": 100000.00, "mode": "cost"},
+                        }
+                    ],
+                },
+                "rattingbar": {
+                    "title": "Ratting Chart",
+                    "date": "2025-01-31",
+                    "categories": ["Bounty", "ESS", "Miscellaneous", "Mining"],
+                    "series": [
+                        {"date": "2024-01", "bounty": 100000},
+                        {
+                            "date": "2024-03",
+                            "bounty": 200000,
+                            "ess": 1133333,
+                            "miscellaneous": 200000,
+                            "mining": 200000,
+                        },
+                    ],
+                },
+                "workflowgauge": {
+                    "title": "Workflow Chart",
+                    "date": "2025-01-31",
+                    "categories": [
+                        "bounty",
+                        "ess",
+                        "mining",
+                        "miscellaneous",
+                    ],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 300000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "MINING": {"value": 200000.00, "mode": "income"},
+                            "MISCELLANEOUS": {"value": 200000.00, "mode": "income"},
+                        }
+                    ],
+                },
+            }
+        }
+    }
+]
+
+CharNoMining = [
+    {
+        "billboard": {
+            "standard": {
+                "charts": {
+                    "title": "Donut Chart",
+                    "date": "2025-01-31",
+                    "categories": [
+                        "bounty",
+                        "contract",
+                        "donation",
+                        "ess",
+                        "insurance",
+                        "market",
+                        "production",
+                    ],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "CONTRACT": {"value": 100000.00, "mode": "income"},
+                            "DONATION": {"value": 100000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "INSURANCE": {"value": 100000.00, "mode": "cost"},
+                            "MARKET": {"value": 200000.00, "mode": "cost"},
+                            "PRODUCTION": {"value": 100000.00, "mode": "cost"},
+                        }
+                    ],
+                },
+                "rattingbar": {
+                    "title": "Ratting Chart",
+                    "date": "2025-01-31",
+                    "categories": ["Bounty", "ESS", "Miscellaneous", "Mining"],
+                    "series": [
+                        {
+                            "date": "2024-03-19",
+                            "bounty": 200000,
+                            "ess": 1133333,
+                            "miscellaneous": 200000,
+                        }
+                    ],
+                },
+                "workflowgauge": {
+                    "title": "Workflow Chart",
+                    "date": "2025-01-31",
+                    "categories": ["bounty", "ess", "mining", "miscellaneous"],
+                    "series": [
+                        {
+                            "date": "2025-01-31",
+                            "BOUNTY": {"value": 200000.00, "mode": "income"},
+                            "ESS": {"value": 1133333.33, "mode": "income"},
+                            "MINING": {"value": 0.00, "mode": None},
+                            "MISCELLANEOUS": {"value": 200000.00, "mode": "income"},
+                        }
+                    ],
+                },
+            }
+        }
+    }
+]
