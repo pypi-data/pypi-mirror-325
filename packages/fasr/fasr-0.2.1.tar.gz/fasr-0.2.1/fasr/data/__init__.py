096cdf34da5957415195185d0b546b62053197d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:16 +0000

    Add package fasr/0.2.1/fasr-0.2.1.tar.gz

---
+++ packages/fasr/fasr-0.2.1.tar.gz/fasr-0.2.1/fasr/data/__init__.py
@@ -0,0 +1,34 @@
+from .audio import (
+    Waveform,
+    Audio,
+    AudioList,
+    AudioChannel,
+    AudioChannelList,
+    AudioSpan,
+    AudioSpanList,
+    AudioToken,
+    AudioTokenList,
+    AudioQueue,
+    AudioExample,
+    AudioEvaluation,
+    AudioChunk,
+    AudioChunkList,
+)
+
+
+__all__ = [
+    "Waveform",
+    "Audio",
+    "AudioList",
+    "AudioChannel",
+    "AudioChannelList",
+    "AudioSpan",
+    "AudioSpanList",
+    "AudioToken",
+    "AudioTokenList",
+    "AudioQueue",
+    "AudioExample",
+    "AudioEvaluation",
+    "AudioChunk",
+    "AudioChunkList",
+]
