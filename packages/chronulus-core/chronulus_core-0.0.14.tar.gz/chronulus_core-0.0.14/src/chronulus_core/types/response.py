9d9c41032cdfd9853fdc7d7ac698cfb02940eb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:06:37 +0000

    Add package chronulus-core/0.0.14/chronulus_core-0.0.14.tar.gz

---
+++ packages/chronulus-core/chronulus_core-0.0.14.tar.gz/chronulus_core-0.0.14/src/chronulus_core/types/response.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+from chronulus_core.types.risk import Scorecard
+
+
+class Response(BaseModel):
+    success: bool
+    message: str = Field(default="")
+
+
+class QueuePredictionResponse(Response):
+    request_id: str
+    prediction_ids: List[str] = Field(default=list())
+
+
+class SessionScorecardResponse(Response):
+    session_id: str
+    scorecard: Optional[Scorecard]
+
+
+class Session(BaseModel):
+    name: str
+    situation: str
+    task: str
+    session_id: str
+
+
+class SessionCreationResponse(Response):
+    session_id: str
+    flagged: bool = Field(default=False)
+
+
+class SessionGetResponse(Response):
+    session: Optional[Session] = Field(default=None)
+
+
+class EstimatorCreationResponse(Response):
+    estimator_id: Optional[str] = Field(default=None)
+
+
+class PredictionGetByIdResponse(Response):
+    response: Optional[dict] = Field(default=None)
+
+
+
+
