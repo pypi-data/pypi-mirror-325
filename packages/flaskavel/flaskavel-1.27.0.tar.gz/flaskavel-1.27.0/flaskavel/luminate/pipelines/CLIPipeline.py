fa8f9a33a9a58c65fe06d5d4b5f319cf7c40d973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:44 +0000

    Add package flaskavel/1.27.0/flaskavel-1.27.0.tar.gz

---
+++ packages/flaskavel/flaskavel-1.27.0.tar.gz/flaskavel-1.27.0/flaskavel/luminate/pipelines/CLIPipeline.py
@@ -0,0 +1,12 @@
+class CLIPipeline:
+    pass
+
+    # def __init_():
+    #     1. Cargar Comandos a Cache
+    #     2. Cargar Rutas (pass)
+    #     3. Cargar Middlewares (pass)
+    #     4. Obtener Variables (arguments)
+    #     5. Ejecutar Middleware (pass)
+    #     6. Ejecutar Comando
+    #     7. Ejecutar after.
+    #     8. finalizar proceso (Limpiar Hilo)
