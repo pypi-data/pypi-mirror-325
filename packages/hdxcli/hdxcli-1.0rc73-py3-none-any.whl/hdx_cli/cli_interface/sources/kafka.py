b126637f6f43f471721f180c79273f9c0b5aa2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:26:59 +0000

    Add package hdxcli/1.0rc73/hdxcli-1.0rc73-py3-none-any.whl

---
+++ packages/hdxcli/hdxcli-1.0rc73-py3-none-any.whl/hdx_cli/cli_interface/sources/kafka.py
@@ -0,0 +1,40 @@
+import click
+
+from ...library_api.utility.decorators import report_error_and_exit, ensure_logged_in
+from ...library_api.common.context import ProfileUserContext
+from ..common.rest_operations import (
+    delete as command_delete,
+    list_ as command_list,
+    show as command_show
+)
+from ..common.misc_operations import settings as command_settings
+from .common_commands import create as command_create
+from .common_commands import any_source_impl
+
+
+@click.group(help="Kafka source operations")
+@click.option('--project', 'project_name', help="Use or override project set in the profile.",
+              metavar='PROJECTNAME', default=None)
+@click.option('--table', 'table_name', help="Use or override table set in the profile.",
+              metavar='TABLENAME', default=None)
+@click.option('--source', 'source_name', help='Source for kinesis/kafka/summary/SIEM streams.',
+              metavar='SOURCENAME', default=None)
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+@ensure_logged_in
+def kafka(ctx: click.Context, project_name: str, table_name: str, source_name: str):
+    user_profile = ctx.parent.obj['usercontext']
+    ProfileUserContext.update_context(
+        user_profile,
+        projectname=project_name,
+        tablename=table_name,
+        kafkaname=source_name
+    )
+    any_source_impl(ctx, 'kafka')
+
+
+kafka.add_command(command_create)
+kafka.add_command(command_delete)
+kafka.add_command(command_list)
+kafka.add_command(command_show)
+kafka.add_command(command_settings)
---
+++ packages/hdxcli/hdxcli-1.0rc73-py3-none-any.whl/hdx_cli/library_api/ddl/extensions/__init__.py
@@ -0,0 +1,4 @@
+from .sql import SqlSourceToTableInfoProcessor, SqlComposedTypeParser
+from .elastic import (ElasticSourceToTableInfoProcessor,
+                      ElasticComposedTypeParser,
+                      ElasticPostProcessingHook)
