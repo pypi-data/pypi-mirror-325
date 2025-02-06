cf8ee3dd0046a7358616bcdbfd4d2db12f50f308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:12 +0000

    Add package baseapp-notifications/0.3.5/baseapp-notifications-0.3.5.tar.gz

---
+++ packages/baseapp-notifications/baseapp-notifications-0.3.5.tar.gz/baseapp-notifications-0.3.5/baseapp_notifications/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class NotificationsConfig(AppConfig):
+    default = True
+    name = "baseapp_notifications"
+    verbose_name = "BaseApp Notifications"
+    default_auto_field = "django.db.models.AutoField"
