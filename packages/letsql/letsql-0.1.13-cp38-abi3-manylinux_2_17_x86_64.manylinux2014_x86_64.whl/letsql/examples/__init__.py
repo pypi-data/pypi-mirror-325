6d46747dd1355df0b24023bb099ef8eb93fdf1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:33 +0000

    Add package letsql/0.1.13/letsql-0.1.13-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/letsql/letsql-0.1.13-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/letsql/examples/__init__.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+import letsql as ls
+
+
+@cache
+def cached_penguins():
+    import pandas as pd
+
+    url = "https://raw.githubusercontent.com/mesejo/palmerpenguins/master/palmerpenguins/data/penguins.csv"
+    return pd.read_csv(url)
+
+
+class Example:
+    def __init__(self, name, load):
+        self.con = None
+        self.load = load
+        self.name = name
+
+    def fetch(self, table_name=None):
+        if self.con is None:
+            self.con = ls.connect()
+        return self.con.register(self.load(), table_name=table_name or self.name)
+
+
+penguins = Example("penguins", cached_penguins)
