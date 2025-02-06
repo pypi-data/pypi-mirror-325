60b2818d7b423ccaaf9ae257b9fafb6d3bc50d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:11 +0000

    Add package zyphra/0.1.2/zyphra-0.1.2.tar.gz

---
+++ packages/zyphra/zyphra-0.1.2.tar.gz/zyphra-0.1.2/zyphra/__init__.py
@@ -0,0 +1,23 @@
+# zyphra-clients/python/zyphra/__init__.py
+
+from .client import ZyphraClient, AsyncZyphraClient, ZyphraError, AudioStreamOptions
+from .models import (
+    ModelType,
+    TTSParams,
+    EmotionWeights,
+    ConditioningSources,
+    ChatMessage,
+    ChatCompletionOptions,
+)
+
+__all__ = [
+    "ZyphraClient",
+    "AsyncZyphraClient",
+    "ZyphraError",
+    "ModelType",
+    "TTSParams",
+    "EmotionWeights",
+    "ConditioningSources",
+    "ChatMessage",
+    "ChatCompletionOptions",
+]
---
+++ packages/zyphra/zyphra-0.1.2.tar.gz/zyphra-0.1.2/zyphra/models/__init__.py
@@ -0,0 +1,12 @@
+from .base import ModelType
+from .audio import TTSParams, EmotionWeights, ConditioningSources
+from .chat import ChatMessage, ChatCompletionOptions
+
+__all__ = [
+    "ModelType",
+    "TTSParams",
+    "EmotionWeights",
+    "ConditioningSources",
+    "ChatMessage",
+    "ChatCompletionOptions",
+]
