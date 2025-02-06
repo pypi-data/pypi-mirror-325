92812f89d24f9ec5dde3d981df1758cc764c0768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:18:53 +0000

    Add package unitcellengine/0.0.12/unitcellengine-0.0.12-py3-none-any.whl

---
+++ packages/unitcellengine/unitcellengine-0.0.12-py3-none-any.whl/unitcellengine/__init__.py
@@ -0,0 +1,9 @@
+""" UnitcellEngine is a geometry and simulation package to model lattice structures """
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# from unitcellengine.design import UnitcellDesign, UnitcellDesigns
+
+__version__ = version = "0.0.12"
