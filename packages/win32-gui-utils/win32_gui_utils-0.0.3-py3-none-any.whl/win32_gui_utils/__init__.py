1f1e1a5a260b3a20729e246b783f54314b4a49b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:48 +0000

    Add package win32-gui-utils/0.0.3/win32_gui_utils-0.0.3-py3-none-any.whl

---
+++ packages/win32-gui-utils/win32_gui_utils-0.0.3-py3-none-any.whl/win32_gui_utils/__init__.py
@@ -0,0 +1,12 @@
+from .win32_gui_utils import (
+    click_button,
+    right_click,
+    write_text,
+    get_control_properties,
+    get_edit_text,
+    get_handle_by_id,
+    find_window,
+    focus_window,
+    select_combo_item,
+    wait_for_control
+)
