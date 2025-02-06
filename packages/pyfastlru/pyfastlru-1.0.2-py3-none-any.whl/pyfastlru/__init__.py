0d5cdf2ac25ae3a0c8f6547e8b584e5dbbaa0439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:28:05 +0000

    Add package pyfastlru/1.0.2/pyfastlru-1.0.2-py3-none-any.whl

---
+++ packages/pyfastlru/pyfastlru-1.0.2-py3-none-any.whl/pyfastlru/__init__.py
@@ -0,0 +1,6 @@
+# pyfastlru v. 1.0
+# Thread-safe LRU cache with the MutableMapping interface.
+# SPDX-FileCopyrightText: Copyright © 2025 Anatoly Petrov <petrov.projects@gmail.com>
+# SPDX-License-Identifier: MIT
+
+"""Lru cache (thread-safe)."""
