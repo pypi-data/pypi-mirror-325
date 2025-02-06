8ffe45193ee70c2c3f3be45c891555f73eb8e675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:18:55 +0000

    Add package detkit/0.11.0/detkit-0.11.0-cp39-cp39-win_amd64.whl

---
+++ packages/detkit/detkit-0.11.0-cp39-cp39-win_amd64.whl/detkit/_device/c_instructions_counter.h
@@ -0,0 +1,50 @@
+/*
+ *  SPDX-FileCopyrightText: Copyright 2022, Siavash Ameli <sameli@berkeley.edu>
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  SPDX-FileType: SOURCE
+ *
+ *  This program is free software: you can redistribute it and/or modify it
+ *  under the terms of the license found in the LICENSE.txt file in the root
+ *  directory of this source tree.
+ */
+
+
+#ifndef _DEVICE_INSTRUCTIONS_COUNTER_H_
+#define _DEVICE_INSTRUCTIONS_COUNTER_H_
+
+// =======
+// Headers
+// =======
+
+#if __linux__
+    #include <linux/perf_event.h>
+#endif
+
+
+// ======================
+// c Instructions Counter
+// ======================
+
+class cInstructionsCounter
+{
+    public:
+        cInstructionsCounter();
+        ~cInstructionsCounter();
+        void set_inst_per_flop(double factor);
+        void start();
+        void stop();
+        void reset();
+        long long get_count();
+        long long get_flops();
+        
+    // Member data
+    private:
+        #if __linux__
+        struct perf_event_attr pe;
+        #endif
+        int fd;
+        long long count;
+        double inst_per_flop;
+};
+
+#endif  // _DEVICE_INSTRUCTIONS_COUNTER_H_
