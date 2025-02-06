bd7292e7d47e43dce0a87a02446895ee2d2c189e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:09:54 +0000

    Add package neo-library/0.2/neo_library-0.2.tar.gz

---
+++ packages/neo-library/neo_library-0.2.tar.gz/neo_library-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neo_library",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=["requests"],  # List dependencies here
+)
