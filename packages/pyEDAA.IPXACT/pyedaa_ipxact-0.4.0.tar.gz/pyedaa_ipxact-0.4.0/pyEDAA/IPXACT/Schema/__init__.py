d1466fa6efea9f80b291c1d10b027a3132d86bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:26 +0000

    Add package pyEDAA.IPXACT/0.4.0/pyedaa_ipxact-0.4.0.tar.gz

---
+++ packages/pyEDAA.IPXACT/pyedaa_ipxact-0.4.0.tar.gz/pyedaa_ipxact-0.4.0/pyEDAA/IPXACT/Schema/__init__.py
@@ -0,0 +1,62 @@
+"""
+This package is a resource package containing various data files.
+
+.. rubric:: Common Files
+
+* :file:`README.md`
+* :file:`LICENSE.md`
+* :file:`NOTICE.md`
+
+.. rubric:: XML Schema Files
+
+* :file:`ipxact-1.0/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ipxact-1.1/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ipxact-1.2/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ipxact-1.4/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ipxact-1.5/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ieee-1685-2009/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ieee-1685-2014/`
+
+  * :file:`README.md`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+* :file:`ieee-1685-2022/`
+
+  * :file:`README.md`
+  * :file:`LICENSE`
+  * :file:`NOTICE`
+  * :file:`index.xsd`
+  * :file:`***.xsd`
+
+"""
