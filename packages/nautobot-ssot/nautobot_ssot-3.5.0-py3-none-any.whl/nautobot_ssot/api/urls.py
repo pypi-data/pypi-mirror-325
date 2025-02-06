aac4050d88be74da68c2735006560a30ee9f17f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:16:57 +0000

    Add package nautobot-ssot/3.5.0/nautobot_ssot-3.5.0-py3-none-any.whl

---
+++ packages/nautobot-ssot/nautobot_ssot-3.5.0-py3-none-any.whl/nautobot_ssot/api/urls.py
@@ -0,0 +1,20 @@
+"""Django API urlpatterns declaration for nautobot_ssot app."""
+
+from nautobot.apps.api import OrderedDefaultRouter
+
+from nautobot_ssot.integrations.utils import each_enabled_integration_module
+
+app_name = "nautobot_ssot-api"
+urlpatterns = []
+router = OrderedDefaultRouter()
+# add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
+
+
+def _add_integrations():
+    for module in each_enabled_integration_module("api.urls"):
+        urlpatterns.extend(module.urlpatterns)
+
+
+_add_integrations()
+
+urlpatterns += router.urls
