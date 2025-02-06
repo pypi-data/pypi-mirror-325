7aeb2670f957e33994af7932142443302c2a6633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:45 +0000

    Add package sqlalchemy-service/0.2.8a0/sqlalchemy_service-0.2.8a0-py3-none-any.whl

---
+++ packages/sqlalchemy-service/sqlalchemy_service-0.2.8a0-py3-none-any.whl/sqlalchemy_service/base_db/base.py
@@ -0,0 +1,57 @@
+"""Module with database connection pool and engine"""
+
+import asyncio
+
+from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+logger.debug('Initialize service.base_db.base')
+
+from db_configure import DBConfigurator
+
+db_configurator = DBConfigurator()
+DATABASE_URL = db_configurator.get_url()
+
+engine = create_async_engine(
+    DATABASE_URL,
+    pool_size=5,
+    max_overflow=0,
+    pool_reset_on_return=True,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=True,
+    autocommit=False,
+)
+
+
+async def init_models():
+    """Migrate models to DB. Better to use alembic"""
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    logger.info('Models initialisation is done')
+
+
+def run_init_models():
+    asyncio.run(init_models())
+
+
+async def get_session():
+    """Generator for database session. Need to be closed"""
+    async with async_session() as session:
+        yield session
+
+
+if __name__ == "__main__":
+    run_init_models()
