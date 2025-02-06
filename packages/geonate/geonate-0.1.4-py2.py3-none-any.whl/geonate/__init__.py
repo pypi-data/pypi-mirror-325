8abf0fdf2218c0a66e42e4768565b8d7ad38e7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:32:01 +0000

    Add package geonate/0.1.4/geonate-0.1.4-py2.py3-none-any.whl

---
+++ packages/geonate/geonate-0.1.4-py2.py3-none-any.whl/geonate/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for geonate."""
+
+__author__ = """Can Nguyen""" 
+__email__ = "ntcan.lres@gmail.com"
+__version__ = "0.1.4"
+
+
+# =================================================================================================================== #
+
+from .geonate import *
