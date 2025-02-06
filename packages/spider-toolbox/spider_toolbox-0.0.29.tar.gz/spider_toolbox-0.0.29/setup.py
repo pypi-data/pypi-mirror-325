5287663f7f4e2ec6b3c91d924b5a7a5a2d31cd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:16:41 +0000

    Add package spider-toolbox/0.0.29/spider_toolbox-0.0.29.tar.gz

---
+++ packages/spider-toolbox/spider_toolbox-0.0.29.tar.gz/spider_toolbox-0.0.29/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.29'
+name = 'spider_toolbox'
+author = 'neco_arc'
+description = '爬虫工具库'
+email = '3306601284@qq.com'
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name=name,
+    version=VERSION,
+    author=author,
+    author_email=email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sweetnotice/spider_toolbox',
+    package_dir={'spider_toolbox': 'src'},
+    packages=['spider_toolbox'],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'PyExecJS',
+        'rich',
+        'pycryptodome'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
