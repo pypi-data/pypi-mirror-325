fcd7afa41e620ce5535badd5056c749972e673c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 04:16:15 +0000

    Add package dfm-tools/0.34.0/dfm_tools-0.34.0.tar.gz

---
+++ packages/dfm-tools/dfm_tools-0.34.0.tar.gz/dfm_tools-0.34.0/tests/test_xarray_helpers.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 17:12:48 2024
+
+@author: veenstra
+"""
+
+import pytest
+import dfm_tools as dfmt
+import pandas as pd
+import xarray as xr
+
+
+@pytest.mark.unittest
+@pytest.mark.requiressecrets
+@pytest.mark.era5slow # temporarily skip these on github
+@pytest.mark.timeout(60) # useful since CDS downloads are terribly slow sometimes, so skip in that case
+def test_merge_meteofiles(file_nc_era5_pattern):
+    # file_nc_era5_pattern comes from file_nc_era5_pattern() in conftest.py
+    ds = dfmt.merge_meteofiles(file_nc=file_nc_era5_pattern, 
+                               preprocess=dfmt.preprocess_ERA5, 
+                               time_slice=slice("2010-01-30","2010-02-01")
+                               )
+    assert ds.sizes["time"] == 72
+    assert ds.time.to_pandas().iloc[0] == pd.Timestamp('2010-01-30')
+    assert ds.time.to_pandas().iloc[-1] == pd.Timestamp('2010-02-01 23:00')
+    assert "msl" in ds.data_vars
+
+
+@pytest.mark.unittest
+def test_preprocess_hisnc():
+    """
+    not too much added value, but good to check dropping of duplicated labels
+    in this case it happens for source_sinks, not sure if this is useful here.
+    More useful would be a hisfile with duplicated station names like
+    p:\\archivedprojects\\11206813-006-kpp2021_rmm-2d\\C_Work\\31_RMM_FMmodel
+    \\computations\\model_setup\\run_206\\results\\RMM_dflowfm_0000_his.nc
+    """
+    file_nc = dfmt.data.fm_grevelingen_his(return_filepath=True)
+    ds1 = xr.open_dataset(file_nc)
+    ds2 = xr.open_mfdataset(file_nc, preprocess=dfmt.preprocess_hisnc)
+    assert ds1.sizes['source_sink'] == 46
+    assert ds2.sizes['source_sink'] == 1
