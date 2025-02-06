b52b5776efb5914f9542d3ec2e308ee4a48344fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:18:52 +0000

    Add package realtimetts/0.4.44/realtimetts-0.4.44-py3-none-any.whl

---
+++ packages/realtimetts/realtimetts-0.4.44-py3-none-any.whl/RealtimeTTS/engines/__init__.py
@@ -0,0 +1,63 @@
+from .base_engine import BaseEngine  # noqa: F401
+
+# Optional dependencies
+try:
+    from .azure_engine import AzureEngine, AzureVoice  # noqa: F401
+    import azure.cognitiveservices.speech as tts
+except ImportError:
+    AzureEngine, AzureVoice, tts = None, None, None
+
+try:
+    from .system_engine import SystemEngine, SystemVoice  # noqa: F401
+    import pyttsx3
+except ImportError:
+    SystemEngine, SystemVoice, pyttsx3 = None, None, None
+
+try:
+    from .elevenlabs_engine import ElevenlabsEngine, ElevenlabsVoice  # noqa: F401
+    from elevenlabs.client import ElevenLabs
+except ImportError:
+    ElevenlabsEngine, ElevenlabsVoice, ElevenLabs = None, None, None
+
+try:
+    from .coqui_engine import CoquiEngine, CoquiVoice  # noqa: F401
+    from TTS.utils.manage import ModelManager
+except ImportError:
+    CoquiEngine, CoquiVoice, ModelManager = None, None, None
+
+try:
+    from .openai_engine import OpenAIEngine, OpenAIVoice  # noqa: F401
+    from openai import OpenAI
+except ImportError:
+    OpenAIEngine, OpenAIVoice, OpenAI = None, None, None
+
+try:
+    from .gtts_engine import GTTSEngine, GTTSVoice  # noqa: F401
+    from gtts import gTTS
+except ImportError:
+    GTTSEngine, GTTSVoice, gTTS = None, None, None
+
+try:
+    from .parler_engine import ParlerEngine, ParlerVoice  # noqa: F401
+except ImportError as e:
+    ParlerEngine, ParlerVoice = None, None
+
+try:
+    from .edge_engine import EdgeEngine, EdgeVoice  # noqa: F401
+except ImportError as e:
+    EdgeEngine, EdgeVoice = None, None
+
+try:
+    from .style_engine import StyleTTSEngine, StyleTTSVoice  # noqa: F401
+except ImportError as e:
+    StyleTTSEngine, StyleTTSVoice = None
+
+try:
+    from .piper_engine import PiperEngine, PiperVoice  # noqa: F401
+except ImportError as e:
+    PiperEngine, PiperVoice = None
+
+try:
+    from .kokoro_engine import KokoroEngine  # noqa: F401
+except ImportError as e:
+    KokoroEngine = None
