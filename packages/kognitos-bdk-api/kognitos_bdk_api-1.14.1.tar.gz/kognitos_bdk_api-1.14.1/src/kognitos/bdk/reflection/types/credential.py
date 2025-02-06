4a3b5407a3c1ed1361016a5fb3f4198bc109cf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 04:05:40 +0000

    Add package kognitos-bdk-api/1.14.1/kognitos_bdk_api-1.14.1.tar.gz

---
+++ packages/kognitos-bdk-api/kognitos_bdk_api-1.14.1.tar.gz/kognitos_bdk_api-1.14.1/src/kognitos/bdk/reflection/types/credential.py
@@ -0,0 +1,15 @@
+class CredentialType:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.name == other.name
+        return False
+
+
+class CredentialScalarType:
+    TEXT = CredentialType("Text")
