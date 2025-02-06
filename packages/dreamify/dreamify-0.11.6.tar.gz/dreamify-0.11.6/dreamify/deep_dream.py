aedfdf48f4a6135d43ceaa27df8c82e4431f13e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:28:08 +0000

    Add package dreamify/0.11.6/dreamify-0.11.6.tar.gz

---
+++ packages/dreamify/dreamify-0.11.6.tar.gz/dreamify-0.11.6/dreamify/deep_dream.py
@@ -0,0 +1,124 @@
+import IPython.display as display
+import numpy as np
+import PIL.Image
+import tensorflow as tf
+
+
+def download(url, max_dim=None):
+    """Download an image and load it as a NumPy array."""
+    name = url.split("/")[-1]
+    image_path = tf.keras.utils.get_file(name, origin=url)
+    img = PIL.Image.open(image_path)
+    if max_dim:
+        img.thumbnail((max_dim, max_dim))
+    return np.array(img)
+
+
+def deprocess(img):
+    """Normalize image for display."""
+    img = 255 * (img + 1.0) / 2.0
+    return tf.cast(img, tf.uint8)
+
+
+def show(img):
+    """Display an image."""
+    display.display(PIL.Image.fromarray(np.array(img)))
+
+
+class DeepDream(tf.Module):
+    def __init__(self, model):
+        self.model = model
+
+    @tf.function(
+        input_signature=(
+            tf.TensorSpec(shape=[None, None, 3], dtype=tf.float32),
+            tf.TensorSpec(shape=[], dtype=tf.int32),
+            tf.TensorSpec(shape=[], dtype=tf.float32),
+        )
+    )
+    def __call__(self, img, steps, step_size):
+        print("Tracing DeepDream computation graph...")
+        loss = tf.constant(0.0)
+        for n in tf.range(steps):
+            with tf.GradientTape() as tape:
+                tape.watch(img)
+                loss = calc_loss(img, self.model)
+                gradients = tape.gradient(loss, img)
+            gradients /= tf.math.reduce_std(gradients) + 1e-8
+            img = img + gradients * step_size
+            img = tf.clip_by_value(img, -1, 1)
+
+        return loss, img
+
+
+def calc_loss(img, model):
+    """Calculate the DeepDream loss by maximizing activations."""
+    img_batch = tf.expand_dims(img, axis=0)
+    layer_activations = model(img_batch)
+    if len(layer_activations) == 1:
+        layer_activations = [layer_activations]
+
+    return tf.reduce_sum([tf.math.reduce_mean(act) for act in layer_activations])
+
+
+def run_deep_dream_simple(img, dream_model, steps=100, step_size=0.01):
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    img = tf.convert_to_tensor(img)
+    step_size = tf.convert_to_tensor(step_size)
+    steps_remaining = steps
+    step = 0
+    while steps_remaining:
+        if steps_remaining > 100:
+            run_steps = tf.constant(100)
+        else:
+            run_steps = tf.constant(steps_remaining)
+        steps_remaining -= run_steps
+        step += run_steps
+
+        loss, img = deepdream(img, run_steps, tf.constant(step_size))
+
+        display.clear_output(wait=True)
+        show(deprocess(img))
+        print("Step {}, loss {}".format(step, loss))
+
+    result = deprocess(img)
+    display.clear_output(wait=True)
+    show(result)
+
+    return result
+
+
+def main():
+    url = (
+        "https://storage.googleapis.com/download.tensorflow.org/"
+        "example_images/YellowLabradorLooking_new.jpg"
+    )
+
+    original_img = download(url, max_dim=500)
+    show(original_img)
+
+    display.display(
+        display.HTML(
+            'Image cc-by: <a href="https://commons.wikimedia.org/wiki/'
+            'File:Felis_catus-cat_on_snow.jpg">Von.grzanka</a>'
+        )
+    )
+
+    base_model = tf.keras.applications.InceptionV3(
+        include_top=False, weights="imagenet"
+    )
+
+    names = ["mixed3", "mixed5"]
+    layers = [base_model.get_layer(name).output for name in names]
+
+    dream_model = tf.keras.Model(inputs=base_model.input, outputs=layers)
+
+    deepdream = DeepDream(dream_model)
+
+    run_deep_dream_simple(
+        img=original_img, dream_model=deepdream, steps=100, step_size=0.01
+    )
+
+
+if __name__ == "__main__":
+    main()
