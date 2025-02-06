906d03a5612fecc1913eddf60a5355bf76a64660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:17:05 +0000

    Add package foundationallm/0.9.3rc12/foundationallm-0.9.3rc12.tar.gz

---
+++ packages/foundationallm/foundationallm-0.9.3rc12.tar.gz/foundationallm-0.9.3rc12/foundationallm/langchain/language_models/language_model_base.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+from langchain_core.embeddings import Embeddings
+from langchain_core.language_models import BaseLanguageModel
+
+from foundationallm.config import Configuration
+from foundationallm.models.language_models import EmbeddingModel
+
+class LanguageModelBase(ABC):
+    """Abstract base class for language models."""
+
+    def __init__(self, config: Configuration):
+        """
+        Initializer
+        
+        Parameters
+        ----------
+        app_config : Configuration
+            Application configuration class for retrieving configuration settings.
+        """
+        self.config = config
+
+    @abstractmethod
+    def get_embedding_model(self, embedding_model: EmbeddingModel) -> Embeddings:
+        """
+        Retrieve the embeddings model.
+        
+        Returns
+        -------
+        Embeddings
+            The embeddings large language model to use.
+        """
