2846ac3f0ae96a5670326ee211af5941df4270d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:44 +0000

    Add package types-boto3-full/1.36.14/types_boto3_full-1.36.14-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.36.14-py3-none-any.whl/types_boto3_connectcases/__init__.py
@@ -0,0 +1,33 @@
+"""
+Main interface for connectcases service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_connectcases/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_connectcases import (
+        Client,
+        ConnectCasesClient,
+        SearchCasesPaginator,
+        SearchRelatedItemsPaginator,
+    )
+
+    session = Session()
+    client: ConnectCasesClient = session.client("connectcases")
+
+    search_cases_paginator: SearchCasesPaginator = client.get_paginator("search_cases")
+    search_related_items_paginator: SearchRelatedItemsPaginator = client.get_paginator("search_related_items")
+    ```
+"""
+
+from .client import ConnectCasesClient
+from .paginator import SearchCasesPaginator, SearchRelatedItemsPaginator
+
+Client = ConnectCasesClient
+
+
+__all__ = ("Client", "ConnectCasesClient", "SearchCasesPaginator", "SearchRelatedItemsPaginator")
