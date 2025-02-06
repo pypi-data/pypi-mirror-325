254982ac87b0d6544ae4a887fe2da08345a1c0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:17:02 +0000

    Add package apdtflow/0.1.2/apdtflow-0.1.2.tar.gz

---
+++ packages/apdtflow/apdtflow-0.1.2.tar.gz/apdtflow-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apdtflow',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchdiffeq',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'tensorboard',
+        'pyyaml',
+    ],
+    description='APDTFlow: A modular forecasting framework for time series data',
+    author='Yotam Barun',
+    author_email='yotambarun93@gmail.com',
+    url='https://github.com/yotambraun/APDTFlow',  # Main homepage URL
+    project_urls={
+        'Homepage': 'https://github.com/yotambraun/APDTFlow',
+        'Documentation': 'https://github.com/yotambraun/APDTFlow',
+        'Source': 'https://github.com/yotambraun/APDTFlow',
+    },
+)
