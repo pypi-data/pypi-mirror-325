9f43794c420048637e42584a56d8e252c7ba1743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:06:40 +0000

    Add package contentctl/5.0.1/contentctl-5.0.1.tar.gz

---
+++ packages/contentctl/contentctl-5.0.1.tar.gz/contentctl-5.0.1/contentctl/objects/data_source.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import Optional, Any
+from pydantic import Field, HttpUrl, model_serializer, BaseModel
+from contentctl.objects.security_content_object import SecurityContentObject
+
+
+class TA(BaseModel):
+    name: str
+    url: HttpUrl | None = None
+    version: str
+
+
+class DataSource(SecurityContentObject):
+    source: str = Field(...)
+    sourcetype: str = Field(...)
+    separator: Optional[str] = None
+    configuration: Optional[str] = None
+    supported_TA: list[TA] = []
+    fields: None | list = None
+    field_mappings: None | list = None
+    convert_to_log_source: None | list = None
+    example_log: None | str = None
+
+    @model_serializer
+    def serialize_model(self):
+        # Call serializer for parent
+        super_fields = super().serialize_model()
+
+        # All fields custom to this model
+        model: dict[str, Any] = {
+            "source": self.source,
+            "sourcetype": self.sourcetype,
+            "separator": self.separator,
+            "configuration": self.configuration,
+            "supported_TA": self.supported_TA,
+            "fields": self.fields,
+            "field_mappings": self.field_mappings,
+            "convert_to_log_source": self.convert_to_log_source,
+            "example_log": self.example_log,
+        }
+
+        # Combine fields from this model with fields from parent
+        super_fields.update(model)
+
+        # return the model
+        return super_fields
