37dc60b0539803ca9242cda9925244c8c25f7c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:26 +0000

    Add package square-administration/2.4.0/square_administration-2.4.0-py3-none-any.whl

---
+++ packages/square-administration/square_administration-2.4.0-py3-none-any.whl/square_administration/pydantic_models/authentication.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class RegisterUsernameV0(BaseModel):
+    username: str
+    password: str
+    admin_password: str
+
+
+class LoginUsernameV0(BaseModel):
+    username: str
+    password: str
