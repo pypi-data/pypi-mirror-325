120fc32b8346348e79efb8eedf38563f9a6b254c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 07:08:07 +0000

    Add package rustymimi/0.4.1/rustymimi-0.4.1.tar.gz

---
+++ packages/rustymimi/rustymimi-0.4.1.tar.gz/rustymimi-0.4.1/moshi-core/src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Kyutai, all rights reserved.
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+
+pub use candle;
+pub use candle_nn;
+
+pub mod conv;
+pub mod lm;
+pub mod lm_generate;
+pub mod lm_generate_multistream;
+pub mod mimi;
+pub mod quantization;
+pub mod quantized_lm;
+pub mod quantized_transformer;
+pub mod seanet;
+pub mod streaming;
+pub mod transformer;
+pub mod tts;
+pub mod tts_streaming;
+pub mod wav;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum NormType {
+    RmsNorm,
+    LayerNorm,
+}
