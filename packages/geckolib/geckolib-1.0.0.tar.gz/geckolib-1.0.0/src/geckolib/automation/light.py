8be7c5ad28a6834854cd7524a2e310936fecc6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 04:05:40 +0000

    Add package geckolib/1.0.0/geckolib-1.0.0.tar.gz

---
+++ packages/geckolib/geckolib-1.0.0.tar.gz/geckolib-1.0.0/src/geckolib/automation/light.py
@@ -0,0 +1,10 @@
+"""Gecko Lights"""
+
+from .switch import GeckoSwitch
+
+
+class GeckoLight(GeckoSwitch):
+    """
+    Lights are based on switches, but might have brightness and
+    colours as options
+    """
