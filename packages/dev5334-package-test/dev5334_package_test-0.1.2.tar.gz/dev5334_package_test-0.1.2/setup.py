1f393d8a169a9d0371e0a1145aced37e2082f8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:06:32 +0000

    Add package dev5334-package-test/0.1.2/dev5334_package_test-0.1.2.tar.gz

---
+++ packages/dev5334-package-test/dev5334_package_test-0.1.2.tar.gz/dev5334_package_test-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="dev5334_package_test",
+    version="0.1.2",
+    packages=find_packages(),
+    author="wizata-lsi",
+    description='My first Custom Python library',
+    url='https://github.com/wizata-lsi/dev5334_package_test'
+)
