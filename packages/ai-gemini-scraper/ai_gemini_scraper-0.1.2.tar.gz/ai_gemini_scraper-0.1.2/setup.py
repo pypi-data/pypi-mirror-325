5e708d82b35304c23c349030929c73c5765c4cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:18 +0000

    Add package ai-gemini-scraper/0.1.2/ai_gemini_scraper-0.1.2.tar.gz

---
+++ packages/ai-gemini-scraper/ai_gemini_scraper-0.1.2.tar.gz/ai_gemini_scraper-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ai-gemini-scraper",
+    version="0.1.2",
+    author="Cyrille Gattiker",
+    author_email="cyrille.gattiker@gmail.com",
+    description="A simple package for scraping websites and generating responses using Google's Generative AI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyruszevirus/ai-scraper",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "google-generativeai",
+        "urllib3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
