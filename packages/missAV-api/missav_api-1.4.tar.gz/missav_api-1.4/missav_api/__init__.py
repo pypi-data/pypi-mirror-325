14111ef8fd3345a72aa4101fa0748ae7669c57cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:27:05 +0000

    Add package missAV-api/1.4/missav_api-1.4.tar.gz

---
+++ packages/missAV-api/missav_api-1.4.tar.gz/missav_api-1.4/missav_api/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["Client", "base_consts", "Video", "Callback"]
+
+from missav_api.missav_api import Client, Video
+from base_api.modules import consts as base_consts
+from base_api.modules.progress_bars import Callback
