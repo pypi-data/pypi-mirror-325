40f5b7933d325728fc30210cf5f2990a68e1e6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:11 +0000

    Add package Webiks-Hebrew-RAGbot/1.0.0/Webiks-Hebrew-RAGbot-1.0.0.tar.gz

---
+++ packages/Webiks-Hebrew-RAGbot/Webiks-Hebrew-RAGbot-1.0.0.tar.gz/Webiks-Hebrew-RAGbot-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Webiks-Hebrew-RAGbot',
+    version='1.0.0',
+    author='Webiks',
+    author_email='shmuel_robinov@webiks.com',
+    description='A search engine using machine learning models and Elasticsearch for advanced document retrieval.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NNLP-IL/Webiks-Hebrew-RAGbot',
+    packages=find_packages(),
+    install_requires=[
+        'elasticsearch==8.14.0',
+        'sentence-transformers==3.0.1',
+        'torch==2.3.1',
+        'transformers==4.42.3'
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
