49f0d602fd63474c75f7060a2c81809eaebf3ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:43:28 +0000

    Add package chatgpt-mirai-qq-bot-onebot-adapter/0.1.4/chatgpt_mirai_qq_bot_onebot_adapter-0.1.4.tar.gz

---
+++ packages/chatgpt-mirai-qq-bot-onebot-adapter/chatgpt_mirai_qq_bot_onebot_adapter-0.1.4.tar.gz/chatgpt_mirai_qq_bot_onebot_adapter-0.1.4/onebot_adapter/__init__.py
@@ -0,0 +1,31 @@
+from framework.logger import get_logger
+from framework.plugin_manager.plugin import Plugin
+from framework.workflow.core.dispatch.dispatcher import WorkflowDispatcher
+from .adapter import OneBotAdapter
+from .config import OneBotConfig
+
+logger = get_logger("OneBot-Adapter")
+
+
+class OneBotAdapterPlugin(Plugin):
+    def on_load(self):
+        class OneBotAdapterFactory:
+            def __init__(self, dispatcher: WorkflowDispatcher):
+                self.dispatcher = dispatcher
+            
+            def __call__(self, config: OneBotConfig):
+                return OneBotAdapter(config, self.dispatcher)
+        
+        self.im_registry.register(
+            "onebot",
+            OneBotAdapterFactory(self.workflow_dispatcher),
+            OneBotConfig
+        )
+        
+        logger.info("OneBotAdapter plugin loaded")
+
+    def on_start(self):
+        logger.info("OneBotAdapter plugin started")
+
+    def on_stop(self):
+        logger.info("OneBotAdapter plugin stopped")
