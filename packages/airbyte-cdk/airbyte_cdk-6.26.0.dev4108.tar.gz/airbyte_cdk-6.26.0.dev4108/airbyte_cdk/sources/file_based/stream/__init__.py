7bdff4c935c74e193462cf4c82cc90a447c7858d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:37:28 +0000

    Add package airbyte-cdk/6.26.0.dev4108/airbyte_cdk-6.26.0.dev4108.tar.gz

---
+++ packages/airbyte-cdk/airbyte_cdk-6.26.0.dev4108.tar.gz/airbyte_cdk-6.26.0.dev4108/airbyte_cdk/sources/file_based/stream/__init__.py
@@ -0,0 +1,5 @@
+from airbyte_cdk.sources.file_based.stream.abstract_file_based_stream import AbstractFileBasedStream
+from airbyte_cdk.sources.file_based.stream.default_file_based_stream import DefaultFileBasedStream
+from airbyte_cdk.sources.file_based.stream.identities_stream import IdentitiesStream
+
+__all__ = ["AbstractFileBasedStream", "DefaultFileBasedStream", "IdentitiesStream"]
