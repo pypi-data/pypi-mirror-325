510743cbce1b7d22ad69fe17ced7017068e77220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:56 +0000

    Add package cmeel-example/0.7.2/cmeel_example-0.7.2-0-cp310-cp310-musllinux_1_2_aarch64.whl

---
+++ packages/cmeel-example/cmeel_example-0.7.2-0-cp310-cp310-musllinux_1_2_aarch64.whl/cmeel.prefix/include/cmeel/example/adder.hpp
@@ -0,0 +1,11 @@
+#ifndef CMEEL_CMEEL_EXAMPLE_ADDER_HPP
+#define CMEEL_CMEEL_EXAMPLE_ADDER_HPP
+
+namespace cmeel {
+namespace example {
+auto add(long a, long b) -> long;
+auto sub(long a, long b) -> long;
+}  // namespace example
+}  // namespace cmeel
+
+#endif
