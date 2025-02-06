283efbda93d68a9a628484eb7f5898b46d7779cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:20:07 +0000

    Add package bcpulseKOR/3.2.0b2/bcpulsekor-3.2.0b2.tar.gz

---
+++ packages/bcpulseKOR/bcpulsekor-3.2.0b2.tar.gz/bcpulsekor-3.2.0b2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(
+    include_package_data=True  # ✅ 추가해야 `files` 폴더가 포함됨
+)
---
+++ packages/bcpulseKOR/bcpulsekor-3.2.0b2.tar.gz/bcpulsekor-3.2.0b2/src/bcps/core/game/gamoto/__init__.py
@@ -0,0 +1,3 @@
+from bcps.core.game.gamoto import catamins, gamatoto, base_materials, ototo, cat_shrine
+
+__all__ = ["catamins", "gamatoto", "base_materials", "ototo", "cat_shrine"]
