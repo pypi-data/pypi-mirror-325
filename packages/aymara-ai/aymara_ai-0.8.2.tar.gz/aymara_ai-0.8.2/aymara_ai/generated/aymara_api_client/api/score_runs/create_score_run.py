7d3ada1214fa921e6de59c621cb76dbe9755b271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 04:05:37 +0000

    Add package aymara-ai/0.8.2/aymara_ai-0.8.2.tar.gz

---
+++ packages/aymara-ai/aymara_ai-0.8.2.tar.gz/aymara_ai-0.8.2/aymara_ai/generated/aymara_api_client/api/score_runs/create_score_run.py
@@ -0,0 +1,185 @@
+from http import HTTPStatus
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.error_schema import ErrorSchema
+from ...models.score_run_in_schema import ScoreRunInSchema
+from ...models.score_run_out_schema import ScoreRunOutSchema
+from ...types import UNSET, Response, Unset
+
+
+def _get_kwargs(
+    *,
+    body: ScoreRunInSchema,
+    workspace_uuid: Union[Unset, str] = UNSET,
+) -> Dict[str, Any]:
+    headers: Dict[str, Any] = {}
+
+    params: Dict[str, Any] = {}
+
+    params["workspace_uuid"] = workspace_uuid
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    _kwargs: Dict[str, Any] = {
+        "method": "post",
+        "url": "/v1/scores/",
+        "params": params,
+    }
+
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[ErrorSchema, ScoreRunOutSchema]]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = ScoreRunOutSchema.from_dict(response.json())
+
+        return response_200
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        response_422 = ErrorSchema.from_dict(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[ErrorSchema, ScoreRunOutSchema]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: AuthenticatedClient,
+    body: ScoreRunInSchema,
+    workspace_uuid: Union[Unset, str] = UNSET,
+) -> Response[Union[ErrorSchema, ScoreRunOutSchema]]:
+    """Create Score Run
+
+    Args:
+        workspace_uuid (Union[Unset, str]):
+        body (ScoreRunInSchema):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorSchema, ScoreRunOutSchema]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+        workspace_uuid=workspace_uuid,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    *,
+    client: AuthenticatedClient,
+    body: ScoreRunInSchema,
+    workspace_uuid: Union[Unset, str] = UNSET,
+) -> Optional[Union[ErrorSchema, ScoreRunOutSchema]]:
+    """Create Score Run
+
+    Args:
+        workspace_uuid (Union[Unset, str]):
+        body (ScoreRunInSchema):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorSchema, ScoreRunOutSchema]
+    """
+
+    return sync_detailed(
+        client=client,
+        body=body,
+        workspace_uuid=workspace_uuid,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: AuthenticatedClient,
+    body: ScoreRunInSchema,
+    workspace_uuid: Union[Unset, str] = UNSET,
+) -> Response[Union[ErrorSchema, ScoreRunOutSchema]]:
+    """Create Score Run
+
+    Args:
+        workspace_uuid (Union[Unset, str]):
+        body (ScoreRunInSchema):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorSchema, ScoreRunOutSchema]]
+    """
+
+    kwargs = _get_kwargs(
+        body=body,
+        workspace_uuid=workspace_uuid,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    *,
+    client: AuthenticatedClient,
+    body: ScoreRunInSchema,
+    workspace_uuid: Union[Unset, str] = UNSET,
+) -> Optional[Union[ErrorSchema, ScoreRunOutSchema]]:
+    """Create Score Run
+
+    Args:
+        workspace_uuid (Union[Unset, str]):
+        body (ScoreRunInSchema):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorSchema, ScoreRunOutSchema]
+    """
+
+    return (
+        await asyncio_detailed(
+            client=client,
+            body=body,
+            workspace_uuid=workspace_uuid,
+        )
+    ).parsed
