37384d43779c0b599cedbe474e065b60ace95ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:13 +0000

    Add package tailwind-processor/1.2.6/tailwind_processor-1.2.6-py3-none-any.whl

---
+++ packages/tailwind-processor/tailwind_processor-1.2.6-py3-none-any.whl/tailwind_processor/tailwind_processor.py
@@ -0,0 +1,73 @@
+import subprocess
+import textwrap
+from pathlib import Path
+from typing import List
+import shutil
+
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class TailwindProcessor:
+    """
+    Process Tailwind classes into raw CSS.
+    """
+
+    def process(self, tailwind_classes: List[str]) -> str:
+        """
+        Process Tailwind classes into CSS.
+        """
+        tailwind_apply = textwrap.dedent("""
+            @tailwind base;
+            @tailwind components;
+            @tailwind utilities;
+        """)
+
+        parent = Path(__file__).parent / "tmp"
+        parent.mkdir(parents=True, exist_ok=True)
+
+        # All temporary files
+        input_file = parent / "input.css"
+        output_file = parent / "output.css"
+        content_file = parent / "content.html"
+        configs = parent / "tailwind.config.js"
+
+        # Write the content file
+        tw_classes = " ".join(tailwind_classes)
+        content_file.write_text(f"<div class='{tw_classes}'></div>")
+
+        # Write the config file
+        config_content = (
+            textwrap.dedent("""
+            /** @type {import('tailwindcss').Config} */
+            module.exports = {
+                content: ['%s'],
+                theme: {
+                    extend: {},
+                },
+                plugins: [],
+            }
+            """)
+        ) % content_file.as_posix()
+
+        configs.write_text(config_content)
+        input_file.write_text(tailwind_apply)
+
+        c = configs.as_posix()
+        i = input_file.as_posix()
+        o = output_file.as_posix()
+
+        command = 'uv run tailwindcss -c "%s" -i "%s" -o "%s" --minify' % (c, i, o)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        output, err = p.communicate()
+        p_status = p.wait()
+
+        log.info("\nStatus: %s\nOutput: %s" % (p_status, output))
+        if err:
+            log.info(err)
+
+        result = output_file.read_text()
+        shutil.rmtree(parent.as_posix())
+        return result
