fbb9ee17319654558443ea850a4f19246e86091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:00 +0000

    Add package ultragpt/1.1.0/ultragpt-1.1.0-py3-none-any.whl

---
+++ packages/ultragpt/ultragpt-1.1.0-py3-none-any.whl/ultragpt/tools/calculator/main.py
@@ -0,0 +1,17 @@
+from .core import calculate
+
+#? Required ------------------------------------------------------------------
+_info = "This allows you to perform mathematic calculations."
+
+def extract_system_message(history):
+    for message in history:
+        if message["role"] == "system" or message["role"] == "developer":
+            return message["content"]
+
+def _execute(message, history, client, config):
+    """Main function to execute the web search tool"""
+    system_message = extract_system_message(history)
+    if system_message == message:
+        return calculate(message, client, config)
+    else:
+        return calculate(message + "\n" + system_message, client, config)
