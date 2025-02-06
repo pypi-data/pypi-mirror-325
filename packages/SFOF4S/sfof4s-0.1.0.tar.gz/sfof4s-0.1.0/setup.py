0363f8c60f4e70aae0e50bd36d34510a490b5af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 07:08:02 +0000

    Add package SFOF4S/0.1.0/sfof4s-0.1.0.tar.gz

---
+++ packages/SFOF4S/sfof4s-0.1.0.tar.gz/sfof4s-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SFOF4S",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Your package description",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",  # Fix for PyPI description issue
+    url="https://github.com/yourusername/SFOF4S",
+    packages=find_packages(),
+    install_requires=[
+        "torch==2.6.0",
+        "torchaudio==2.4.1",
+        "torchvision==0.21.0",
+        "numpy==1.26.4",
+        "opencv-python==4.10.0",
+        "scipy==1.15.1",
+        "tqdm==4.67.1",
+        "pandas==2.2.3",
+        "natsort==8.4.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
