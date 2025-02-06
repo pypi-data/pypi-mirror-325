555e4667a7447de40cf47ecd2a1b31573d830628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:07:35 +0000

    Add package flaskavel/1.26.0/flaskavel-1.26.0.tar.gz

---
+++ packages/flaskavel/flaskavel-1.26.0.tar.gz/flaskavel-1.26.0/flaskavel/luminate/pipelines/CLIPipeline.py
@@ -0,0 +1,9 @@
+class CLIPipeline:
+    pass
+
+    # def __init_():
+
+    #     1. Cargar Comandos a Cache
+    #     2. Cargar Rutas
+    #     2. Cargar Middlewares
+    #     3. Obtener nombre de comando
