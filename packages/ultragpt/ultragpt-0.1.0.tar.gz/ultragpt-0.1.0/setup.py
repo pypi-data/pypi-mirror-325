c84c9e9e4c807ffd8908c0c5b76934a5de7141ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:39 +0000

    Add package ultragpt/0.1.0/ultragpt-0.1.0.tar.gz

---
+++ packages/ultragpt/ultragpt-0.1.0.tar.gz/ultragpt-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Get the long description from the README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='ultragpt',
+    version='0.1.0',
+    license="MIT",
+    author='Ranit Bhowmick',
+    author_email='bhowmickranitking@duck.com',
+    description='UltraGPT: A modular library for advanced GPT-based reasoning and step pipelines',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Kawai-Senpai',
+    install_requires=[
+        'pydantic',
+        'openai',
+        'ultraprint>=3.1.0',
+        'tqdm',
+        'click'
+    ],
+    python_requires='>=3.6',
+)
