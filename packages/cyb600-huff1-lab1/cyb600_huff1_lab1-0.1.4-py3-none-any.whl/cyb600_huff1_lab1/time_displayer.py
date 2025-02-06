fcdad31a132662990c63ba87bc2b1d1b66b1e3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 07:08:02 +0000

    Add package cyb600-huff1-lab1/0.1.4/cyb600_huff1_lab1-0.1.4-py3-none-any.whl

---
+++ packages/cyb600-huff1-lab1/cyb600_huff1_lab1-0.1.4-py3-none-any.whl/cyb600_huff1_lab1/time_displayer.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+from datetime import datetime
+
+def time_displayer(port=8000):
+    class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(now.encode())
+
+    with socketserver.TCPServer(("", port), MyRequestHandler) as httpd:
+        print(f"Server started at port {port}...")
+        httpd.serve_forever()
+
+time_displayer()
