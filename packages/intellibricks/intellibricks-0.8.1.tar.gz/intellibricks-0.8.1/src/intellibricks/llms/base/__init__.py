d5dadcc8112d2ca0abafbdd93661c90dec832ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:17:02 +0000

    Add package intellibricks/0.8.1/intellibricks-0.8.1.tar.gz

---
+++ packages/intellibricks/intellibricks-0.8.1.tar.gz/intellibricks-0.8.1/src/intellibricks/llms/base/__init__.py
@@ -0,0 +1,9 @@
+from .contracts import LanguageModel, TranscriptionModel
+from .types import FileContent, Language
+
+__all__: list[str] = [
+    "LanguageModel",
+    "TranscriptionModel",
+    "Language",
+    "FileContent",
+]
