35009794181124073bd62c599a0ea9a55262ce70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 06:17:04 +0000

    Add package AutoCarver/7.0.1/autocarver-7.0.1-py3-none-any.whl

---
+++ packages/AutoCarver/autocarver-7.0.1-py3-none-any.whl/AutoCarver/selectors/regression_selector.py
@@ -0,0 +1,59 @@
+"""Tools to select the best Quantitative and Qualitative features for a Regression task."""
+
+from .filters import BaseFilter, SpearmanFilter, TschuprowtFilter, ValidFilter
+from .measures import BaseMeasure, KruskalMeasure, ModeMeasure, NanMeasure, SpearmanMeasure
+from .utils.base_selector import BaseSelector
+
+
+class RegressionSelector(BaseSelector):
+    """A pipeline of measures to perform a feature pre-selection that maximizes association
+    with a quantitative target.
+
+    Get your best features with ``RegressionSelector.select()``!
+    """
+
+    __name__ = "RegressionSelector"
+
+    def _initiate_measures(self, requested_measures: list[BaseMeasure] = None) -> list[BaseMeasure]:
+        """initiates the list of measures with default values"""
+
+        # initating to requested ones
+        measures = requested_measures
+
+        # if no measure were passed
+        if measures is None:
+            measures = [NanMeasure(), ModeMeasure(), SpearmanMeasure(), KruskalMeasure()]
+
+        # adding default measure of mode
+        if all(measure.__name__ != ModeMeasure.__name__ for measure in measures):
+            measures = [ModeMeasure()] + measures
+
+        # adding default measure of nan
+        if all(measure.__name__ != NanMeasure.__name__ for measure in measures):
+            measures = [NanMeasure()] + measures
+
+        # checking for measures for qualitative target
+        if not all(
+            measure.is_y_quantitative
+            or (measure.reverse_xy() and measure.is_y_quantitative)
+            or measure.is_default
+            for measure in measures
+        ):
+            raise ValueError(f"[{self}] Provide measures for quantitative target!")
+        return measures
+
+    def _initiate_filters(self, requested_filters: list[BaseFilter] = None) -> list[BaseFilter]:
+        """initiates the list of measures with default values"""
+
+        # initating to requested ones
+        filters = requested_filters
+
+        # if no measure were passed
+        if filters is None:
+            filters = [ValidFilter(), TschuprowtFilter(), SpearmanFilter()]
+
+        # adding default validity filter (based on measures)
+        if all(filter.__name__ != ValidFilter.__name__ for filter in filters):
+            filters = [ValidFilter()] + filters
+
+        return filters
