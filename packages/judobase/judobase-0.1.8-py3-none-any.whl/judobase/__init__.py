4463cd36ebd65d68f594369792864ae907dd153c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 05:17:04 +0000

    Add package judobase/0.1.8/judobase-0.1.8-py3-none-any.whl

---
+++ packages/judobase/judobase-0.1.8-py3-none-any.whl/judobase/__init__.py
@@ -0,0 +1,5 @@
+from .judobase_api import JudoBase
+from .schemas import Competition, Contest, Country, Judoka
+from .version import __version__
+
+__all__ = ["Competition", "Contest", "Country", "JudoBase", "Judoka", "__version__"]
