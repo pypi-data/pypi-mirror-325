45eabbc632edee5823bf7984c4ab6611729e1716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:39 +0000

    Add package mojentic/0.2.1/mojentic-0.2.1.tar.gz

---
+++ packages/mojentic/mojentic-0.2.1.tar.gz/mojentic-0.2.1/src/mojentic/llm/gateways/tokenizer_gateway.py
@@ -0,0 +1,15 @@
+from typing import List
+
+import structlog
+import tiktoken
+
+logger = structlog.get_logger()
+
+
+class TokenizerGateway:
+    def __init__(self, model: str = "cl100k_base"):
+        self.tokenizer = tiktoken.get_encoding(model)
+
+    def encode(self, text: str) -> List:
+        logger.debug("encode", text=text)
+        return self.tokenizer.encode(text)
