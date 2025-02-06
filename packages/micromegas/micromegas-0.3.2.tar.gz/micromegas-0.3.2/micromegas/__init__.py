c7a4b4a6ff262568b806ea0eed10ff440afebfc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 14:19:12 +0000

    Add package micromegas/0.3.2/micromegas-0.3.2.tar.gz

---
+++ packages/micromegas/micromegas-0.3.2.tar.gz/micromegas-0.3.2/micromegas/__init__.py
@@ -0,0 +1,13 @@
+import os
+import grpc
+from . import time
+from . import request
+from . import client
+from . import perfetto
+from . import flightsql
+
+
+def connect():
+    "connect to the analytics service using default values"
+    channel_cred = grpc.local_channel_credentials()
+    return flightsql.client.FlightSQLClient("localhost:50051", channel_cred)
