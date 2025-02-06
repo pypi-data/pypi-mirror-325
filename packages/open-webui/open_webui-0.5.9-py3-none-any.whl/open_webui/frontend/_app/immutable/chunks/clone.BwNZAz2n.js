14da9cf4418405074b884816dee68016441712eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-06 03:31:08 +0000

    Add package open-webui/0.5.9/open_webui-0.5.9-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.9-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/clone.BwNZAz2n.js
@@ -0,0 +1,2 @@
+import{b as r}from"./graph.Cvd4-ijf.js";var e=4;function a(o){return r(o,e)}export{a as c};
+//# sourceMappingURL=clone.BwNZAz2n.js.map
---
+++ packages/open-webui/open_webui-0.5.9-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.DM7hZfAC.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.RLaQsWRp.js";export{t as start};
+//# sourceMappingURL=start.DM7hZfAC.js.map
