e01a680675e2825dcf0cd6b9fa39f6e7f587abf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:19:11 +0000

    Add package daisy-dtb/0.0.9/daisy_dtb-0.0.9-py3-none-any.whl

---
+++ packages/daisy-dtb/daisy_dtb-0.0.9-py3-none-any.whl/sources/__init__.py
@@ -0,0 +1,5 @@
+from sources.folder_source import FolderDtbSource
+from sources.source import DtbSource
+from sources.zip_source import ZipDtbSource
+
+__all__ = [DtbSource, FolderDtbSource, ZipDtbSource]
