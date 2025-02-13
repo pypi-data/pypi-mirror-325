6036f1b0d20ed4b1b103d390e087da1464f4dbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:09:43 +0000

    Add package cdi-bf/25.1.0/cdi_bf-25.1.0.tar.gz

---
+++ packages/cdi-bf/cdi_bf-25.1.0.tar.gz/cdi_bf-25.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cdi_bf",  # Name of your package
+    version="25.1.0",  # Version number
+    author="Your Name",
+    author_email="geniestat.andal@gmail.com",
+    description="This package allow excel data handling",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/simeanhamado/exceldatahandler",  # Link to your repo
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[  # List of dependencies
+        "numpy",
+        "pandas",
+        "openpyxl",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Python version requirement
+)
