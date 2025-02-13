089f1724c4b1f07fba9df047d4479e6e9c9ceef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 20:09:42 +0000

    Add package coastal-resilience-utilities/0.1.32/coastal_resilience_utilities-0.1.32-py3-none-any.whl

---
+++ packages/coastal-resilience-utilities/coastal_resilience_utilities-0.1.32-py3-none-any.whl/coastal_resilience_utilities/dataconf.py
@@ -0,0 +1,12 @@
+import os
+import dotenv
+
+DATA_ENV_FILE = os.getenv("DATA_ENV_FILE", "../.data.env")
+
+dotenv.load_dotenv(DATA_ENV_FILE)
+
+BUILDING_AREA = os.getenv("BUILDING_AREA", "gs://supporting-data2/WSF3d_v02_BuildingArea.tif")
+GADM = os.getenv("GADM", "gs://supporting-data2/gadm_country_bounds.parquet")
+POPULATION = os.getenv("POPULATION", 'gs://supporting-data2/GHS_POP_E2020.tif')
+OPEN_BUILDINGS = os.getenv("OPEN_BUILDINGS", "gs://supporting-data2/google-microsoft-open-buildings.parquet")
+NSI = os.getenv("NSI", "gs://geopmaker-output-staging/nsi.geoparquet/")
