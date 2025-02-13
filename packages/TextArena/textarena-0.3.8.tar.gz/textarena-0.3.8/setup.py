449ce680c16c2c43c2aaa62616240d70f4e4bde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 20:09:42 +0000

    Add package TextArena/0.3.8/textarena-0.3.8.tar.gz

---
+++ packages/TextArena/textarena-0.3.8.tar.gz/textarena-0.3.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textarena",
+    version="0.3.8",
+    url="https://github.com/LeonGuertler/TextArena",
+    author="Leon Guertler",
+    author_email="Guertlerlo@cfar.a-star.edu.sg",
+    description="[WIP] A Collection of Competitive Text-Based Games for Language Model Evaluation and Reinforcement Learning",
+    packages=find_packages(include=["textarena", "textarena.*"]),
+    include_package_data=True,
+    package_data={
+        "": ["*.json"],  # Include all JSON files in any package
+        "textarena.envs.two_player.TruthAndDeception": ["*.json"],  # Explicitly include JSON files in this directory
+        "textarena": ["envs/**/*.json"],  # Recursive include from textarena root
+    },
+    install_requires=[
+        "requests",
+        "aiohttp",
+        "backoff",
+        "rich",
+        "networkx",
+        "openai",
+        "imageio",
+        "imageio[ffmpeg]",
+        "e2b_code_interpreter",
+        "transformers",
+        "playwright",
+        "opencv-python",
+        "chess",
+        "pyenchant",
+        "google-generativeai"
+    ],
+    python_requires='>=3.9',
+)
