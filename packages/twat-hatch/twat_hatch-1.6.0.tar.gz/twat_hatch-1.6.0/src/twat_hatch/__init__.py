6551e0c0674ee46d901b96fc8020d3dd92696b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:18:41 +0000

    Add package twat-hatch/1.6.0/twat_hatch-1.6.0.tar.gz

---
+++ packages/twat-hatch/twat_hatch-1.6.0.tar.gz/twat_hatch-1.6.0/src/twat_hatch/__init__.py
@@ -0,0 +1,5 @@
+'Package initialization functionality for twat.'
+from importlib import metadata
+from.hatch import PackageInitializer
+__version__=metadata.version(__name__)
+__all__=['PackageInitializer','__version__']
