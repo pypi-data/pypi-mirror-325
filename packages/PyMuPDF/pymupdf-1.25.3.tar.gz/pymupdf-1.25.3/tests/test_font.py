5a1f7cd7be175b00ede81a03136a30a14f3e4daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:09:48 +0000

    Add package PyMuPDF/1.25.3/pymupdf-1.25.3.tar.gz

---
+++ packages/PyMuPDF/pymupdf-1.25.3.tar.gz/pymupdf-1.25.3/tests/test_font.py
@@ -0,0 +1,202 @@
+"""
+Tests for the Font class.
+"""
+import pymupdf
+import os
+
+def test_font1():
+    text = "PyMuPDF"
+    font = pymupdf.Font("helv")
+    assert font.name == "Helvetica"
+    tl = font.text_length(text, fontsize=20)
+    cl = font.char_lengths(text, fontsize=20)
+    assert len(text) == len(cl)
+    assert abs(sum(cl) - tl) < pymupdf.EPSILON
+    for i in range(len(cl)):
+        assert cl[i] == font.glyph_advance(ord(text[i])) * 20
+    font2 = pymupdf.Font(fontbuffer=font.buffer)
+    codepoints1 = font.valid_codepoints()
+    codepoints2 = font2.valid_codepoints()
+    print('')
+    print(f'{len(codepoints1)=}')
+    print(f'{len(codepoints2)=}')
+    if 0:
+        for i, (ucs1, ucs2) in enumerate(zip(codepoints1, codepoints2)):
+            print(f'    {i}: {ucs1=} {ucs2=} {"" if ucs2==ucs2 else "*"}')
+    assert font2.valid_codepoints() == font.valid_codepoints()
+    
+    # Also check we can get font's bbox.
+    bbox1 = font.bbox
+    print(f'{bbox1=}')
+    if hasattr(pymupdf, 'mupdf'):
+        bbox2 = font.this.fz_font_bbox()
+        assert bbox2 == bbox1
+
+
+def test_font2():
+    """Old and new length computation must be the same."""
+    font = pymupdf.Font("helv")
+    text = "PyMuPDF"
+    assert font.text_length(text) == pymupdf.get_text_length(text)
+
+
+def test_fontname():
+    """Assert a valid PDF fontname."""
+    doc = pymupdf.open()
+    page = doc.new_page()
+    assert page.insert_font()  # assert: a valid fontname works!
+    detected = False  # preset indicator
+    try:  # fontname check will fail first - don't need a font at all here
+        page.insert_font(fontname="illegal/char", fontfile="unimportant")
+    except ValueError as e:
+        if str(e).startswith("bad fontname chars"):
+            detected = True  # illegal fontname detected
+    assert detected
+
+def test_2608():
+    flags = (pymupdf.TEXT_DEHYPHENATE | pymupdf.TEXT_MEDIABOX_CLIP)
+    with pymupdf.open(os.path.abspath(f'{__file__}/../../tests/resources/2201.00069.pdf')) as doc:
+        page = doc[0]
+        blocks = page.get_text_blocks(flags=flags)
+        text = blocks[10][4]
+        with open(os.path.abspath(f'{__file__}/../../tests/test_2608_out'), 'wb') as f:
+            f.write(text.encode('utf8'))
+        path_expected = os.path.normpath(f'{__file__}/../../tests/resources/test_2608_expected')
+        with open(path_expected, 'rb') as f:
+            expected = f.read().decode('utf8')
+        # Github windows x32 seems to insert \r characters; maybe something to
+        # do with the Python installation's line endings settings.
+        expected = expected.replace('\r', '')
+        print(f'test_2608(): {text.encode("utf8")=}')
+        print(f'test_2608(): {expected.encode("utf8")=}')
+        assert text == expected
+
+def test_fontarchive():
+    import subprocess
+    arch = pymupdf.Archive()
+    css = pymupdf.css_for_pymupdf_font("notos", archive=arch, name="sans-serif")
+    print(css)
+    print(arch.entry_list)
+    assert arch.entry_list == \
+            [
+                {
+                    'fmt': 'tree',
+                    'entries':
+                    [
+                        'notosbo', 'notosbi', 'notosit', 'notos'
+                    ],
+                    'path': None
+                }
+            ]
+
+def test_load_system_font():
+    if not hasattr(pymupdf, 'mupdf'):
+        print(f'test_load_system_font(): Not running on classic.')
+        return
+    trace = list()
+    def font_f(name, bold, italic, needs_exact_metrics):
+        trace.append((name, bold, italic, needs_exact_metrics))
+        print(f'test_load_system_font():font_f(): Looking for font: {name=} {bold=} {italic=} {needs_exact_metrics=}.')
+        return None
+    def f_cjk(name, ordering, serif):
+        trace.append((name, ordering, serif))
+        print(f'test_load_system_font():f_cjk(): Looking for font: {name=} {ordering=} {serif=}.')
+        return None
+    def f_fallback(script, language, serif, bold, italic):
+        trace.append((script, language, serif, bold, italic))
+        print(f'test_load_system_font():f_fallback(): looking for font: {script=} {language=} {serif=} {bold=} {italic=}.')
+        return None
+    pymupdf.mupdf.fz_install_load_system_font_funcs(font_f, f_cjk, f_fallback)
+    f = pymupdf.mupdf.fz_load_system_font("some-font-name", 0, 0, 0)
+    assert trace == [
+            ('some-font-name', 0, 0, 0),
+            ], f'Incorrect {trace=}.'
+    print(f'test_load_system_font(): {f.m_internal=}')
+
+
+def test_mupdf_subset_fonts2():
+    if not hasattr(pymupdf, 'mupdf'):
+        print('Not running on rebased.')
+        return
+    path = os.path.abspath(f'{__file__}/../../tests/resources/2.pdf')
+    with pymupdf.open(path) as doc:
+        n = len(doc)
+        pages = [i*2 for i in range(n//2)]
+        print(f'{pages=}.')
+        pymupdf.mupdf.pdf_subset_fonts2(pymupdf._as_pdf_document(doc), pages)
+
+
+def test_3677():
+    pymupdf.TOOLS.set_subset_fontnames(True)
+    path = os.path.abspath(f'{__file__}/../../tests/resources/test_3677.pdf')
+    font_names_expected = [
+            'BCDEEE+Aptos',
+            'BCDFEE+Aptos',
+            'BCDGEE+Calibri-Light',
+            'BCDHEE+Calibri-Light',
+            ]
+    font_names = list()
+    with pymupdf.open(path) as document:
+        for page in document:
+             for block in page.get_text('dict')['blocks']:
+                    if block['type'] == 0:
+                        if 'lines' in block.keys():
+                            for line in block['lines']:
+                                for span in line['spans']:
+                                    font_name=span['font']
+                                    print(font_name)
+                                    font_names.append(font_name)
+    assert font_names == font_names_expected, f'{font_names=}'
+
+
+def test_3933():
+    path = os.path.normpath(f'{__file__}/../../tests/resources/test_3933.pdf')
+    with pymupdf.open(path) as document:
+        page = document[0]
+        print(f'{len(page.get_fonts())=}')
+    
+        expected = {
+                'BCDEEE+Calibri': 39,
+                'BCDFEE+SwissReSan-Regu':  53,
+                'BCDGEE+SwissReSan-Ital':  20,
+                'BCDHEE+SwissReSan-Bold':  20,
+                'BCDIEE+SwissReSan-Regu':  53,
+                'BCDJEE+Calibri':  39,
+                }
+                
+        for xref, _, _, name, _, _ in page.get_fonts():
+            _, _, _, content = document.extract_font(xref)
+
+            if content:
+                font = pymupdf.Font(fontname=name, fontbuffer=content)
+                supported_symbols = font.valid_codepoints()
+                print(f'Font {name}: {len(supported_symbols)=}.', flush=1)
+                if pymupdf.mupdf_version_tuple < (1, 24, 11):
+                    assert len(supported_symbols) == 0
+                else:
+                    assert len(supported_symbols) == expected.get(name)
+
+
+def test_3780():
+    path = os.path.normpath(f'{__file__}/../../tests/resources/test_3780.pdf')
+    with pymupdf.open(path) as document:
+        for page_i, page in enumerate(document):
+            for itm in page.get_fonts():
+                buff=document.extract_font(itm[0])[-1]
+                font=pymupdf.Font(fontbuffer=buff)
+                print(f'{page_i=}: xref {itm[0]} {font.name=} {font.ascender=} {font.descender=}.')
+            if page_i == 0:
+                d = page.get_text('dict')
+                #for n, v in d.items():
+                #    print(f'    {n}: {v!r}')
+                for i, block in enumerate(d['blocks']):
+                    print(f'block {i}:')
+                    for j, line in enumerate(block['lines']):
+                        print(f'    line {j}:')
+                        for k, span in enumerate(line['spans']):
+                            print(f'        span {k}:')
+                            for n, v in span.items():
+                                print(f'            {n}: {v!r}')
+            
+
+            
---
+++ packages/PyMuPDF/pymupdf-1.25.3.tar.gz/pymupdf-1.25.3/tests/test_insertpdf.py
@@ -0,0 +1,223 @@
+"""
+* Join multiple PDFs into a new one.
+* Compare with stored earlier result:
+    - must have identical object definitions
+    - must have different trailers
+* Try inserting files in a loop.
+"""
+
+import io
+import os
+import re
+import pymupdf
+
+scriptdir = os.path.abspath(os.path.dirname(__file__))
+resources = os.path.join(scriptdir, "resources")
+
+def approx_parse( text):
+    '''
+    Splits <text> into sequence of (text, number) pairs. Where sequence of
+    [0-9.] is not convertible to a number (e.g. '4.5.6'), <number> will be
+    None.
+    '''
+    ret = []
+    for m in re.finditer('([^0-9]+)([0-9.]*)', text):
+        text = m.group(1)
+        try:
+            number = float( m.group(2))
+        except Exception:
+            text += m.group(2)
+            number = None
+        ret.append( (text, number))
+    return ret
+
+def approx_compare( a, b, max_delta):
+    '''
+    Compares <a> and <b>, allowing numbers to differ by up to <delta>.
+    '''
+    aa = approx_parse( a)
+    bb = approx_parse( b)
+    if len(aa) != len(bb):
+        return 1
+    ret = 1
+    for (at, an), (bt, bn) in zip( aa, bb):
+        if at != bt:
+            break
+        if an is not None and bn is not None:
+            if abs( an - bn) >= max_delta:
+                print( f'diff={an-bn}: an={an} bn={bn}')
+                break
+        elif (an is None) != (bn is None):
+            break
+    else:
+        ret = 0
+    if ret:
+        print( f'Differ:\n    a={a!r}\n    b={b!r}')
+    return ret
+        
+
+def test_insert():
+    all_text_original = []  # text on input pages
+    all_text_combined = []  # text on resulting output pages
+    # prepare input PDFs
+    doc1 = pymupdf.open()
+    for i in range(5):  # just arbitrary number of pages
+        text = f"doc 1, page {i}"  # the 'globally' unique text
+        page = doc1.new_page()
+        page.insert_text((100, 72), text)
+        all_text_original.append(text)
+
+    doc2 = pymupdf.open()
+    for i in range(4):
+        text = f"doc 2, page {i}"
+        page = doc2.new_page()
+        page.insert_text((100, 72), text)
+        all_text_original.append(text)
+
+    doc3 = pymupdf.open()
+    for i in range(3):
+        text = f"doc 3, page {i}"
+        page = doc3.new_page()
+        page.insert_text((100, 72), text)
+        all_text_original.append(text)
+
+    doc4 = pymupdf.open()
+    for i in range(6):
+        text = f"doc 4, page {i}"
+        page = doc4.new_page()
+        page.insert_text((100, 72), text)
+        all_text_original.append(text)
+
+    new_doc = pymupdf.open()  # make combined PDF of input files
+    new_doc.insert_pdf(doc1)
+    new_doc.insert_pdf(doc2)
+    new_doc.insert_pdf(doc3)
+    new_doc.insert_pdf(doc4)
+    # read text from all pages and store in list
+    for page in new_doc:
+        all_text_combined.append(page.get_text().replace("\n", ""))
+    # the lists must be equal
+    assert all_text_combined == all_text_original
+
+
+def test_issue1417_insertpdf_in_loop():
+    """Using a context manager instead of explicitly closing files"""
+    f = os.path.join(resources, "1.pdf")
+    big_doc = pymupdf.open()
+    fd1 = os.open( f, os.O_RDONLY)
+    os.close( fd1)
+    for n in range(0, 1025):
+        with pymupdf.open(f) as pdf:
+            big_doc.insert_pdf(pdf)
+        # Create a raw file descriptor. If the above pymupdf.open() context leaks
+        # a file descriptor, fd will be seen to increment.
+        fd2 = os.open( f, os.O_RDONLY)
+        assert fd2 == fd1
+        os.close( fd2)
+    big_doc.close()
+
+
+def _test_insert_adobe():
+    path = os.path.abspath( f'{__file__}/../../../PyMuPDF-performance/adobe.pdf')
+    if not os.path.exists(path):
+        print(f'Not running test_insert_adobe() because does not exist: {os.path.relpath(path)}')
+        return
+    a = pymupdf.Document()
+    b = pymupdf.Document(path)
+    a.insert_pdf(b)
+
+
+def _2861_2871_merge_pdf(content: bytes, coverpage: bytes):
+    with pymupdf.Document(stream=coverpage, filetype="pdf") as coverpage_pdf:
+        with pymupdf.Document(stream=content, filetype="pdf") as content_pdf:
+            coverpage_pdf.insert_pdf(content_pdf)
+            doc = coverpage_pdf.write()
+            return doc 
+
+def test_2861():
+    path = os.path.abspath(f'{__file__}/../../tests/resources/test_2861.pdf')
+    with open(path, "rb") as content_pdf:
+        with open(path, "rb") as coverpage_pdf:
+            content = content_pdf.read()
+            coverpage = coverpage_pdf.read()
+            _2861_2871_merge_pdf(content, coverpage)
+
+def test_2871():
+    path = os.path.abspath(f'{__file__}/../../tests/resources/test_2871.pdf')
+    with open(path, "rb") as content_pdf:
+        with open(path, "rb") as coverpage_pdf:
+            content = content_pdf.read()
+            coverpage = coverpage_pdf.read()
+            _2861_2871_merge_pdf(content, coverpage)
+
+
+def test_3789():
+    
+    file_path = os.path.abspath(f'{__file__}/../../tests/resources/test_3789.pdf')
+    result_path = os.path.abspath(f'{__file__}/../../tests/test_3789_out')
+    pages_per_split = 5
+    
+    # Clean pdf
+    doc = pymupdf.open(file_path)
+    tmp = io.BytesIO()
+    tmp.write(doc.write(garbage=4, deflate=True))
+    
+    source_doc = pymupdf.Document('pdf', tmp.getvalue())
+    tmp.close()
+
+    # Calculate the number of pages per split file and the number of split files
+    page_range = pages_per_split - 1
+    split_range = range(0, source_doc.page_count, pages_per_split)
+    num_splits = len(split_range)
+
+    # Loop through each split range and create a new PDF file
+    for i, start in enumerate(split_range):
+        output_doc = pymupdf.open()
+
+        # Determine the ending page for this split file
+        to_page = start + page_range if i < num_splits - 1 else -1
+        output_doc.insert_pdf(source_doc, from_page=start, to_page=to_page)
+
+        # Save the output document to a file and add the path to the list of split files
+        path = f'{result_path}_{i}.pdf'
+        output_doc.save(path, garbage=2)
+        print(f'Have saved to {path=}.')
+
+        # If this is the last split file, exit the loop
+        if to_page == -1:
+            break
+
+
+def test_widget_insert():
+    """Confirm copy of form fields / widgets."""
+    from pymupdf import mupdf
+    tar = pymupdf.open(os.path.join(resources, "cms-etc-filled.pdf"))
+    pc0 = tar.page_count  # for later assertion
+    src = pymupdf.open(os.path.join(resources, "interfield-calculation.pdf"))
+    pc1 = src.page_count  # for later assertion
+
+    tarpdf = pymupdf._as_pdf_document(tar)
+    tar_field_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(tarpdf), "Root/AcroForm/Fields")
+    )
+    tar_co_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(tarpdf), "Root/AcroForm/CO")
+    )
+    srcpdf = pymupdf._as_pdf_document(src)
+    src_field_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(srcpdf), "Root/AcroForm/Fields")
+    )
+    src_co_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(srcpdf), "Root/AcroForm/CO")
+    )
+
+    tar.insert_pdf(src)
+    new_field_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(tarpdf), "Root/AcroForm/Fields")
+    )
+    new_co_count = mupdf.pdf_array_len(
+        mupdf.pdf_dict_getp(mupdf.pdf_trailer(tarpdf), "Root/AcroForm/CO")
+    )
+    assert tar.page_count == pc0 + pc1
+    assert new_field_count == tar_field_count + src_field_count
+    assert new_co_count == tar_co_count + src_co_count
