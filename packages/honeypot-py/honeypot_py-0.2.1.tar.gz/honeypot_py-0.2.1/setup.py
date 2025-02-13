673ce97531693416b94bf99dcb7761f5cdebd5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:31:28 +0000

    Add package honeypot-py/0.2.1/honeypot_py-0.2.1.tar.gz

---
+++ packages/honeypot-py/honeypot_py-0.2.1.tar.gz/honeypot_py-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="honeypot-py",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+    author="Honeypot",
+    author_email="support@honeypot.run",
+    description="A simple client for tracking events and user behavior",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/honeypot-run/honeypot-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
