2f2c708f389542009d06459414de0b85524b0ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 15:09:55 +0000

    Add package collate-sqllineage/1.6.4/collate_sqllineage-1.6.4.tar.gz

---
+++ packages/collate-sqllineage/collate_sqllineage-1.6.4.tar.gz/collate_sqllineage-1.6.4/tests/test_insert.py
@@ -0,0 +1,13 @@
+from .helpers import assert_table_lineage_equal
+
+
+def test_insert_into():
+    assert_table_lineage_equal("INSERT INTO tab1 VALUES (1, 2)", set(), {"tab1"})
+
+
+def test_insert_into_select():
+    assert_table_lineage_equal(
+        "INSERT INTO tab1 SELECT * FROM tab2;",
+        {"tab2"},
+        {"tab1"},
+    )
