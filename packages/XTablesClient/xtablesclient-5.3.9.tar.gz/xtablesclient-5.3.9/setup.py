be0d213a70486a6f1b651538d802bcaa9ccb91b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:10:46 +0000

    Add package XTablesClient/5.3.9/xtablesclient-5.3.9.tar.gz

---
+++ packages/XTablesClient/xtablesclient-5.3.9.tar.gz/xtablesclient-5.3.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="XTablesClient",
+    version="5.3.9",
+    description="A high-performance Python client for real-time management of XTablesServer network tables, "
+                "designed for robotics and complex data-driven systems.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Kobe Lei",
+    author_email="kobelei335@gmail.com",
+    url="https://github.com/Kobeeeef/XTABLES",
+    packages=find_packages(),
+    install_requires=open('requirements.txt', encoding='utf-8').read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
