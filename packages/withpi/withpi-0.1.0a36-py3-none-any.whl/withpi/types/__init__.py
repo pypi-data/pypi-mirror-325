10717a93bd1eab421462812ac69bfed9ae8ca52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:31:02 +0000

    Add package withpi/0.1.0a36/withpi-0.1.0a36-py3-none-any.whl

---
+++ packages/withpi/withpi-0.1.0a36-py3-none-any.whl/withpi/types/__init__.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import Contract as Contract, Dimension as Dimension, SubDimension as SubDimension
+from .contract_score_params import ContractScoreParams as ContractScoreParams
+from .data_generation_status import DataGenerationStatus as DataGenerationStatus
+from .prompt_optimize_params import PromptOptimizeParams as PromptOptimizeParams
+from .contracts_score_metrics import ContractsScoreMetrics as ContractsScoreMetrics
+from .prompt_optimization_status import PromptOptimizationStatus as PromptOptimizationStatus
+from .prompt_stream_messages_response import PromptStreamMessagesResponse as PromptStreamMessagesResponse
+from .contract_generate_dimensions_params import ContractGenerateDimensionsParams as ContractGenerateDimensionsParams
