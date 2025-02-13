eea9fc501a361b9fa78fa3289598e24238d0aa2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:08:08 +0000

    Add package nqlstore/0.0.2/nqlstore-0.0.2-py3-none-any.whl

---
+++ packages/nqlstore/nqlstore-0.0.2-py3-none-any.whl/nqlstore/_mongo.py
@@ -0,0 +1,213 @@
+"""MongoDB implementation"""
+
+import re
+from typing import Any, Iterable, Mapping, TypeVar
+
+from beanie import *
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorClientSession
+from pydantic import BaseModel
+from pydantic import Field as _Field
+from pydantic.main import ModelT, create_model
+
+from nqlstore._base import BaseStore
+
+_T = TypeVar("_T", bound=Document)
+_Filter = Mapping[str, Any] | bool
+_UPDATE_OP_REGEX = re.compile(r"\$\w*")
+
+
+class MongoStore(BaseStore):
+    """The store that persists its data in mongo db"""
+
+    def __init__(self, uri: str, database: str, **kwargs):
+        """
+        Args:
+            uri: the URI of the mongodb server to connect to
+            database: the name of the database
+            kwargs: extra key-word args to pass to AsyncIOMotorClient
+        """
+        super().__init__(uri, **kwargs)
+        self._client = AsyncIOMotorClient(uri, **kwargs)
+        self._db = self._client[database]
+        self._db_name = database
+
+    async def register(
+        self,
+        models: list[type[_T]],
+        allow_index_dropping: bool = False,
+        recreate_views: bool = False,
+        multiprocessing_mode: bool = False,
+        skip_indexes: bool = False,
+    ):
+        await init_beanie(
+            self._db,
+            document_models=models,
+            allow_index_dropping=allow_index_dropping,
+            recreate_views=recreate_views,
+            multiprocessing_mode=multiprocessing_mode,
+            skip_indexes=skip_indexes,
+        )
+
+    async def insert(
+        self,
+        model: type[_T],
+        items: Iterable[_T | dict],
+        session: AsyncIOMotorClientSession | None = None,
+        link_rule: WriteRules = WriteRules.DO_NOTHING,
+        **pymongo_kwargs: Any,
+    ) -> list[_T]:
+        parsed_items = [v if isinstance(v, model) else model(**v) for v in items]
+        results = await model.insert_many(
+            parsed_items, session=session, link_rule=link_rule, **pymongo_kwargs
+        )
+        return await model.find(
+            {"_id": {"$in": results.inserted_ids}}, session=session
+        ).to_list()
+
+    async def find(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        skip: int = 0,
+        limit: int | None = None,
+        sort: None | str | list[tuple[str, SortDirection]] = None,
+        session: AsyncIOMotorClientSession | None = None,
+        ignore_cache: bool = False,
+        fetch_links: bool = False,
+        with_children: bool = False,
+        lazy_parse: bool = False,
+        nesting_depth: int | None = None,
+        nesting_depths_per_field: dict[str, int] | None = None,
+        **pymongo_kwargs: Any,
+    ) -> list[_T]:
+        return await model.find(
+            *filters,
+            skip=skip,
+            limit=limit,
+            session=session,
+            ignore_cache=ignore_cache,
+            fetch_links=fetch_links,
+            with_children=with_children,
+            lazy_parse=lazy_parse,
+            nesting_depth=nesting_depth,
+            nesting_depths_per_field=nesting_depths_per_field,
+            **pymongo_kwargs,
+        ).to_list()
+
+    async def update(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        updates: dict | None = None,
+        session: AsyncIOMotorClientSession | None = None,
+        ignore_cache: bool = False,
+        fetch_links: bool = False,
+        with_children: bool = False,
+        lazy_parse: bool = False,
+        nesting_depth: int | None = None,
+        nesting_depths_per_field: dict[str, int] | None = None,
+        bulk_writer: BulkWriter | None = None,
+        upsert=False,
+        **pymongo_kwargs: Any,
+    ) -> list[_T]:
+        if updates is None:
+            updates = {}
+
+        mongo_updates = _to_mongo_updates(updates)
+
+        cursor = model.find(
+            *filters,
+            session=session,
+            ignore_cache=ignore_cache,
+            fetch_links=fetch_links,
+            with_children=with_children,
+            lazy_parse=lazy_parse,
+            nesting_depth=nesting_depth,
+            nesting_depths_per_field=nesting_depths_per_field,
+            **pymongo_kwargs,
+        )
+        ids = [v.id async for v in cursor.project(_IdOnly)]
+        await cursor.update(
+            mongo_updates, session=session, bulk_writer=bulk_writer, upsert=upsert
+        )
+        return await model.find({"_id": {"$in": ids}}).to_list()
+
+    async def delete(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        session: AsyncIOMotorClientSession | None = None,
+        ignore_cache: bool = False,
+        fetch_links: bool = False,
+        with_children: bool = False,
+        lazy_parse: bool = False,
+        nesting_depth: int | None = None,
+        nesting_depths_per_field: dict[str, int] | None = None,
+        bulk_writer: BulkWriter | None = None,
+        **pymongo_kwargs: Any,
+    ) -> list[_T]:
+        cursor = model.find(
+            *filters,
+            session=session,
+            ignore_cache=ignore_cache,
+            fetch_links=fetch_links,
+            with_children=with_children,
+            lazy_parse=lazy_parse,
+            nesting_depth=nesting_depth,
+            nesting_depths_per_field=nesting_depths_per_field,
+            **pymongo_kwargs,
+        )
+        deleted_items = await cursor.to_list()
+        await cursor.delete(session=session, bulk_writer=bulk_writer)
+        return deleted_items
+
+
+def MongoModel(name: str, schema: type[ModelT], /) -> type[ModelT]:
+    """Creates a new Mongo Model for the given schema
+
+    A new model can be defined by::
+
+        Model = MongoModel("Model", Schema)
+
+    Args:
+        name: the name of the model
+        schema: the schema from which the model is to be made
+
+    Returns:
+        a Mongo model class with the given name
+    """
+    return create_model(
+        name,
+        __doc__=schema.__doc__,
+        __slots__=schema.__slots__,
+        __base__=(
+            Document,
+            schema,
+        ),
+        **{
+            field_name: (field.annotation, field)
+            for field_name, field in schema.model_fields.items()
+        },
+    )
+
+
+class _IdOnly(BaseModel):
+    """Class used for projecting only id"""
+
+    id: PydanticObjectId = _Field(alias="_id")
+
+
+def _to_mongo_updates(updates: dict[str, Any]) -> dict[str, Any]:
+    """Converts the updates dict to a MongoLike update dict if it is not one already
+
+    Args:
+        updates: the update dict to convert
+
+    Returns:
+        the mongo update with update operators
+    """
+    for key in updates:
+        if _UPDATE_OP_REGEX.match(key):
+            return updates
+
+    return {"$set": updates}
---
+++ packages/nqlstore/nqlstore-0.0.2-py3-none-any.whl/nqlstore/_sql.py
@@ -0,0 +1,153 @@
+"""SQL implementation"""
+
+from typing import Any, Iterable, TypeVar
+
+from pydantic import create_model
+from pydantic.main import ModelT
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.sql._typing import (
+    _ColumnExpressionArgument,
+    _ColumnExpressionOrStrLabelArgument,
+)
+from sqlmodel import SQLModel as _SQLModel
+from sqlmodel import delete, insert, select, update
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from ._base import BaseStore
+from ._field import Field
+from .query.parsers import QueryParser
+from .query.selectors import QuerySelector
+
+_T = TypeVar("_T", bound=_SQLModel)
+_Filter = _ColumnExpressionArgument[bool] | bool
+
+
+class SQLStore(BaseStore):
+    """The store based on SQL relational database"""
+
+    def __init__(self, uri: str, parser: QueryParser | None = None, **kwargs):
+        super().__init__(uri, parser=parser, **kwargs)
+        self._engine = create_async_engine(uri, **kwargs)
+
+    async def register(self, models: list[type[_T]], checkfirst: bool = True):
+        tables = [v.__table__ for v in models]
+        async with self._engine.begin() as conn:
+            await conn.run_sync(
+                _SQLModel.metadata.create_all, tables=tables, checkfirst=checkfirst
+            )
+
+    async def insert(
+        self, model: type[_T], items: Iterable[_T | dict], **kwargs
+    ) -> list[_T]:
+        parsed_items = [v if isinstance(v, model) else model(**v) for v in items]
+        async with AsyncSession(self._engine) as session:
+            stmt = insert(model).returning(model)
+            cursor = await session.stream_scalars(stmt, parsed_items)
+            await session.commit()
+            results = await cursor.all()
+            return list(results)
+
+    async def find(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        query: QuerySelector | None = None,
+        skip: int = 0,
+        limit: int | None = None,
+        sort: tuple[_ColumnExpressionOrStrLabelArgument[Any]] = (),
+        **kwargs,
+    ) -> list[_T]:
+        async with AsyncSession(self._engine) as session:
+            nql_filters = ()
+            if query:
+                nql_filters = self._parser.to_sql(model, query=query)
+
+            cursor = await session.stream_scalars(
+                select(model)
+                .where(*filters, *nql_filters)
+                .limit(limit)
+                .offset(skip)
+                .order_by(*sort)
+            )
+            results = await cursor.all()
+            return list(results)
+
+    async def update(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        query: QuerySelector | None = None,
+        updates: dict | None = None,
+        **kwargs,
+    ) -> list[_T]:
+        async with AsyncSession(self._engine) as session:
+            nql_filters = ()
+            if query:
+                nql_filters = self._parser.to_sql(model, query=query)
+
+            stmt = (
+                update(model)
+                .where(*filters, *nql_filters)
+                .values(**updates)
+                .returning(model.__table__)
+            )
+            cursor = await session.stream(stmt)
+            results = await cursor.fetchall()
+            await session.commit()
+            return [model(**row._mapping) for row in results]
+
+    async def delete(
+        self,
+        model: type[_T],
+        *filters: _Filter,
+        query: QuerySelector | None = None,
+        **kwargs,
+    ) -> list[_T]:
+        async with AsyncSession(self._engine) as session:
+            nql_filters = ()
+            if query:
+                nql_filters = self._parser.to_sql(model, query=query)
+
+            cursor = await session.stream(
+                delete(model).where(*filters, *nql_filters).returning(model.__table__)
+            )
+            results = await cursor.all()
+            await session.commit()
+            return [model(**row._mapping) for row in results]
+
+
+class _SQLModelMeta(_SQLModel):
+    """The base class for all SQL models"""
+
+    id: int | None = Field(default=None, primary_key=True)
+
+
+def SQLModel(name: str, schema: type[ModelT], /) -> type[ModelT]:
+    """Creates a new SQLModel for the given schema for redis
+
+    A new model can be defined by::
+
+        Model = SQLModel("Model", Schema)
+
+    Args:
+        name: the name of the model
+        schema: the schema from which the model is to be made
+
+    Returns:
+        a SQLModel model class with the given name
+    """
+    # FIXME: Handle scenario where a pk is defined
+    return create_model(
+        name,
+        __doc__=schema.__doc__,
+        __slots__=schema.__slots__,
+        __cls_kwargs__={"table": True},
+        __base__=(
+            _SQLModelMeta,
+            schema,
+        ),
+        **{
+            field_name: (field.annotation, field)
+            for field_name, field in schema.model_fields.items()
+        },
+    )
