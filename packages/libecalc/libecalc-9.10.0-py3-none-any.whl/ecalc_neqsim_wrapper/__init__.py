14930439a2ea6fe3b61ff86f190d342d5a9dec61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:21:21 +0000

    Add package libecalc/9.10.0/libecalc-9.10.0-py3-none-any.whl

---
+++ packages/libecalc/libecalc-9.10.0-py3-none-any.whl/ecalc_neqsim_wrapper/__init__.py
@@ -0,0 +1,18 @@
+from ecalc_neqsim_wrapper.java_service import start_server
+
+java_gateway = start_server()
+neqsim = java_gateway.jvm.neqsim
+
+from ecalc_neqsim_wrapper.thermo import NeqsimFluid
+
+
+def methods(check_class):
+    """
+    Print list of available methods for a java class
+    """
+    try:
+        list_of_methods = check_class.getClass().getMethods()
+        for method in list_of_methods:
+            print(method.getName())
+    except AttributeError:
+        print(f"Unknown attribute for {check_class}, the class might not be a java class")
