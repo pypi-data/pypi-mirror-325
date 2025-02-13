590515f04ea4c4eee9d9d6b1801aba819c35b804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:09:31 +0000

    Add package workforce/1.0.21/workforce-1.0.21-py3-none-any.whl

---
+++ packages/workforce/workforce-1.0.21-py3-none-any.whl/workforce-1.0.21.data/scripts/workforce_run_node.py
@@ -0,0 +1,39 @@
+#!python
+import os
+import subprocess
+import networkx as nx
+from filelock import FileLock
+import argparse
+
+def update_node_status(filename, node, status):
+    """Update the status of a node in the GraphML file."""
+    with FileLock(f"{filename}.lock"):
+        G = nx.read_graphml(filename)
+        nx.set_node_attributes(G, {node: status}, 'status')
+        nx.write_graphml(G, filename)
+
+def run_node(filename, node, prefix='bash -c'):
+    """Execute a command associated with a node and update its status."""
+    G = nx.read_graphml(filename)
+    update_node_status(filename, node, 'running')
+
+    try:
+        label = G.nodes[node].get('label', '')
+        escaped_label = label.replace('"', '\\"')
+        command = f"{prefix} \"{escaped_label}\""
+        subprocess.run(command, shell=True, check=True)
+        update_node_status(filename, node, 'ran')
+    except subprocess.CalledProcessError:
+        update_node_status(filename, node, 'fail')
+
+def parse_arguments():
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(description="Run a node's associated command.")
+    parser.add_argument("filename", type=str, help="Path to the input GraphML file.")
+    parser.add_argument("node", type=str, help="Node to execute.")
+    parser.add_argument("--prefix", '-p', default='bash -c', required=False, type=str, help="Prefix for command execution.")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run_node(args.filename, args.node, args.prefix)
