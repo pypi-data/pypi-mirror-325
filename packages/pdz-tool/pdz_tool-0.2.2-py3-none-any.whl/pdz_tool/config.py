a0761d22c1fbadb1cb91e023ac510479e44c9692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 20:09:30 +0000

    Add package pdz-tool/0.2.2/pdz_tool-0.2.2-py3-none-any.whl

---
+++ packages/pdz-tool/pdz_tool-0.2.2-py3-none-any.whl/pdz_tool/config.py
@@ -0,0 +1,19 @@
+"""
+Config file for PDZ tool
+"""
+
+"""
+PDZ Tool version
+"""
+VERSION = "0.2.0"
+
+"""
+Supoorted PDZ versions
+{
+    record_type: version
+}
+"""
+SUPPORTED_PDZ_VERSIONS = {
+    25: "pdz25",
+    257: "pdz24"
+}
