f038d73ec6a65aec5cecdb0de49b90f6df67a088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:09:33 +0000

    Add package mteb/1.34.4/mteb-1.34.4-py3-none-any.whl

---
+++ packages/mteb/mteb-1.34.4-py3-none-any.whl/mteb/tasks/Reranking/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from .ara.NamaaMrTydiReranking import *
+from .eng.AskUbuntuDupQuestions import *
+from .eng.MindSmallReranking import *
+from .eng.SciDocsReranking import *
+from .eng.StackOverflowDupQuestions import *
+from .eng.WebLINXCandidatesReranking import *
+from .fra.AlloprofReranking import *
+from .fra.SyntecReranking import *
+from .jpn.MMarcoReranking import *
+from .multilingual.ESCIReranking import *
+from .multilingual.MIRACLReranking import *
+from .multilingual.WikipediaRerankingMultilingual import *
+from .rus.RuBQReranking import *
+from .zho.CMTEBReranking import *
