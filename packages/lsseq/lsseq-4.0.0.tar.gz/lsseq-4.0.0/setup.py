6c889074d9c3e96a29bb26f3076740bf9534bec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 18:18:34 +0000

    Add package lsseq/4.0.0/lsseq-4.0.0.tar.gz

---
+++ packages/lsseq/lsseq-4.0.0.tar.gz/lsseq-4.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name            = 'lsseq',
+    version         = '4.0.0',
+    description='ls-like command for image-sequences',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url             = 'https://github.com/jrowellfx/lsseq',
+    author          = 'James Philip Rowell',
+    author_email    = 'james@alpha-eleven.com',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Development Status :: 5 - Production/Stable',
+    ],
+
+    packages        = ['lsseq'],
+    python_requires = '>=3.7, <4',
+    install_requires=['seqLister>=1.0.0'],
+
+    entry_points = {
+        'console_scripts': [
+            'lsseq = lsseq.__main__:main',
+        ]
+    }
+)
