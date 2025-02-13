0324b8380949b5d4be5a0241712477a1f7bbebff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:39:55 +0000

    Add package topk-sdk/0.1.10/topk_sdk-0.1.10.tar.gz

---
+++ packages/topk-sdk/topk_sdk-0.1.10.tar.gz/topk_sdk-0.1.10/topk-protos/src/control/v1/collection_ext.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+use super::*;
+
+impl Collection {
+    pub fn new(
+        name: impl Into<String>,
+        org_id: impl Into<String>,
+        project_id: impl Into<String>,
+        schema: impl Into<HashMap<String, FieldSpec>>,
+    ) -> Self {
+        Collection {
+            name: name.into(),
+            org_id: org_id.into(),
+            project_id: project_id.into(),
+            schema: schema.into(),
+        }
+    }
+}
