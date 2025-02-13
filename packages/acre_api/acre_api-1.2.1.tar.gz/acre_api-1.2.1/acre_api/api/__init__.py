d0df083e91efe1dbef971589c1b1439eeacc3965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:10:02 +0000

    Add package acre_api/1.2.1/acre_api-1.2.1.tar.gz

---
+++ packages/acre_api/acre_api-1.2.1.tar.gz/acre_api-1.2.1/acre_api/api/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+
+# import apis into api package
+from acre_api.api.authentication_api import AuthenticationApi
+from acre_api.api.case_api import CaseApi
+from acre_api.api.client_api import ClientApi
+from acre_api.api.commission_structure_api import CommissionStructureApi
+from acre_api.api.document_api import DocumentApi
+from acre_api.api.document_requirement_api import DocumentRequirementApi
+from acre_api.api.general_insurance_product_api import GeneralInsuranceProductApi
+from acre_api.api.group_api import GroupApi
+from acre_api.api.internal_api import InternalApi
+from acre_api.api.mortgage_sourcing_api import MortgageSourcingApi
+from acre_api.api.mortgage_mortgage_product_api import MortgageMortgageProductApi
+from acre_api.api.note_api import NoteApi
+from acre_api.api.organisation_api import OrganisationApi
+from acre_api.api.permission_api import PermissionApi
+from acre_api.api.property_api import PropertyApi
+from acre_api.api.protection_product_api import ProtectionProductApi
+from acre_api.api.protection_product_api import ProtectionProductApi
+from acre_api.api.role_api import RoleApi
+from acre_api.api.service_api import ServiceApi
+from acre_api.api.template_api import TemplateApi
+from acre_api.api.user_api import UserApi
+
