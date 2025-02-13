22c8d3715973564976f9a2a32fd788969ed8ea31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:30:59 +0000

    Add package Ornator/0.0.2/Ornator-0.0.2-py3-none-any.whl

---
+++ packages/Ornator/Ornator-0.0.2-py3-none-any.whl/ornator/__init__.py
@@ -0,0 +1,5 @@
+from ornator.class_decorator import AfterClassDecorator, BeforeClassDecorator, DualClassDecorator, EmptyClassDecorator
+from ornator.decorator import AfterDecorator, BeforeDecorator, DualDecorator, EmptyDecorator
+
+__version__ = '0.0.2'
+__all__ = ['AfterClassDecorator', 'AfterDecorator', 'BeforeClassDecorator', 'BeforeDecorator', 'DualClassDecorator', 'DualDecorator', 'EmptyClassDecorator', 'EmptyDecorator']
