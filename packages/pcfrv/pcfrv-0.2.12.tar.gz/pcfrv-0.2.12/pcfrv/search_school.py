c533d0a6867204772852dfa005508967b65e7fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:07:57 +0000

    Add package pcfrv/0.2.12/pcfrv-0.2.12.tar.gz

---
+++ packages/pcfrv/pcfrv-0.2.12.tar.gz/pcfrv-0.2.12/pcfrv/search_school.py
@@ -0,0 +1,32 @@
+from httpx import get
+from fake_useragent import UserAgent
+from pydantic import validate_call
+
+from re import findall
+from json import loads
+from urllib import parse
+from dataclasses import dataclass
+
+@dataclass
+class School:
+  name: str
+  period: str
+  school_code: int
+  period_code: int
+
+comcigan_url = 'http://comci.net:4082'
+
+@validate_call(validate_return=True)
+def get_code() -> str:
+  resp = get(f"{comcigan_url}/st", headers={"User-Agent": UserAgent(platforms=["desktop"]).random})
+  resp.encoding = 'euc-kr'
+  return findall('\\.\\/[0-9]+\\?[0-9]+l', resp.text)[0][1:]
+
+@validate_call(validate_return=True)
+def get_school_code(school_name: str) -> list[School]:
+  resp = get(f"{comcigan_url}{get_code()}{parse.quote(school_name, encoding='euc-kr')}")
+  resp.encoding = 'UTF-8'
+  schools = [School(i[2],i[1],i[0],int(i[3])) for i in loads(resp.text.strip(chr(0)))["학교검색"]]
+  if len(schools) > 0 and schools[-1].period_code == 0 and schools[-1].school_code == 0:
+    del schools[-1]
+  return schools
