611c3bdcfa947488542d41644eb9880db32733df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:07:57 +0000

    Add package pcfrv/0.2.12/pcfrv-0.2.12-py3-none-any.whl

---
+++ packages/pcfrv/pcfrv-0.2.12-py3-none-any.whl/pcfrv/search_school.pyi
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class School:
+    name: str
+    period: str
+    school_code: int
+    period_code: int
+    def __init__(self, name, period, school_code, period_code) -> None: ...
+
+comcigan_url: str
+
+def get_code() -> str: ...
+def get_school_code(school_name: str) -> list[School]: ...
