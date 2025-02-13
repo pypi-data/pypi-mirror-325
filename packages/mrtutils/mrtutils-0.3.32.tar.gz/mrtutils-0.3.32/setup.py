0805c8431d6adcd78f2c48080f04a28eedeec923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:21:10 +0000

    Add package mrtutils/0.3.32/mrtutils-0.3.32.tar.gz

---
+++ packages/mrtutils/mrtutils-0.3.32.tar.gz/mrtutils-0.3.32/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+     name='mrtutils',
+     version="0.3.32",
+     author="Jason Berger",
+     author_email="JBerger@up-rev.com",
+     description="Utilities for MrT",
+     long_description=long_description,
+     scripts=['mrtutils/mrt-config','mrtutils/mrt-device', 'mrtutils/mrt-ble', 'mrtutils/mrt-config-gui','mrtutils/mrt-doc', 'mrtutils/mrt-version','mrtutils/mrt-gen'],
+     long_description_content_type="text/x-rst",
+     url="https://mrt.readthedocs.io/en/latest/index.html",
+     packages=setuptools.find_packages(),
+     package_data={
+     'mrtutils':['templates/*', 'templates/*/*','templates/*/*/*','templates/*/*/*/*']
+     },
+     install_requires=[
+        'markdown',
+        'mako',
+        'pyyaml',
+        'polypacket',
+        'update_notipy',
+        'kconfiglib',
+        'gitpython'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
