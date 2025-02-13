f6026d99673e668ec56efa6b25b1f1af9471e5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:30:54 +0000

    Add package netbox-config-backup/2.1.5/netbox_config_backup-2.1.5.tar.gz

---
+++ packages/netbox-config-backup/netbox_config_backup-2.1.5.tar.gz/netbox_config_backup-2.1.5/netbox_config_backup/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from dcim.api.serializers import DeviceSerializer
+from ipam.api.serializers import IPAddressSerializer
+from netbox.api.serializers import NetBoxModelSerializer
+
+from netbox_config_backup.models import Backup
+
+
+__all__ = (
+    'BackupSerializer',
+)
+
+
+class BackupSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='plugins-api:netbox_config_backup-api:backup-detail'
+    )
+    device = DeviceSerializer(nested=True, required=False, allow_null=True),
+    ip = IPAddressSerializer(nested=True, required=False, allow_null=True)
+
+    class Meta:
+        model = Backup
+        fields = [
+            'id', 'url', 'display', 'name', 'device', 'ip',
+            'uuid', 'status', 'config_status',
+        ]
+        brief_fields = ('display', 'id', 'name', 'url')
