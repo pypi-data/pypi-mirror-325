2e1e4ed6a3fe04d84dd541c91a8ea58eae9b7218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:09:56 +0000

    Add package sms-spam-detection/0.1.2/sms_spam_detection-0.1.2.tar.gz

---
+++ packages/sms-spam-detection/sms_spam_detection-0.1.2.tar.gz/sms_spam_detection-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+setup(
+    name="sms_spam_detection",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires = Path("requirements.txt").read_text().splitlines()
+)
