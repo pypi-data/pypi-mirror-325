28d5a0fae413c6ff7d4e17f5a32af49dfa308499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:09:25 +0000

    Add package sinhala-llm-evaluator/0.3.4/sinhala_llm_evaluator-0.3.4.tar.gz

---
+++ packages/sinhala-llm-evaluator/sinhala_llm_evaluator-0.3.4.tar.gz/sinhala_llm_evaluator-0.3.4/README.md
@@ -0,0 +1,8 @@
+# llm_evaluator
+
+`sinhala_llm_evaluator` is a Python package that provides tools for evaluating language models during training.
+
+## Installation
+
+```bash
+pip install sinhala_llm_evaluator
---
+++ packages/sinhala-llm-evaluator/sinhala_llm_evaluator-0.3.4.tar.gz/sinhala_llm_evaluator-0.3.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sinhala_llm_evaluator",
+    version="0.3.4",  
+    description="A module for evaluating language models during training",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/udithishanka",  # Your repo or homepage
+    author="Udith Ishanka",
+    author_email="udithishanka.s@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "transformers",
+        "datasets",
+        "evaluate",
+        "numpy",
+        "nltk",
+        "absl-py",
+        "rouge-score",
+        "scikit-learn",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
