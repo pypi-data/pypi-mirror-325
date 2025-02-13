a8c6fda977082e237bebf342ef7b0c017cfb29ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:10:43 +0000

    Add package dreamify/0.19.2/dreamify-0.19.2.tar.gz

---
+++ packages/dreamify/dreamify-0.19.2.tar.gz/dreamify-0.19.2/dreamify/utils/deep_dream_utils/deep_dream.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+
+class DeepDream(tf.Module):
+    def __init__(self, model):
+        self.model = model
+
+    @tf.function(
+        input_signature=(
+            tf.TensorSpec(shape=[None, None, 3], dtype=tf.float32),
+            tf.TensorSpec(shape=[], dtype=tf.int32),
+            tf.TensorSpec(shape=[], dtype=tf.float32),
+        )
+    )
+    def __call__(self, img, steps, step_size):
+        print("Tracing DeepDream computation graph...")
+        loss = tf.constant(0.0)
+        for n in tf.range(steps):
+            with tf.GradientTape() as tape:
+                tape.watch(img)
+                loss = calc_loss(img, self.model)
+                gradients = tape.gradient(loss, img)
+            gradients /= tf.math.reduce_std(gradients) + 1e-8
+            img = img + gradients * step_size
+            img = tf.clip_by_value(img, -1, 1)
+
+        return loss, img
