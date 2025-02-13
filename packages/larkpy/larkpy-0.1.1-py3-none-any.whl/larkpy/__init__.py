99ed23ecf8c2bfa113a5028a8547fd0a43c1ec82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 18:29:28 +0000

    Add package larkpy/0.1.1/larkpy-0.1.1-py3-none-any.whl

---
+++ packages/larkpy/larkpy-0.1.1-py3-none-any.whl/larkpy/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.1"
+__author__ = "Benature"
+__github__ = "https://github.com/Benature/larkpy"
+
+from .bot import LarkBot
+
+from .api import LarkAPI
+from .docx import LarkDocx
+from .bitTable import LarkBitTable
+from .im import LarkMessage
+from .calendar import LarkCalendar
