6944202718974d291f115308fc5c4370f3c14f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:29:28 +0000

    Add package veeder-root-tls-socket-library/1.0.5/veeder_root_tls_socket_library-1.0.5-py3-none-any.whl

---
+++ packages/veeder-root-tls-socket-library/veeder_root_tls_socket_library-1.0.5-py3-none-any.whl/tls_client.py
@@ -0,0 +1,43 @@
+# tls_client.py - A command-line interface for communicating with TLS automatic tank gauges.
+# This is not included with the veeder_root_tls_socket_library wheel or PyPI package.
+
+from argparse import ArgumentParser
+from veeder_root_tls_socket_library.socket import TlsSocket
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("ip", help="IP address of the TLS system.", type=str)
+    parser.add_argument("port", help="Port number used to connect to the" \
+        "serial interface remotely.", type=int)
+    args = parser.parse_args()
+
+    with TlsSocket(args.ip, args.port) as tls:
+        print(f"You are connected to {args.ip} using port {args.port}.")
+
+        while True:
+            command = input("\n>> ")
+
+            match command.lower():
+                case "help":
+                    print("Veeder-Root tank gauge commands can be found in the Serial Interface" \
+                        " manual for that model of tank gauge.")
+                    print("You can exit this program by using the 'exit' command at any time.")
+
+                case "exit": 
+                    break
+
+                # Handling other inputs by simply running them against the TLS system.
+                case _:
+                    try:                      output = tls.execute(command)
+                    except ValueError as err: output = err.args[0] 
+
+                    # Error arguments may be output as a dictionary.
+                    if type(output) == dict:
+                        for key in output.keys():
+                            print(f"{key}: {output[key]}")
+
+                    # Print any other output type to the terminal.
+                    else:
+                        print(output)
+
+        print("Connection ended.\n")
---
+++ packages/veeder-root-tls-socket-library/veeder_root_tls_socket_library-1.0.5-py3-none-any.whl/veeder_root_tls_socket_library/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py - This file is here to indicate that this directory is a package.
+
+from veeder_root_tls_socket_library import *
