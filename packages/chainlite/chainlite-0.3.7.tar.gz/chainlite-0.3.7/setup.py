a3da00a6d12b402ce28f6d06dc0ca46d8274a090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:18:46 +0000

    Add package chainlite/0.3.7/chainlite-0.3.7.tar.gz

---
+++ packages/chainlite/chainlite-0.3.7.tar.gz/chainlite-0.3.7/setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="chainlite",
+    version="0.3.7",
+    author="Sina Semnani",
+    author_email="sinaj@cs.stanford.edu",
+    description="A Python package that uses LangChain and LiteLLM to call large language model APIs easily",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/stanford-oval/chainlite",
+    packages=find_packages(),
+    install_requires=[
+        "tqdm",
+        "langchain==0.3.17",
+        "langchain-community==0.3.3",
+        "langchain-core==0.3.33",
+        "langchain-text-splitters==0.3.5",
+        "langgraph==0.2.39",
+        "langsmith==0.1.137",
+        "langgraph-checkpoint==2.0.1",
+        "langgraph-sdk==0.1.33",
+        "litellm==1.60.4",
+        "numpydoc",  # needed for function calling with LiteLLM
+        "grandalf",  # to visualize LangGraph graphs
+        "pydantic>=2.5",
+        "redis[hiredis]",
+    ],
+    extras_require={
+        "dev": [
+            "invoke",  # for running tasks and scripts
+            "pytest",  # for testing
+            "pytest-asyncio",  # for testing async code
+            "setuptools",  # for building wheels
+            "wheel",  # for building wheels
+            "twine",  # for uploading to PyPI
+            "isort",  # for code formatting
+            "black",  # for code formatting
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    license="Apache License 2.0",
+)
