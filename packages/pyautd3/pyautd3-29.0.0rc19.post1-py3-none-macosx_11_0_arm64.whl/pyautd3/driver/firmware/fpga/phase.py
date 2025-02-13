2ceea9299922f179f737d1a0e9d2e442705a19eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:10:03 +0000

    Add package pyautd3/29.0.0rc19.post1/pyautd3-29.0.0rc19.post1-py3-none-macosx_11_0_arm64.whl

---
+++ packages/pyautd3/pyautd3-29.0.0rc19.post1-py3-none-macosx_11_0_arm64.whl/pyautd3/driver/firmware/fpga/phase.py
@@ -0,0 +1,33 @@
+from typing import Self
+
+from pyautd3.driver.defined import Angle
+from pyautd3.native_methods.autd3 import Phase as Phase_
+from pyautd3.native_methods.autd3capi import NativeMethods as Base
+
+
+class Phase:
+    value: int
+
+    def __init__(self: Self, phase: "int | Angle") -> None:
+        match phase:
+            case int():
+                self.value = phase
+            case Angle():
+                self.value = int(Base().phase_from_rad(phase.radian()))
+            case _:
+                raise TypeError
+
+    def radian(self: Self) -> float:
+        return float(Base().phase_to_rad(self._inner()))
+
+    def __eq__(self: Self, other: object) -> bool:
+        return isinstance(other, Phase) and self.value == other.value
+
+    def __str__(self: Self) -> str:
+        return f"Phase({self.value})"
+
+    def __repr__(self: Self) -> str:
+        return f"Phase({self.value})"
+
+    def _inner(self: Self) -> Phase_:
+        return Phase_(self.value)
