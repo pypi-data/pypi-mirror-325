77c6cbdf3060cb12d97a05c0371fe3120a26fbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:18:43 +0000

    Add package chik-rs/0.17.0/chik_rs-0.17.0.tar.gz

---
+++ packages/chik-rs/chik_rs-0.17.0.tar.gz/chik_rs-0.17.0/wheel/src/run_program.rs
@@ -0,0 +1,54 @@
+use chik_consensus::allocator::make_allocator;
+use chik_consensus::gen::flags::ALLOW_BACKREFS;
+use chik_protocol::LazyNode;
+use klvmr::chik_dialect::ChikDialect;
+use klvmr::cost::Cost;
+use klvmr::reduction::Response;
+use klvmr::run_program::run_program;
+use klvmr::serde::{
+    node_from_bytes, node_from_bytes_backrefs, node_to_bytes, serialized_length_from_bytes,
+};
+use pyo3::buffer::PyBuffer;
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+use std::rc::Rc;
+
+#[allow(clippy::borrow_deref_ref)]
+#[pyfunction]
+pub fn serialized_length(program: PyBuffer<u8>) -> PyResult<u64> {
+    assert!(program.is_c_contiguous(), "program must be contiguous");
+    let program =
+        unsafe { std::slice::from_raw_parts(program.buf_ptr() as *const u8, program.len_bytes()) };
+    Ok(serialized_length_from_bytes(program)?)
+}
+
+#[allow(clippy::borrow_deref_ref)]
+#[pyfunction]
+pub fn run_chik_program(
+    py: Python<'_>,
+    program: &[u8],
+    args: &[u8],
+    max_cost: Cost,
+    flags: u32,
+) -> PyResult<(Cost, LazyNode)> {
+    let mut allocator = make_allocator(flags);
+
+    let reduction = (|| -> PyResult<Response> {
+        let deserialize = if (flags & ALLOW_BACKREFS) != 0 {
+            node_from_bytes_backrefs
+        } else {
+            node_from_bytes
+        };
+        let program = deserialize(&mut allocator, program)?;
+        let args = deserialize(&mut allocator, args)?;
+        let dialect = ChikDialect::new(flags);
+
+        Ok(py.allow_threads(|| run_program(&mut allocator, &dialect, program, args, max_cost)))
+    })()?
+    .map_err(|e| {
+        let blob = node_to_bytes(&allocator, e.0).ok().map(hex::encode);
+        PyValueError::new_err((e.1, blob))
+    })?;
+    let val = LazyNode::new(Rc::new(allocator), reduction.1);
+    Ok((reduction.0, val))
+}
