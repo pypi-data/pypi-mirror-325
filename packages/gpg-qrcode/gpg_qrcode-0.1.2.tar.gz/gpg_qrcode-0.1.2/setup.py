afbd846e4e74b53a0ce124ba3be35858a1ad7037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:26 +0000

    Add package gpg-qrcode/0.1.2/gpg_qrcode-0.1.2.tar.gz

---
+++ packages/gpg-qrcode/gpg_qrcode-0.1.2.tar.gz/gpg_qrcode-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gpg-qrcode',
+    version='0.1.2',
+    license_file="LICENSE",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'qrcode',
+        'requests',
+        'pillow',
+           ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ubuntupunk/gpg-qrcode",
+    author="David Robert Lewis",
+    author_email="ubuntupunk@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'gpg-qrcode=gpg_qr.main:main',
+        ],
+    },
+    python_requires=">=3.6",
+    )
