9ac8e418d8c3e3abdd9be53a076e64b8b23651a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:09:50 +0000

    Add package blue-sandbox/5.320.1/blue_sandbox-5.320.1.tar.gz

---
+++ packages/blue-sandbox/blue_sandbox-5.320.1.tar.gz/blue_sandbox-5.320.1/blue_sandbox/.abcli/alias.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+alias @damages=blue_sandbox_microsoft_building_damage_assessment
+
+alias sagesemseg=blue_sandbox_sagesemseg
+
+alias @sandbox=blue_sandbox
+
+alias gpt=openai_commands_gpt
