5662ae21f3a44302118918d6fd8bef59d7b066d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:28:47 +0000

    Add package blender-utils/0.0.2/blender_utils-0.0.2-py3-none-any.whl

---
+++ packages/blender-utils/blender_utils-0.0.2-py3-none-any.whl/blender_utils/collection.py
@@ -0,0 +1,11 @@
+from .get_b_vars import get_context, get_collections
+
+def create_collection (collection_name):
+  new_collection = get_collections().new(collection_name)
+  # 将新集合添加到当前场景中
+  get_context().scene.collection.children.link(new_collection)
+
+  return new_collection
+
+def get_collection (collection_name):
+  return  get_collections().get(collection_name)
---
+++ packages/blender-utils/blender_utils-0.0.2-py3-none-any.whl/blender_utils/ui.py
@@ -0,0 +1,26 @@
+from .get_b_vars import get_scene, get_props
+
+def add_row (layout, data, prop, text):
+  row = layout.column().row()
+  row.prop(data, prop, text = text)
+
+def add_row_with_label (layout, label, data, prop, factor):
+  split = layout.column().split(factor = factor)
+  row_label = split.row()
+  row_label.label(text = label)
+  row_prop = split.row()
+  row_prop.prop(data, prop, text = "")
+
+def add_row_with_operator (layout, operator, text):
+  row = layout.column().row()
+  row.operator(operator, text = text)
+
+def add_scene_custom_prop (prop, type, default = None, desc = ''):
+  fn = getattr(get_props(), f'{ type }Property')
+  scene = get_scene()
+
+  if default != None:
+    setattr(scene, prop, fn(name = prop, description = desc, default = default))
+  else:
+    setattr(scene, prop, fn(name = prop, description = desc))
+  
