d443f25356e7553dd396bab71df6e2d717e144f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:18 +0000

    Add package docu-devs-api-client/0.1.2/docu_devs_api_client-0.1.2-py3-none-any.whl

---
+++ packages/docu-devs-api-client/docu_devs_api_client-0.1.2-py3-none-any.whl/docudevs/models/upload_document_body.py
@@ -0,0 +1,76 @@
+from io import BytesIO
+from typing import Any, Dict, List, Type, TypeVar
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import File
+
+T = TypeVar("T", bound="UploadDocumentBody")
+
+
+@_attrs_define
+class UploadDocumentBody:
+    """
+    Attributes:
+        document (File):
+    """
+
+    document: File
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        document = self.document.to_tuple()
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "document": document,
+            }
+        )
+
+        return field_dict
+
+    def to_multipart(self) -> Dict[str, Any]:
+        document = self.document.to_tuple()
+
+        field_dict: Dict[str, Any] = {}
+        for prop_name, prop in self.additional_properties.items():
+            field_dict[prop_name] = (None, str(prop).encode(), "text/plain")
+
+        field_dict.update(
+            {
+                "document": document,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        document = File(payload=BytesIO(d.pop("document")))
+
+        upload_document_body = cls(
+            document=document,
+        )
+
+        upload_document_body.additional_properties = d
+        return upload_document_body
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
