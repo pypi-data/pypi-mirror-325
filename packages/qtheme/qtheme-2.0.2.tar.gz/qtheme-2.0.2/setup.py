ba2197e86cc7575dac45f72e8e8a56662684b467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 20:09:42 +0000

    Add package qtheme/2.0.2/qtheme-2.0.2.tar.gz

---
+++ packages/qtheme/qtheme-2.0.2.tar.gz/qtheme-2.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name="qtheme",
+    version="2.0.2",
+    description="Tools for management qtile environment",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Jesus Machaca",
+    author_email="falcorgd@gmail.com",
+    url="https://github.com/loadept/qtheme",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Terminals :: Terminal Emulators/X Terminals"
+    ],
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "qtheme = qtheme.main:main"
+        ]
+    },
+)
