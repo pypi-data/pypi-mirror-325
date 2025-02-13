e6d2e987ce183ebc28d9ffe05d4cd9ce95875674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:08:04 +0000

    Add package achatbot/0.0.8.7/achatbot-0.0.8.7-py3-none-any.whl

---
+++ packages/achatbot/achatbot-0.0.8.7-py3-none-any.whl/achatbot/modules/speech/asr/__init__.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+from achatbot.common.types import MODELS_DIR
+from achatbot.common import interface
+from achatbot.common.factory import EngineClass, EngineFactory
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+class ASREnvInit:
+    @staticmethod
+    def getEngine(tag, **kwargs) -> interface.IAsr | EngineClass:
+        if "sense_voice" in tag:
+            from . import sense_voice_asr
+        elif "groq" in tag:
+            from . import whisper_groq_asr
+        elif "whisper_" in tag:
+            from . import whisper_asr
+
+        engine = EngineFactory.get_engine_by_tag(EngineClass, tag, **kwargs)
+        return engine
+
+    @staticmethod
+    def initASREngine(
+        tag: str | None = None,
+        # kwargs: dict | None = None
+    ) -> interface.IAsr | EngineClass:
+        # asr
+        tag = tag or os.getenv("ASR_TAG", "whisper_timestamped_asr")
+        kwargs = {}
+        kwargs["model_name_or_path"] = os.getenv("ASR_MODEL_NAME_OR_PATH", "base")
+        kwargs["download_path"] = MODELS_DIR
+        kwargs["verbose"] = bool(os.getenv("ASR_VERBOSE", "True"))
+        kwargs["language"] = os.getenv("ASR_LANG", "zh")
+        engine = ASREnvInit.getEngine(tag, **kwargs)
+        logging.info(f"initASREngine: {tag}, {engine}")
+        return engine
