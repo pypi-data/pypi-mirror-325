c79e4a09b6e36902b39763b8a8115421e711ba9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:21:03 +0000

    Add package asreview-datatools/1.4/asreview_datatools-1.4.tar.gz

---
+++ packages/asreview-datatools/asreview_datatools-1.4.tar.gz/asreview_datatools-1.4/asreviewcontrib/datatools/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from asreviewcontrib.datatools._version import __version__
+    from asreviewcontrib.datatools._version import __version_tuple__
+except ImportError:
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
