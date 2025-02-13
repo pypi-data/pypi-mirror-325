d6ec9a6d07c326bd911b98c7517a9701420608c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:31:01 +0000

    Add package CaseOpenerAPI/0.0.1/caseopenerapi-0.0.1.tar.gz

---
+++ packages/CaseOpenerAPI/caseopenerapi-0.0.1.tar.gz/caseopenerapi-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='CaseOpenerAPI',
+  version='0.0.1',
+  author='Narkshubat',
+  author_email='markshubat@gmail.com',
+  description='Case Opener api tools',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.12',
+    'Operating System :: OS Independent'
+  ],
+  keywords='skin generation caseopener',
+  python_requires='>=3.6'
+)
