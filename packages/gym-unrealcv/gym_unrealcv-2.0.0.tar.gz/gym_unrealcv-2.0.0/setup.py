cfc6602daec47696d44ab4f86a231c657a21955f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:18:30 +0000

    Add package gym-unrealcv/2.0.0/gym_unrealcv-2.0.0.tar.gz

---
+++ packages/gym-unrealcv/gym_unrealcv-2.0.0.tar.gz/gym_unrealcv-2.0.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_unrealcv',
+      version='2.0.0',
+      install_requires=['gym==0.10.9', 'matplotlib', 'numpy', 'unrealcv', 'wget', 'opencv-python','simple_pid', 'pynput','docker'],  # And any other dependencies foo needs
+)
