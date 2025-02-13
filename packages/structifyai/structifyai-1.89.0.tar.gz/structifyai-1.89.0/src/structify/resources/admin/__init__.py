d8be9c4e82ade92c279ca923f08037e79c6c1eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:39:42 +0000

    Add package structifyai/1.89.0/structifyai-1.89.0.tar.gz

---
+++ packages/structifyai/structifyai-1.89.0.tar.gz/structifyai-1.89.0/src/structify/resources/admin/__init__.py
@@ -0,0 +1,61 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .admin import (
+    AdminResource,
+    AsyncAdminResource,
+    AdminResourceWithRawResponse,
+    AsyncAdminResourceWithRawResponse,
+    AdminResourceWithStreamingResponse,
+    AsyncAdminResourceWithStreamingResponse,
+)
+from .users import (
+    UsersResource,
+    AsyncUsersResource,
+    UsersResourceWithRawResponse,
+    AsyncUsersResourceWithRawResponse,
+    UsersResourceWithStreamingResponse,
+    AsyncUsersResourceWithStreamingResponse,
+)
+from .human_llm import (
+    HumanLlmResource,
+    AsyncHumanLlmResource,
+    HumanLlmResourceWithRawResponse,
+    AsyncHumanLlmResourceWithRawResponse,
+    HumanLlmResourceWithStreamingResponse,
+    AsyncHumanLlmResourceWithStreamingResponse,
+)
+from .training_datasets import (
+    TrainingDatasetsResource,
+    AsyncTrainingDatasetsResource,
+    TrainingDatasetsResourceWithRawResponse,
+    AsyncTrainingDatasetsResourceWithRawResponse,
+    TrainingDatasetsResourceWithStreamingResponse,
+    AsyncTrainingDatasetsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "HumanLlmResource",
+    "AsyncHumanLlmResource",
+    "HumanLlmResourceWithRawResponse",
+    "AsyncHumanLlmResourceWithRawResponse",
+    "HumanLlmResourceWithStreamingResponse",
+    "AsyncHumanLlmResourceWithStreamingResponse",
+    "UsersResource",
+    "AsyncUsersResource",
+    "UsersResourceWithRawResponse",
+    "AsyncUsersResourceWithRawResponse",
+    "UsersResourceWithStreamingResponse",
+    "AsyncUsersResourceWithStreamingResponse",
+    "TrainingDatasetsResource",
+    "AsyncTrainingDatasetsResource",
+    "TrainingDatasetsResourceWithRawResponse",
+    "AsyncTrainingDatasetsResourceWithRawResponse",
+    "TrainingDatasetsResourceWithStreamingResponse",
+    "AsyncTrainingDatasetsResourceWithStreamingResponse",
+    "AdminResource",
+    "AsyncAdminResource",
+    "AdminResourceWithRawResponse",
+    "AsyncAdminResourceWithRawResponse",
+    "AdminResourceWithStreamingResponse",
+    "AsyncAdminResourceWithStreamingResponse",
+]
