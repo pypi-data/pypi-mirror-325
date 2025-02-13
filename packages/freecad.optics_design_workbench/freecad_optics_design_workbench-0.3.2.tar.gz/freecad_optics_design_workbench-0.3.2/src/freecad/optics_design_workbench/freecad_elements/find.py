8176314f7b1649f4133af03b3ab83cd7b376967a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:50 +0000

    Add package freecad.optics_design_workbench/0.3.2/freecad_optics_design_workbench-0.3.2.tar.gz

---
+++ packages/freecad.optics_design_workbench/freecad_optics_design_workbench-0.3.2.tar.gz/freecad_optics_design_workbench-0.3.2/src/freecad/optics_design_workbench/freecad_elements/find.py
@@ -0,0 +1,95 @@
+__license__ = 'LGPL-3.0-or-later'
+__copyright__ = 'Copyright 2024  W. Braun (epiray GmbH)'
+__authors__ = 'P. Bredol'
+__url__ = 'https://github.com/zaphB/freecad.optics_design_workbench'
+
+
+try:
+  import FreeCADGui as Gui
+  import FreeCAD as App
+except ImportError:
+  pass
+
+import os
+import warnings
+
+from . import point_source
+from . import optical_group
+from . import simulation_settings
+
+def iconpath(name):
+  return os.path.join(os.path.dirname(__file__), '../icons', name+'.svg')
+
+
+def _allObjects():
+  for obj in App.activeDocument().Objects:
+    # make sure TypeId attribute can be read without exception 
+    # before yiedling to avoid yielding deleted objects
+    try:
+      obj.TypeId
+    except Exception:
+      pass
+    else:
+      yield obj
+
+
+def lightSources():
+  '''
+  yield all light source objects in the current project.
+  '''
+  for obj in _allObjects():
+    if ( obj.TypeId == 'App::LinkGroupPython'
+            and isinstance(obj.Proxy, point_source.GenericSourceProxy) ):
+      yield obj
+
+
+def relevantOpticalObjects(lightSource=None):
+  '''
+  yield all optical objects in project
+  '''
+  ignoreList = []
+  if lightSource:
+    ignoreList = lightSource.IgnoredOpticalElements
+  for obj in _allObjects():
+    if ( obj.TypeId == 'App::LinkGroupPython'
+          and isinstance(obj.Proxy, optical_group.OpticalGroupProxy)
+          and obj not in ignoreList):
+      yield obj
+
+
+def simulationSettings():
+  '''
+  yield all simulation object of the current project.
+  '''
+  for obj in _allObjects():
+    if ( obj.TypeId == 'Part::FeaturePython'
+            and isinstance(obj.Proxy, simulation_settings.SimulationSettingsProxy) ):
+      yield obj
+
+
+def activeSimulationSettings(default=None):
+  '''
+  Return active simulation object of the current project or None if no 
+  active simulation settings object exists.
+  '''
+  active = []
+  allSettings = []
+  for obj in simulationSettings():
+    allSettings.append(obj)
+    if obj.Active:
+      active.append(obj)
+  if len(active) > 1:
+    raise ValueError('only one simulation settings object may have its "Active" property '
+                     'set to true, but the following objects are active: '
+                     +', '.join([obj.Name for obj in active]))
+  if len(active) == 1:
+    return active[0]
+
+  # if settings exist but for some reason not a single one is active, 
+  # make the first one active
+  if len(allSettings):
+    if default is None:
+      default = allSettings[0]
+    warnings.warn(f'found simulation settings objects but none of them seems to '
+                  f'be active, defaulting to {default.Name}')
+    return default
