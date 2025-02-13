57b6e136b6a62e44ba9d23b782bbf862022d587f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 12:39:58 +0000

    Add package mavetools/0.3.0/mavetools-0.3.0.tar.gz

---
+++ packages/mavetools/mavetools-0.3.0.tar.gz/mavetools-0.3.0/README.md
@@ -0,0 +1,5 @@
+[![PyPI version](https://badge.fury.io/py/mavetools.svg)](https://badge.fury.io/py/mavetools)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+# mavetools
+Useful functions for manipulating Multiplex Assay of Variant Effect datasets.
---
+++ packages/mavetools/mavetools-0.3.0.tar.gz/mavetools-0.3.0/tests/test_convert/test_enrich.py
@@ -0,0 +1,62 @@
+import unittest
+
+from mavehgvs import Variant
+
+from mavetools.convert.enrich import seqid_to_variant
+
+
+class TestSeqidToVariant(unittest.TestCase):
+    def test_protein_wt(self):
+        wtseq = "MGLYTAKLVEN*"
+        seqid_tuples = [
+            ("p.Met1Leu", "0-L"),
+            ("p.Tyr4Asp", "3-D"),
+            ("p.[Met1Leu;Tyr4Asp]", "0,3-L,D"),
+            ("p.[Met1Leu;Tyr4Asp;Ter12Asn]", "0,3,11-L,D,N"),
+            ("p.Tyr4Ter", "3-*"),
+            ("p.Ter12Gly", "11-G"),
+        ]
+
+        for hgvs, seqid in seqid_tuples:
+            with self.subTest(hgvs=hgvs, seqid=seqid, wtseq=wtseq):
+                self.assertEqual(Variant(hgvs), seqid_to_variant(seqid, wtseq))
+
+    def test_invalid_seqids(self):
+        invalid_seqids = [
+            "MGLYTAKLVEN*",
+            "0,3-L",
+            "-L",
+            "-1-D",
+            "3-L,D",
+            "0,3;L,D",
+            "0;3-L;D",
+            "0,3-L,B",
+            "0-X",
+        ]
+
+        for seqid in invalid_seqids:
+            with self.subTest(seqid=seqid):
+                with self.assertRaises(ValueError):
+                    seqid_to_variant(seqid, "AAA")
+
+    def test_wt_length(self):
+        wtseq = "WT"
+        valid_seqids = ["3-D", "0,3-L,D", "0,3,12-L,D,N", "3-*"]
+
+        for seqid in valid_seqids:
+            with self.subTest(seqid=seqid, wtseq=wtseq):
+                with self.assertRaises(ValueError):
+                    seqid_to_variant(seqid, wtseq)
+
+    def test_synonymous_not_allowed(self):
+        wtseq = "WT*"
+        valid_seqids = ["0-W", "0,3-W,D", "0,1-L,T", "2-*"]
+
+        for seqid in valid_seqids:
+            with self.subTest(seqid=seqid, wtseq=wtseq):
+                with self.assertRaises(ValueError):
+                    seqid_to_variant(seqid, wtseq)
+
+
+if __name__ == "__main__":
+    unittest.main()
