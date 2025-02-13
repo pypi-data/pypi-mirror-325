6f8fbd77972a3d7e8778846b74afdf6ce6782b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:50 +0000

    Add package testing-compose-deployment-v0-ti/0.0.1/testing_compose_deployment_v0_ti-0.0.1.tar.gz

---
+++ packages/testing-compose-deployment-v0-ti/testing_compose_deployment_v0_ti-0.0.1.tar.gz/testing_compose_deployment_v0_ti-0.0.1/testing_compose_deployment_v0_ti/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from testing_compose_deployment_v0_ti.api.gifs_api import GifsApi
+from testing_compose_deployment_v0_ti.api.stickers_api import StickersApi
+
