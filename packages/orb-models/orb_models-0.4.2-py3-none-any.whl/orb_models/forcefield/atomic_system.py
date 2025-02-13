713937a3a198386af210c52b057802f554b62b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:19:32 +0000

    Add package orb-models/0.4.2/orb_models-0.4.2-py3-none-any.whl

---
+++ packages/orb-models/orb_models-0.4.2-py3-none-any.whl/orb_models/forcefield/atomic_system.py
@@ -0,0 +1,227 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+import ase
+import torch
+from ase import constraints
+from ase.calculators.singlepoint import SinglePointCalculator
+
+from orb_models.forcefield import featurization_utilities
+from orb_models.forcefield.base import AtomGraphs
+
+
+@dataclass
+class SystemConfig:
+    """Config controlling how to featurize a system of atoms.
+
+    Args:
+        radius: radius for edge construction
+        max_num_neighbors: maximum number of neighbours each node can send messages to.
+        use_timestep_0: (unused - purely for compatibility with internal models)
+    """
+
+    radius: float
+    max_num_neighbors: int
+    use_timestep_0: bool = True
+
+
+def atom_graphs_to_ase_atoms(
+    graphs: AtomGraphs,
+    energy: Optional[torch.Tensor] = None,
+    forces: Optional[torch.Tensor] = None,
+    stress: Optional[torch.Tensor] = None,
+) -> List[ase.Atoms]:
+    """Converts a list of graphs to a list of ase.Atoms."""
+    graphs = graphs.to("cpu")
+    if "atomic_numbers_embedding" in graphs.node_features:
+        atomic_numbers = torch.argmax(
+            graphs.node_features["atomic_numbers_embedding"], dim=-1
+        )
+    else:
+        atomic_numbers = graphs.node_features["atomic_numbers"]
+    atomic_numbers_split = torch.split(atomic_numbers, graphs.n_node.tolist())
+    positions_split = torch.split(graphs.positions, graphs.n_node.tolist())
+    assert graphs.tags is not None and graphs.system_features is not None
+    tags = torch.split(graphs.tags, graphs.n_node.tolist())
+
+    calculations = {}
+    if energy is not None:
+        energy_list = torch.unbind(energy.cpu().detach())
+        assert len(energy_list) == len(atomic_numbers_split)
+        calculations["energy"] = energy_list
+    if forces is not None:
+        forces_list = torch.split(forces.cpu().detach(), graphs.n_node.tolist())
+        assert len(forces_list) == len(atomic_numbers_split)
+        calculations["forces"] = forces_list  # type: ignore
+    if stress is not None:
+        stress_list = torch.unbind(stress.cpu().detach())
+        assert len(stress_list) == len(atomic_numbers_split)
+        calculations["stress"] = stress_list
+
+    atoms_list = []
+    for index, (n, p, c, t) in enumerate(
+        zip(atomic_numbers_split, positions_split, graphs.cell, tags)
+    ):
+        atoms = ase.Atoms(
+            numbers=n.detach(),
+            positions=p.detach(),
+            cell=c.detach(),
+            tags=t.detach(),
+            pbc=torch.any(c != 0),
+        )
+        if calculations != {}:
+            # note: important to save scalar energy as a float not array
+            spc = SinglePointCalculator(
+                atoms=atoms,
+                **{
+                    key: (
+                        val[index].item()
+                        if val[index].nelement() == 1
+                        else val[index].numpy()
+                    )
+                    for key, val in calculations.items()
+                },
+            )
+            atoms.calc = spc
+        atoms_list.append(atoms)
+
+    return atoms_list
+
+
+def ase_atoms_to_atom_graphs(
+    atoms: ase.Atoms,
+    *,
+    wrap: bool = True,
+    brute_force_knn: Optional[bool] = None,
+    device: Optional[torch.device] = None,
+    system_config: Optional[SystemConfig] = None,
+    system_id: Optional[int] = None,
+) -> AtomGraphs:
+    """Generate AtomGraphs from an ase.Atoms object.
+
+    Args:
+        atoms: ase.Atoms object
+        wrap: whether to wrap atomic positions into the central unit cell (if there is one).
+            NOTE: there can be numerical differences from ase's .wrap() method when an atom is near a cell boundary.
+        brute_force_knn: whether to use a 'brute force' knn approach with torch.cdist for kdtree construction.
+            Defaults to None, in which case brute_force is used if we a GPU is avaiable (2-6x faster),
+            but not on CPU (1.5x faster - 4x slower). For very large systems, brute_force may OOM on GPU,
+            so it is recommended to set to False in that case.
+        device: device to put the tensors on. By default, uses the GPU if available.
+        system_config: SystemConfig object, specifying the max radius and max num_neighbors 
+            used in the k-nearest neighbors graph construction.
+        system_id: Optional index, for tracking the identity of a datapoint.
+
+    Returns:
+        AtomGraphs object
+    """
+    if device is None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if system_config is None:
+        system_config = SystemConfig(radius=10.0, max_num_neighbors=20)
+
+    atomic_numbers = torch.from_numpy(atoms.numbers).to(torch.long)
+    atom_type_embedding = torch.nn.functional.one_hot(
+        atomic_numbers, num_classes=118
+    ).type(torch.float32)
+
+    positions = torch.from_numpy(atoms.positions).to(torch.float32)
+    cell = torch.from_numpy(atoms.cell.array).to(torch.float32)
+    if wrap and torch.any(cell != 0):
+        positions = featurization_utilities.map_to_pbc_cell(positions, cell)
+
+    node_feats = {
+        "atomic_numbers": atomic_numbers.to(torch.int64),
+        "atomic_numbers_embedding": atom_type_embedding.to(torch.float32),
+        # NOTE: positions are stored as features on the AtomGraphs,
+        # but not actually used as input features to the model.
+        "positions": positions,
+    }
+    system_feats = {"cell": cell.unsqueeze(0)}
+    edge_feats, senders, receivers = _get_edge_feats(
+        positions,
+        cell,
+        system_config.radius,
+        system_config.max_num_neighbors,
+        brute_force=brute_force_knn,
+        device=device,
+    )
+
+    num_atoms = len(node_feats["positions"])  # type: ignore
+    atom_graph = AtomGraphs(
+        senders=senders,
+        receivers=receivers,
+        n_node=torch.tensor([num_atoms]),
+        n_edge=torch.tensor([len(senders)]),
+        node_features=node_feats,
+        edge_features=edge_feats,
+        system_features=system_feats,
+        system_id=torch.LongTensor([system_id]) if system_id is not None else system_id,
+        fix_atoms=ase_fix_atoms_to_tensor(atoms),
+        tags=_get_ase_tags(atoms),
+        radius=system_config.radius,
+        max_num_neighbors=system_config.max_num_neighbors,
+    )
+    return atom_graph.to(device)
+
+
+def _get_edge_feats(
+    positions: torch.Tensor,
+    cell: torch.Tensor,
+    radius: float,
+    max_num_neighbours: int,
+    brute_force: Optional[bool],
+    device: torch.device,
+):
+    """Get edge features.
+
+    Args:
+        positions: (n_nodes, 3) positions tensor
+        cell: 3x3 tensor unit cell for a system
+        radius: radius for edge construction
+        max_num_neighbours: maximum number of neighbours each node can send messages to.
+        n_kdtree_workers: number of workers to use for kdtree construction.
+        brute_force: whether to use brute force for kdtree construction.
+        device: device to put the tensors on.
+    """
+    # Construct a graph from a 3x3 supercell (as opposed to an infinite supercell).
+    # This could be innaccurate for thin unit cells, but we have yet to encounter a
+    # major issue and this approach is faster.
+    (
+        edge_index,
+        edge_vectors,
+    ) = featurization_utilities.compute_pbc_radius_graph(
+        positions=positions,
+        periodic_boundaries=cell,
+        radius=radius,
+        max_number_neighbors=max_num_neighbours,
+        brute_force=brute_force,
+        device=device,
+    )
+    edge_feats = {
+        "vectors": edge_vectors.to(torch.float32),
+        "r": edge_vectors.norm(dim=-1),
+    }
+    senders, receivers = edge_index[0], edge_index[1]
+    return edge_feats, senders, receivers
+
+
+def _get_ase_tags(atoms: ase.Atoms) -> torch.Tensor:
+    """Get tags from ase.Atoms object."""
+    tags = atoms.get_tags()
+    if tags is not None:
+        tags = torch.Tensor(tags)
+    else:
+        tags = torch.zeros(len(atoms))
+    return tags
+
+
+def ase_fix_atoms_to_tensor(atoms: ase.Atoms) -> Optional[torch.Tensor]:
+    """Get fixed atoms from ase.Atoms object."""
+    fixed_atoms = None
+    if atoms.constraints is not None and len(atoms.constraints) > 0:
+        constraint = atoms.constraints[0]
+        if isinstance(constraint, constraints.FixAtoms):
+            fixed_atoms = torch.zeros((len(atoms)), dtype=torch.bool)
+            fixed_atoms[constraint.index] = True
+    return fixed_atoms
---
+++ packages/orb-models/orb_models-0.4.2-py3-none-any.whl/orb_models/forcefield/pretrained.py
@@ -0,0 +1,329 @@
+# flake8: noqa: E501
+from typing import Union, Optional
+
+import torch
+from cached_path import cached_path
+from functools import partial
+
+from orb_models.forcefield.featurization_utilities import (
+    get_device,
+    gaussian_basis_function,
+)
+from orb_models.forcefield.gns import MoleculeGNS
+from orb_models.forcefield.graph_regressor import (
+    EnergyHead,
+    GraphHead,
+    GraphRegressor,
+    NodeHead,
+)
+from orb_models.forcefield.rbf import ExpNormalSmearing
+
+global HAS_MESSAGED_FOR_TF32_MATMUL
+HAS_MESSAGED_FOR_TF32_MATMUL = False
+
+torch.set_float32_matmul_precision("high")
+
+
+def get_base(
+    latent_dim: int = 256,
+    mlp_hidden_dim: int = 512,
+    num_message_passing_steps: int = 15,
+    num_edge_in_features: int = 23,
+    distance_cutoff: bool = True,
+    attention_gate: str = "sigmoid",
+    rbf_transform: str = "gaussian",
+) -> MoleculeGNS:
+    """Define the base pretrained model architecture."""
+    return MoleculeGNS(
+        num_node_in_features=256,
+        num_node_out_features=3,
+        num_edge_in_features=num_edge_in_features,
+        latent_dim=latent_dim,
+        interactions="simple_attention",
+        interaction_params={
+            "distance_cutoff": distance_cutoff,
+            "polynomial_order": 4,
+            "cutoff_rmax": 6,
+            "attention_gate": attention_gate,
+        },
+        num_message_passing_steps=num_message_passing_steps,
+        num_mlp_layers=2,
+        mlp_hidden_dim=mlp_hidden_dim,
+        rbf_transform=(
+            ExpNormalSmearing(num_rbf=50, cutoff_upper=10.0)
+            if rbf_transform == "exp_normal_smearing"
+            else partial(gaussian_basis_function, num_bases=20, radius=10.0)
+        ),
+        use_embedding=True,
+        node_feature_names=["feat"],
+        edge_feature_names=["feat"],
+    )
+
+
+def load_model_for_inference(
+    model: torch.nn.Module,
+    weights_path: str,
+    device: Union[torch.device, str, None] = None,
+) -> torch.nn.Module:
+    """Load a pretrained model in inference mode, using GPU if available."""
+    global HAS_MESSAGED_FOR_TF32_MATMUL
+    local_path = cached_path(weights_path)
+    state_dict = torch.load(local_path, map_location="cpu")
+    model.load_state_dict(state_dict, strict=True)
+    device = get_device(device)
+    if (
+        device.type == "cuda"
+        and torch.get_float32_matmul_precision() == "high"
+        and not HAS_MESSAGED_FOR_TF32_MATMUL
+    ):
+        print(
+            "GPU tensorfloat matmuls precision set to 'high'. "
+            "This can achieve up to 2x speedup on Nvidia A100 and H100 devices."
+        )
+        HAS_MESSAGED_FOR_TF32_MATMUL = True
+    model = model.to(device)
+    model = model.eval()
+    for param in model.parameters():
+        param.requires_grad = False
+
+    return model
+
+
+def orb_v2(
+    weights_path: str = "https://orbitalmaterials-public-models.s3.us-west-1.amazonaws.com/forcefields/orb-v2-20241011.ckpt",  # noqa: E501
+    device: Union[torch.device, str, None] = None,
+) -> GraphRegressor:
+    """Load ORB v2."""
+    base = get_base()
+
+    model = GraphRegressor(
+        graph_head=EnergyHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="energy",
+            node_aggregation="mean",
+            reference_energy_name="vasp-shifted",
+            train_reference=True,
+            predict_atom_avg=True,
+        ),
+        node_head=NodeHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="forces",
+            remove_mean=True,
+        ),
+        stress_head=GraphHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="stress",
+            compute_stress=True,
+        ),
+        model=base,
+    )
+
+    model = load_model_for_inference(model, weights_path, device)
+
+    return model
+
+
+def orb_mptraj_only_v2(
+    weights_path: str = "https://orbitalmaterials-public-models.s3.us-west-1.amazonaws.com/forcefields/orb-mptraj-only-v2-20241014.ckpt",  # noqa: E501
+    device: Union[torch.device, str, None] = None,
+):
+    """Load ORB MPTraj Only v2."""
+
+    return orb_v2(weights_path, device)
+
+
+def orb_d3_v2(
+    weights_path: str = "https://orbitalmaterials-public-models.s3.us-west-1.amazonaws.com/forcefields/orb-d3-v2-20241011.ckpt",  # noqa: E501
+    device: Union[torch.device, str, None] = None,
+) -> GraphRegressor:
+    """Load ORB D3 v2."""
+    base = get_base()
+
+    model = GraphRegressor(
+        graph_head=EnergyHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="energy",
+            node_aggregation="mean",
+            reference_energy_name="vasp-shifted",
+            train_reference=True,
+            predict_atom_avg=True,
+        ),
+        node_head=NodeHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="forces",
+            remove_mean=True,
+        ),
+        stress_head=GraphHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="stress",
+            compute_stress=True,
+        ),
+        model=base,
+    )
+
+    model = load_model_for_inference(model, weights_path, device)
+
+    return model
+
+
+def orb_d3_sm_v2(
+    weights_path: str = "https://orbitalmaterials-public-models.s3.us-west-1.amazonaws.com/forcefields/orb-d3-sm-v2-20241011.ckpt",  # noqa: E501
+    device: Union[torch.device, str, None] = None,
+) -> GraphRegressor:
+    """Load ORB D3 v2."""
+    base = get_base(
+        num_message_passing_steps=10,
+    )
+
+    model = GraphRegressor(
+        graph_head=EnergyHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="energy",
+            node_aggregation="mean",
+            reference_energy_name="vasp-shifted",
+            train_reference=True,
+            predict_atom_avg=True,
+        ),
+        node_head=NodeHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="forces",
+            remove_mean=True,
+        ),
+        stress_head=GraphHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="stress",
+            compute_stress=True,
+        ),
+        model=base,
+    )
+
+    model = load_model_for_inference(model, weights_path, device)
+
+    return model
+
+
+def orb_d3_xs_v2(
+    weights_path: str = "https://orbitalmaterials-public-models.s3.us-west-1.amazonaws.com/forcefields/orb-d3-xs-v2-20241011.ckpt",  # noqa: E501
+    device: Union[torch.device, str, None] = None,
+) -> GraphRegressor:
+    """Load ORB D3 xs v2."""
+    base = get_base(
+        num_message_passing_steps=5,
+    )
+
+    model = GraphRegressor(
+        graph_head=EnergyHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="energy",
+            node_aggregation="mean",
+            reference_energy_name="vasp-shifted",
+            train_reference=True,
+            predict_atom_avg=True,
+        ),
+        node_head=NodeHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="forces",
+            remove_mean=True,
+        ),
+        stress_head=GraphHead(
+            latent_dim=256,
+            num_mlp_layers=1,
+            mlp_hidden_dim=256,
+            target="stress",
+            compute_stress=True,
+        ),
+        model=base,
+    )
+
+    model = load_model_for_inference(model, weights_path, device)
+
+    return model
+
+
+def _deprecated_model(model_name: str):
+    """Deprecated model."""
+
+    raise ValueError(
+        f"{model_name} is deprecated. Please use orb-v2 instead."
+        "Orb V2 models are more accurate, more robust under simulation, and run faster."
+    )
+
+
+def orb_v1(
+    weights_path: Optional[str] = None,
+    device: Union[torch.device, str, None] = None,
+):
+    """Deprecated model."""
+
+    _deprecated_model("orb-v1")
+
+
+def orb_d3_v1(
+    weights_path: Optional[str] = None,
+    device: Union[torch.device, str, None] = None,
+):
+    """Deprecated model."""
+
+    _deprecated_model("orb-d3-v1")
+
+
+def orb_d3_sm_v1(
+    weights_path: Optional[str] = None,
+    device: Union[torch.device, str, None] = None,
+):
+    """Deprecated model."""
+
+    _deprecated_model("orb-d3-sm-v1")
+
+
+def orb_d3_xs_v1(
+    weights_path: Optional[str] = None,
+    device: Union[torch.device, str, None] = None,
+):
+    """Deprecated model."""
+    _deprecated_model("orb-d3-xs-v1")
+
+
+def orb_v1_mptraj_only(
+    weights_path: Optional[str] = None,
+    device: Union[torch.device, str, None] = None,
+):
+    """Deprecated model."""
+    _deprecated_model("orb-mptraj-only-v1")
+
+
+ORB_PRETRAINED_MODELS = {
+    "orb-v2": orb_v2,
+    "orb-d3-v2": orb_d3_v2,
+    "orb-d3-sm-v2": orb_d3_sm_v2,
+    "orb-d3-xs-v2": orb_d3_xs_v2,
+    "orb-mptraj-only-v2": orb_mptraj_only_v2,
+    # Deprecated models
+    "orb-v1": orb_v1,
+    "orb-d3-v1": orb_d3_v1,
+    "orb-d3-sm-v1": orb_d3_sm_v1,
+    "orb-d3-xs-v1": orb_d3_xs_v1,
+    "orb-v1-mptraj-only": orb_v1_mptraj_only,
+}
