6271893826b62520263e617a294e8082e690d069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:31:29 +0000

    Add package aqua-blue/0.0.9/aqua_blue-0.0.9-py3-none-any.whl

---
+++ packages/aqua-blue/aqua_blue-0.0.9-py3-none-any.whl/aqua_blue/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.9"
+__authors__ = ["Jacob Jeffries", "Ameen Mahmood"]
+__author_emails__ = ["jacob.jeffries@chicagoclubteam.org", "ameen.mahmood@chicagoclubteam.org"]
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
+
+from .reservoir import EchoStateNetwork as EchoStateNetwork
+from .reservoir import InstabilityWarning as InstabilityWarning
+from .time_series import TimeSeries as TimeSeries
