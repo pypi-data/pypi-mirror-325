348165fd5c67c39163793838f1c2b5d24dff8c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:19:11 +0000

    Add package daisy-dtb/0.0.9/daisy_dtb-0.0.9.tar.gz

---
+++ packages/daisy-dtb/daisy_dtb-0.0.9.tar.gz/daisy_dtb-0.0.9/src/models/__init__.py
@@ -0,0 +1,9 @@
+from models.audio import Audio
+from models.metadata import MetaData
+from models.reference import Reference
+from models.section import Section
+from models.smil import Smil
+from models.text import Text
+from models.toc_entry import TocEntry
+
+__all__ = [MetaData, Audio, Reference, Section, Smil, Text, TocEntry]
---
+++ packages/daisy-dtb/daisy_dtb-0.0.9.tar.gz/daisy_dtb-0.0.9/src/navigators/__init__.py
@@ -0,0 +1,6 @@
+from navigators.base_navigator import BaseNavigator
+from navigators.book_navigator import BookNavigator
+from navigators.clip_navigator import ClipNavigator
+from navigators.section_navigator import SectionNavigator
+
+__all__ = [BaseNavigator, BookNavigator, SectionNavigator, ClipNavigator]
