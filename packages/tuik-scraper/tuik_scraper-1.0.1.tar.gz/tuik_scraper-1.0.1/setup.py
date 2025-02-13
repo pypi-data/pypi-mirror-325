8b9761634c54ff4a42751dc6a2b21c84019b98b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 19:19:34 +0000

    Add package tuik-scraper/1.0.1/tuik_scraper-1.0.1.tar.gz

---
+++ packages/tuik-scraper/tuik_scraper-1.0.1.tar.gz/tuik_scraper-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tuik_scraper",
+    version="1.0.1",  # Versiyonu yükseltin
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pandas",
+        "beautifulsoup4",
+        "selenium",
+        "webdriver-manager"
+    ],
+    author="Bora Kaya",
+    author_email="bora.587@hotmail.com",
+    description="TÜİK veri çekme ve indirme kütüphanesi",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaboya19/tuik_scraper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
