071c9ff6d092da9fe4cda3c0c7ef095099be8cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:10:34 +0000

    Add package glucklabutils/0.0.1/glucklabutils-0.0.1.tar.gz

---
+++ packages/glucklabutils/glucklabutils-0.0.1.tar.gz/glucklabutils-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'GluckLab utils'
+LONG_DESCRIPTION = 'GluckLab useful tools'
+
+# Setting up
+setup(
+        name="glucklabutils", 
+        version=VERSION,
+        author="Jose Mojica Perez",
+        author_email="jlmojicaperez@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["pandas"], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
