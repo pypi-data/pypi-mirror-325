cc0ae7c3ffdf20b52f56cb85d03e136ef45568ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:18:33 +0000

    Add package Unislate/0.1.1/Unislate-0.1.1.tar.gz

---
+++ packages/Unislate/Unislate-0.1.1.tar.gz/Unislate-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import sys
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+install_requires = []
+if sys.platform == "win32":
+    install_requires.append("windows-curses")
+
+setup(
+    name="Unislate",
+    version="0.1.1",
+    description="Минималистичный консольный редактор с подсветкой синтаксиса",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Qwez",
+    url="https://github.com/qwez-source/unislate",    
+    py_modules=["unislate"], 
+    entry_points={
+        "console_scripts": [
+            "unislate = unislate:main", 
+        ],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
