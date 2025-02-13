0659491b879c806c2a191b6c201bc407b37b5664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:48 +0000

    Add package pyloggrid/2.5.0/pyloggrid-2.5.0-py3-none-any.whl

---
+++ packages/pyloggrid/pyloggrid-2.5.0-py3-none-any.whl/pyloggrid/LogGrid/setup_windows.py
@@ -0,0 +1,19 @@
+"""Cython settings for windows"""
+
+import numpy
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+ext_modules = [
+    Extension(
+        "compute_convolution_kernel",
+        ["compute_convolution_kernel.pyx"],
+        extra_compile_args=["/openmp"],
+    )
+]
+setup(
+    ext_modules=cythonize(ext_modules, annotate=True, compiler_directives={"language_level": "3"}),
+    include_dirs=[numpy.get_include()],
+    name="log-grid",
+    author="A. Barral",
+)
