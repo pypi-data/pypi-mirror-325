6319917a59939d186420591dd9d34b7b9413b29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 15:09:18 +0000

    Add package cp-mgmt-api-sdk/1.9.0/cp_mgmt_api_sdk-1.9.0.tar.gz

---
+++ packages/cp-mgmt-api-sdk/cp_mgmt_api_sdk-1.9.0.tar.gz/cp_mgmt_api_sdk-1.9.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="cp-mgmt-api-sdk",
+    version="1.9.0",
+    author="API team",
+    author_email="api_team@checkpoint.com",
+    license='Apache 2.0',
+    description="Check Point Management API SDK",
+    long_description="Check Point API Python Development Kit simplifies the usage of the Check Point Management APIs. "
+                     "The kit contains the API library project, "
+                     "and sample projects demonstrating the capabilities of the library. "
+                     "The kit is python 2 and 3 compatible code.",
+    long_description_content_type="text/plain",
+    url="https://github.com/CheckPointSW/cp_mgmt_api_python_sdk.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    metadata_version="1.2",
+
+)
