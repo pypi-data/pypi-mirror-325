77e9c6a6fdd1a6672f0e4b0a822be62538f6aa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:09:47 +0000

    Add package dreamify/0.19.19/dreamify-0.19.19-py3-none-any.whl

---
+++ packages/dreamify/dreamify-0.19.19-py3-none-any.whl/dreamify/utils/common.py
@@ -0,0 +1,15 @@
+import IPython.display as display
+import numpy as np
+import PIL.Image
+import tensorflow as tf
+
+
+def show(img):
+    """Display an image."""
+    display.display(PIL.Image.fromarray(np.array(img)))
+
+
+def deprocess(img):
+    """Normalize image for display."""
+    img = 255 * (img + 1.0) / 2.0
+    return tf.cast(img, tf.uint8)
---
+++ packages/dreamify/dreamify-0.19.19-py3-none-any.whl/dreamify/utils/dream_utils/__init__.py
@@ -0,0 +1,15 @@
+from dreamify.utils.dream_utils.utils import (
+    configure_settings,
+    deprocess_image,
+    gradient_ascent_loop,
+    preprocess_image,
+    to_video,
+)
+
+__all__ = [
+    configure_settings,
+    preprocess_image,
+    deprocess_image,
+    gradient_ascent_loop,
+    to_video,
+]
