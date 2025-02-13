8d2dd777406d9892d2977980477cd7c3403b1099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 15:21:26 +0000

    Add package odoo14-addon-edi-sale-oca/14.0.2.2.1/odoo14_addon_edi_sale_oca-14.0.2.2.1-py3-none-any.whl

---
+++ packages/odoo14-addon-edi-sale-oca/odoo14_addon_edi_sale_oca-14.0.2.2.1-py3-none-any.whl/odoo/addons/edi_sale_oca/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EDI Sales",
+    "summary": """
+        Configuration and special behaviors for EDI on sales.
+    """,
+    "version": "14.0.2.2.1",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "maintainers": ["simahawk"],
+    "website": "https://github.com/OCA/edi",
+    "depends": [
+        "edi_oca",
+        "edi_record_metadata_oca",
+        "sale",
+    ],
+    "data": [
+        "data/edi_configuration.xml",
+        "views/res_partner.xml",
+        "views/sale_order.xml",
+        "views/res_partner.xml",
+        "views/edi_exchange_record.xml",
+    ],
+    "demo": [
+        "demo/edi_backend.xml",
+        "demo/edi_exchange_type.xml",
+        "demo/edi_configuration.xml",
+    ],
+}
