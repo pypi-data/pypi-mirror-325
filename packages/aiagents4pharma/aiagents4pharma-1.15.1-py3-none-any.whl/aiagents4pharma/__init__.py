000612e912a07e833814d83415def83c1f251b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:39:53 +0000

    Add package aiagents4pharma/1.15.1/aiagents4pharma-1.15.1-py3-none-any.whl

---
+++ packages/aiagents4pharma/aiagents4pharma-1.15.1-py3-none-any.whl/aiagents4pharma/__init__.py
@@ -0,0 +1,11 @@
+"""
+This file is used to import aiagents4pharma modules.
+"""
+
+from . import (
+    configs,
+    talk2biomodels,
+    talk2cells,
+    talk2competitors,
+    talk2knowledgegraphs,
+)
