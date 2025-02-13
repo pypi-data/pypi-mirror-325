497547e37a17808f0df14624d29213198d1ab644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 15:09:17 +0000

    Add package xraygpt/0.2.2/xraygpt-0.2.2-py3-none-any.whl

---
+++ packages/xraygpt/xraygpt-0.2.2-py3-none-any.whl/xraygpt/flow.py
@@ -0,0 +1,43 @@
+import shelve
+
+from langchain_community.callbacks import get_openai_callback
+from loguru import logger
+from tqdm import tqdm
+
+from xraygpt.db.chroma import ChromaDatabase
+from xraygpt.llm import get_ebd, get_llm
+from xraygpt.ner.agent import recognize_entities
+from xraygpt.output import dumpDatabese
+from xraygpt.reader import EPubReader
+
+
+def epubSummaryFlow(filename):
+    people = set()
+    for item in EPubReader(filename):
+        logger.debug(item)
+        people |= recognize_entities([item])
+        logger.info(f"# people found so far: {len(people)}")
+
+
+async def epubPeopleFlow(filename):
+    state = shelve.open(filename + ".shelve")
+    llm = get_llm()
+    ebd = get_ebd()
+    db = ChromaDatabase(ebd, filename + ".chroma")
+    book = EPubReader(filename)
+
+    for ix, item in enumerate(tqdm(book, total=len(book))):
+        if ix <= state.get("last_processed", -1):
+            logger.debug(f"Skipping {ix}")
+            continue
+        # logger.debug(item)
+        with get_openai_callback() as cb:
+            await recognize_entities(item, llm, db)
+            logger.trace(f"Total tokens: {cb.total_tokens}")
+            state["total_tokens"] = cb.total_tokens + state.get("total_tokens", 0)
+
+        state["last_processed"] = ix
+
+    logger.success("Total tokens: {total_tokens}", total_tokens=state.get('total_tokens', 0))
+
+    dumpDatabese(filename, db)
---
+++ packages/xraygpt/xraygpt-0.2.2-py3-none-any.whl/xraygpt/reader.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+from ebooklib import ITEM_DOCUMENT, epub
+
+
+class EPubReader:
+    def __init__(self, filename):
+        self.book = epub.read_epub(filename, {"ignore_ncx": True})
+
+    def _get_items(self):
+        return self.book.get_items_of_type(ITEM_DOCUMENT)
+
+    def __iter__(self):
+        self.it = self._get_items()
+        return self
+
+    def __next__(self):
+        try:
+            item = next(self.it)
+        except StopIteration:
+            raise StopIteration
+
+        soup = BeautifulSoup(item.get_body_content(), "html.parser")
+        return soup.get_text()
+
+    def __len__(self):
+        return sum(1 for _ in self._get_items())
+
+
+if __name__ == "__main__":
+    for item in EPubReader("workdir/InfiniteJest.epub"):
+        print(len(item))
