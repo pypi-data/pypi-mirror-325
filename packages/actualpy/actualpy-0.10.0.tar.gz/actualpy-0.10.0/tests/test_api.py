775aa57edec99b9a7e1e845a2f0bf2774f1ca0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 20:09:36 +0000

    Add package actualpy/0.10.0/actualpy-0.10.0.tar.gz

---
+++ packages/actualpy/actualpy-0.10.0.tar.gz/actualpy-0.10.0/tests/test_api.py
@@ -0,0 +1,50 @@
+from unittest.mock import patch
+
+import pytest
+from requests import Session
+
+from actual import Actual, reflect_model
+from actual.exceptions import ActualError, AuthorizationError, UnknownFileId
+from actual.protobuf_models import Message
+from tests.conftest import RequestsMock
+
+
+def test_api_apply(mocker, session):
+    mocker.patch("actual.Actual.validate")
+    actual = Actual(token="foo")
+    actual.engine = session.bind
+    actual._meta = reflect_model(session.bind)
+    # not found table
+    m = Message(dict(dataset="foo", row="foobar", column="bar"))
+    m.set_value("foobar")
+    with pytest.raises(ActualError, match="table 'foo' not found"):
+        actual.apply_changes([m])
+    m.dataset = "accounts"
+    with pytest.raises(ActualError, match="column 'bar' at table 'accounts' not found"):
+        actual.apply_changes([m])
+
+
+def test_rename_delete_budget_without_file(mocker):
+    mocker.patch("actual.Actual.validate")
+    actual = Actual(token="foo")
+    actual._file = None
+    with pytest.raises(UnknownFileId, match="No current file loaded"):
+        actual.delete_budget()
+    with pytest.raises(UnknownFileId, match="No current file loaded"):
+        actual.rename_budget("foo")
+
+
+@patch.object(Session, "post", return_value=RequestsMock({"status": "error", "reason": "proxy-not-trusted"}))
+def test_api_login_unknown_error(_post, mocker):
+    mocker.patch("actual.Actual.validate")
+    actual = Actual(token="foo")
+    actual.api_url = "localhost"
+    actual.cert = False
+    with pytest.raises(AuthorizationError, match="Something went wrong on login"):
+        actual.login("foo")
+
+
+def test_no_certificate(mocker):
+    mocker.patch("actual.Actual.validate")
+    actual = Actual(token="foo", cert=False)
+    assert actual._requests_session.verify is False
