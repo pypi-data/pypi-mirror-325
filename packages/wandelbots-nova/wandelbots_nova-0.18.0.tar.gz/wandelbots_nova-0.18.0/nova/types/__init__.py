c11a61388c3c1dbbfbfbc2287ae157d81badd056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:09:39 +0000

    Add package wandelbots-nova/0.18.0/wandelbots_nova-0.18.0.tar.gz

---
+++ packages/wandelbots-nova/wandelbots_nova-0.18.0.tar.gz/wandelbots_nova-0.18.0/nova/types/__init__.py
@@ -0,0 +1,24 @@
+# This is the stream of requests that is send to execute trajectory websocket
+from typing import AsyncGenerator, Callable
+
+import wandelbots_api_client as wb
+
+from nova.types.collision_scene import CollisionScene
+from nova.types.pose import Pose
+from nova.types.vector3d import Vector3d
+from nova.types.state import MotionState, RobotState
+
+LoadPlanResponse = wb.models.PlanSuccessfulResponse
+InitialMovementStream = AsyncGenerator[wb.models.StreamMoveResponse, None]
+InitialMovementConsumer = Callable[[wb.models.StreamMoveResponse], None]
+
+__all__ = [
+    "Vector3d",
+    "Pose",
+    "CollisionScene",
+    "LoadPlanResponse",
+    "InitialMovementStream",
+    "InitialMovementConsumer",
+    "MotionState",
+    "RobotState",
+]
