28e2fee2b79f7143709d1a8a29506cf2d061b08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 21:09:51 +0000

    Add package sphinxcontrib-doxylink/1.12.4/sphinxcontrib_doxylink-1.12.4-py3-none-any.whl

---
+++ packages/sphinxcontrib-doxylink/sphinxcontrib_doxylink-1.12.4-py3-none-any.whl/sphinxcontrib/doxylink/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.12.4"
+
+
+def setup(app):
+    from .doxylink import setup_doxylink_roles
+    app.add_config_value('doxylink', {}, 'env')
+    app.add_config_value('doxylink_pdf_files', {}, 'env')
+    app.connect('builder-inited', setup_doxylink_roles)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
