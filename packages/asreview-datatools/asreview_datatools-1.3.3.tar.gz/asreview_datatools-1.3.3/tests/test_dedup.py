ba7bd78649033eac305556dbc15415b4e5b19f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 16:20:54 +0000

    Add package asreview-datatools/1.3.3/asreview_datatools-1.3.3.tar.gz

---
+++ packages/asreview-datatools/asreview_datatools-1.3.3.tar.gz/asreview_datatools-1.3.3/tests/test_dedup.py
@@ -0,0 +1,136 @@
+from pathlib import Path
+
+from asreview.data import ASReviewData
+
+from asreviewcontrib.datatools.dedup import deduplicate_data
+
+test_dir = Path(__file__).parent
+file_without_doi = Path(test_dir, "demo_data", "duplicate_data_without_doi.csv")
+file_with_doi = Path(test_dir, "demo_data", "duplicate_data_with_doi.csv")
+
+
+def test_dedup_without_doi():
+    """
+    Test deduplication without DOI.
+
+    The test data contains 5 records, 1 of which is an exact duplicate.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_without_doi.csv
+    Not using doi for deduplication because there is no such data.
+    Found 1 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_without_doi)
+    deduplicate_data(data)
+    assert len(data.df) == 4
+
+
+def test_output(tmpdir):
+    data = ASReviewData.from_file(file_without_doi)
+    output_path = Path(tmpdir, "test_dedup.csv")
+    deduplicate_data(data, output_path=output_path)
+    as_test = ASReviewData.from_file(output_path)
+    assert len(data.df) == 4
+    assert len(as_test.df) == 4
+
+
+def test_dedup_with_doi():
+    """
+    Test deduplication with DOI.
+
+    The test data contains 5 records, 1 of which is an exact duplicate
+    and 1 of which is a duplicate based on DOI.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_with_doi.csv
+    Found 2 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_with_doi)
+    deduplicate_data(data)
+    assert len(data.df) == 3
+
+
+def test_dedup_with_similarity_without_doi():
+    """
+    Test deduplication with similarity without DOI.
+
+    The test data contains 5 records, 1 of which is an exact duplicate
+    and 1 of which is a duplicate based on similarity.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_without_doi.csv --similar \
+        --threshold 0.95
+    Not using doi for deduplication because there is no such data.
+    Found 2 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_without_doi)
+    deduplicate_data(data, similar=True, threshold=0.95)
+    assert len(data.df) == 3, "Original data should have 5 records."
+
+
+def test_dedup_with_similarity_with_doi():
+    """
+    Test deduplication with similarity with DOI.
+
+    The test data contains 5 records, 1 of which is an exact duplicate,
+    1 of which is a duplicate based on DOI, and 1 of which is a duplicate
+    based on similarity.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_with_doi.csv --similar \
+        --threshold 0.95
+    Found 3 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_with_doi)
+    deduplicate_data(data, similar=True, threshold=0.95)
+    assert len(data.df) == 2
+
+
+def test_dedup_with_similarity_without_doi_stopwords():
+    """
+    Test deduplication with similarity without DOI and removing stopwords.
+
+    The test data contains 5 records, 1 of which is an exact duplicate,
+    1 of which is a duplicate based on similarity, and 1 of which is a
+    duplicate based on similarity without stopwords.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_without_doi.csv --similar \
+        --threshold 0.95 --stopwords
+    Not using doi for deduplication because there is no such data.
+    Found 3 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_without_doi)
+    deduplicate_data(data, similar=True, threshold=0.95, stopwords_language="english")
+    assert len(data.df) == 2
+
+
+def test_dedup_with_similarity_with_doi_stopwords():
+    """
+    Test deduplication with similarity with DOI and removing stopwords.
+
+    The test data contains 5 records, 1 of which is an exact duplicate,
+    1 of which is a duplicate based on DOI, 1 of which is a duplicate
+    based on similarity, and 1 of which is a duplicate based on similarity
+    without stopwords.
+
+    Same as:
+
+    asreview data dedup tests/demo_data/duplicate_data_with_doi.csv --similar \
+        --threshold 0.95 --stopwords
+    Found 4 duplicates in dataset with 5 records.
+    """
+    data = ASReviewData.from_file(file_with_doi)
+    deduplicate_data(data, similar=True, threshold=0.95, stopwords_language="english")
+    assert len(data.df) == 1
+
+
+def test_threshold_zero():
+    data = ASReviewData.from_file(file_with_doi)
+    deduplicate_data(data, similar=True, threshold=0)
+    assert len(data.df) == 1
---
+++ packages/asreview-datatools/asreview_datatools-1.3.3.tar.gz/asreview_datatools-1.3.3/tests/test_snowball.py
@@ -0,0 +1,159 @@
+from pathlib import Path
+
+import pandas as pd
+import pyalex
+
+from asreviewcontrib.datatools.snowball import backward_snowballing
+from asreviewcontrib.datatools.snowball import forward_snowballing
+from asreviewcontrib.datatools.snowball import openalex_from_doi
+from asreviewcontrib.datatools.snowball import snowball
+
+INPUT_DIR = Path(__file__).parent / "demo_data"
+EMAIL = "asreview@uu.nl"
+
+pyalex.config.email = EMAIL
+
+# These works were chosen for testing forward snowballing.
+# They have a DOI, they cite and are cited by, their cited_by_count is less than 400,
+# so it takes only two requests to get all citing works. And they are from the previous
+# century so the cited_by_count is unlikely to change very much.
+# These are also the same records as in the demo datasets 'snowballing_doi.csv' and
+# 'snowballing_openalex.csv'.
+WORKS = [
+    {
+        "id": "https://openalex.org/W2051970045",
+        "doi": "https://doi.org/10.1071/bt9750475",
+        "title": "Myrmecochorous plants in Australia and their dispersal by ants",
+        "cited_by_count": 372,
+        "cited_by": "https://openalex.org/W2174650845",
+        "cites": "https://openalex.org/W1538725992",
+    },
+    {
+        "id": "https://openalex.org/W104454400",
+        "doi": "https://doi.org/10.1007/bf00699039",
+        "title": (
+            "Mimicking the one-dimensional marginal distributions of processes having"
+            " an ito differential"
+        ),
+        "cited_by_count": 299,
+        "cited_by": "https://openalex.org/W1842249978",
+        "cites": "https://openalex.org/W1513091520",
+    },
+]
+
+
+def test_openalex_from_doi():
+    dois = [
+        "https://doi.org/10.1042/cs20220150",
+        "https://doi.org/10.1042/bst20220734",
+        "not_a_doi",
+    ]
+
+    assert openalex_from_doi(dois) == {
+        "10.1042/cs20220150": "https://openalex.org/W4386305682",
+        "10.1042/bst20220734": "https://openalex.org/W4312006214",
+        "not_a_doi": None,
+    }
+
+
+def test_backward_snowballing():
+    identifiers = [
+        "https://openalex.org/W4281483266",
+        "https://openalex.org/W2008620264",
+    ]
+
+    backwards_citations = backward_snowballing(identifiers)
+
+    assert "https://openalex.org/W1864285629" in [
+        field_dict["id"] for field_dict in backwards_citations[identifiers[0]]
+    ]
+    assert "https://openalex.org/W950821216" in [
+        field_dict["id"] for field_dict in backwards_citations[identifiers[1]]
+    ]
+
+
+def test_forward_snowballing():
+    identifiers = [work["id"] for work in WORKS]
+
+    forwards_citations = forward_snowballing(identifiers)
+
+    assert WORKS[0]["cited_by"] in [
+        field_dict["id"] for field_dict in forwards_citations[identifiers[0]]
+    ]
+    assert WORKS[1]["cited_by"] in [
+        field_dict["id"] for field_dict in forwards_citations[identifiers[1]]
+    ]
+
+
+def test_openalex_id_forward(tmpdir):
+    out_fp = Path(tmpdir, "forward.csv")
+    snowball(
+        input_path=INPUT_DIR / "snowballing_openalex.csv",
+        output_path=out_fp,
+        forward=True,
+        backward=False,
+        use_all=False,
+        email=EMAIL,
+    )
+    df = pd.read_csv(out_fp)
+    assert len(df) >= 364
+
+    all_out_fp = Path(tmpdir, "forward_all.csv")
+    snowball(
+        input_path=INPUT_DIR / "snowballing_openalex.csv",
+        output_path=all_out_fp,
+        forward=True,
+        backward=False,
+        use_all=True,
+        email=EMAIL,
+    )
+    df_all = pd.read_csv(all_out_fp)
+    assert len(df_all) >= 656
+
+
+def test_openalex_id_backward(tmpdir):
+    out_fp = Path(tmpdir, "backward.csv")
+    snowball(
+        input_path=INPUT_DIR / "snowballing_openalex.csv",
+        output_path=out_fp,
+        forward=False,
+        backward=True,
+        use_all=False,
+        email=EMAIL,
+    )
+    df = pd.read_csv(out_fp)
+    # Actual value at time of writing test is 40 (2024-08-26).
+    # In theory the number of results should be stable for backward snowballing,
+    # but OpenAlex sometimes makes changes, so we allow for a margin.
+    # The margins of this assert and the next assert should not overlap,
+    # otherwise we don't test if 'use_all' works.
+    assert 38 <= len(df) <= 42
+
+    all_out_fp = Path(tmpdir, "backward_all.csv")
+    snowball(
+        input_path=INPUT_DIR / "snowballing_openalex.csv",
+        output_path=all_out_fp,
+        forward=False,
+        backward=True,
+        use_all=True,
+        email=EMAIL,
+    )
+    df_all = pd.read_csv(all_out_fp)
+    # Actual value at time of writing test is 46 (2024-08-26).
+    assert 43 <= len(df_all) <= 49
+
+
+def test_snowballing_from_doi(tmpdir):
+    out_fp = Path(tmpdir, "doi_all.csv")
+    snowball(
+        input_path=INPUT_DIR / "snowballing_doi.csv",
+        output_path=out_fp,
+        forward=False,
+        backward=True,
+        use_all=True,
+        email=EMAIL,
+    )
+    df = pd.read_csv(out_fp)
+    # Actual value at time of writing test is 46 (2024-08-26).
+    # See comments in 'test_openalex_id_backward'.
+    assert 43 <= len(df) <= 49
