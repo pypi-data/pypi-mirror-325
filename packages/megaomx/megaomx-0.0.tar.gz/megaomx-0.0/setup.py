321d276e98d449b9ed24629b61bada809ec1d832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:28:54 +0000

    Add package megaomx/0.0/megaomx-0.0.tar.gz

---
+++ packages/megaomx/megaomx-0.0.tar.gz/megaomx-0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+
+setup( name='megaomx',
+       version = '0.0',
+       package=find_packages(),
+       install_requires=['requests'],
+       author='ww4u',
+       author_email='wellau@qq.com',
+       description='The omx series control',
+       license='MIT',
+       keywords='megarobo om ovl',
+       url='' )
