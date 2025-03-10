e944619a0ff436d37a869c40d90a38b2e8633552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 23:09:12 +0000

    Add package my-test-package-XyZEFg/0.1.0/my_test_package_xyzefg-0.1.0.tar.gz

---
+++ packages/my-test-package-XyZEFg/my_test_package_xyzefg-0.1.0.tar.gz/my_test_package_xyzefg-0.1.0/README.md
@@ -0,0 +1,10 @@
+# Test Package
+
+Test package for PyPi
+
+## Usage
+
+```python
+from test_package import hello
+
+print(hello())
---
+++ packages/my-test-package-XyZEFg/my_test_package_xyzefg-0.1.0.tar.gz/my_test_package_xyzefg-0.1.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='my_test_package_XyZEFg',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple test package for PyPI',
+    author='Julia Joseph',
+    classifiers=['Programming Language :: Python'],
+    python_requires='>=3.6'
+)
