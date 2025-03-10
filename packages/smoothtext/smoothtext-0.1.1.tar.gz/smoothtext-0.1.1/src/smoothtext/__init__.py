1cde39d3d3ce172f358152f78ed8fb290de60615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 23:08:56 +0000

    Add package smoothtext/0.1.1/smoothtext-0.1.1.tar.gz

---
+++ packages/smoothtext/smoothtext-0.1.1.tar.gz/smoothtext-0.1.1/src/smoothtext/__init__.py
@@ -0,0 +1,20 @@
+#  SmoothText - https://github.com/smoothtext
+#
+#  Copyright (c) 2025. All rights reserved.
+#  Tuğrul Güngör - https://tugrulgungor.me
+#
+#  Distributed under the MIT License.
+#  https://opensource.org/license/mit/
+
+from .backend import Backend
+from .language import Language
+from .readability import ReadabilityFormula
+from .smoothtext import SmoothText
+
+__version__ = (0, 1, 1)
+
+for module in (Backend, Language, SmoothText):
+    for attr in dir(module):
+        if callable(getattr(module, attr)):
+            if not attr.startswith("_"):
+                globals()[attr] = getattr(module, attr)
