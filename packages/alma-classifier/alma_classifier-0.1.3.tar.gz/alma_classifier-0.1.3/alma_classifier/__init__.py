a2950a45e5af2bbd1da55cb5e1d58f51f98f4dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 23:09:02 +0000

    Add package alma-classifier/0.1.3/alma_classifier-0.1.3.tar.gz

---
+++ packages/alma-classifier/alma_classifier-0.1.3.tar.gz/alma_classifier-0.1.3/alma_classifier/__init__.py
@@ -0,0 +1,7 @@
+"""ALMA Classifier package for epigenomic classification."""
+from .predictor import ALMAPredictor
+from .utils import export_results
+
+__version__ = "0.1.3"
+
+__all__ = ["ALMAPredictor", "export_results"]
