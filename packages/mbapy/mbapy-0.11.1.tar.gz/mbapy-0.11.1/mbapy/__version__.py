533e1501102a8189a76255a855d365661f92576c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 23:18:41 +0000

    Add package mbapy/0.11.1/mbapy-0.11.1.tar.gz

---
+++ packages/mbapy/mbapy-0.11.1.tar.gz/mbapy-0.11.1/mbapy/__version__.py
@@ -0,0 +1,16 @@
+'''
+Author: BHM-Bob 2262029386@qq.com
+Date: 2022-11-01 22:16:49
+LastEditors: BHM-Bob 2262029386@qq.com
+LastEditTime: 2025-02-07 09:37:34
+Description: 
+'''
+
+__title__ = "mbapy"
+__description__ = "MyBA in Python"
+__url__ = "https://github.com/BHM-Bob/BA_PY"
+__version__ = "0.11.1"
+__build__ = "20250207_1"
+__author__ = "BHM-Bob G"
+__author_email__ = "bhmfly@foxmail.com"
+__license__ = "MIT"
