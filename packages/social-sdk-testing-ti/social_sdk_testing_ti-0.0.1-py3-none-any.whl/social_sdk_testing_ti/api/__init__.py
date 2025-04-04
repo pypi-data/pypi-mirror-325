ceb9baa93edb0b909e16e6e93bfccd01134aa75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:26 +0000

    Add package social-sdk-testing-ti/0.0.1/social_sdk_testing_ti-0.0.1-py3-none-any.whl

---
+++ packages/social-sdk-testing-ti/social_sdk_testing_ti-0.0.1-py3-none-any.whl/social_sdk_testing_ti/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from social_sdk_testing_ti.api.brand_api import BrandApi
+from social_sdk_testing_ti.api.generation_api import GenerationApi
+from social_sdk_testing_ti.api.healthcheck_api import HealthcheckApi
+from social_sdk_testing_ti.api.prompt_api import PromptApi
+from social_sdk_testing_ti.api.source_api import SourceApi
+from social_sdk_testing_ti.api.swagger_api import SwaggerApi
+from social_sdk_testing_ti.api.swaggers_api import SwaggersApi
+from social_sdk_testing_ti.api.tenant_api import TenantApi
+from social_sdk_testing_ti.api.worker_api import WorkerApi
+from social_sdk_testing_ti.api.default_api import DefaultApi
+
