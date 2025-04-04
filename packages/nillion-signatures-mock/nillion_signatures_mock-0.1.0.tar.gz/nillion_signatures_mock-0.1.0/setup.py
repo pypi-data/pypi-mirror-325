b42041e8e24e505485694168ac51b38551b70125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:35 +0000

    Add package nillion-signatures-mock/0.1.0/nillion_signatures_mock-0.1.0.tar.gz

---
+++ packages/nillion-signatures-mock/nillion_signatures_mock-0.1.0.tar.gz/nillion_signatures_mock-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nillion-signatures-mock",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=3.4.7",
+    ],
+    author="Dave Butler",
+    author_email="",
+    description="A mocked first version of a signature library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NillionNetwork/signatures-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
