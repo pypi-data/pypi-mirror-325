b8c5a539a1eb1def8c7d9b125a2c73a12cd02ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:31:10 +0000

    Add package xctph/1.1.3/xctph-1.1.3.tar.gz

---
+++ packages/xctph/xctph-1.1.3.tar.gz/xctph-1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xctph',
+    version='1.1.3',
+    description='Package for computing exciton phonon matrix elements.',
+    long_description='Package for computing exciton phonon matrix elements.',
+    author='Krishnaa Vadivel',
+    author_email='krishnaa.vadivel@yale.edu',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy', 
+        'scipy', 
+        'h5py', 
+        'xmltodict', 
+        'setuptools',
+        'fp_workflow',
+    ],
+    # scripts=[     # Does not seem to work.
+    #     'xctph/write_eph_h5.py',
+    #     'xctph/write_xct_h5.py',
+    #     'xctph/compute_xctph.py',
+    #     'xctph/print_eph.py',
+    #     'xctph/print_xctph.py',
+    # ]
+)
