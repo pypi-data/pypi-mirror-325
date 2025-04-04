0a2d52754c325a9721b825368c1e293fdacef512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:18 +0000

    Add package workforce/1.0.22/workforce-1.0.22-py3-none-any.whl

---
+++ packages/workforce/workforce-1.0.22-py3-none-any.whl/workforce-1.0.22.data/scripts/workforce_run.py
@@ -0,0 +1,39 @@
+#!python
+
+from workforce_schedule import schedule_tasks
+from workforce_edit_element import edit_element_status
+import argparse
+import networkx as nx
+import subprocess
+import time
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Schedule tasks with a graph.")
+    parser.add_argument("filename", help="Path to the input GraphML file.")
+    parser.add_argument("--prefix", '-p', default='bash -c', type=str, help="Prefix for node execution")
+    parser.add_argument("--run_task", action="store_true", help="Run a single task and exit")
+    parser.add_argument("--speed", type=float, default=2, help="Seconds inbetween job submission")
+    return parser.parse_args()
+
+def run_tasks(filename, prefix='bash -c'):
+    G = nx.read_graphml(filename)
+    node_status = nx.get_node_attributes(G, "status")
+    run_nodes = {node for node, status in node_status.items() if status == 'run'}
+    
+    if run_nodes:
+        node_to_run = run_nodes.pop()
+        edit_element_status(filename,'node',node_to_run,'run')
+        subprocess.Popen(["workforce_run_node.py", filename, node_to_run, "-p", prefix])
+
+def worker(filename, prefix='bash -c', speed=2):
+    while schedule_tasks(filename) != 'complete':
+        time.sleep(speed)
+        run_tasks(filename, prefix)
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    if args.run_task:
+        run_tasks(args.filename, args.prefix)
+    else:
+        worker(args.filename, args.prefix, args.speed)
---
+++ packages/workforce/workforce-1.0.22-py3-none-any.whl/workforce-1.0.22.data/scripts/workforce_run_node.py
@@ -0,0 +1,29 @@
+#!python
+
+from workforce_edit_element import edit_element_status
+import argparse
+import networkx as nx
+import subprocess
+
+def run_node(filename, node, prefix='bash -c'):
+    """Execute a command associated with a node and update its status."""
+    G = nx.read_graphml(filename)
+    try:
+        label = G.nodes[node].get('label', '')
+        escaped_label = label.replace('"', '\\"')
+        command = f"{prefix} \"{escaped_label}\""
+        subprocess.run(command, shell=True, check=True)
+        edit_element_status(filename,'node',node,'ran')
+    except subprocess.CalledProcessError:
+        edit_element_status(filename,'node',node,'fail')
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Run a node's associated command.")
+    parser.add_argument("filename", type=str, help="Path to the input GraphML file.")
+    parser.add_argument("node", type=str, help="Node to execute.")
+    parser.add_argument("--prefix", '-p', default='bash -c', required=False, type=str, help="Prefix for command execution.")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run_node(args.filename, args.node, args.prefix)
