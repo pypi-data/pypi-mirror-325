067880343b72472ded8ae6d216bd6817f55da6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:31:04 +0000

    Add package TestDataX/0.1.0/testdatax-0.1.0.tar.gz

---
+++ packages/TestDataX/testdatax-0.1.0.tar.gz/testdatax-0.1.0/src/__init__.py
@@ -0,0 +1,7 @@
+"""TestDataX package initialization."""
+
+__version__ = "0.1.0"
+
+from src.cli import app  # noqa
+
+__all__ = ["app"]
---
+++ packages/TestDataX/testdatax-0.1.0.tar.gz/testdatax-0.1.0/src/providers/__init__.py
@@ -0,0 +1,4 @@
+from .base import DataProvider
+from .faker_provider import FakerProvider
+
+__all__ = ["DataProvider", "FakerProvider"]
