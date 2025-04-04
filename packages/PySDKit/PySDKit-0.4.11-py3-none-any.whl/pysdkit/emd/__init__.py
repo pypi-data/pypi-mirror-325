9846000772d40b3bc0a1e92a8ec5f9569fe434ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:22 +0000

    Add package PySDKit/0.4.11/PySDKit-0.4.11-py3-none-any.whl

---
+++ packages/PySDKit/PySDKit-0.4.11-py3-none-any.whl/pysdkit/emd/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025/02/03 18:36:18
+@author: Whenxuan Wang
+@email: wwhenxuan@gmail.com
+"""
+
+from .emd import EMD
+
+from .eemd import EEMD
+
+from .ceemdan import CEEMDAN
+
+from .memd import MEMD
---
+++ packages/PySDKit/PySDKit-0.4.11-py3-none-any.whl/pysdkit/vmd2d/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025/02/02 17:00:39
+@author: Whenxuan Wang
+@email: wwhenxuan@gmail.com
+"""
+from .vmd2d import VMD2D
