a21d5b2c7536c5b5e39b7e90b1152b4918b58139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:37 +0000

    Add package znsocket/0.2.8/znsocket-0.2.8-py3-none-any.whl

---
+++ packages/znsocket/znsocket-0.2.8-py3-none-any.whl/znsocket/client.py
@@ -0,0 +1,229 @@
+import dataclasses
+import datetime
+import functools
+import logging
+import typing as t
+
+import socketio.exceptions
+import typing_extensions as tyex
+from redis import Redis
+
+from znsocket import exceptions
+from znsocket.abc import RefreshDataTypeDict
+from znsocket.utils import parse_url
+
+log = logging.getLogger(__name__)
+
+
+def _handle_data(data: dict):
+    if "type" in data:
+        if data["type"] == "set":
+            return set(data["data"])
+        else:
+            raise TypeError(f"Can not convert type '{data['type']}'")
+    return data["data"]
+
+
+def _handle_error(result):
+    """Handle errors in the server response."""
+
+    if "error" not in result:
+        return
+
+    error_map = {
+        "DataError": exceptions.DataError,
+        "TypeError": TypeError,
+        "IndexError": IndexError,
+        "KeyError": KeyError,
+        "UnknownEventError": exceptions.UnknownEventError,
+        "ResponseError": exceptions.ResponseError,
+    }
+
+    error_type = result["error"].get("type")
+    error_msg = result["error"].get("msg", "Unknown error")
+
+    # Raise the mapped exception if it exists, else raise a generic ZnSocketError
+    raise error_map.get(error_type, exceptions.ZnSocketError)(error_msg)
+
+
+@dataclasses.dataclass
+class Client:
+    """Client to interact with a znsocket server.
+
+    Attributes
+    ----------
+    address : str
+        The address of the znsocket server.
+    decode_responses : bool
+        Whether to decode responses from the server. Default is True.
+    namespace : str
+        The namespace to connect to. Default is "/znsocket".
+    refresh_callbacks : dict
+        A dictionary of callbacks to call when a refresh event is received.
+    retry : int
+        The number of times to retry a failed call. Default is 3.
+    delay_between_calls : datetime.timedelta
+        The time to wait between calls. Default is None.
+    """
+
+    address: str
+    decode_responses: bool = True
+    sio: socketio.Client = dataclasses.field(
+        default_factory=socketio.Client, repr=False, init=False
+    )
+    namespace: str = "/znsocket"
+    refresh_callbacks: dict = dataclasses.field(default_factory=dict)
+    retry: int = 3
+    delay_between_calls: datetime.timedelta | None = None
+
+    _last_call: datetime.datetime = dataclasses.field(
+        default_factory=datetime.datetime.now, init=False
+    )
+
+    def pipeline(self, *args, **kwargs) -> "Pipeline":
+        return Pipeline(self, *args, **kwargs)
+
+    @classmethod
+    def from_url(cls, url, namespace: str = "/znsocket", **kwargs) -> "Client":
+        """Connect to a znsocket server using a URL.
+
+        Parameters
+        ----------
+        url : str
+            The URL of the znsocket server. Should be in the format
+            "znsocket://127.0.0.1:5000".
+        namespace : str
+            The namespace to connect to. Default is "/znsocket".
+        """
+        return cls(
+            address=url.replace("znsocket://", "http://"), namespace=namespace, **kwargs
+        )
+
+    def __post_init__(self):
+        @self.sio.on("refresh", namespace=self.namespace)
+        def refresh(data: RefreshDataTypeDict):
+            for key in self.refresh_callbacks:
+                if data["target"] == key:
+                    self.refresh_callbacks[key](data["data"])
+
+        _url, _path = parse_url(self.address)
+        try:
+            self.sio.connect(
+                _url,
+                namespaces=[self.namespace],
+                wait=True,
+                socketio_path=f"{_path}/socket.io" if _path else "socket.io",
+            )
+
+        except socketio.exceptions.ConnectionError as err:
+            raise exceptions.ConnectionError(self.address) from err
+
+        if not self.decode_responses:
+            raise NotImplementedError("decode_responses=False is not supported yet")
+
+    def call(self, event: str, *args, **kwargs) -> t.Any:
+        """Call an event on the server."""
+        if self.delay_between_calls:
+            time_since_last_call = datetime.datetime.now() - self._last_call
+            delay_needed = self.delay_between_calls - time_since_last_call
+            if delay_needed > datetime.timedelta(0):
+                self.sio.sleep(delay_needed.total_seconds())
+            self._last_call = datetime.datetime.now()
+
+        for idx in reversed(range(self.retry + 1)):
+            try:
+                return self.sio.call(event, [args, kwargs], namespace=self.namespace)
+            except socketio.exceptions.TimeoutError:
+                if idx == 0:
+                    raise
+                log.warning(f"Connection error. Retrying... {idx} attempts left")
+                self.sio.sleep(1)
+
+    def _redis_command(self, command, *args, **kwargs):
+        """Generic handler for Redis commands."""
+        result = self.call(command, *args, **kwargs)
+
+        if result is None:
+            raise exceptions.ZnSocketError("No response from server")
+        _handle_error(result)
+
+        return _handle_data(result)
+
+    def __getattr__(self, name):
+        """Intercepts method calls to dynamically route Redis commands."""
+        # Check if name corresponds to a Redis command
+        if hasattr(Redis, name):
+            return functools.partial(self._redis_command, name)
+        raise AttributeError(
+            f"'{type(self).__name__}' object has no attribute '{name}'"
+        )
+
+    @tyex.deprecated("hmset() is deprecated. Use hset() instead.")
+    def hmset(self, name, mapping):
+        return self.hset(name, mapping=mapping)
+
+
+@dataclasses.dataclass
+class Pipeline:
+    """A pipeline of Redis commands to be executed as a batch on the server.
+
+    Arguments
+    ---------
+    client : Client
+        The client to send the pipeline to.
+    max_commands_per_call : int
+        The maximum number of commands to send in a single call to the server.
+        Decrease this number for large commands to avoid hitting the message size limit.
+        Increase it for small commands to reduce latency.
+    """
+
+    client: Client
+    max_commands_per_call: int = 1_000_000
+    pipeline: list = dataclasses.field(default_factory=list, init=False)
+
+    def _add_to_pipeline(self, command, *args, **kwargs):
+        """Generic handler to add Redis commands to the pipeline."""
+        self.pipeline.append((command, [args, kwargs]))
+        return self
+
+    def __getattr__(self, name):
+        """Intercepts method calls to dynamically add Redis commands to the pipeline."""
+        # Check if the name corresponds to a Redis command
+        if hasattr(Redis, name):
+            return functools.partial(self._add_to_pipeline, name)
+        raise AttributeError(
+            f"'{type(self).__name__}' object has no attribute '{name}'"
+        )
+
+    def _send_message(self, message) -> list:
+        """Send a message to the server and process the response."""
+        result = self.client.call(
+            "pipeline",
+            message=message,
+        )
+
+        if result is None:
+            raise exceptions.ZnSocketError("No response from server")
+        _handle_error(result)
+
+        return [_handle_data(res) for res in result["data"]]
+
+    def execute(self):
+        """Executes the pipeline of commands as a batch on the server."""
+        # iterate over self.pipeline and keep adding until the size is greater than max_message_size
+        # then send the message, collect the results and continue
+
+        message = []
+        results = []
+        for idx, entry in enumerate(self.pipeline):
+            message.append(entry)
+            if len(message) > self.max_commands_per_call:
+                log.debug(
+                    f"splitting message at index {idx} due to max_message_chunk",
+                )
+                results.extend(self._send_message(message))
+                message = []
+        if len(message) > 0:
+            results.extend(self._send_message(message))
+
+        return results
---
+++ packages/znsocket/znsocket-0.2.8-py3-none-any.whl/znsocket/server.py
@@ -0,0 +1,361 @@
+import dataclasses
+import typing as t
+from copy import deepcopy
+
+import eventlet.wsgi
+import socketio
+
+from znsocket.abc import RefreshDataTypeDict
+from znsocket.exceptions import DataError, ResponseError
+
+
+@dataclasses.dataclass
+class Storage:
+    content: dict = dataclasses.field(default_factory=dict)
+
+    def hset(
+        self,
+        name: str,
+        key: t.Optional[str] = None,
+        value: t.Optional[str] = None,
+        mapping: t.Optional[dict] = None,
+        items: t.Optional[list] = None,
+    ):
+        if key is None and not mapping and not items:
+            raise DataError("'hset' with no key value pairs")
+        if value is None and not mapping and not items:
+            raise DataError(f"Invalid input of type {type(value)}")
+        pieces = []
+        if items:
+            pieces.extend(items)
+        if key is not None:
+            pieces.extend((key, value))
+        if mapping:
+            for pair in mapping.items():
+                pieces.extend(pair)
+
+        if name not in self.content:
+            self.content[name] = {}
+        for i in range(0, len(pieces), 2):
+            self.content[name][pieces[i]] = pieces[i + 1]
+        return len(pieces) // 2
+
+    def hget(self, name, key):
+        try:
+            return self.content[name][key]
+        except KeyError:
+            return None
+
+    def hmget(self, name, keys):
+        response = []
+        for key in keys:
+            try:
+                response.append(self.content[name][key])
+            except KeyError:
+                response.append(None)
+        return response
+
+    def hkeys(self, name):
+        try:
+            return list(self.content[name].keys())
+        except KeyError:
+            return []
+
+    def delete(self, name):
+        try:
+            del self.content[name]
+            return 1
+        except KeyError:
+            return 0
+
+    def exists(self, name):
+        return 1 if name in self.content else 0
+
+    def llen(self, name):
+        try:
+            return len(self.content[name])
+        except KeyError:
+            return 0
+
+    def rpush(self, name, value):
+        try:
+            self.content[name].append(value)
+        except KeyError:
+            self.content[name] = [value]
+
+        return len(self.content[name])
+
+    def lpush(self, name, value):
+        try:
+            self.content[name].insert(0, value)
+        except KeyError:
+            self.content[name] = [value]
+
+        return len(self.content[name])
+
+    def lindex(self, name, index):
+        if index is None:
+            raise DataError("Invalid input of type None")
+        try:
+            return self.content[name][index]
+        except KeyError:
+            return None
+        except IndexError:
+            return None
+        except TypeError:  # index is not an integer
+            return None
+
+    def set(self, name, value):
+        if value is None or name is None:
+            raise DataError("Invalid input of type None")
+        self.content[name] = value
+        return True
+
+    def get(self, name, default=None):
+        return self.content.get(name, default)
+
+    def smembers(self, name):
+        try:
+            response = self.content[name]
+        except KeyError:
+            response = set()
+
+        if not isinstance(response, set):
+            raise ResponseError(
+                "WRONGTYPE Operation against a key holding the wrong kind of value"
+            )
+        return response
+
+    def lrange(self, name, start, end):
+        if end == -1:
+            end = None
+        elif end >= 0:
+            end += 1
+        try:
+            return self.content[name][start:end]
+        except KeyError:
+            return []
+
+    def lset(self, name, index, value):
+        try:
+            self.content[name][index] = value
+        except KeyError:
+            raise ResponseError("no such key")
+        except IndexError:
+            raise ResponseError("index out of range")
+
+    def lrem(self, name, count, value):
+        if count is None or value is None or name is None:
+            raise DataError("Invalid input of type None")
+        if count == 0:
+            try:
+                self.content[name] = [x for x in self.content[name] if x != value]
+            except KeyError:
+                return 0
+        else:
+            removed = 0
+            while removed < count:
+                try:
+                    self.content[name].remove(value)
+                    removed += 1
+                except KeyError:
+                    return 0
+
+    def sadd(self, name, value):
+        try:
+            self.content[name].add(value)
+        except KeyError:
+            self.content[name] = {value}
+
+    def flushall(self):
+        self.content.clear()
+
+    def srem(self, name, value):
+        try:
+            self.content[name].remove(value)
+            return 1
+        except KeyError:
+            return 0
+
+    def linsert(self, name, where, pivot, value):
+        try:
+            index = self.content[name].index(pivot)
+            if where == "BEFORE":
+                self.content[name].insert(index, value)
+            elif where == "AFTER":
+                self.content[name].insert(index + 1, value)
+        except KeyError:
+            return 0
+        except ValueError:
+            return -1
+
+    def hexists(self, name, key):
+        try:
+            return 1 if key in self.content[name] else 0
+        except KeyError:
+            return 0
+
+    def hdel(self, name, key):
+        try:
+            del self.content[name][key]
+            return 1
+        except KeyError:
+            return 0
+
+    def hlen(self, name):
+        try:
+            return len(self.content[name])
+        except KeyError:
+            return 0
+
+    def hvals(self, name):
+        try:
+            return list(self.content[name].values())
+        except KeyError:
+            return []
+
+    def lpop(self, name):
+        try:
+            return self.content[name].pop(0)
+        except KeyError:
+            return None
+        except IndexError:
+            return None
+
+    def scard(self, name):
+        try:
+            return len(self.content[name])
+        except KeyError:
+            return 0
+
+    def hgetall(self, name):
+        try:
+            return self.content[name]
+        except KeyError:
+            return {}
+
+    def copy(self, src, dst):
+        if src == dst:
+            return False
+        if src not in self.content:
+            return False
+        if dst in self.content:
+            return False
+        self.content[dst] = deepcopy(self.content[src])
+        return True
+
+
+@dataclasses.dataclass
+class Server:
+    port: int = 5000
+    max_http_buffer_size: t.Optional[int] = None
+    async_mode: t.Optional[str] = None
+    logger: bool = False
+
+    @classmethod
+    def from_url(cls, url: str, **kwargs) -> "Server":
+        # server url looks like "znsocket://127.0.0.1:5000"
+        if not url.startswith("znsocket://"):
+            raise ValueError("Invalid URL")
+        port = int(url.split(":")[-1])
+        return cls(port=port, **kwargs)
+
+    def run(self) -> None:
+        """Run the server (blocking)."""
+        sio = get_sio(
+            max_http_buffer_size=self.max_http_buffer_size,
+            async_mode=self.async_mode,
+            logger=self.logger,
+            engineio_logger=self.logger,
+        )
+        server_app = socketio.WSGIApp(sio)
+        eventlet.wsgi.server(eventlet.listen(("0.0.0.0", self.port)), server_app)
+
+
+def get_sio(
+    max_http_buffer_size: t.Optional[int] = None,
+    async_mode: t.Optional[str] = None,
+    **kwargs,
+) -> socketio.Server:
+    # We set these as kwargs, because their default
+    # is not None, so if None we leave them out
+    if max_http_buffer_size is not None:
+        kwargs["max_http_buffer_size"] = max_http_buffer_size
+    if async_mode is not None:
+        kwargs["async_mode"] = async_mode
+    sio = socketio.Server(**kwargs)
+    attach_events(sio, namespace="/znsocket")
+    return sio
+
+
+def attach_events(  # noqa: C901
+    sio: socketio.Server, namespace: str = "/znsocket", storage=None
+) -> None:
+    if storage is None:
+        storage = Storage()
+
+    @sio.on("*", namespace=namespace)
+    def handle_all_events(event, sid, data):
+        """Handle any event dynamically by mapping event name to storage method."""
+        args, kwargs = data
+        if hasattr(storage, event):
+            try:
+                result = {"data": getattr(storage, event)(*args, **kwargs)}
+                if isinstance(result["data"], set):
+                    result["data"] = list(result["data"])
+                    result["type"] = "set"
+                return result
+            except TypeError as e:
+                return {
+                    "error": {
+                        "msg": f"Invalid arguments for {event}: {str(e)}",
+                        "type": "TypeError",
+                    }
+                }
+            except Exception as e:
+                return {"error": {"msg": str(e), "type": type(e).__name__}}
+        else:
+            return {
+                "error": {"msg": f"Unknown event: {event}", "type": "UnknownEventError"}
+            }
+
+    @sio.event(namespace=namespace)
+    def refresh(sid, data: RefreshDataTypeDict) -> None:
+        sio.emit("refresh", data, namespace=namespace, skip_sid=sid)
+
+    @sio.event(namespace=namespace)
+    def pipeline(sid, data):
+        args, kwargs = data
+        message = kwargs.pop("message")
+        results = []
+        for cmd in message:
+            event = cmd[0]
+            args = cmd[1][0]
+            kwargs = cmd[1][1]
+
+            if hasattr(storage, event):
+                try:
+                    result = {"data": getattr(storage, event)(*args, **kwargs)}
+                    if isinstance(result["data"], set):
+                        result["data"] = list(result["data"])
+                        result["type"] = "set"
+                    results.append(result)
+                except TypeError as e:
+                    return {
+                        "error": {
+                            "msg": f"Invalid arguments for {event}: {str(e)}",
+                            "type": "TypeError",
+                        }
+                    }
+                except Exception as e:
+                    return {"error": {"msg": str(e), "type": type(e).__name__}}
+            else:
+                return {
+                    "error": {
+                        "msg": f"Unknown event: {event}",
+                        "type": "UnknownEventError",
+                    }
+                }
+        return {"data": results}
+
+    return sio
