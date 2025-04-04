43f857f64f8e5288f76a4c4145d358636d645e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:41:46 +0000

    Add package miaospeedlib/0.1.2/miaospeedlib-0.1.2.tar.gz

---
+++ packages/miaospeedlib/miaospeedlib-0.1.2.tar.gz/miaospeedlib-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="miaospeedlib",
+    version="0.1.2",
+    author="airportr",
+    url='https://github.com/AirportR/miaolib',
+    author_email="airportroster@gmail.com",
+    description="MiaoSpeed Client Library Implementations for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'aiohttp>=3.9.2',
+        'async_timeout>=4.0.2',
+        'loguru>=0.6.0'
+    ],
+    python_requires=">=3.9",
+)
