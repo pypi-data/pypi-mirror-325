0c8fccbbc904040229905cb568a9813e832b2531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:21 +0000

    Add package kuzu/0.8.1.dev2/kuzu-0.8.1.dev2.tar.gz

---
+++ packages/kuzu/kuzu-0.8.1.dev2.tar.gz/sdist/kuzu-source/src/expression_evaluator/literal_evaluator.cpp
@@ -0,0 +1,45 @@
+#include "expression_evaluator/literal_evaluator.h"
+
+#include "common/types/value/value.h"
+
+using namespace kuzu::common;
+using namespace kuzu::storage;
+using namespace kuzu::main;
+
+namespace kuzu {
+namespace evaluator {
+
+void LiteralExpressionEvaluator::evaluate() {
+    resultVector->setState(flatState);
+    auto cnt = localState.count;
+    for (auto i = 1ul; i < cnt; i++) {
+        resultVector->copyFromVectorData(i, resultVector.get(), 0);
+    }
+    if (cnt > 1) {
+        unflatState->getSelVectorUnsafe().setSelSize(cnt);
+        resultVector->state = unflatState;
+    }
+}
+
+bool LiteralExpressionEvaluator::selectInternal(SelectionVector&) {
+    KU_ASSERT(resultVector->dataType.getLogicalTypeID() == LogicalTypeID::BOOL);
+    auto pos = resultVector->state->getSelVector()[0];
+    KU_ASSERT(pos == 0u);
+    return resultVector->getValue<bool>(pos) && (!resultVector->isNull(pos));
+}
+
+void LiteralExpressionEvaluator::resolveResultVector(const processor::ResultSet& /*resultSet*/,
+    MemoryManager* memoryManager) {
+    resultVector = std::make_shared<ValueVector>(value.getDataType().copy(), memoryManager);
+    flatState = DataChunkState::getSingleValueDataChunkState();
+    unflatState = std::make_shared<DataChunkState>();
+    resultVector->setState(flatState);
+    if (value.isNull()) {
+        resultVector->setNull(0 /* pos */, true);
+    } else {
+        resultVector->copyFromValue(resultVector->state->getSelVector()[0], value);
+    }
+}
+
+} // namespace evaluator
+} // namespace kuzu
