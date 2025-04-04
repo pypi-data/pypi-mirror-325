35f3d1a6224a5cca3121f9768adda285b4df2826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:20:24 +0000

    Add package anypoint/0.13.0/anypoint-0.13.0.tar.gz

---
+++ packages/anypoint/anypoint-0.13.0.tar.gz/anypoint-0.13.0/anypoint/api/api_manager.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import logging
+import typing
+
+from anypoint.models.api import Asset
+
+if typing.TYPE_CHECKING:
+    from anypoint import Anypoint
+
+
+class ApiManagerApi:
+    def __init__(self, client: Anypoint, log: logging.Logger):
+        self._client = client
+        self._log = log
+
+    def get_apis(self, organization_id: str, environment_id: str) -> list[Asset]:
+        path = f"/apimanager/api/v1/organizations/{organization_id}/environments/{environment_id}/apis"
+        assets = self._client.request(path).get("assets", [])
+        with open("assets.json", "w") as f:
+            import json
+
+            json.dump(assets, f, indent=2)
+        return [Asset(asset, self) for asset in assets]
