5cd34fe73896e7c9f3f9fe24f49e192aa2f13337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-04 23:31:08 +0000

    Add package pychemiq/1.1.2/pychemiq-1.1.2-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/pychemiq/pychemiq-1.1.2-cp310-cp310-macosx_13_0_arm64.whl/pychemiq/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.1.2"
+
+from .pychemiq import *
+from .Transform.Mapping import *
+from .Optimizer import *
+from .Circuit.Ansatz import *
+from .Molecules import *
+from .Utils import * 
+from .RealChip import *
