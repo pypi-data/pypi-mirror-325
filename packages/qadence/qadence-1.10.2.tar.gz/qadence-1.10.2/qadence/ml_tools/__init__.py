4778da8872fde4601403e20eb5954492c87d7098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:24:34 +0000

    Add package qadence/1.10.2/qadence-1.10.2.tar.gz

---
+++ packages/qadence/qadence-1.10.2.tar.gz/qadence-1.10.2/qadence/ml_tools/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from .callbacks.saveload import load_checkpoint, load_model, write_checkpoint
+from .config import AnsatzConfig, FeatureMapConfig, TrainConfig
+from .constructors import create_ansatz, create_fm_blocks, observable_from_config
+from .data import DictDataLoader, InfiniteTensorDataset, OptimizeResult, to_dataloader
+from .models import QNN
+from .optimize_step import optimize_step as default_optimize_step
+from .parameters import get_parameters, num_parameters, set_parameters
+from .tensors import numpy_to_tensor, promote_to, promote_to_tensor
+from .trainer import Trainer
+
+# Modules to be automatically added to the qadence namespace
+__all__ = [
+    "AnsatzConfig",
+    "create_ansatz",
+    "create_fm_blocks",
+    "DictDataLoader",
+    "FeatureMapConfig",
+    "load_checkpoint",
+    "observable_from_config",
+    "QNN",
+    "TrainConfig",
+    "OptimizeResult",
+    "Trainer",
+    "write_checkpoint",
+]
