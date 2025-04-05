ca0459d23f09ad4e900c8bf648c16929108f3cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:24:24 +0000

    Add package daops/0.13.0/daops-0.13.0.tar.gz

---
+++ packages/daops/daops-0.13.0.tar.gz/daops-0.13.0/tests/test_data_utils/test_var_utils.py
@@ -0,0 +1,25 @@
+import xarray as xr
+from daops.data_utils.var_utils import add_data_var
+
+
+def test_add_data_var(stratus):
+    ds = xr.open_mfdataset(
+        f"{stratus.path}/badc/cmip5/data/cmip5/output1/INM/"
+        "inmcm4/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/*.nc",
+        combine="by_coords",
+        use_cftime=True,
+    )
+
+    ds_id = "cmip5.output1.INM.inmcm4.rcp45.mon.ocean.Omon.r1i1p1.latest.zostoga"
+
+    assert "realization" not in ds.data_vars
+
+    operands = {
+        "var_id": "realization",
+        "value": "1",
+        "dtype": "int32",
+        "attrs": {"long_name": "realization", "comment": "example"},
+    }
+
+    ds = add_data_var(ds_id, ds, **operands)
+    assert "realization" in ds.data_vars
