3e4e345daa150a73c0d88d304fee0c44ccf113f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:24:59 +0000

    Add package PyBuilder-maid/0.0.3/pybuilder_maid-0.0.3.tar.gz

---
+++ packages/PyBuilder-maid/pybuilder_maid-0.0.3.tar.gz/pybuilder_maid-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# from setuptools import setup, find_packages
+
+
+# def readme():
+#   with open('README.md', 'r') as f:
+#     return f.read()
+
+
+# setup(
+#   name='PyBuilder-maid',
+#   version='0.0.1',
+#   author='Thoru-san',
+#   author_email='thoru.fun@gmail.com',
+#   description='Build system for C/C++ projects on python.',
+#   long_description=readme(),
+#   long_description_content_type='text/markdown',
+#   url='',
+#   packages=find_packages(),
+#   install_requires=['requests>=2.25.1'],
+#   classifiers=[
+#     'Programming Language :: Python :: 3.11',
+#     'License :: OSI Approved :: MIT License',
+#     'Operating System :: OS Independent'
+#   ],
+#   keywords='builder build-system C/C++',
+#   project_urls={
+#     'GitHub': 'https://github.com/Thoru-san'
+#   },
+#   python_requires='>=3.6'
+# )
