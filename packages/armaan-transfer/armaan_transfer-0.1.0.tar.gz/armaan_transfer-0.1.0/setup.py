c4711eb6650b64c2396b961512b855aae74c1650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:24:16 +0000

    Add package armaan-transfer/0.1.0/armaan_transfer-0.1.0.tar.gz

---
+++ packages/armaan-transfer/armaan_transfer-0.1.0.tar.gz/armaan_transfer-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="armaan-transfer",  # Pick a unique name
+    version="0.1.0",
+    packages=["quicktransfer"],
+    package_dir={"": "src"},
+    install_requires=["tqdm"],
+    entry_points={
+        "console_scripts": ["ftransfer=quicktransfer.transfer:main"]
+    },
+)
