121a6f04788bc3f5edcf0b132e2d40540b3cdbec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:25:34 +0000

    Add package uniquepaybd/0.1/uniquepaybd-0.1.tar.gz

---
+++ packages/uniquepaybd/uniquepaybd-0.1.tar.gz/uniquepaybd-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uniquepaybd',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # আপনার লাইব্রেরি প্রয়োজনীয় প্যাকেজ গুলি এখানে যুক্ত করুন
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
