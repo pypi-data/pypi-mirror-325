a27ffde190dbbb04b6ef39da7aef5cb35b94297f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-05 00:24:20 +0000

    Add package quixstreams/3.8.1.dev1/quixstreams-3.8.1.dev1-py3-none-any.whl

---
+++ packages/quixstreams/quixstreams-3.8.1.dev1-py3-none-any.whl/quixstreams/dataframe/windows/__init__.py
@@ -0,0 +1,13 @@
+from .base import WindowResult
+from .definitions import (
+    HoppingTimeWindowDefinition,
+    SlidingTimeWindowDefinition,
+    TumblingTimeWindowDefinition,
+)
+
+__all__ = [
+    "HoppingTimeWindowDefinition",
+    "SlidingTimeWindowDefinition",
+    "TumblingTimeWindowDefinition",
+    "WindowResult",
+]
