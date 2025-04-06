7c601a39037ec08fd44e6abb0c030a5a689a6e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:24:04 +0000

    Add package ropt/0.13.1/ropt-0.13.1-py3-none-any.whl

---
+++ packages/ropt/ropt-0.13.1-py3-none-any.whl/ropt/plugins/plan/_utils.py
@@ -0,0 +1,127 @@
+"""This module defines the abstract base class for optimization steps."""
+
+from __future__ import annotations
+
+from typing import cast
+
+import numpy as np
+
+from ropt.results import FunctionResults, Results
+
+
+def _get_new_optimal_result(
+    optimal_result: FunctionResults | None, results: FunctionResults
+) -> FunctionResults | None:
+    if optimal_result is None:
+        return results
+    assert optimal_result.functions is not None
+    assert results.functions is not None
+    optimal = optimal_result.functions.weighted_objective
+    objective = results.functions.weighted_objective
+    if objective < optimal:
+        return results
+    return None
+
+
+def _check_bound_constraints(
+    results: FunctionResults, constraint_tolerance: float
+) -> bool:
+    if results.bound_constraints is not None:
+        lower_violations = results.bound_constraints.scaled_lower_violations
+        if lower_violations is None:
+            lower_violations = results.bound_constraints.lower_violations
+        upper_violations = results.bound_constraints.scaled_upper_violations
+        if upper_violations is None:
+            upper_violations = results.bound_constraints.upper_violations
+        for violations in (lower_violations, upper_violations):
+            if (
+                violations is not None
+                and np.any(violations > constraint_tolerance).item()
+            ):
+                return False
+    return True
+
+
+def _check_constraints(
+    results: FunctionResults, constraint_tolerance: float | None
+) -> bool:
+    if constraint_tolerance is None:
+        return True
+
+    if not _check_bound_constraints(results, constraint_tolerance):
+        return False
+
+    if results.linear_constraints is not None and (
+        results.linear_constraints.violations is not None
+        and np.any(results.linear_constraints.violations > constraint_tolerance).item()
+    ):
+        return False
+
+    if results.nonlinear_constraints is not None:
+        violations = results.nonlinear_constraints.scaled_violations
+        if violations is None:
+            violations = results.nonlinear_constraints.violations
+        if violations is not None and np.any(violations > constraint_tolerance).item():
+            return False
+
+    return True
+
+
+def _get_last_result(
+    results: tuple[Results, ...],
+    transformed_results: tuple[Results, ...],
+    constraint_tolerance: float | None,
+) -> FunctionResults | None:
+    return next(
+        (
+            cast(FunctionResults, unscaled_item)
+            for unscaled_item, scaled_item in zip(
+                reversed(results), reversed(transformed_results), strict=False
+            )
+            if (
+                isinstance(scaled_item, FunctionResults)
+                and scaled_item.functions is not None
+                and _check_constraints(scaled_item, constraint_tolerance)
+            )
+        ),
+        None,
+    )
+
+
+def _update_optimal_result(
+    optimal_result: FunctionResults | None,
+    results: tuple[Results, ...],
+    transformed_results: tuple[Results, ...],
+    constraint_tolerance: float | None,
+) -> FunctionResults | None:
+    return_result: FunctionResults | None = None
+    for unscaled_item, scaled_item in zip(results, transformed_results, strict=False):
+        if (
+            isinstance(scaled_item, FunctionResults)
+            and scaled_item.functions is not None
+            and _check_constraints(scaled_item, constraint_tolerance)
+        ):
+            assert isinstance(unscaled_item, FunctionResults)
+            new_optimal_result = _get_new_optimal_result(optimal_result, unscaled_item)
+            if new_optimal_result is not None:
+                optimal_result = new_optimal_result
+                return_result = new_optimal_result
+    return return_result
+
+
+def _get_all_results(
+    results: tuple[Results, ...],
+    transformed_results: tuple[Results, ...],
+    constraint_tolerance: float | None,
+) -> tuple[FunctionResults, ...]:
+    return tuple(
+        cast(FunctionResults, unscaled_item)
+        for unscaled_item, scaled_item in zip(
+            results, transformed_results, strict=False
+        )
+        if (
+            isinstance(scaled_item, FunctionResults)
+            and scaled_item.functions is not None
+            and _check_constraints(scaled_item, constraint_tolerance)
+        )
+    )
---
+++ packages/ropt/ropt-0.13.1-py3-none-any.whl/ropt/results/_nonlinear_constraints.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+import numpy as np  # noqa: TC002
+
+from ropt.enums import ResultAxis
+from ropt.utils.scaling import scale_constraints
+
+from ._result_field import ResultField
+from ._utils import (
+    _get_nonlinear_constraint_values,
+    _get_nonlinear_constraint_violations,
+    _immutable_copy,
+)
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from ropt.config.enopt import EnOptConfig
+
+    from ._functions import Functions
+
+
+@dataclass(slots=True)
+class NonlinearConstraints(ResultField):
+    """This class stores constraint values and violations.
+
+    The following information is stored:
+
+    1. Differences between the non-linear constraints and their right-hand-side
+       values.
+    2. Violations of constraints, defined as the absolute value of the
+       difference if the constraint is violated, or else zero.
+    3. Scaled versions for these values.
+
+    Attributes:
+        values:            Non-linear constraint values.
+        violations:        Violations of the nonlinear constraints.
+        scaled_values:     Optional scaled non-linear constraint values.
+        scaled_violations: Optional scaled violations of the nonlinear constraints.
+    """
+
+    values: NDArray[np.float64] | None = field(
+        default=None,
+        metadata={"__axes__": (ResultAxis.NONLINEAR_CONSTRAINT,)},
+    )
+    violations: NDArray[np.float64] | None = field(
+        default=None,
+        metadata={
+            "__axes__": (ResultAxis.NONLINEAR_CONSTRAINT,),
+        },
+    )
+    scaled_values: NDArray[np.float64] | None = field(
+        default=None,
+        metadata={
+            "__axes__": (ResultAxis.NONLINEAR_CONSTRAINT,),
+        },
+    )
+    scaled_violations: NDArray[np.float64] | None = field(
+        default=None,
+        metadata={
+            "__axes__": (ResultAxis.NONLINEAR_CONSTRAINT,),
+        },
+    )
+
+    def __post_init__(self) -> None:
+        """Make all array fields immutable copies.
+
+        # noqa
+        """
+        self.values = _immutable_copy(self.values)
+        self.violations = _immutable_copy(self.violations)
+        self.scaled_values = _immutable_copy(self.scaled_values)
+        self.scaled_violations = _immutable_copy(self.scaled_violations)
+
+    @classmethod
+    def create(
+        cls,
+        config: EnOptConfig,
+        functions: Functions | None,
+        constraint_auto_scales: NDArray[np.float64] | None,
+    ) -> NonlinearConstraints | None:
+        """Add constraint information.
+
+        This factory function creates a `NonlinearConstraints` object with the
+        correct information on nonlinear constraints, based on the given
+        evaluations and functions.
+
+        Args:
+            config:                 The ensemble optimizer configuration object.
+            functions:              An instance of the Function class.
+            constraint_auto_scales: Optional constraint scales.
+
+        Returns:
+            A newly created NonlinearConstraints object.
+        """
+        if functions is None:
+            return None
+
+        def _get_scaled(
+            unscaled: NDArray[np.float64] | None,
+        ) -> NDArray[np.float64] | None:
+            if unscaled is not None:
+                assert config is not None
+                return scale_constraints(
+                    config,
+                    unscaled,
+                    None if constraint_auto_scales is None else constraint_auto_scales,
+                    axis=-1,
+                )
+            return None
+
+        if config.nonlinear_constraints is not None:
+            assert functions.constraints is not None
+            values = _get_nonlinear_constraint_values(
+                config, functions.constraints, axis=-1
+            )
+            violations = _get_nonlinear_constraint_violations(config, values)
+            scaled_values = _get_scaled(values)
+            scaled_violations = _get_scaled(violations)
+            return NonlinearConstraints(
+                values=values,
+                violations=violations,
+                scaled_values=scaled_values,
+                scaled_violations=scaled_violations,
+            )
+
+        return None
