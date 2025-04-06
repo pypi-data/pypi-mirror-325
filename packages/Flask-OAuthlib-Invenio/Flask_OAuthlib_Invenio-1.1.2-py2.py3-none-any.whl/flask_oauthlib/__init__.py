148cf8117ff6a54ac8636a9faba67781d6320bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:44:22 +0000

    Add package Flask-OAuthlib-Invenio/1.1.2/Flask_OAuthlib_Invenio-1.1.2-py2.py3-none-any.whl

---
+++ packages/Flask-OAuthlib-Invenio/Flask_OAuthlib_Invenio-1.1.2-py2.py3-none-any.whl/flask_oauthlib/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_oauthlib
+    ~~~~~~~~~~~~~~
+
+    Flask-OAuthlib is an extension for Flask that allows you to interact with
+    remote OAuth enabled applications, and also helps you creating your own
+    OAuth servers.
+
+    :copyright: (c) 2013 by Hsiaoming Yang.
+    :copyright: (c) Graz University of Technology
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = "1.1.2"
+__author__ = "Hsiaoming Yang <me@lepture.com>"
+__homepage__ = 'https://github.com/inveniosoftware/flask-oauthlib-invenio'
+__license__ = 'BSD'
