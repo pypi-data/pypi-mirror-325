c15ae421cb03636a39a87b2537f65f782d6e4c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:44:18 +0000

    Add package url-remote/0.0.111/url_remote-0.0.111.tar.gz

---
+++ packages/url-remote/url_remote-0.0.111.tar.gz/url_remote-0.0.111/url_remote/src/url_circlez.py
@@ -0,0 +1,6 @@
+import warnings
+
+from .our_url import OurUrl  # noqa: backward compatibility
+
+warnings.warn("Deprecated: Please use 'from url_remote.our_url import OurUrl' instead of "
+              "'from url_remote.url_circlez import OurUrl'", DeprecationWarning)
