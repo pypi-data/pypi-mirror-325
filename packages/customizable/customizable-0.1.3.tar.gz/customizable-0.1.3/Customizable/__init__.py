cfc52b56ab9e04f9d94cab01adf311c59a483478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:54:39 +0000

    Add package customizable/0.1.3/customizable-0.1.3.tar.gz

---
+++ packages/customizable/customizable-0.1.3.tar.gz/customizable-0.1.3/Customizable/__init__.py
@@ -0,0 +1,3 @@
+from .config import Customizable, Schema, TypedCustomizable, GlobalConfig, ValidationError, Config
+
+__all__ = ["Customizable", "Schema", "TypedCustomizable", "GlobalConfig", "ValidationError", "Config"]
