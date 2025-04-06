2652d1088ea9fe2eabec1d272573861f3b625227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:44:18 +0000

    Add package ssdk-alpha-1-ti/0.0.1/ssdk_alpha_1_ti-0.0.1.tar.gz

---
+++ packages/ssdk-alpha-1-ti/ssdk_alpha_1_ti-0.0.1.tar.gz/ssdk_alpha_1_ti-0.0.1/ssdk_alpha_1_ti/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from ssdk_alpha_1_ti.api.brand_api import BrandApi
+from ssdk_alpha_1_ti.api.generation_api import GenerationApi
+from ssdk_alpha_1_ti.api.healthcheck_api import HealthcheckApi
+from ssdk_alpha_1_ti.api.prompt_api import PromptApi
+from ssdk_alpha_1_ti.api.source_api import SourceApi
+from ssdk_alpha_1_ti.api.swagger_api import SwaggerApi
+from ssdk_alpha_1_ti.api.tenant_api import TenantApi
+from ssdk_alpha_1_ti.api.worker_api import WorkerApi
+from ssdk_alpha_1_ti.api.default_api import DefaultApi
+
