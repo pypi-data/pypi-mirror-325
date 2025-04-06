969d8bf9d3dede086fd83031f5817726842ba583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:44:24 +0000

    Add package pipeline-flow/0.1.0a2/pipeline_flow-0.1.0a2-py3-none-any.whl

---
+++ packages/pipeline-flow/pipeline_flow-0.1.0a2-py3-none-any.whl/pipeline_flow/core/parsers/__init__.py
@@ -0,0 +1,5 @@
+from .pipeline_parser import parse_pipelines
+from .plugin_parser import PluginParser
+from .yaml_parser import YamlParser
+
+__all__ = ["PluginParser", "YamlParser", "parse_pipelines"]
---
+++ packages/pipeline-flow/pipeline_flow-0.1.0a2-py3-none-any.whl/pipeline_flow/entrypoint.py
@@ -0,0 +1,43 @@
+# Standard Imports
+import logging
+
+# # Project Imports
+from pipeline_flow.common.logger import setup_logger
+from pipeline_flow.core.loaders import load_plugins
+from pipeline_flow.core.orchestrator import PipelineOrchestrator
+from pipeline_flow.core.parsers import YamlParser, parse_pipelines
+
+
+async def start(yaml_text: str | None = None, file_path: str | None = None) -> bool:
+    # Set up the logger configuration
+    setup_logger()
+
+    # Parse YAML and initialize YAML config (engine, concurrency)
+    if yaml_text is None and file_path is None:
+        raise ValueError("Either yaml_text or file_path must be provided.")
+
+    yaml_parser = (
+        await YamlParser.from_file(file_path) if file_path else YamlParser.from_text(yaml_text) if yaml_text else None
+    )
+    if not yaml_parser:
+        raise ValueError("YamlParser could not be initialized.")
+
+    yaml_config = yaml_parser.initialize_yaml_config()
+    plugins_payload = yaml_parser.get_plugins_dict()
+
+    # Parse plugins directly within the load_plugins function
+    load_plugins(yaml_config.engine, plugins_payload)
+
+    # Parse pipelines and execute them using the orchestrator
+    pipelines = parse_pipelines(yaml_parser.get_pipelines_dict())
+
+    try:
+        orchestrator = PipelineOrchestrator(yaml_config)
+        await orchestrator.execute_pipelines(pipelines)
+
+    except Exception as e:
+        logging.error("The following error occurred: %s", e)
+        logging.error("The original cause is: %s", e.__cause__)
+        raise
+    else:
+        return True
