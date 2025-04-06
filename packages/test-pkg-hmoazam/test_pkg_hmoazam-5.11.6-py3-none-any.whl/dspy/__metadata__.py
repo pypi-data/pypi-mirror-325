7fff4483aa55f0a3f2f9c224357f65446a21e973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:54:46 +0000

    Add package test-pkg-hmoazam/5.11.6/test_pkg_hmoazam-5.11.6-py3-none-any.whl

---
+++ packages/test-pkg-hmoazam/test_pkg_hmoazam-5.11.6-py3-none-any.whl/dspy/__metadata__.py
@@ -0,0 +1,8 @@
+#replace_package_name_marker
+__name__="test-pkg-hmoazam"
+#replace_package_version_marker
+__version__="5.11.6"
+__description__="DSPy"
+__url__="https://github.com/stanfordnlp/dspy"
+__author__="Omar Khattab"
+__author_email__="okhattab@stanford.edu"
