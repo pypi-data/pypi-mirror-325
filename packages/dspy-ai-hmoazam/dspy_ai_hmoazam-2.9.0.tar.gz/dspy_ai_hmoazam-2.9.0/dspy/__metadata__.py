92d753276a1741d0b2d8ec0cf5cbe2b4985acc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:23:59 +0000

    Add package dspy-ai-hmoazam/2.9.0/dspy_ai_hmoazam-2.9.0.tar.gz

---
+++ packages/dspy-ai-hmoazam/dspy_ai_hmoazam-2.9.0.tar.gz/dspy_ai_hmoazam-2.9.0/dspy/__metadata__.py
@@ -0,0 +1,8 @@
+#replace_package_name_marker
+__name__="test-pkg-hmoazam"
+#replace_package_version_marker
+__version__="2.9.0"
+__description__="DSPy"
+__url__="https://github.com/stanfordnlp/dspy"
+__author__="Omar Khattab"
+__author_email__="okhattab@stanford.edu"
